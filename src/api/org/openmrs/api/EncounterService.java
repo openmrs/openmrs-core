57af39f08cac4d0560d98af9e27e1c4e03652e04
no_autho <noauthor@xyz.com>
2005-09-09 22:35:35 +0000

    git-svn-id: http://svn.openmrs.org@19 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/api/EncounterService.java
+++ src/api/org/openmrs/api/EncounterService.java
@@ -1,12 +1,45 @@
 package org.openmrs.api;
 
+import org.openmrs.Encounter;
+
 /**
  * Provides encounter-related services within the current context.
  * 
- * @author Burke Mamlin
+ * @author Ben Wolfe
  * @version 1.0
  */
 public interface EncounterService {
 
+	/**
+	 * Creates a new encounter
+	 * 
+	 * @param encounter to be created
+	 * @return newly created encounter
+	 * @throws APIException
+	 */
+	public Encounter createEncounter(Encounter encounter) throws APIException;
+
+	/**
+	 * Get encounter by internal identifier
+	 * 
+	 * @param encounter encounter object
+	 * @return encounter with given internal identifier
+	 * @throws APIException
+	 */
+	public Encounter getEncounter(Integer encounterId) throws APIException;
+
+	/**
+	 * Save changes to encounter
+	 * @param encounter
+	 * @throws APIException
+	 */
+	public void updateEncounter(Encounter encounter) throws APIException;
+	
+	/**
+	 * Delete encounter from database.
+	 * 
+	 * @param encounter encounter object to be deleted 
+	 */
+	public void deleteEncounter(Encounter encounter) throws APIException;
 	
 }
---
+++ src/api/org/openmrs/api/ibatis/IbatisEncounterService.java
@@ -0,0 +1,116 @@
+package org.openmrs.api.ibatis;
+
+import java.sql.SQLException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.Encounter;
+import org.openmrs.User;
+import org.openmrs.api.APIException;
+import org.openmrs.api.EncounterService;
+import org.openmrs.context.Context;
+
+/**
+ * Ibatis-specific implementation of org.openmrs.api.EncounterService
+ * 
+ * @see org.openmrs.api.EncounterService
+ * 
+ * @author Ben Wolfe
+ * @version 1.0
+ */
+public class IbatisEncounterService implements EncounterService {
+
+	private final Log log = LogFactory.getLog(getClass());
+
+	private Context context;
+
+	/**
+	 * Service must be constructed within a <code>context</code>
+	 * 
+	 * @param context
+	 * @see org.openmrs.context.Context
+	 */
+	public IbatisEncounterService(Context context) {
+		this.context = context;
+	}
+
+	/**
+	 * @see org.openmrs.api.EncounterService#createEncounter(Encounter)
+	 */
+	public Encounter createEncounter(Encounter encounter) throws APIException {
+
+		User authenticatedUser = context.getAuthenticatedUser();
+		encounter.setCreator(authenticatedUser);
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				SqlMap.instance().insert("createEncounter", encounter);
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		return encounter;
+	}
+
+	/**
+	 * @see org.openmrs.api.EncounterService#getEncounter(Integer)
+	 */
+	public Encounter getEncounter(Integer encounterId) throws APIException {
+		Encounter encounter;
+		try {
+			encounter = (Encounter) SqlMap.instance().queryForObject(
+					"getEncounter", encounterId);
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		return encounter;
+	}
+
+	/**
+	 * @see org.openmrs.api.EncounterService#updateEncounter(Encounter)
+	 */
+	public void updateEncounter(Encounter encounter) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+
+				User authenticatedUser = context.getAuthenticatedUser();
+
+				if (encounter.getCreator() == null) {
+					encounter.setCreator(authenticatedUser);
+					SqlMap.instance().insert("createEncounter", encounter);
+				} else {
+					SqlMap.instance().update("updateEncounter", encounter);
+				}
+
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+
+	}
+
+	/**
+	 * @see org.openmrs.api.EncounterService#deleteEncounter(Encounter)
+	 */
+	public void deleteEncounter(Encounter encounter) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				SqlMap.instance().delete("deleteEncounter", encounter);
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+	}
+
+}
