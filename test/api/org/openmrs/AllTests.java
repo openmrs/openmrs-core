745e5814abc63ed6bf8a38b9911a5c0eca8e9e98
no_autho <noauthor@xyz.com>
2005-09-09 18:06:24 +0000

    added IbatisPatientServiceTest, removed ContextTest
    git-svn-id: http://svn.openmrs.org@11 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/AllTests.java
+++ test/api/org/openmrs/AllTests.java
@@ -4,8 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.openmrs.api.ibatis.IbatisTest;
-import org.openmrs.context.ContextTest;
+import org.openmrs.api.ibatis.*;
+//import org.openmrs.context.ContextTest;
 
 public class AllTests extends TestCase {
 
@@ -13,7 +13,8 @@ public class AllTests extends TestCase {
 		TestSuite suite = new TestSuite();
 		
 		suite.addTest(IbatisTest.suite());
-		suite.addTest(ContextTest.suite());
+		suite.addTest(IbatisPatientServiceTest.suite());
+//		suite.addTest(ContextTest.suite());
 //		suite.addTest(HibernateTest.suite());
 		
 		return suite;
