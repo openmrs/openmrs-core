7b69dca0584d7843910d9bf8d22b36c6bd484454
no_autho <noauthor@xyz.com>
2005-09-09 21:22:17 +0000

    ConceptWord is not going to be a domain object
    git-svn-id: http://svn.openmrs.org@15 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/api/ibatis/IbatisPatientServiceTest.java
+++ test/api/org/openmrs/api/ibatis/IbatisPatientServiceTest.java
@@ -4,11 +4,13 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.openmrs.Patient;
+import org.openmrs.*;
+import org.openmrs.PatientName;
 import org.openmrs.context.IbatisContext;
 import org.openmrs.context.*;
 import org.openmrs.api.*;
 import java.util.*;
+import java.util.Date;
 
 public class IbatisPatientServiceTest extends TestCase {
 	
@@ -20,23 +22,67 @@ public class IbatisPatientServiceTest extends TestCase {
 	}
 	public void testGetPatient() throws APIException {
 		
-		Patient patient = (Patient)ps.getPatient(1);
-		assertNotNull(patient);
+		Patient patient;
+		
 		patient = ps.getPatient(-1);
 		assertNull(patient);
+
+		patient = (Patient)ps.getPatient(1);
+		assertNotNull(patient);
 	}
 	
 	public void testGetPatientByIdentifier() throws APIException {
-		// should we be sending strings like %-% ? 
-		// ...or have PatientService insert %'s ?
-		List patientList = ps.getPatientByIdentifier("%-%");
-		assertNotNull(patientList);
-		assertTrue(patientList.size() > 0);
+
+		List patientList;
 		
 		patientList = ps.getPatientByIdentifier("???");
 		assertNotNull(patientList);
 		assertTrue(patientList.size() == 0);
 		
+		// should we be sending strings like %-% ? 
+		// ...or have PatientService insert %'s ?
+		patientList = ps.getPatientByIdentifier("%-%");
+		assertNotNull(patientList);
+		assertTrue(patientList.size() > 0);
+	}
+	
+	public void testCreateDeletePatient() throws APIException {
+		
+		Patient patient = new Patient();
+		
+		PatientName pName = new PatientName();
+		pName.setGivenName("Tom");
+		pName.setMiddleName("E.");
+		pName.setFamilyName("Patient");
+		patient.addName(pName);
+		
+		PatientAddress pAddress = new PatientAddress();
+		pAddress.setAddress1("123 My street");
+		pAddress.setAddress2("Apt 402");
+		pAddress.setCityVillage("Anywhere city");
+		pAddress.setCountry("Some Country");
+		List<PatientAddress> pAddressList = patient.getAddresses();
+		pAddressList.add(pAddress);
+		patient.setAddresses(pAddressList);
+		
+		patient.setBirthdate(new Date());
+		
+		patient.setBirthdateEstimated(true);
+		
+		patient.setBirthplace("Little town outside of nowhere");
+		
+		Patient createdPatient = ps.createPatient(patient);
+		assertNotNull(createdPatient);
+		
+		Integer pId = createdPatient.getPatientId();
+		assertNotNull(pId);
+		
+		Patient createdPatientById = ps.getPatient(pId);
+		assertNotNull(createdPatientById);
+		
+		ps.deletePatient(pId);
+		Patient deletedPatientById = ps.getPatient(pId);
+		assertNull(deletedPatientById);
 	}
 	
 	public static Test suite() {
