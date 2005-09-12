78ef15ce9b520e1f9ac096ebceca64121d8d8ad4
no_autho <noauthor@xyz.com>
2005-09-12 15:29:39 +0000

    void method addition and comments update
    git-svn-id: http://svn.openmrs.org@29 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/api/ObsService.java
+++ src/api/org/openmrs/api/ObsService.java
@@ -4,12 +4,48 @@ import org.openmrs.*;
 
 public interface ObsService {
 
-	public void createObs(Obs obs);
+	/**
+	 * Create an observation 
+	 * @param Obs
+	 * @return newly created observation
+	 * @throws APIException
+	 */
+	public Obs createObs(Obs obs) throws APIException;
 
-	public Obs getObs(Long obsId);
+	/**
+	 * Get an observation
+	 * @param integer obsId of observation desired
+	 * @return matching Obs
+	 * @throws APIException
+	 */
+	public Obs getObs(Integer obsId) throws APIException;
 
-	public void saveOrUpdate(Obs obs);
+	/**
+	 * Save changes to observation
+	 * @param Obs
+	 * @throws APIException
+	 */
+	public void updateObs(Obs obs) throws APIException;
 
-	public void voidObs(Obs obs);
+	/**
+	 * Equivalent to deleting an observation
+	 * @param obs
+	 * @throws APIException
+	 */
+	public void voidObs(Obs obs) throws APIException;
+	
+	/**
+	 * Revive an observation (pull a Lazarus)
+	 * @param Obs
+	 * @throws APIException
+	 */
+	public void unVoidObs(Obs obs) throws APIException;
 
+	/**
+	 * Delete an observation.  SHOULD NOT BE CALLED unless caller is lower-level.
+	 * @param Obs
+	 * @throws APIException
+	 * @see voidObs(Obs)
+	 */
+	public void deleteObs(Obs obs) throws APIException;
 }
