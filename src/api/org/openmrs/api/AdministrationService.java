606bd1837f927a33809b5c8d940f726ffd00150a
Ben Wolfe <ben@openmrs.org>
2005-09-14 22:09:54 +0000

    completing admin add/update/delete type functions
    git-svn-id: http://svn.openmrs.org@96 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/api/AdministrationService.java
+++ src/api/org/openmrs/api/AdministrationService.java
@@ -1,6 +1,11 @@
 package org.openmrs.api;
 
 import org.openmrs.EncounterType;
+import org.openmrs.FieldType;
+import org.openmrs.MimeType;
+import org.openmrs.OrderType;
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.Tribe;
 
 /**
  * Admin-related services
@@ -32,5 +37,113 @@ public interface AdministrationService {
 	 */
 	public void deleteEncounterType(EncounterType encounterType) throws APIException;
 
+	/**
+	 * Create a new PatientIdentifierType
+	 * @param PatientIdentifierType to create
+	 * @return newly created PatientIdentifierType
+	 * @throws APIException
+	 */
+	public PatientIdentifierType createPatientIdentifierType(PatientIdentifierType patientIdentifierType) throws APIException;
+
+	/**
+	 * Update PatientIdentifierType
+	 * @param PatientIdentifierType to update
+	 * @throws APIException
+	 */
+	public void updatePatientIdentifierType(PatientIdentifierType patientIdentifierType) throws APIException;
+
+	/**
+	 * Delete PatientIdentifierType
+	 * @param PatientIdentifierType to delete
+	 * @throws APIException
+	 */
+	public void deletePatientIdentifierType(PatientIdentifierType patientIdentifierType) throws APIException;
+
+	/**
+	 * Create a new Tribe
+	 * @param Tribe to create
+	 * @return newly created Tribe
+	 * @throws APIException
+	 */
+	public Tribe createTribe(Tribe tribe) throws APIException;
+
+	/**
+	 * Update Tribe
+	 * @param Tribe to update
+	 * @throws APIException
+	 */
+	public void updateTribe(Tribe tribe) throws APIException;
+
+	/**
+	 * Delete Tribe
+	 * @param Tribe to delete
+	 * @throws APIException
+	 */
+	public void deleteTribe(Tribe tribe) throws APIException;	
 	
+	/**
+	 * Create a new OrderType
+	 * @param OrderType to create
+	 * @return newly created OrderType
+	 * @throws APIException
+	 */
+	public OrderType createOrderType(OrderType orderType) throws APIException;
+
+	/**
+	 * Update OrderType
+	 * @param OrderType to update
+	 * @throws APIException
+	 */
+	public void updateOrderType(OrderType orderType) throws APIException;
+
+	/**
+	 * Delete OrderType
+	 * @param OrderType to delete
+	 * @throws APIException
+	 */
+	public void deleteOrderType(OrderType orderType) throws APIException;
+	
+	/**
+	 * Create a new FieldType
+	 * @param FieldType to create
+	 * @return newly created FieldType
+	 * @throws APIException
+	 */
+	public FieldType createFieldType(FieldType fieldType) throws APIException;
+
+	/**
+	 * Update FieldType
+	 * @param FieldType to update
+	 * @throws APIException
+	 */
+	public void updateFieldType(FieldType fieldType) throws APIException;
+
+	/**
+	 * Delete FieldType
+	 * @param FieldType to delete
+	 * @throws APIException
+	 */
+	public void deleteFieldType(FieldType fieldType) throws APIException;
+	
+	/**
+	 * Create a new MimeType
+	 * @param MimeType to create
+	 * @return newly created MimeType
+	 * @throws APIException
+	 */
+	public MimeType createMimeType(MimeType mimeType) throws APIException;
+
+	/**
+	 * Update MimeType
+	 * @param MimeType to update
+	 * @throws APIException
+	 */
+	public void updateMimeType(MimeType mimeType) throws APIException;
+
+	/**
+	 * Delete MimeType
+	 * @param MimeType to delete
+	 * @throws APIException
+	 */
+	public void deleteMimeType(MimeType mimeType) throws APIException;	
 }
--- src/api/org/openmrs/api/ibatis/IbatisAdministrationService.java
+++ src/api/org/openmrs/api/ibatis/IbatisAdministrationService.java
@@ -5,6 +5,11 @@ import java.sql.SQLException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.EncounterType;
+import org.openmrs.FieldType;
+import org.openmrs.MimeType;
+import org.openmrs.OrderType;
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.Tribe;
 import org.openmrs.User;
 import org.openmrs.api.APIException;
 import org.openmrs.api.AdministrationService;
@@ -58,6 +63,217 @@ public class IbatisAdministrationService implements AdministrationService {
 	}
 
 	/**
+	 * @see org.openmrs.api.AdministrationService#createFieldType(org.openmrs.FieldType)
+	 */
+	public FieldType createFieldType(FieldType fieldType) throws APIException {
+		fieldType.setCreator(context.getAuthenticatedUser());
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				
+				SqlMap.instance().insert("createFieldType", fieldType);
+				
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		return fieldType;
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#createMimeType(org.openmrs.MimeType)
+	 */
+	public MimeType createMimeType(MimeType mimeType) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				
+				SqlMap.instance().insert("createMimeType", mimeType);
+				
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		return mimeType;
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#createOrderType(org.openmrs.OrderType)
+	 */
+	public OrderType createOrderType(OrderType orderType) throws APIException {
+		orderType.setCreator(context.getAuthenticatedUser());
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				
+				SqlMap.instance().insert("createOrderType", orderType);
+				
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		return orderType;
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#createPatientIdentifierType(org.openmrs.PatientIdentifierType)
+	 */
+	public PatientIdentifierType createPatientIdentifierType(PatientIdentifierType patientIdentifierType) throws APIException {
+		patientIdentifierType.setCreator(context.getAuthenticatedUser());
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				
+				SqlMap.instance().insert("createPatientIdentifierType", patientIdentifierType);
+				
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		return patientIdentifierType;
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#createTribe(org.openmrs.Tribe)
+	 */
+	public Tribe createTribe(Tribe tribe) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				
+				SqlMap.instance().insert("createTribe", tribe);
+				
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		return tribe;
+	}
+	
+	/**
+	 * @see org.openmrs.api.EncounterTypeService#deleteEncounterType(EncounterType)
+	 */
+	public void deleteEncounterType(EncounterType encounterType) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				SqlMap.instance().delete("deleteEncounterType", encounterType);
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#deleteFieldType(org.openmrs.FieldType)
+	 */
+	public void deleteFieldType(FieldType fieldType) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				SqlMap.instance().delete("deleteFieldType", fieldType);
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#deleteMimeType(org.openmrs.MimeType)
+	 */
+	public void deleteMimeType(MimeType mimeType) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				SqlMap.instance().delete("deleteMimeType", mimeType);
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#deleteOrderType(org.openmrs.OrderType)
+	 */
+	public void deleteOrderType(OrderType orderType) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				SqlMap.instance().delete("deleteOrderType", orderType);
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+
+		
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#deletePatientIdentifierType(org.openmrs.PatientIdentifierType)
+	 */
+	public void deletePatientIdentifierType(PatientIdentifierType patientIdentifierType) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				SqlMap.instance().delete("deletePatientIdentifierType", patientIdentifierType);
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+
+		
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#deleteTribe(org.openmrs.Tribe)
+	 */
+	public void deleteTribe(Tribe tribe) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+				SqlMap.instance().delete("deleteTribe", tribe);
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+
+		
+	}
+
+	/**
 	 * @see org.openmrs.api.EncounterTypeService#updateEncounterType(EncounterType)
 	 */
 	public void updateEncounterType(EncounterType encounterType) throws APIException {
@@ -79,15 +295,21 @@ public class IbatisAdministrationService implements AdministrationService {
 			throw new APIException(e);
 		}
 	}
-
+	
 	/**
-	 * @see org.openmrs.api.EncounterTypeService#deleteEncounterType(EncounterType)
+	 * @see org.openmrs.api.AdministrationService#updateFieldType(org.openmrs.FieldType)
 	 */
-	public void deleteEncounterType(EncounterType encounterType) throws APIException {
+	public void updateFieldType(FieldType fieldType) throws APIException {
 		try {
 			try {
 				SqlMap.instance().startTransaction();
-				SqlMap.instance().delete("deleteEncounterType", encounterType);
+
+				if (fieldType.getCreator() == null) {
+					this.createFieldType(fieldType);
+				} else {
+					SqlMap.instance().update("updateFieldType", fieldType);
+				}
+
 				SqlMap.instance().commitTransaction();
 			} finally {
 				SqlMap.instance().endTransaction();
@@ -95,5 +317,104 @@ public class IbatisAdministrationService implements AdministrationService {
 		} catch (SQLException e) {
 			throw new APIException(e);
 		}
+		
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#updateMimeType(org.openmrs.MimeType)
+	 */
+	public void updateMimeType(MimeType mimeType) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+
+				//TODO check if mimetype is in the table already
+				if (false) {
+					this.createMimeType(mimeType);
+				} else {
+					SqlMap.instance().update("updateMimeType", mimeType);
+				}
+
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#updateOrderType(org.openmrs.OrderType)
+	 */
+	public void updateOrderType(OrderType orderType) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+
+				if (orderType.getCreator() == null) {
+					this.createOrderType(orderType);
+				} else {
+					SqlMap.instance().update("updateOrderType", orderType);
+				}
+
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#updatePatientIdentifierType(org.openmrs.PatientIdentifierType)
+	 */
+	public void updatePatientIdentifierType(PatientIdentifierType patientIdentifierType) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+
+				if (patientIdentifierType.getCreator() == null) {
+					this.createPatientIdentifierType(patientIdentifierType);
+				} else {
+					SqlMap.instance().update("updatePatientIdentifierType", patientIdentifierType);
+				}
+
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		
+	}
+
+	/**
+	 * @see org.openmrs.api.AdministrationService#updateTribe(org.openmrs.Tribe)
+	 */
+	public void updateTribe(Tribe tribe) throws APIException {
+		try {
+			try {
+				SqlMap.instance().startTransaction();
+
+				//TODO check if tribe is alread in db
+				if (false) {
+					this.createTribe(tribe);
+				} else {
+					SqlMap.instance().update("updateTribe", tribe);
+				}
+
+				SqlMap.instance().commitTransaction();
+			} finally {
+				SqlMap.instance().endTransaction();
+			}
+		} catch (SQLException e) {
+			throw new APIException(e);
+		}
+		
 	}
 }
