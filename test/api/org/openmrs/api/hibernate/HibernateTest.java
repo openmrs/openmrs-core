f34e48d7ee49c5a550dfb710b27a1a4271e4d057
Ben Wolfe <ben@openmrs.org>
2005-09-22 20:25:23 +0000

    git-svn-id: http://svn.openmrs.org@174 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/api/hibernate/HibernateTest.java
+++ test/api/org/openmrs/api/hibernate/HibernateTest.java
@@ -12,6 +12,9 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.openmrs.Role;
 import org.openmrs.User;
+import org.openmrs.api.UserService;
+import org.openmrs.context.Context;
+import org.openmrs.context.ContextFactory;
 
 public class HibernateTest extends TestCase {
 
@@ -25,7 +28,7 @@ public class HibernateTest extends TestCase {
 		HibernateUtil.session.set(null);
 	}
 	
-	public void testRead() {
+	public void xtestRead() {
 		Session session = HibernateUtil.currentSession();
 		Transaction tx = session.beginTransaction();
 		User user = (User) session.get(User.class, 1);
@@ -35,7 +38,7 @@ public class HibernateTest extends TestCase {
 			System.out.println("  " + role.getRole());			
 		}
 		System.out.println("middle name (pre) = " + user.getMiddleName());
-		user.setMiddleName("William");
+		user.setMiddleName("Williams");
 //		Role newRole = (Role) session.get(Role.class, "nurse");
 //		user.removeRole(newRole);
 		User user2 = (User) session.get(User.class, 1);
@@ -44,6 +47,27 @@ public class HibernateTest extends TestCase {
 		HibernateUtil.closeSession();
 	}
 		
+	public void testGetName() throws Exception {
+		
+		Context context = ContextFactory.getContext();
+		
+		//context.authenticate("admin", "test");
+		
+		UserService us = context.getUserService();
+		
+		User u = us.getUserByUsername("admin");
+		
+		System.out.println("last name: " + u.getLastName());
+		
+		u.setLastName(u.getLastName() + "1");
+		
+		us.updateUser(u);
+		
+		User u2 = us.getUserByUsername("admin");
+		
+		System.out.println("last name (post): " + u.getLastName());
+	}
+	
 	public static Test suite() {
 		return new TestSuite(HibernateTest.class, "Basic Hibernate functionality");
 	}
