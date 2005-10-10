230efff1f857c9a3522c4f724dd8b252927d1870
Ben Wolfe <ben@openmrs.org>
2005-10-10 19:26:25 +0000

    git-svn-id: http://svn.openmrs.org@238 5bac5841-c719-aa4e-b3fe-cce5062f897a

---
+++ src/web/org/openmrs/web/FormDownloadServlet.java
@@ -0,0 +1,36 @@
+package org.openmrs.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.openmrs.context.Context;
+
+public class FormDownloadServlet extends HttpServlet {
+
+	public static final long serialVersionUID = 123423L;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession httpSession = request.getSession();
+		
+		Context context = (Context)httpSession.getAttribute(Constants.OPENMRS_CONTEXT_HTTPSESSION_ATTR);
+		
+		//context.logout();
+		//httpSession.setAttribute("openmrs_msg", "You are now logged out");
+		//response.sendRedirect("/openmrs/login.jsp");
+	}
+
+}
