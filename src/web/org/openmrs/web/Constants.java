3b8b9ddcad446353e422042378cc7738101792bd
Ben Wolfe <ben@openmrs.org>
2005-10-28 14:08:37 +0000

    git-svn-id: http://svn.openmrs.org@279 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/web/org/openmrs/web/Constants.java
+++ src/web/org/openmrs/web/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	public static final String OPENMRS_MSG_ATTR = "openmrs_msg";
 	public static final String OPENMRS_ERROR_ATTR = "openmrs_error";
 	
-	// TODO implement multiple language support
-	// TODO create constants for all text on all pages.
+	// TODO implement multiple language support here?
+	// TODO create constants for all text on all pages (In resource file?)
 	
 }
