c15bbfbf17db0f317e1fe83e71df652bb1ea01c5
Darius Jazayeri <jazayeri@alum.mit.edu>
2005-12-06 19:28:47 +0000

    git-svn-id: http://svn.openmrs.org@344 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/reporting/PatientDataSetAggregator.java
+++
@@ -1,9 +0,0 @@
-package org.openmrs.reporting;
-
-import java.util.Map;
-
-public interface PatientDataSetAggregator {
-
-	public Map<Object, Object> aggregatePatientDataSets(Map<Object, PatientDataSet> input);
-	
-}
