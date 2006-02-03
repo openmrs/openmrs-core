990e132c7437ded6aa2b40c7b86e73e87c2a9e9f
Ben Wolfe <ben@openmrs.org>
2006-02-03 21:03:13 +0000

    git-svn-id: http://svn.openmrs.org@440 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/api/ConceptService.java
+++ src/api/org/openmrs/api/ConceptService.java
@@ -42,9 +42,6 @@ public class ConceptService {
 	}
 	
 	private ConceptDAO getConceptDAO() {
-		if (!context.hasPrivilege(OpenmrsConstants.PRIV_VIEW_CONCEPTS))
-			throw new APIAuthenticationException("Privilege required: " + OpenmrsConstants.PRIV_VIEW_CONCEPTS);
-		
 		return daoContext.getConceptDAO();
 	}
 
