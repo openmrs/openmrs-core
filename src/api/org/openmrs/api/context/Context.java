b9728febb1db155b546f1de39aa2d9e1c99e542b
Justin Miranda <justin.miranda@gmail.com>
2006-02-14 05:18:28 +0000

    - getDAOContext() -> getDaoContext() for readability when/if we use dependency injection - added message service - getMessageService() uses bean factory
    git-svn-id: http://svn.openmrs.org@492 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/api/context/Context.java
+++ src/api/org/openmrs/api/context/Context.java
@@ -18,11 +18,22 @@ import org.openmrs.api.OrderService;
 import org.openmrs.api.PatientService;
 import org.openmrs.api.PatientSetService;
 import org.openmrs.api.UserService;
+import org.openmrs.api.MessageService;
 import org.openmrs.api.db.DAOContext;
 import org.openmrs.api.db.hibernate.HibernateDAOContext;
 import org.openmrs.formentry.FormEntryService;
 import org.openmrs.reporting.ReportService;
 import org.openmrs.util.OpenmrsConstants;
+
+//TODO: Need to remove these dependencies
+//import org.openmrs.api.MessageServiceImpl;  
+//import org.openmrs.notification.mail.MailMessageSender;
+//import org.openmrs.notification.mail.velocity.VelocityMessagePreparator;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  * Represents an OpenMRS <code>Context</code>, which may be used to
@@ -35,13 +46,14 @@ import org.openmrs.util.OpenmrsConstants;
  * @author Burke Mamlin
  * @version 1.0
  */
-public class Context {
+public class Context implements ApplicationContextAware {
 
 	private final Log log = LogFactory.getLog(getClass());
 
 	DAOContext daoContext;
-
 	private User user = null;
+	
+	// Services
 	private ConceptService conceptService;
 	private EncounterService encounterService;
 	private ObsService obsService;
@@ -55,16 +67,59 @@ public class Context {
 	private ReportService reportService;
 	private FormEntryService formEntryService;
 	private List<String> proxies = new Vector<String>();
-
-	public Context() {
-		
-	}
 	
-	private DAOContext getDAOContext() {
+
+	/**
+	 *  Default public constructor
+	 */
+	public Context() { }
+
+	
+	/**
+	 *  Set application context
+	 */
+	public void setApplicationContext(ApplicationContext context) { 
+		this.applicationContext = context;
+	}
+
+	
+	/**
+	 * Gets the DAO context.  
+	 * 
+	 * NOTE:  Instantiates a new DAO context if one does not already exist.  This means that a new DAO
+	 * context is created for every context.  If we have 100 users currently logged in, that means there
+	 * will be 100 DAO context instances in memory.  
+	 * 
+	 * We should be using dependency injection.  There is a context instance for each new http session
+	 * that is created, so this becomes a bit more difficult.  There are two separate paradigms in play here:
+	 * (1) user-specific context (2) service locator.  One is a user specific object, the other is 
+	 * an app-specific object.  We should have define these more clearly and keep the two distinct to 
+	 * avoid issues like this.   By the way, I changed the method to use camel-case for readability
+	 * if/when we use dependency injection in the future.
+	 * 
+	 * TODO:  Refactor into separate classes (user context vs. service locator) or refactor into user 
+	 * context (ONLY) and use dependency injection in Controller (and other client) classes to access
+	 * services layer.  
+	 * 
+	 * TODO:  Remove dependency of context within services layer.  
+	 * 
+	 * @return
+	 */
+	public DAOContext getDaoContext() { 
 		if (daoContext == null)
 			daoContext = new HibernateDAOContext(this);
 		return daoContext;
 	}
+	
+	/**
+	 * Used to set the DAO context for the application.
+	 * 
+	 * @param daoContext
+	 */
+	public void setDaoContext(DAOContext daoContext) { 
+		this.daoContext = daoContext;
+	}
+	
 	
 	/**
 	 * Used to authenticate user within the context
@@ -75,8 +130,8 @@ public class Context {
 	 */
 	public void authenticate(String username, String password)
 			throws ContextAuthenticationException {
-		getDAOContext().authenticate(username, password);
-		user = getDAOContext().getAuthenticatedUser();
+		getDaoContext().authenticate(username, password);
+		user = getDaoContext().getAuthenticatedUser();
 	}
 
 	/**
@@ -84,7 +139,7 @@ public class Context {
 	 */
 	public ConceptService getConceptService() {
 		if (conceptService == null)
-			conceptService = new ConceptService(this, getDAOContext());
+			conceptService = new ConceptService(this, getDaoContext());
 		return conceptService;
 	}
 	
@@ -93,7 +148,7 @@ public class Context {
 	 */
 	public EncounterService getEncounterService() {
 		if (encounterService == null)
-			encounterService = new EncounterService(this, getDAOContext());
+			encounterService = new EncounterService(this, getDaoContext());
 		return encounterService;
 	}
 	
@@ -102,7 +157,7 @@ public class Context {
 	 */
 	public ObsService getObsService() {
 		if (obsService == null)
-			obsService = new ObsService(this, getDAOContext());
+			obsService = new ObsService(this, getDaoContext());
 		return obsService;
 	}
 
@@ -111,7 +166,7 @@ public class Context {
 	 */
 	public PatientService getPatientService() {
 		if (patientService == null)
-			patientService = new PatientService(this, getDAOContext());
+			patientService = new PatientService(this, getDaoContext());
 		return patientService;		
 	}
 	
@@ -120,7 +175,7 @@ public class Context {
 	 */
 	public FormEntryService getFormEntryService() {
 		if (formEntryService == null)
-			formEntryService = new FormEntryService(this, getDAOContext());
+			formEntryService = new FormEntryService(this, getDaoContext());
 		return formEntryService;
 	}
 	
@@ -129,7 +184,7 @@ public class Context {
 	 */
 	public PatientSetService getPatientSetService() {
 		if (patientSetService == null) {
-			patientSetService = new PatientSetService(this, getDAOContext());
+			patientSetService = new PatientSetService(this, getDaoContext());
 		}
 		return patientSetService;
 	}
@@ -139,7 +194,7 @@ public class Context {
 	 */
 	public UserService getUserService() {
 		if (userService == null)
-			userService = new UserService(this, getDAOContext());
+			userService = new UserService(this, getDaoContext());
 		return userService;
 	}
 
@@ -148,7 +203,7 @@ public class Context {
 	 */
 	public OrderService getOrderService() {
 		if (orderService == null)
-			orderService = new OrderService(this, getDAOContext());
+			orderService = new OrderService(this, getDaoContext());
 		return orderService;
 	}
 	
@@ -157,7 +212,7 @@ public class Context {
 	 */
 	public FormService getFormService() {
 		if (formService == null)
-			formService = new FormService(this, getDAOContext());
+			formService = new FormService(this, getDaoContext());
 		return formService;
 	}
 	
@@ -170,7 +225,7 @@ public class Context {
 			return null;
 		}
 		if (reportService == null)
-			reportService = new ReportService(this, getDAOContext());
+			reportService = new ReportService(this, getDaoContext());
 		return reportService;
 	}
 
@@ -184,16 +239,83 @@ public class Context {
 			return null;
 		}
 		if (administrationService == null)
-			administrationService = new AdministrationService(this, getDAOContext());
+			administrationService = new AdministrationService(this, getDaoContext());
 		return administrationService;
 	}
 
+	/** 
+	 * Get the message service.
+	 * 
+	 * There are several ways to deal with the service layer objects.
+	 * 
+	 * (1) Dependency injection (preferred)
+	 * (2) Instantiate new instance within service (current implementation)
+	 * (3) Use bean factory to get reference to bean
+	 * (4) Use application context to get reference to bean
+	 * 
+	 * NOTE:  I prefer method (1) but will not be able to get it to work correctly until I can 
+	 * refactor the Context class.  The main issue is that the Context object is instantiated all 
+	 * over the place instead of being defined once in the bean definition file.  Therefore, I cannot "inject"
+	 * the message service (or any other service) because the client has control over instantiating the object.
+	 * I don't like method (2) because I don't want the context to instantiate as there is  
+	 * a lot of work that goes into setting up the message service object.  I couldn't figure out to 
+	 * get the "openmrs-servlet.xml" resource so I abandoned method (3).  Therefore, I 
+	 * have decided to go with method (4) for now.  It ties us (somewhat loosely) to the spring framework
+	 * as we now have the Context object implement ApplicationContextAware.  However, my plan is to make
+	 * Context an interface and implements this interface as the SpringContext so that certain Spring services
+	 * can be used (i.e. event publishing).  
+	 *  
+	 * @return message service
+	 */
+	public MessageService getMessageService() { 
+		if ( messageService == null ) { 
+			messageService = (MessageService) applicationContext.getBean("messageService");
+		}
+		return messageService;
+	}
+	 
+	/*
+	public MessageService getMessageService() { 
+		if ( messageService == null ) { 
+			try { 
+				log.info("Instantiating message service");
+				Resource beanDefinition = new ClassPathResource("openmrs-servlet.xml");
+				XmlBeanFactory beanFactory = new XmlBeanFactory( beanDefinition );
+				messageService = (MessageService)beanFactory.getBean("messageService");
+				log.info("Message service = " + messageService);
+			} catch (Exception e) { 
+				e.printStackTrace();
+			}
+		}
+	}	
+	*/
+	
+	/*
+	public MessageService getMessageService() {
+		if (messageService == null) {
+			try { 								
+				messageService = new MessageServiceImpl(getDaoContext());
+				
+				javax.mail.Session mailSession = (javax.mail.Session) = 
+					new InitialContext().lookup("java:comp/env/mail/OpenmrsMailSession");
+
+				messageService.setMailSession( mailSession );
+				messageService.setMessageSender( new MailMessageSender() );
+				messageService.setMessagePreparator( new VelocityMessagePreparator() );
+			} catch (Exception e) {
+				log.error( "Could not instantiate message service: ", e );
+			}
+		}
+		return messageService;
+	}*/
+
+	
 	/**
 	 * @return "active" user who has been authenticated, 
 	 *         otherwise <code>null</code> 
 	 */
 	public User getAuthenticatedUser() {
-		user = getDAOContext().getAuthenticatedUser();
+		user = getDaoContext().getAuthenticatedUser();
 		return user;
 	}
 	
@@ -210,7 +332,7 @@ public class Context {
 	 */
 	public void logout() {
 		user = null;
-		getDAOContext().logout();
+		getDaoContext().logout();
 	}
 
 	/**
@@ -288,10 +410,10 @@ public class Context {
 	}
 	
 	public void startTransaction() {
-		getDAOContext().openSession();
+		getDaoContext().openSession();
 	}
 	
 	public void endTransaction() {
-		getDAOContext().closeSession();
+		getDaoContext().closeSession();
 	}
 }
