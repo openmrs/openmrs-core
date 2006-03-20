803efc91dfcc039ec8577baf4430dfe4298e3485
Burke Mamlin <burke@openmrs.org>
2006-03-20 03:01:18 +0000

    Added timestamp to javadoc footer
    git-svn-id: http://svn.openmrs.org@546 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/ConceptNumeric.java
+++ src/api/org/openmrs/ConceptNumeric.java
@@ -1,6 +1,13 @@
 package org.openmrs;
 
 
+/**
+ * ConceptNumeric
+ * 
+ * @author Burke Mamlin
+ * @author Ben Wolfe
+ * @version 1.0
+ */
 public class ConceptNumeric extends Concept implements java.io.Serializable {
 
 	public static final long serialVersionUID = 47323L;
