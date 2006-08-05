5ec0390871f6abaeb14525a755027f2e9a8fd1c8
Darius Jazayeri <jazayeri@alum.mit.edu>
2006-08-05 09:20:53 +0000

    This should compile. It won't run right, but nobody else is using this part of the code.
    git-svn-id: http://svn.openmrs.org/branches/alpha@863 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/web/org/openmrs/web/propertyeditor/WorkflowCollectionEditor.java
+++ src/web/org/openmrs/web/propertyeditor/WorkflowCollectionEditor.java
@@ -1,10 +1,14 @@
 package org.openmrs.web.propertyeditor;
 
 import java.beans.PropertyEditorSupport;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Vector;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -25,23 +29,64 @@ public class WorkflowCollectionEditor extends PropertyEditorSupport {
 	}
 	
 	/**
-	 * Note that this takes conceptIds, not programWorkflowIds!
+	 * Note that this takes {programId}.{conceptId}, not programWorkflowIds!
 	 */
 	public void setAsText(String text) throws IllegalArgumentException {
 		if (context != null && StringUtils.hasText(text)) {
 			ConceptService cs = context.getConceptService();
 			ProgramWorkflowService pws = context.getProgramWorkflowService();
 			String[] conceptIds = text.split(" ");
-			List<Integer> requestConceptIds = new Vector<Integer>();
+			List<String> uniqueEntries = new ArrayList<String>();
 			for (String id : conceptIds) {
 				id = id.trim();
-				if (!id.equals("") && !requestConceptIds.contains(Integer.valueOf(id))) //remove whitespace, blank lines, and duplicate entries
-					requestConceptIds.add(Integer.valueOf(id));
+				if (id.equals("") || uniqueEntries.contains(id)) {
+					continue;
+				}
+				uniqueEntries.add(id);
+			}
+			
+			// remove duplicates, keeping the one that already has a programId
+			Collections.sort(uniqueEntries, new Comparator<String>() {
+					public int compare(String left, String right) {
+						Integer l = left.startsWith(".") ? 1 : 0;
+						Integer r = right.startsWith(".") ? 1 : 0;
+						return l.compareTo(r);
+					}
+				});
+			Set<Integer> conceptIdsSoFar = new HashSet<Integer>();
+			for (Iterator<String> i = uniqueEntries.iterator(); i.hasNext(); ) {
+				String id = i.next();
+				Integer programId = null;
+				Integer conceptId = null;
+				if (id.startsWith(".")) {
+					conceptId = Integer.valueOf(id);
+				} else {
+					String[] temp = id.split("\\.");
+					programId = Integer.valueOf(temp[0]);
+					conceptId = Integer.valueOf(temp[1]);
+				}
+				if (conceptIdsSoFar.contains(conceptId)) {
+					i.remove();
+				} else {
+					conceptIdsSoFar.add(conceptId);
+				}
 			}
 			
 			Collection<ProgramWorkflow> newConceptList = new HashSet<ProgramWorkflow>();
-			for (Integer conceptId : requestConceptIds) {
-				ProgramWorkflow workflow = pws.getProgramWorkflowByConceptId(conceptId);
+			for (String id : uniqueEntries) {
+				Integer programId = null;
+				Integer conceptId = null;
+				if (id.startsWith(".")) {
+					conceptId = Integer.valueOf(id);
+				} else {
+					String[] temp = id.split("\\.");
+					programId = Integer.valueOf(temp[0]);
+					conceptId = Integer.valueOf(temp[1]);
+				}
+				ProgramWorkflow workflow = null;
+				if (programId != null) {
+					workflow = pws.findWorkflowByProgramAndConcept(programId, conceptId);
+				}
 				if (workflow == null) {
 					workflow = new ProgramWorkflow();
 					workflow.setConcept(cs.getConcept(conceptId));
