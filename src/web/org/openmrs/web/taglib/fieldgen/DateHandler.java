bf0f47350a6145a4d06607fc2302d33cad9fc511
Michael Seaton <mseaton@pih.org>
2006-08-16 05:24:16 +0000

    change javascript references to use htmlinclude tag
    git-svn-id: http://svn.openmrs.org/branches/alpha@975 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/web/org/openmrs/web/taglib/fieldgen/DateHandler.java
+++ src/web/org/openmrs/web/taglib/fieldgen/DateHandler.java
@@ -8,10 +8,8 @@ public class DateHandler extends AbstractFieldGenHandler implements FieldGenHand
 	
 	public void run() {
 		setUrl(defaultUrl);
-
-		htmlInclude("/scripts/calendar/calendar.js");
 		
-		String datePattern = OpenmrsConstants.OPENMRS_LOCALE_DATE_PATTERNS().get(getContext().getLocale().toString().toLowerCase());
+		String datePattern = OpenmrsConstants.OPENMRS_LOCALE_DATE_PATTERNS_JS().get(getContext().getLocale().toString().toLowerCase());
 		String needScript = "true";
 		
 		if ( getRequest().getAttribute("org.openmrs.widget.dateField.needScript") != null ) {
