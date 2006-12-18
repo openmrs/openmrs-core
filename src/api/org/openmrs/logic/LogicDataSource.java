ff053ea1dc05f1b5e9dc3bcb9cf453345f7259ff
Vibha Anand <vanand@iupui.edu>
2006-12-18 21:19:57 +0000

    Added Method - public Result eval(Patient patient, Aggregation aggregation, Concept concept, Constraint constraint); in the interface definition.
    git-svn-id: http://svn.openmrs.org/branches/alpha@1312 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/logic/LogicDataSource.java
+++ src/api/org/openmrs/logic/LogicDataSource.java
@@ -10,7 +10,9 @@ public interface LogicDataSource {
 	public Result eval(Patient patient, String token, Object[] args);
 
 	public Result eval(Patient patient, Rule rule, Object[] args);
-
+	
+	public Result eval(Patient patient, Aggregation aggregation, Concept concept, Constraint constraint);
+	
 	public Result eval(Patient patient, Aggregation aggregation, String token, Constraint constraint, Object[] args);
 
 	public Result eval(Patient patient, Aggregation aggregation, Rule rule, Constraint constraint, Object[] args);
