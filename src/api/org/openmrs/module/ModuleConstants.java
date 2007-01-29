d3da3fda047c1a0f6959bc71b4435de1a7cc3c37
Ben Wolfe <ben@openmrs.org>
2007-01-29 18:51:52 +0000

    Committing files for latest module changes
    git-svn-id: http://svn.openmrs.org/branches/alpha@1424 5bac5841-c719-aa4e-b3fe-cce5062f897a

---
+++ src/api/org/openmrs/module/ModuleConstants.java
@@ -0,0 +1,19 @@
+package org.openmrs.module;
+
+/**
+ * Utility methods for working and manipulating modules
+ * 
+ * @author Ben Wolfe
+ * @version 1.0
+ */
+public class ModuleConstants {
+	//private static Log log = LogFactory.getLog(ModuleConstants.class);
+	
+	public static String UPDATE_FILE_NAME = "update.rdf";
+	
+	public static String PROPERTY_REPOSITORY_FOLDER = "module_repository_folder";
+	public static String PROPERTY_REPOSITORY_FOLDER_DEFAULT = "modules";
+	
+	public static String RUNTIMEPROPERTY_ALLOW_UPLOAD = "module.allow_upload";
+		
+}
