0dce443b78555c29e1c0b1e0e9ebc7b47d219620
Ben Wolfe <ben@openmrs.org>
2007-05-07 18:48:12 +0000

    Updated mysql database setup scripts to 1.1 Updated install package to 1.1
    git-svn-id: http://svn.openmrs.org/branches/alpha@1763 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- metadata/install/install/installDB.cmd
+++ metadata/install/install/installDB.cmd
@@ -9,17 +9,17 @@ ECHO **************************************
 ECHO Creating Database
 "$MySQL_Install_Directory\bin\mysql.exe" -u$MySQL_Admin_User -p$MySQL_Admin_Password < "$INSTALL_PATH\model\openmrs_createdb-mysql.sql"
 ECHO **************************************
-ECHO Installing Datamodel 1.0
-"$MySQL_Install_Directory\bin\mysql.exe" -u$MySQL_Admin_User -p$MySQL_Admin_Password -D$Database_Name < "$INSTALL_PATH\model\openmrs_1.0.0-mysql.sql"
+ECHO Installing Datamodel 1.1
+"$MySQL_Install_Directory\bin\mysql.exe" -u$MySQL_Admin_User -p$MySQL_Admin_Password -D$Database_Name < "$INSTALL_PATH\model\openmrs_1.1.0-mysql.sql"
 ECHO **************************************
-ECHO Adding Data
-"$MySQL_Install_Directory\bin\mysql.exe" -u$MySQL_Admin_User -p$MySQL_Admin_Password -D$Database_Name < "$INSTALL_PATH\model\openmrs_1.0.0-data-mysql.sql"
+ECHO Adding Base Data Set
+"$MySQL_Install_Directory\bin\mysql.exe" -u$MySQL_Admin_User -p$MySQL_Admin_Password -D$Database_Name < "$INSTALL_PATH\model\openmrs_1.1.0-data-mysql.sql"
 ECHO **************************************
-ECHO Adding Patient Data
-"$MySQL_Install_Directory\bin\mysql.exe" -u$MySQL_Admin_User -p$MySQL_Admin_Password -D$Database_Name < "$INSTALL_PATH\model\openmrs_1.0.0-patient-data-mysql.sql"
+ECHO Adding Demo Patient Data
+"$MySQL_Install_Directory\bin\mysql.exe" -u$MySQL_Admin_User -p$MySQL_Admin_Password -D$Database_Name < "$INSTALL_PATH\model\openmrs_1.1.0-patient-data-mysql.sql"
 ECHO **************************************
-ECHO Upgrading to Latest Datamodel - This will take some time...
-"$MySQL_Install_Directory\bin\mysql.exe" -u$MySQL_Admin_User -p$MySQL_Admin_Password -D$Database_Name < "$INSTALL_PATH\model\openmrs_1.0.0-to-latest-mysqldiff.sql"
+ECHO Upgrading to Latest Datamodel - This may take some time to complete...
+"$MySQL_Install_Directory\bin\mysql.exe" -u$MySQL_Admin_User -p$MySQL_Admin_Password -D$Database_Name < "$INSTALL_PATH\model\openmrs_1.1.0-to-latest-mysqldiff.sql"
 REM ECHO NOT DONE - UPGRADE FAILS WITH DATA
 
 ECHO ******************
