c4b386eddff98f901f582b6c06b6a7200a59f2bd
Maros <maros6677@gmail.com>
2007-08-14 14:21:02 +0000

    data_synchronization branch: removed Comparable - no need to implement compareTo() semantics on SyncItemKey.
    git-svn-id: http://svn.openmrs.org/openmrs/branches/data_synchronization@2188 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/synchronization/engine/SyncItemKey.java
+++ src/api/org/openmrs/synchronization/engine/SyncItemKey.java
@@ -6,64 +6,53 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * This class represents a unique identifier of a <i>SyncItem</i> item in a repository.
+ * This class represents a unique identifier of a <i>SyncItem</i> item based on a simple generic.  
  *
  */
-public class SyncItemKey implements Comparable<SyncItemKey>, Serializable {
+public class SyncItemKey<T> implements Serializable {
     
     public static final long serialVersionUID = 0L;
     public Log log = LogFactory.getLog(this.getClass());
 
     // Fields
-    private Object keyValue = null;
+    private T keyValue = null;
 
     // Constructors
-    public SyncItemKey(Object keyValue) {
+    public SyncItemKey() {}
+    public SyncItemKey(T keyValue) {
         assert (keyValue != null);
         this.keyValue = keyValue;
     }
 
     // Properties
-    public Object getKeyValue(){
+    public T getKeyValue(){
         return keyValue;
-    }
-    
-    public void setKeyValue(Object keyValue){
+    } 
+    public void setKeyValue (T keyValue){
         assert (keyValue != null);
         this.keyValue = keyValue;
     }
 
     // Methods
-    public String getKeyAsString() {
-        return keyValue.toString();
-    }
-
+       
+    //equality is determined based on T.equals()
+    @Override
     public boolean equals(Object o) {
         if (!(o instanceof SyncItemKey)) return false;
-        return ((SyncItemKey)o).getKeyAsString().equals(getKeyAsString());
+        return ((SyncItemKey)o).getKeyValue().equals(getKeyValue());
     }
-
-    public int compareTo(SyncItemKey otherKey) {
-        if (keyValue != null && otherKey.getKeyValue() != null) {
-            return keyValue.toString().compareTo(otherKey.getKeyValue().toString());
-        }
-        
-        if (keyValue == null && otherKey.getKeyValue() == null) {
-            return 0;
-        }
-
-        return -1;
-    }
-   
+    
+    @Override
     public int hashCode() {
-        return getKeyAsString().hashCode();
+        return getKeyValue().hashCode();
     }
 
+    @Override
     public String toString() {
         StringBuffer sb = new StringBuffer();
 
         sb.append( " { keyValue: "  );
-        sb.append( getKeyAsString() );
+        sb.append( getKeyValue() );
         sb.append( " } "            );
 
         return sb.toString();
