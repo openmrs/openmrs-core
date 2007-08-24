b00138df11eb95166890a94b4b65c22d3e6f7059
Maros <maros6677@gmail.com>
2007-08-24 11:57:49 +0000

    data_synchronization branch: added code to base sync test to read junit uid/pwd from runtime props file, if not present revert to default BaseTest behavior.
    git-svn-id: http://svn.openmrs.org/openmrs/branches/data_synchronization@2262 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/synchronization/engine/SyncBaseTest.java
+++ test/api/org/openmrs/synchronization/engine/SyncBaseTest.java
@@ -1,10 +1,10 @@
 package org.openmrs.synchronization.engine;
 
+import java.util.Properties;
 
 import org.openmrs.BaseTest;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.SynchronizationService;
-
 
 public class SyncBaseTest extends BaseTest {
 
@@ -12,13 +12,32 @@ public class SyncBaseTest extends BaseTest {
     protected void onSetUpBeforeTransaction() throws Exception {
         super.onSetUpBeforeTransaction();
         authenticate();
-        
+
     }
 
-    //uncomment and put in your test user credentials here
-    //@Override
-    //public synchronized String[] getUsernameAndPassword(String message) {
-    //    return (new String[] {"admin","<put your pwd here>"});
-    //}
-    
+    /** Pull credentials from runtime file, if available, if not default to prompt.
+     * 
+     * @see org.openmrs.BaseTest#getUsernameAndPassword(java.lang.String)
+     */
+    @Override
+    public synchronized String[] getUsernameAndPassword(String message) {
+
+        Properties props = null;
+        String username = null;
+        String userpwd = null;
+        
+        try {            
+        props = this.getRuntimeProperties();
+        username = (!props.containsKey("junit.username")) ?  null : props.getProperty("junit.username");
+        userpwd = (!props.containsKey("junit.userpwd")) ?  null : props.getProperty("junit.userpwd");
+        }
+        catch(Exception e) {
+            // anything happens, default to calling supper
+        }
+        
+        if (username == null || userpwd == null)
+            return super.getUsernameAndPassword(message);
+        else
+            return (new String[] {username,userpwd});
+    }
 }
