e18406a811e5e6c9b7f719624cc8b418f9af12ce
Maros <maros6677@gmail.com>
2007-12-13 17:59:54 +0000

    data_synchronization_bidirectional branch: Cleanup.
    git-svn-id: http://svn.openmrs.org/openmrs/branches/data_synchronization_bidirectional@2859 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- metadata/model/openmrs_1.1.0-add-synchronization.sql
+++ metadata/model/openmrs_1.1.0-add-synchronization.sql
@@ -15,10 +15,11 @@ delimiter //
 CREATE PROCEDURE sync_setup_procedure()
  BEGIN
 
-	#--------------------------------------
-	# Anders Gjendem 14 Aug 2007 9:00PM
-	# Add table for Synchronization Journal
-	#--------------------------------------
+	#Drop tables with FK relationships first
+	DROP TABLE IF EXISTS synchronization_server_class;
+	DROP TABLE IF EXISTS synchronization_server_record;
+
+	#synchronization_journal
 	DROP TABLE IF EXISTS synchronization_journal;
 	CREATE TABLE `synchronization_journal` (
 	  `record_id` int(11) NOT NULL auto_increment,
@@ -29,9 +30,12 @@ CREATE PROCEDURE sync_setup_procedure()
 	  `retry_count` int(11) default NULL,
 	  `state` varchar(20) default NULL,
 	  `payload` longtext,
+	  `contained_classes` varchar(1000) default '',
+	  `original_guid` varchar(36) NOT NULL,	  
 	  PRIMARY KEY  (`record_id`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+	#synchronization_import
 	DROP TABLE IF EXISTS synchronization_import;
 	CREATE TABLE `synchronization_import` (
 	  `record_id` int(11) NOT NULL auto_increment,
@@ -43,10 +47,10 @@ CREATE PROCEDURE sync_setup_procedure()
 	  `state` varchar(20) default NULL,
 	  `payload` longtext,
 	  `error_message` varchar(255) default NULL,
-	  `resulting_record_guid` char(36) default NULL,
 	  PRIMARY KEY  (`record_id`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+	#synchronization_server
 	DROP TABLE IF EXISTS synchronization_server;
 	CREATE TABLE `synchronization_server` (
 	  `server_id` int(11) NOT NULL auto_increment,
@@ -55,31 +59,14 @@ CREATE PROCEDURE sync_setup_procedure()
 	  `server_type` varchar(20) NOT NULL,
 	  `username` varchar(255) default NULL,
 	  `password` varchar(255) default NULL,
-	  `guid` char(36) default '',
+	  `guid` char(36) default NULL,
+	  `last_sync` datetime default NULL,
+	  `disabled` tinyint(1) NOT NULL,
+	  `child_username` varchar(50) default '',
 	  PRIMARY KEY  (`server_id`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
- 
-	 # Create/fill-in server guid global property
-	 if (SELECT count(*) FROM global_property where property = 'synchronization.server_guid') > 0  then
-	    if (SELECT count(*) FROM global_property
-	               where property = 'synchronization.server_guid'
-	               and (property_value is null or property_value = '') ) = 0  then
-	       #already there do nothing
-	       select 'server guid already assigned, no change made' from dual;
-	    else
-	       #row there but empty, assign new
-	       update global_property set property_value = uuid() where property = 'synchronization.server_guid';
-	    end if;
-	 else
-	    insert into global_property (property, property_value, description, guid)
-	    values ('synchronization.server_guid',
-	           uuid(),
-	           'Globally unique server id used to identify a given data source in synchronization.',
-	           uuid());
-	 end if;
 
-	ALTER TABLE `synchronization_server` ADD COLUMN `last_sync` datetime default NULL;
-
+	#synchronization_class
 	DROP TABLE IF EXISTS synchronization_class;
 	CREATE TABLE `synchronization_class` (
 	  `class_id` int(11) NOT NULL auto_increment,
@@ -88,8 +75,7 @@ CREATE PROCEDURE sync_setup_procedure()
 	  `default_to` tinyint(1) NOT NULL,
 	  `default_from` tinyint(1) NOT NULL,
 	  PRIMARY KEY  (`class_id`)
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-	
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;	
 	INSERT INTO `synchronization_class` (`name`,`type`,`default_to`,`default_from`) values
 	('Cohort', 'PATIENT', 1, 1),
 	('ComplexObs', 'PATIENT', 1, 1),
@@ -143,7 +129,7 @@ CREATE PROCEDURE sync_setup_procedure()
 	('Tribe', 'PATIENT', 1, 1),
 	('User', 'REQUIRED', 1, 1);
 
-	DROP TABLE IF EXISTS synchronization_server_class;
+	#synchronization_server_class
 	CREATE TABLE `synchronization_server_class` (
 		`server_class_id` int(11) NOT NULL auto_increment,
 		`class_id` int(11) NOT NULL,
@@ -157,9 +143,7 @@ CREATE PROCEDURE sync_setup_procedure()
 		CONSTRAINT `server_class_server` FOREIGN KEY (`server_id`) REFERENCES `synchronization_server` (`server_id`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	ALTER TABLE `synchronization_journal` ADD COLUMN `contained_classes` varchar(1000) default '';
-
-	DROP TABLE IF EXISTS synchronization_server_record;
+	#synchronization_server_record
 	CREATE TABLE `synchronization_server_record` (
 		`server_record_id` int(11) NOT NULL auto_increment,
 		`server_id` int(11) NOT NULL,
@@ -173,8 +157,27 @@ CREATE PROCEDURE sync_setup_procedure()
 		CONSTRAINT `server_record_record` FOREIGN KEY (`record_id`) REFERENCES `synchronization_journal` (`record_id`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	ALTER TABLE `synchronization_server` ADD COLUMN `disabled` tinyint(1) NOT NULL;
-	
+  
+	 # Create/fill-in server guid global property
+	 if (SELECT count(*) FROM global_property where property = 'synchronization.server_guid') > 0  then
+	    if (SELECT count(*) FROM global_property
+	               where property = 'synchronization.server_guid'
+	               and (property_value is null or property_value = '') ) = 0  then
+	       #already there do nothing
+	       select 'server guid already assigned, no change made' from dual;
+	    else
+	       #row there but empty, assign new
+	       update global_property set property_value = uuid() where property = 'synchronization.server_guid';
+	    end if;
+	 else
+	    insert into global_property (property, property_value, description, guid)
+	    values ('synchronization.server_guid',
+	           uuid(),
+	           'Globally unique server id used to identify a given data source in synchronization.',
+	           uuid());
+	 end if;
+
+	#fill out the default role for sync
 	IF( SELECT count(*) > 0 FROM global_property WHERE property = 'synchronization.default_role' ) THEN
 		UPDATE global_property SET property_value='System Developer' where property='synchronization.default_role';
 	ELSE
@@ -182,18 +185,10 @@ CREATE PROCEDURE sync_setup_procedure()
 		('synchronization.default_role', 'System Developer', '', UUID());	
 	END IF;
 
-	ALTER TABLE `synchronization_server` ADD COLUMN `child_username` varchar(50) default '';
-
-	ALTER TABLE `synchronization_journal` ADD COLUMN `original_guid` varchar(36) NOT NULL;
-
-	ALTER TABLE `synchronization_import` DROP COLUMN `resulting_record_guid`;
-
-	ALTER TABLE `synchronization_server` MODIFY COLUMN `guid` varchar (36) default '';
-
  END;
 //
 delimiter ;
 select 'Executing sync_static_data_procedure completed..' as ' ';
 call sync_setup_procedure();
-select 'Sync_setup_procedure completed.';
+select 'Sync_setup_procedure completed.' as '';
 drop procedure sync_setup_procedure;
