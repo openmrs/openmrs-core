0b5da0fa756e5ec45ef7f98ef20aa763c81e3af4
Ben Wolfe <ben@openmrs.org>
2007-12-31 18:51:44 +0000

    Updated database sql scripts to 1.2.0
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@2967 5bac5841-c719-aa4e-b3fe-cce5062f897a

---
+++ metadata/model/openmrs_1.2.0-to-latest-mysqldiff.sql
@@ -0,0 +1,11 @@
+#--------------------------------------
+# USE:
+#  The diffs are ordered by datamodel version number.
+#--------------------------------------
+
+
+#-----------------------------------
+# Clean up - Keep this section at the very bottom of diff script
+#-----------------------------------
+
+DROP PROCEDURE IF EXISTS diff_procedure;
