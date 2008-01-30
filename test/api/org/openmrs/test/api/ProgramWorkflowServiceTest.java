b384a8cc927ef50104c8dcd57d8e7d6e414d759d
Brian McKown <bmckown@regenstrief.org>
2008-01-30 01:10:35 +0000

    Fixed ticket #590 in trunk.  Programs correctly update on patient dashboard.  Added feature prohibits update when completion date comes before enrollment date.
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@3247 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/test/api/ProgramWorkflowServiceTest.java
+++ test/api/org/openmrs/test/api/ProgramWorkflowServiceTest.java
@@ -16,6 +16,7 @@ package org.openmrs.test.api;
 import java.util.Date;
 
 import org.openmrs.PatientProgram;
+import org.openmrs.User;
 import org.openmrs.api.AdministrationService;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.ProgramWorkflowService;
@@ -38,6 +39,8 @@ public class ProgramWorkflowServiceTest extends BaseContextSensitiveTest {
 	@Override
 	protected void onSetUpInTransaction() throws Exception {
 		initializeInMemoryDatabase();
+		executeDataSet(CREATE_PATIENT_PROGRAMS_XML);
+
 		authenticate();
 		
 		if (pws == null) {
@@ -48,30 +51,71 @@ public class ProgramWorkflowServiceTest extends BaseContextSensitiveTest {
 	}
 
 	/**
+	 * @see org.openmrs.test.BaseContextSensitiveTest#useInMemoryDatabase()
+	 *
+	@Override
+	public Boolean useInMemoryDatabase( ) {
+		return false;
+	}
+	 */
+		
+	/**
 	 * Tests fetching a PatientProgram, updating and saving it, and 
 	 * subsequently fetching the updated value.
 	 * 
+	 * To use in MySQL database:
+	 * Uncomment method useInMemoryDatabase() and comment out 
+	 * call to initializeInMemoryDatabase() and executeDataSet()
+	 * within onSetupTransaction() .
+	 * 
 	 * @throws Exception
 	 */
-	public void testGetPatientProgram() throws Exception {
+	public void testUpdatePatientProgram( ) throws Exception {
 		
 		Date today = new Date();
-		executeDataSet(CREATE_PATIENT_PROGRAMS_XML);
 		
 		PatientProgram patientProgram = pws.getPatientProgram(1);
-		assertEquals(patientProgram.getProgram().getProgramId(), Integer.valueOf(1));
+		Date dateCompleted = patientProgram.getDateCompleted();
+		Date dateChanged = patientProgram.getDateChanged();
+		User changedBy = patientProgram.getChangedBy();
+		if (null != dateCompleted) {
+			System.out.println("Date Completed: " + dateCompleted);
+		}
+		if (null != dateChanged) {
+			System.out.println("Date Changed: " + dateChanged);
+		}
+		if (null != changedBy) {
+			System.out.println("Changed By: " + changedBy.toString());
+		}
 		
 		patientProgram.setDateCompleted(today);
 		patientProgram.setChangedBy(Context.getAuthenticatedUser());
 		patientProgram.setDateChanged(today);
 		pws.updatePatientProgram(patientProgram);
 		
+		// Uncomment to commit to database
+		// setComplete( );
+		
 		PatientProgram ptProg = pws.getPatientProgram(1);
-		assertEquals(ptProg.getDateCompleted(), today);
+		Date dateCompleted2 = patientProgram.getDateCompleted();
+		Date dateChanged2 = patientProgram.getDateChanged();
+		User changedBy2 = patientProgram.getChangedBy();
+
+		if (null != dateCompleted2) {
+			System.out.println("Date Completed: " + dateCompleted2);
+		}
+		if (null != dateChanged2) {
+			System.out.println("Date Changed: " + dateChanged2);
+		}
+		if (null != changedBy2) {
+			System.out.println("Changed By: " + changedBy2.toString());
+		}
+		
+		assertNotNull(ptProg.getDateCompleted());
+		assertEquals(today, ptProg.getDateCompleted());
+		
 	}
-	
-	
-	
+		
 //	/**
 //	 * This method should be uncommented when you want to examine the actual hibernate
 //	 * sql calls being made.  The calls that should be limiting the number of returned
