853ad836a81bbc92808bc54402ea72a659c10812
Maros <maros6677@gmail.com>
2008-02-01 14:36:14 +0000

    data_synchronization_bidirectional branch. Added simple dump of sync records into stdout for diagnostics purposes.
    git-svn-id: http://svn.openmrs.org/openmrs/branches/data_synchronization_bidirectional@3268 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/synchronization/engine/SyncBaseTest.java
+++ test/api/org/openmrs/synchronization/engine/SyncBaseTest.java
@@ -9,6 +9,12 @@ import org.openmrs.BaseContextSensitiveTest;
 import org.openmrs.api.context.Context;
 import org.openmrs.synchronization.server.RemoteServer;
 
+import org.openmrs.serialization.Package;
+import org.openmrs.serialization.FilePackage;
+import org.openmrs.serialization.Record;
+import org.openmrs.serialization.IItem;
+import org.openmrs.serialization.Item;
+
 /**
  * Placeholder to setup common routines and initialization for all sync tests.
  *
@@ -51,7 +57,21 @@ public abstract class SyncBaseTest extends BaseContextSensitiveTest {
 		List<SyncRecord> syncRecords = Context.getSynchronizationService().getSyncRecords();
 		if (syncRecords == null || syncRecords.size() == 0)
 			assertFalse("No changes found (i.e. sync records size is 0)", true);
-		
+
+        //dump the sync records state to stdout
+        Package pkg = new FilePackage();
+        Record record = pkg.createRecordForWrite("SyncTest");
+        Item top = record.getRootItem();
+        for (SyncRecord r : syncRecords) {
+            ((IItem) r).save(record, top);
+        }
+        System.out.println ("*** serialized state ***");
+        try {
+            System.out.println(record.toString());
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            fail("Serialization failed with an exception: " + e.getMessage());
+        }		
 		deleteAllData();
 		executeDataSet("org/openmrs/synchronization/engine/include/SyncCreateTest.xml");
 		executeDataSet("org/openmrs/synchronization/engine/include/SyncRemoteChildServer.xml");
