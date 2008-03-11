8f3cafff1793e74c8ac526f08b2ab6da05127b9f
Darius Jazayeri <jazayeri@alum.mit.edu>
2008-03-11 04:41:54 +0000

    slight fix to a data convention check
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@3586 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- metadata/model/test-data-conventions.sql
+++ metadata/model/test-data-conventions.sql
@@ -11,8 +11,10 @@ select dt.name, count(*)
 from obs o
 	inner join concept c on o.concept_id = c.concept_id
 	inner join concept_datatype dt on c.datatype_id = dt.concept_datatype_id
-where (dt.hl7_abbreviation in ('NM', 'SN') and o.value_numeric is null)
-   or (dt.hl7_abbreviation in ('DT', 'TM', 'TS') and o.value_datetime is null)
-   or (dt.hl7_abbreviation in ('CWE') and o.value_coded is null)
-   or (dt.hl7_abbreviation in ('ST') and o.value_text is null)
+where
+  o.voided = false
+  and ((dt.hl7_abbreviation in ('NM', 'SN', 'BIT') and o.value_numeric is null) or
+       (dt.hl7_abbreviation in ('DT', 'TM', 'TS') and o.value_datetime is null) or
+       (dt.hl7_abbreviation in ('CWE') and o.value_coded is null)  or
+       (dt.hl7_abbreviation in ('ST') and o.value_text is null))
 group by dt.name;
