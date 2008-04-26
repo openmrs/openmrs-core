9ec4eaca12d9a09f71b262f824e4ec9a14430c6b
Chase Yarbrough <vergil@gmail.com>
2008-04-26 20:00:50 +0000

    synchronization-bidirectional: decided that this change to the data model should go in the trunk, and the trunk should be merged into this branch.
    git-svn-id: http://svn.openmrs.org/openmrs/branches/data_synchronization_bidirectional@4038 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- metadata/model/openmrs_1.2.0-to-latest-mysqldiff.sql
+++ metadata/model/openmrs_1.2.0-to-latest-mysqldiff.sql
@@ -3,7 +3,6 @@
 #  The diffs are ordered by datamodel version number.
 #--------------------------------------
 
-ALTER TABLE `obs` ADD INDEX `obs_group_id`(`obs_group_id`);
 
 #-----------------------------------
 # Clean up - Keep this section at the very bottom of diff script
