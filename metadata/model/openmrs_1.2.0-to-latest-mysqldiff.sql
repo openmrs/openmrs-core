17223564609404904b2c819688bd05d7802956df
Chase Yarbrough <vergil@gmail.com>
2008-04-26 18:54:01 +0000

    synchronization-birdirectional: Added obs_group_id as an index.
    git-svn-id: http://svn.openmrs.org/openmrs/branches/data_synchronization_bidirectional@4036 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- metadata/model/openmrs_1.2.0-to-latest-mysqldiff.sql
+++ metadata/model/openmrs_1.2.0-to-latest-mysqldiff.sql
@@ -3,6 +3,7 @@
 #  The diffs are ordered by datamodel version number.
 #--------------------------------------
 
+ALTER TABLE `obs` ADD INDEX `obs_group_id`(`obs_group_id`);
 
 #-----------------------------------
 # Clean up - Keep this section at the very bottom of diff script
