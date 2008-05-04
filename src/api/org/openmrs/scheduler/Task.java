a15b51bacda2cd38716ab843b3e38e9b7e91c25c
Chase Yarbrough <vergil@gmail.com>
2008-05-04 11:07:06 +0000

    Set the svn:eol-style property to CRLF.
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@4095 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/scheduler/Task.java
+++ src/api/org/openmrs/scheduler/Task.java
@@ -1,1 +1,32 @@
-package org.openmrs.scheduler;public interface Task {      /**     *  Executes the task defined in the task definition.    */    void execute();       /**     * Initializes the task and sets the task definition.     *      * @param config     */    void initialize(TaskDefinition config);            /**     * Returns true if the task is currently in its execute() method.     *      * @return true if task is executing, false otherwise     */    boolean isExecuting();	        /**     * Callback method used to clean up resources used during the tasks execution.     */    void shutdown();        }
+package org.openmrs.scheduler;
+
+
+
+public interface Task {
+   
+   /** 
+    *  Executes the task defined in the task definition.
+    */
+    void execute();
+    
+   /** 
+    * Initializes the task and sets the task definition. 
+    *  
+    * @param config 
+    */
+    void initialize(TaskDefinition config);
+        
+    /**
+     * Returns true if the task is currently in its execute() method.
+     * 
+     * @return true if task is executing, false otherwise
+     */
+    boolean isExecuting();
+	    
+    /**
+     * Callback method used to clean up resources used during the tasks execution.
+     */
+    void shutdown();
+    
+    
+}
