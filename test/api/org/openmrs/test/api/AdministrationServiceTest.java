10605392ea6a91ff59a22daf4d2d7bae5e4aefc0
Chase Yarbrough <vergil@gmail.com>
2008-05-21 22:30:25 +0000

    Added unit test to try to recreate this problem:
    http://opensource.atlassian.com/projects/hibernate/browse/HHH-1615
    
    See ticket #728
    
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@4318 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/test/api/AdministrationServiceTest.java
+++ test/api/org/openmrs/test/api/AdministrationServiceTest.java
@@ -451,9 +451,10 @@ public class AdministrationServiceTest extends BaseContextSensitiveTest {
 	public void testExecuteSqlGroupBy() throws Exception {
 		
 		String sql = "select encounter1_.location_id, encounter1_.creator, encounter1_.encounter_type, encounter1_.form_id, location2_.location_id, count(obs0_.obs_id) from obs obs0_ right outer join encounter encounter1_ on obs0_.encounter_id=encounter1_.encounter_id inner join location location2_ on encounter1_.location_id=location2_.location_id inner join users user3_ on encounter1_.creator=user3_.user_id inner join person user3_1_ on user3_.user_id=user3_1_.person_id inner join encounter_type encountert4_ on encounter1_.encounter_type=encountert4_.encounter_type_id inner join form form5_ on encounter1_.form_id=form5_.form_id where encounter1_.date_created>='20070505' and encounter1_.date_created<= '2008-05-05' group by encounter1_.location_id, encounter1_.creator , encounter1_.encounter_type , encounter1_.form_id";
-		
 		as.executeSQL(sql, true);
 		
+		String sql2 = "select encounter_id, count(*) from encounter encounter_id group by encounter_id";
+		as.executeSQL(sql2, true);
 	}
 	
 }
