3953f34eff4a5e3e91da7f4797220b87d4c310c6
Ben Wolfe <ben@openmrs.org>
2008-05-27 18:52:21 +0000

    Setting conceptName.concept_id column to be nullable to allow for hibernate insert quirk.
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@4391 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/api/db/hibernate/HibernateProgramWorkflowDAO.java
+++ src/api/org/openmrs/api/db/hibernate/HibernateProgramWorkflowDAO.java
@@ -119,10 +119,10 @@ public class HibernateProgramWorkflowDAO implements ProgramWorkflowDAO {
     public PatientProgram savePatientProgram(PatientProgram patientProgram) throws DAOException {
 		if (patientProgram.getPatientProgramId() == null) {
 			sessionFactory.getCurrentSession().save(patientProgram);
-	}
+		}
 		else {
 			sessionFactory.getCurrentSession().merge(patientProgram);
-	}
+		}
 		return patientProgram;
 	}
 
--- src/api/org/openmrs/api/impl/ConceptServiceImpl.java
+++ src/api/org/openmrs/api/impl/ConceptServiceImpl.java
@@ -126,10 +126,19 @@ public class ConceptServiceImpl extends BaseOpenmrsService implements ConceptSer
 	 * @see org.openmrs.api.ConceptService#saveConcept(org.openmrs.Concept)
 	 */
     public Concept saveConcept(Concept concept) throws APIException {
+    	
+    	// make sure the administrator hasn't turned off concept editing
 		checkIfLocked();
+		
+		// set that creator/dateCreated properties on the concept and child objects
 		this.modifyCollections(concept);
-		this.updateConceptWord(concept);
-		return dao.saveConcept(concept);    
+		
+		Concept conceptToReturn = dao.saveConcept(concept);
+		
+		// add/remove entries in the concept_word table (used for searching)
+		this.updateConceptWord(conceptToReturn);
+		
+		return conceptToReturn;
 	}
 
 	/**
