d8eeb5b93baf3469ffa8c8891f3a2c1374308e3f
Chase Yarbrough <vergil@gmail.com>
2008-05-28 23:10:39 +0000

    sync_bidirectional: Fixed potential number format problem by doing checking first.  should backport this.
    git-svn-id: http://svn.openmrs.org/openmrs/branches/data_synchronization_bidirectional@4448 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/patient/impl/VerhoeffIdentifierValidator.java
+++ src/api/org/openmrs/patient/impl/VerhoeffIdentifierValidator.java
@@ -65,14 +65,12 @@ public class VerhoeffIdentifierValidator extends BaseHyphenatedIdentifierValidat
 	public boolean isValid(String identifier)
     	throws UnallowedIdentifierException {
     	
-    	boolean result = super.isValid(identifier);
-    	
     	if(Character.isDigit(identifier.charAt(identifier.length()-1)))
     		throw new UnallowedIdentifierException("Check digit can not be numeric.");
     	if(identifier.length() != VERHOEFF_ID_LENGTH)
     		throw new UnallowedIdentifierException("Identifier must be " + VERHOEFF_ID_LENGTH + " digits long.");
-    		
-    	return result;
+    	    	    		
+    	return super.isValid(identifier);
     }
     /**
      * @override to disallow identifiers that are not exactly VERHOEFF_UNDECORATED_ID_LENGTH long.
