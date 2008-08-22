bd32d45fa4232a10398a25edaf89637df1df21a1
Justin Miranda <jcm62@columbia.edu>
2008-08-22 19:33:58 +0000

    Initial example of behavior driven JUnit test case.
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@5345 5bac5841-c719-aa4e-b3fe-cce5062f897a

---
+++ test/api/org/openmrs/test/propertyeditor/DrugEditorTest.java
@@ -0,0 +1,52 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.test.propertyeditor;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openmrs.Drug;
+import org.openmrs.api.ConceptService;
+import org.openmrs.api.context.Context;
+import org.openmrs.propertyeditor.DrugEditor;
+import org.springframework.util.StringUtils;
+
+
+/**
+ * 
+ */
+public class DrugEditorTest {
+		
+	/**
+	 * @verifies {@link DrugEditor#setAsText} 
+	 * 		set value to the drug with the specified identifier 
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void setAsText_shouldSetValueToTheDrugWithTheSpecifiedIdentifier() throws Exception { 		
+		DrugEditor drugEditor = new DrugEditor();
+		drugEditor.setValue("2");
+		Drug drug = (Drug) drugEditor.getValue();
+		Assert.assertEquals("", new Integer(2), drug.getDrugId());
+	}
+
+	/*
+	@should set value to null if given empty string 
+	@should set value to null if given null value
+	@should fail if drug does not exist with non-empty identifier
+	@should return drug identifier as string when editor has a value
+	@should return empty string when editor has a null value
+	*/
+}
