02867084c74b71de493f8849db915c34cdb75104
Ben Wolfe <ben@openmrs.org>
2008-12-18 21:35:54 +0000

    trunk and 1.4.x: Updating the update-scripts-to-latest.sh script to make it more reusable
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@6427 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- metadata/model/util/update-scripts-to-latest.sh
+++ metadata/model/util/update-scripts-to-latest.sh
@@ -7,9 +7,16 @@
 #
 
 # user defined variables
-export currentversion=1.3.0
-export newversion=1.4.0
 export dbuser=root
+export dbname=openmrs
+
+# Ask for the current version of the files
+echo -n "Current version of the files (e.g: 1.3.2): "
+read currentversion
+
+# Ask for the current version of the files
+echo -n "New version of the files (e.g: 1.3.3): "
+read newversion
 
 # Ask for the user's database password
 echo -n "MySQL $dbuser password: "
@@ -20,31 +27,40 @@ stty echo
 # Add a break after the user input
 echo ""
 
-# Do the createdb script first so that the 'openmrs' database is dropped and recreated
+# Do the createdb script first so that the '$dbname' database is dropped and recreated
 echo Creating createdb sql file
+mysql -u$dbuser -p$dbpass -e"drop database if exists $dbname; create database openmrs default character set utf8" -Dopenmrs
 mysql -u$dbuser -p$dbpass -e"source $currentversion-createdb-from-scratch-with-demo-data.sql"
-mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -Dopenmrs
-mysqldump -u$dbuser -p$dbpass -e -q --add-drop-database --skip-add-locks --skip-add-drop-table -N -r"./$newversion-createdb-from-scratch-with-demo-data.sql" --databases openmrs
+mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -D$dbname
+echo Running it again
+mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -D$dbname
+mysqldump -u$dbuser -p$dbpass -e -q --add-drop-database --skip-add-locks --skip-add-drop-table -N -r"./$newversion-createdb-from-scratch-with-demo-data.sql" --databases $dbname
 
 # Note that this mysqldump has the -d flag to ignore the rows (because this file is table schema only)
 echo Creating schema only sql file
-mysql -u$dbuser -p$dbpass -e"source $currentversion-schema-only.sql" -Dopenmrs
-mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -Dopenmrs
-mysqldump -u$dbuser -p$dbpass -e -q -d --add-drop-table --skip-add-locks -N -r"./$newversion-schema-only.sql" openmrs
+mysql -u$dbuser -p$dbpass -e"drop database if exists $dbname; create database openmrs default character set utf8" -Dopenmrs
+mysql -u$dbuser -p$dbpass -e"source $currentversion-schema-only.sql" -D$dbname
+mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -D$dbname
+mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -D$dbname
+mysqldump -u$dbuser -p$dbpass -e -q -d --add-drop-table --skip-add-locks -N -r"./$newversion-schema-only.sql" $dbname
 
 echo Creating schema with core and demo data sql file
-mysql -u$dbuser -p$dbpass -e"source $currentversion-schema-with-core-and-demo-data.sql" -Dopenmrs
-mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -Dopenmrs
-mysqldump -u$dbuser -p$dbpass -e -q --add-drop-table --skip-add-locks -N -r"./$newversion-schema-with-core-and-demo-data.sql" openmrs
+mysql -u$dbuser -p$dbpass -e"drop database if exists $dbname; create database openmrs default character set utf8" -Dopenmrs
+mysql -u$dbuser -p$dbpass -e"source $currentversion-schema-with-core-and-demo-data.sql" -D$dbname
+mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -D$dbname
+mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -D$dbname
+mysqldump -u$dbuser -p$dbpass -e -q --add-drop-table --skip-add-locks -N -r"./$newversion-schema-with-core-and-demo-data.sql" $dbname
 
 echo Creating schema with core data only sql file
-mysql -u$dbuser -p$dbpass -e"source $currentversion-schema-with-core-data.sql" -Dopenmrs
-mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -Dopenmrs
-mysqldump -u$dbuser -p$dbpass -e -q --add-drop-table --skip-add-locks -N -r"./$newversion-schema-with-core-data.sql" openmrs
+mysql -u$dbuser -p$dbpass -e"drop database if exists $dbname; create database openmrs default character set utf8" -Dopenmrs
+mysql -u$dbuser -p$dbpass -e"source $currentversion-schema-with-core-data.sql" -D$dbname
+mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -D$dbname
+mysql -u$dbuser -p$dbpass -e"source update-to-latest-db.mysqldiff.sql" -D$dbname
+mysqldump -u$dbuser -p$dbpass -e -q --add-drop-table --skip-add-locks -N -r"./$newversion-schema-with-core-data.sql" $dbname
 
-#drop database if exists openmrs;
-#create database openmrs default charset utf8;
-#use openmrs;
+#drop database if exists $dbname;
+#create database $dbname default charset utf8;
+#use $dbname;
 echo Done.
 
 echo ""
@@ -52,13 +68,13 @@ echo ""
 echo WARNING!!!
 echo WARNING!!!
 echo WARNING!!!
-echo Change the \"create database\" line to: \"create database openmrs default character set utf8\;\"
+echo Change the \"create database\" line to: \"create database $dbname default character set utf8\;\"
 echo You will also have to put the following lines into the createdb-from-scratch after that \"create database...\" statement
 echo DELETE FROM mysql.user WHERE User=\'test\'\;
 echo flush privileges\;
 echo CREATE USER \'test\'@\'localhost\' IDENTIFIED BY \'test\'\;
-echo GRANT ALL ON openmrs.* TO test\;
+echo GRANT ALL ON $dbname.* TO test\;
 echo ""
-echo Also change the \"schema only\" file to have : \"INSERT INTO global_property VALUES \(\'database_version\',\'1.3.0.14\',NULL\)\;\" with the __latest_version__
+echo Also change the \"schema only\" file to have : \"INSERT INTO global_property VALUES \(\'database_version\',\'1.x.x.xx\',NULL\)\;\" with the __latest_version__
 
 echo ""
