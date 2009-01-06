95172d22dd0eba4cf6075b4d344243f6844971a2
Ben Wolfe <ben@openmrs.org>
2009-01-06 16:53:17 +0000

    Added validator for PersonAttributeTypes to check for blank names and names already in use - #868 Author: Knoll_Frank
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@6548 5bac5841-c719-aa4e-b3fe-cce5062f897a

---
+++ src/api/org/openmrs/validator/PersonAttributeTypeValidator.java
@@ -0,0 +1,50 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.validator;
+
+import org.openmrs.PersonAttributeType;
+import org.openmrs.api.PersonService;
+import org.openmrs.api.context.Context;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+/**
+ * This class validates properties on the {@link PersonAttributeType} object.
+ */
+public class PersonAttributeTypeValidator implements Validator {
+	
+	/**
+	 * @see org.springframework.validation.Validator#supports(java.lang.Class)
+	 */
+	@SuppressWarnings("unchecked")
+    public boolean supports(Class c) {
+		return PersonAttributeType.class.isAssignableFrom(c);
+	}
+	
+	/**
+	 * @see org.springframework.validation.Validator#validate(java.lang.Object,
+	 *      org.springframework.validation.Errors)
+	 */
+	public void validate(Object obj, Errors errors) {
+		PersonAttributeType patObj = (PersonAttributeType) obj;
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "PersonAttributeType.error.nameEmpty");
+		
+		PersonService ps = Context.getPersonService();		
+		PersonAttributeType pat = ps.getPersonAttributeTypeByName(patObj.getName());
+		if (pat != null && !pat.getPersonAttributeTypeId().equals(patObj.getPersonAttributeTypeId())) {
+			errors.rejectValue("name", "PersonAttributeType.error.nameAlreadyInUse");
+		}
+	}
+}
