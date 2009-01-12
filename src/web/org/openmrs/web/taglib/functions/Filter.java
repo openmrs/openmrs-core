da3bc9a31b32ec5fd57dd57c07e5c06ddc01b8fb
Ben Wolfe <ben@openmrs.org>
2009-01-12 20:49:31 +0000

    Fixing runtime error from last commit dealing with set to list change
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@6596 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/web/org/openmrs/web/taglib/functions/Filter.java
+++ src/web/org/openmrs/web/taglib/functions/Filter.java
@@ -13,6 +13,7 @@
  */
 package org.openmrs.web.taglib.functions;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -22,9 +23,18 @@ import org.apache.commons.logging.LogFactory;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
 
+/**
+ * Functions used within taglibs in a webapp jsp page.
+ * <br/><br/>
+ * Example:
+ * <pre>
+ * &lt;c:forEach items="${openmrs:filterObsByConcept(observations, concept)}" var="o" end="0">
+ *   ....
+ *   ....
+ * &lt;/c:forEach>
+ * </pre>
+ */
 public class Filter {
-	
-	public static final long serialVersionUID = 1L;
 	
 	private static Log log = LogFactory.getLog(Filter.class);
 	
@@ -35,7 +45,7 @@ public class Filter {
 	 * @param type: EncounterTypeId to match
 	 * @return: Subset of passed encounters that match EncounterTypeId
 	 */
-	public static Set<Encounter> filterEncountersByType(Set<Encounter> encs, Integer type) {
+	public static Set<Encounter> filterEncountersByType(Collection<Encounter> encs, Integer type) {
 		log.debug("Filtering encounters for encounter type id: " + type);
 		Set<Encounter> ret = new HashSet<Encounter>();
 		if (encs != null) {
@@ -56,7 +66,7 @@ public class Filter {
 	 * @param concept: ConceptId to match
 	 * @return: Subset of passed obs that match ConceptId
 	 */
-	public static Set<Obs> filterObsByConcept(Set<Obs> obs, Integer concept) {
+	public static Set<Obs> filterObsByConcept(Collection<Obs> obs, Integer concept) {
 		log.debug("Filtering obs for concept id: " + concept);
 		Set<Obs> ret = new HashSet<Obs>();
 		if (obs != null) {
