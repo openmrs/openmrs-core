e96398f5d7645ecb765f490c349d32ef7c742200
Ben Wolfe <ben@openmrs.org>
2009-01-16 22:36:00 +0000

    Fixed error message on Concept description objects
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@6645 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/Concept.java
+++ src/api/org/openmrs/Concept.java
@@ -1104,12 +1104,12 @@ public class Concept implements java.io.Serializable, Attributable<Concept> {
 			// no description with the given locale was found.
 			// return null if exact match desired
 			if (exact) {
-				log.warn("No concept name found for concept id " + conceptId + " for locale " + desiredLocale.toString());
+				log.warn("No concept description found for concept id " + conceptId + " for locale " + desiredLocale.toString());
 			} else {
 				// returning default description locale ("en") if exact match
 				// not desired
 				if (defaultDescription == null)
-					log.warn("No concept name found for default locale for concept id " + conceptId);
+					log.warn("No concept description found for default locale for concept id " + conceptId);
 				else {
 					foundDescription = defaultDescription;
 				}
