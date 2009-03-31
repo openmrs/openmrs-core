c2ce56594a02bce1c353a8d3d2591ba35a31caa3
Ben Wolfe <ben@openmrs.org>
2009-03-31 13:45:32 +0000

    1.4.x: Cleaned up the concept.getDescription method and hid the warnings about missing descriptions
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.4.x@7228 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/Concept.java
+++ src/api/org/openmrs/Concept.java
@@ -1146,10 +1146,14 @@ public class Concept implements java.io.Serializable, Attributable<Concept> {
 		Locale desiredLocale = locale;
 		
 		ConceptDescription defaultDescription = null;
-		for (Iterator<ConceptDescription> i = getDescriptions().iterator(); i.hasNext() && (foundDescription == null);) {
+		for (Iterator<ConceptDescription> i = getDescriptions().iterator(); i.hasNext();) {
 			ConceptDescription availableDescription = i.next();
 			Locale availableLocale = availableDescription.getLocale();
-			if (availableLocale.equals(desiredLocale))
+			if (availableLocale.equals(desiredLocale)) {
+				foundDescription = availableDescription;
+				break; // skip out now because we found an exact locale match
+			}
+			if (!exact && LocaleUtility.areCompatible(availableLocale, desiredLocale))
 				foundDescription = availableDescription;
 			if (availableLocale.equals(LocaleUtility.DEFAULT_LOCALE))
 				defaultDescription = availableDescription;
@@ -1159,13 +1163,13 @@ public class Concept implements java.io.Serializable, Attributable<Concept> {
 			// no description with the given locale was found.
 			// return null if exact match desired
 			if (exact) {
-				log.warn("No concept description found for concept id " + conceptId + " for locale "
+				log.debug("No concept description found for concept id " + conceptId + " for locale "
 				        + desiredLocale.toString());
 			} else {
 				// returning default description locale ("en") if exact match
 				// not desired
 				if (defaultDescription == null)
-					log.warn("No default concept description found for default locale for concept id " + conceptId);
+					log.debug("No concept description found for default locale for concept id " + conceptId);
 				else {
 					foundDescription = defaultDescription;
 				}
