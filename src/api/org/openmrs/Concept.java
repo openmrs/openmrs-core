fd8ed25ab0a1370d513f2700cb0a71782f1c80cb
Ben Wolfe <ben@openmrs.org>
2009-04-03 14:17:53 +0000

    Changed concept description warning to debug statements - #1372
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@7255 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/Concept.java
+++ src/api/org/openmrs/Concept.java
@@ -1102,13 +1102,13 @@ public class Concept implements java.io.Serializable, Attributable<Concept> {
 			// no description with the given locale was found.
 			// return null if exact match desired
 			if (exact) {
-				log.warn("No concept description found for concept id " + conceptId + " for locale "
+				log.debug("No concept description found for concept id " + conceptId + " for locale "
 				        + desiredLocale.toString());
 			} else {
 				// returning default description locale ("en") if exact match
 				// not desired
 				if (defaultDescription == null)
-					log.warn("No concept description found for default locale for concept id " + conceptId);
+					log.debug("No concept description found for default locale for concept id " + conceptId);
 				else {
 					foundDescription = defaultDescription;
 				}
