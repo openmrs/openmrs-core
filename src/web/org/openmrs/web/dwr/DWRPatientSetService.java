9ea85e6a155f5a1a16c4d72ec921410272b54997
Darius Jazayeri <jazayeri@alum.mit.edu>
2009-04-09 19:50:17 +0000

    Fixing #1399 (Remove "Work with these patients" and "My Patient Set" feature from core) (Previous commit didn't fully remove the feature. Sorry.)
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@7291 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/web/org/openmrs/web/dwr/DWRPatientSetService.java
+++ src/web/org/openmrs/web/dwr/DWRPatientSetService.java
@@ -19,7 +19,6 @@ import java.util.Vector;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.Cohort;
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
 
@@ -27,56 +26,6 @@ public class DWRPatientSetService {
 	
 	protected final Log log = LogFactory.getLog(getClass());
 	
-	public void clearMyPatientSet() {
-		Context.getPatientSetService().clearMyPatientSet();
-	}
-	
-	public void setMyPatientSet(String patientIds) {
-		Context.getPatientSetService().setMyPatientSet(new Cohort(patientIds));
-	}
-	
-	public String getMyPatientSet() {
-		return Context.getPatientSetService().getMyPatientSet().getCommaSeparatedPatientIds();
-	}
-	
-	public Integer getMyPatientSetSize() {
-		return Context.getPatientSetService().getMyPatientSet().size();
-	}
-	
-	public Vector<PatientListItem> getFromMyPatientSet(Integer fromIndex, Integer pageSize) {
-		Vector<PatientListItem> ret = new Vector<PatientListItem>();
-		List<Integer> temp = new ArrayList<Integer>(Context.getPatientSetService().getMyPatientSet().getMemberIds());
-		if (fromIndex >= temp.size()) {
-			return ret;
-		}
-		if (fromIndex < 0) {
-			fromIndex = 0;
-		}
-		int toIndex = Math.min(fromIndex + pageSize, temp.size());
-		List<Patient> patients = Context.getPatientSetService().getPatients(temp.subList(fromIndex, toIndex));
-		for (Patient patient : patients) {
-			ret.add(new PatientListItem(patient));
-		}
-		return ret;
-	}
-	
-	/* Not sure if you can make this call from DWR--argument is a collection
-	public Vector<PatientListItem> getPatients(Collection<Integer> patientIds) {
-		Context Context = (Context) WebContextFactory.get().getSession()
-			.getAttribute(WebConstants.OPENMRS_CONTEXT_HTTPSESSION_ATTR);
-		if (Context != null) {
-			Vector<PatientListItem> ret = new Vector<PatientListItem>();
-			List<Patient> patients = Context.getPatientSetService().getPatients(patientIds);
-			for (Patient patient : patients) {
-				ret.add(new PatientListItem(patient));
-			}
-			return ret;
-		} else {
-			return null;
-		}
-	}
-	*/
-
 	public Vector<PatientListItem> getPatients(String patientIds) {
 		Vector<PatientListItem> ret = new Vector<PatientListItem>();
 		List<Integer> ptIds = new ArrayList<Integer>();
@@ -92,13 +41,5 @@ public class DWRPatientSetService {
 		}
 		return ret;
 	}
-	
-	public void addToMyPatientSet(Integer ptId) {
-		Context.getPatientSetService().addToMyPatientSet(ptId);
-	}
-	
-	public void removeFromMyPatientSet(Integer ptId) {
-		Context.getPatientSetService().removeFromMyPatientSet(ptId);
-	}
-	
+		
 }
