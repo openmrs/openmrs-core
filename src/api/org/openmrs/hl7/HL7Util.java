b62acc151d2ed3bc466866a36816b4ac8825d0b0
Darius Jazayeri <jazayeri@alum.mit.edu>
2009-04-10 02:21:31 +0000

    Added @should annotations to Hl7Util
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@7297 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/hl7/HL7Util.java
+++ src/api/org/openmrs/hl7/HL7Util.java
@@ -43,6 +43,26 @@ public class HL7Util {
 	 * @param s HL7 timestamp to be parsed
 	 * @return Date object
 	 * @throws HL7Exception
+	 * 
+	 * @should fail on 78
+	 * @should handle 1978
+	 * @should fail on 19784 
+	 * @should handle 197804
+	 * @should fail on 197841
+	 * @should handle 19780411
+	 * @should fail on 197804116
+	 * @should handle 1978041106
+	 * @should fail on 19780411065
+	 * @should handle 197804110615
+	 * @should fail on 1978041106153
+	 * @should handle 19780411061538
+	 * @should handle 19780411061538.1
+	 * @should handle 19780411061538.12
+	 * @should handle 19780411061538.123
+	 * @should handle 19780411061538.1234
+	 * @should fail on 197804110615-5
+	 * @should handle 197804110615-05
+	 * @should handle 197804110615-0500
 	 */
 	public static Date parseHL7Timestamp(String s) throws HL7Exception {
 		
@@ -137,6 +157,15 @@ public class HL7Util {
 	 * @param s HL7 time to be converted
 	 * @return Date object set to time specified by HL7
 	 * @throws HL7Exception
+	 * 
+	 * @should fail on 197804110615
+	 * @should handle 0615
+	 * @should handle 061538
+	 * @should handle 061538.1
+	 * @should handle 061538.12
+	 * @should handle 061538.123
+	 * @should handle 061538.1234
+	 * @should handle 061538-0300 
 	 */
 	public static Date parseHL7Time(String s) throws HL7Exception {
 		
