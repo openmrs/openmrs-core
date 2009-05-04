5a4604ab6e8b30460a3b22caa43a7b9baa0751de
Ben Wolfe <ben@openmrs.org>
2009-05-04 13:40:54 +0000

    Fixed the unit test that tests the unit tests. Renamed the ReportSchemaXmlTestNonContext so that it ends with Test Renamed the PatientIdentifierValidatorTest#test1375() method to start with should
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@7544 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/api/PatientServiceTest.java
+++ test/api/org/openmrs/api/PatientServiceTest.java
@@ -518,7 +518,7 @@ public class PatientServiceTest extends BaseContextSensitiveTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void testAllowUsersWhoArePatients() throws Exception {
+	public void shouldAllowGettingPatientsThatWereCreatedByUsersWhoArePatients() throws Exception {
 		executeDataSet(USERS_WHO_ARE_PATIENTS_XML);
 		
 		// we must fetch this person first, because this person is
@@ -697,5 +697,23 @@ public class PatientServiceTest extends BaseContextSensitiveTest {
 		
 		patient.getAttributes().size();
 		person.getAttributes().size();
+	}
+	
+	/**
+	 * Regression test for http://dev.openmrs.org/ticket/1375
+	 * 
+	 * @see {@link PatientService#savePatient(Patient)}
+	 */
+	@Test
+	@Verifies(value = "should not throw a NonUniqueObjectException when called with a hand constructed patient", method = "savePatient(Patient)")
+	public void savePatient_shouldNotThrowANonUniqueObjectExceptionWhenCalledWithAHandConstructedPatient() throws Exception {
+		Patient patient = new Patient();
+		patient.setGender("M");
+		patient.setPatientId(2);
+		//patient.setCreator(new User(1));
+		//patient.setDateCreated date_created="2005-09-22 00:00:00.0" changed_by="1" date_changed="2008-08-18 12:29:59.0"
+		patient.addName(new PersonName("This", "Isa", "Test"));
+		patient.addIdentifier(new PatientIdentifier("101-6", new PatientIdentifierType(1), new Location(1)));
+		Context.getPatientService().savePatient(patient);
 	}
 }
