e80c980bc960e1aab90fcf51a8c784fd16c56057
konnikov <konnikov@gmail.com>
2009-05-05 05:07:53 +0000

    global-property-types: Rename property_type to property_type_id
    git-svn-id: http://svn.openmrs.org/openmrs/branches/global-property-types@7561 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- doc/db-schema/global_property.sql
+++ doc/db-schema/global_property.sql
@@ -2,14 +2,16 @@ CREATE TABLE `global_property` (
 	`property` VARCHAR(255) DEFAULT '' NOT NULL,
 	`property_value` MEDIUMTEXT,
 	`description` TEXT,
-	`property_type` INTEGER UNSIGNED DEFAULT 0 NOT NULL,
+	`property_type_id` INTEGER UNSIGNED DEFAULT 0 NOT NULL,
 	`default_value` MEDIUMTEXT DEFAULT '' NOT NULL,
 	PRIMARY KEY (`property`)
 );
 
-CREATE INDEX `type_of_global_property` ON `global_property` (`property_type` ASC);
+CREATE INDEX `type_of_global_property` ON `global_property` (`property_type_id` ASC);
 
-ALTER TABLE `global_property` ADD CONSTRAINT `type_of_global_property` FOREIGN KEY (`property_type`)
+ALTER TABLE `global_property` ADD PRIMARY KEY (`property`);
+
+ALTER TABLE `global_property` ADD CONSTRAINT `type_of_global_property` FOREIGN KEY (`property_type_id`)
 	REFERENCES `global_property_type` (`global_property_type_id`)
 	ON UPDATE CASCADE;
 
