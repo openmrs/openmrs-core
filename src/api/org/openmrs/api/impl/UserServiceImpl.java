f59da66a15b2ee188834a85d8d38ae2777f8e05f
Ben Wolfe <ben@openmrs.org>
2009-05-22 13:58:42 +0000

    Minor fixes to person and user service found during the testathon
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@7987 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/api/impl/UserServiceImpl.java
+++ src/api/org/openmrs/api/impl/UserServiceImpl.java
@@ -428,7 +428,7 @@ public class UserServiceImpl extends BaseOpenmrsService implements UserService {
 	 */
 	public User setUserProperty(User user, String key, String value) {
 		if (user != null) {
-			if (!user.hasPrivilege(OpenmrsConstants.PRIV_EDIT_USERS) && !user.equals(Context.getAuthenticatedUser()))
+			if (!Context.hasPrivilege(OpenmrsConstants.PRIV_EDIT_USERS) && !user.equals(Context.getAuthenticatedUser()))
 				throw new APIException("You are not authorized to change " + user.getUserId() + "'s properties");
 			
 			user.setUserProperty(key, value);
