be23017d921ef79afa484d5662fa918b977772df
Ben Wolfe <ben@openmrs.org>
2009-06-08 21:16:41 +0000

    1.5.x: Added "create" and "update" as triggers for the SaveHandlers so that calls to deprecated methods still work. Fixeed error on creating relationship due to missing uuid - #1529
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.5.x@8332 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/aop/RequiredDataAdvice.java
+++ src/api/org/openmrs/aop/RequiredDataAdvice.java
@@ -98,7 +98,9 @@ public class RequiredDataAdvice implements MethodBeforeAdvice {
 	public void before(Method method, Object[] args, Object target) throws Throwable {
 		String methodName = method.getName();
 		
-		if (methodName.startsWith("save")) {
+		// the "create" and "update" are in to cover old deprecated methods since AOP doesn't occur
+		// on method calls within a class, only on calls to methods from external classes to methods
+		if (methodName.startsWith("save") || methodName.startsWith("create") || methodName.startsWith("update")) {
 			Object mainArgument = args[0];
 			
 			// fail early on a null parameter
