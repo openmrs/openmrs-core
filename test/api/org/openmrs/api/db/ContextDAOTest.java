e22b753e1c6b903dc4676d68cec08ba88483f616
Darius Jazayeri <jazayeri@alum.mit.edu>
2009-06-19 17:43:35 +0000

    1.5.x branch: Fixing #1580 (If you type your password wrong, then log in correctly, the API will not lock you out after multiple login attempts in the future)
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.5.x@8622 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/api/db/ContextDAOTest.java
+++ test/api/org/openmrs/api/db/ContextDAOTest.java
@@ -271,5 +271,48 @@ public class ContextDAOTest extends BaseContextSensitiveTest {
 	public void authenticate_shouldAuthenticateWithIncorrectHashedPassword() throws Exception {
 		dao.authenticate("incorrect", "test");
 	}
+
+    /**
+     * #1580: If you type your password wrong, then log in correctly, the API will not lock you out after multiple login attempts in the future
+     * @see {@link ContextDAO#authenticate(String,String)}
+     */
+	@Test(expected = ContextAuthenticationException.class)
+    @Verifies(value = "should pass regression test for 1580", method = "authenticate(String,String)")
+    public void authenticate_shouldPassRegressionTestFor1580() throws Exception {
+		// logout after the base setup
+		Context.logout();
+		
+		// first we fail a login attempt
+		try {
+			dao.authenticate("admin", "not the right password");
+			Assert.fail("Not sure why this username/password combo worked");
+		} catch (ContextAuthenticationException authException) {
+			// pass
+		}
+		
+		// next we log in correctly
+		try {
+			dao.authenticate("admin", "test");
+		}
+		catch (ContextAuthenticationException authException) {
+			Assert.fail("There must be an admin:test user for this test to run properly");
+		}
+		Context.logout();
+		
+		for (int x = 1; x <= 5; x++) {
+			// now we fail several login attempts 
+			try {
+				dao.authenticate("admin", "not the right password");
+				Assert.fail("Not sure why this username/password combo worked");
+			}
+			catch (ContextAuthenticationException authException) {
+				// pass
+			}
+		}
+		
+		// those were the first five, now the sixth request 
+		// (with the same user and right pw) should fail
+		dao.authenticate("admin", "test");
+    }
 	
 }
