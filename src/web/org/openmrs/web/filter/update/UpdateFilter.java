7486a70f4fa045e08bc35e1c01bf682486be4d12
Ben Wolfe <ben@openmrs.org>
2009-06-22 22:02:59 +0000

    Added ajax refresh to see progress of liquibase database updates at startup - #1512 Author: Knoll_Frank
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@8685 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/web/org/openmrs/web/filter/update/UpdateFilter.java
+++ src/web/org/openmrs/web/filter/update/UpdateFilter.java
@@ -19,8 +19,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -33,16 +35,21 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import liquibase.ChangeSet;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Appender;
+import org.apache.log4j.Logger;
 import org.openmrs.api.context.Context;
 import org.openmrs.util.DatabaseUpdateException;
 import org.openmrs.util.DatabaseUpdater;
 import org.openmrs.util.InputRequiredException;
+import org.openmrs.util.MemoryAppender;
 import org.openmrs.util.OpenmrsConstants;
 import org.openmrs.util.Security;
+import org.openmrs.util.DatabaseUpdater.ChangeSetExecutorCallback;
 import org.openmrs.web.Listener;
-import org.openmrs.web.WebConstants;
 import org.openmrs.web.filter.StartupFilter;
 import org.springframework.web.context.ContextLoader;
 
@@ -65,6 +72,8 @@ public class UpdateFilter extends StartupFilter {
 	 */
 	private final String REVIEW_CHANGES = "reviewchanges.vm";
 	
+	private final String PROGRESS_VM_AJAXREQUEST = "updateProgress.vm.ajaxRequest";
+	
 	/**
 	 * The model object behind this set of screens
 	 */
@@ -82,12 +91,15 @@ public class UpdateFilter extends StartupFilter {
 	 */
 	private boolean authenticatedSuccessfully = false;
 	
+	private UpdateFilterCompletion updateJob;
+	
 	/**
 	 * Called by {@link #doFilter(ServletRequest, ServletResponse, FilterChain)} on GET requests
 	 * 
 	 * @param httpRequest
 	 * @param httpResponse
 	 */
+	@Override
 	protected void doGet(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException,
 	                                                                                      ServletException {
 		
@@ -105,6 +117,7 @@ public class UpdateFilter extends StartupFilter {
 	 * @see org.openmrs.web.filter.StartupFilter#doPost(javax.servlet.http.HttpServletRequest,
 	 *      javax.servlet.http.HttpServletResponse)
 	 */
+	@Override
 	protected void doPost(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException,
 	                                                                                       ServletException {
 		
@@ -124,8 +137,6 @@ public class UpdateFilter extends StartupFilter {
 				// set a variable so we know that the user started here
 				authenticatedSuccessfully = true;
 				renderTemplate(REVIEW_CHANGES, referenceMap, writer);
-				
-				return;
 			} else {
 				// if not authenticated, show main page again
 				try {
@@ -139,53 +150,48 @@ public class UpdateFilter extends StartupFilter {
 				errors.add("Unable to authenticate as a " + OpenmrsConstants.SUPERUSER_ROLE
 				        + ". Invalid username or password");
 				renderTemplate(DEFAULT_PAGE, referenceMap, writer);
-				return;
 			}
-			
 		} // step two
 		else if (REVIEW_CHANGES.equals(page)) {
 			
 			if (!authenticatedSuccessfully) {
 				// throw the user back to the main page because they are cheating
 				renderTemplate(DEFAULT_PAGE, referenceMap, writer);
-			}
-			
-			try {
-				DatabaseUpdater.executeChangelog();
-			}
-			catch (InputRequiredException inputRequired) {
-				// the user would be stepped through the questions returned here.
-				log.error("Not implemented", inputRequired);
-				errors.add("Input during database updates is not yet implemented. " + inputRequired.getMessage());
-				model.updateChanges();
-				renderTemplate(REVIEW_CHANGES, referenceMap, writer);
-				return;
-			}
-			catch (DatabaseUpdateException e) {
-				log.error("Unable to update the database", e);
-				errors.add("Unable to update the database.  See server error logs for the full stacktrace.");
-				errors.addAll(Arrays.asList(e.getMessage().split("\n")));
-				model.updateChanges();
-				renderTemplate(REVIEW_CHANGES, referenceMap, writer);
 				return;
 			}
 			
+			updateJob = new UpdateFilterCompletion();
+			updateJob.start();
+			
+			referenceMap.put("updateJobStarted", true);
+			renderTemplate(REVIEW_CHANGES, referenceMap, writer);
+			
+		} else if (PROGRESS_VM_AJAXREQUEST.equals(page)) {
+			
+			httpResponse.setContentType("text/json");
+			httpResponse.setHeader("Cache-Control", "no-cache");
+			Map<String, Object> result = new HashMap<String, Object>();
+			if (updateJob != null) {
+				result.put("hasErrors", updateJob.hasErrors());
+				if (updateJob.hasErrors()) {
+					errors.addAll(updateJob.getErrors());
+				}
+				
+				result.put("updatesRequired", updatesRequired());
+				result.put("message", updateJob.getMessage());
+				result.put("changesetIds", updateJob.getChangesetIds());
+				Appender appender = Logger.getRootLogger().getAppender("MEMORY_APPENDER");
+				if (appender instanceof MemoryAppender) {
+					MemoryAppender memoryAppender = (MemoryAppender) appender;
+					result.put("logLines", memoryAppender.getLogLines());
+				} else {
+					result.put("logLines", new ArrayList<String>());
+				}
+			}
+			
+			String jsonText = toJSONString(result);
+			writer.write(jsonText);
 		}
-		
-		try {
-			startOpenmrs(filterConfig.getServletContext());
-		}
-		catch (Throwable t) {
-			log.error("Unable to complete the startup.", t);
-			errors.add("Unable to complete the startup.  See the server error log for the complete stacktrace."
-			        + t.getMessage());
-			renderTemplate(DEFAULT_PAGE, null, writer);
-			return;
-		}
-		
-		// set this so that the wizard isn't run again on next page load
-		updatesRequired = false;
-		httpResponse.sendRedirect("/" + WebConstants.WEBAPP_NAME);
 	}
 	
 	/**
@@ -289,6 +295,7 @@ public class UpdateFilter extends StartupFilter {
 	/**
 	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
 	 */
+	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		super.init(filterConfig);
 		
@@ -315,13 +322,14 @@ public class UpdateFilter extends StartupFilter {
 		} else {
 			// the wizard runs the updates, so they will not need any updates.
 			log.debug("Setting updates required to false because the user doesn't have any runtime properties yet");
-			updatesRequired = false;
+			setUpdatesRequired(false);
 		}
 	}
 	
 	/**
 	 * @see org.openmrs.web.filter.StartupFilter#getModel()
 	 */
+	@Override
 	protected Object getModel() {
 		// this object was initialized in the #init(FilterConfig) method
 		return model;
@@ -330,8 +338,9 @@ public class UpdateFilter extends StartupFilter {
 	/**
 	 * @see org.openmrs.web.filter.StartupFilter#skipFilter()
 	 */
+	@Override
 	public boolean skipFilter(HttpServletRequest httpRequest) {
-		return !updatesRequired;
+		return !PROGRESS_VM_AJAXREQUEST.equals(httpRequest.getParameter("page")) && !updatesRequired();
 	}
 	
 	/**
@@ -341,15 +350,177 @@ public class UpdateFilter extends StartupFilter {
 	 * @see #init(FilterConfig)
 	 * @see Listener#setupNeeded
 	 */
-	public static boolean updatesRequired() {
+	public static synchronized boolean updatesRequired() {
 		return updatesRequired;
+	}
+	
+	/**
+	 * @param updatesRequired the updatesRequired to set
+	 */
+	protected static synchronized void setUpdatesRequired(boolean updatesRequired) {
+		UpdateFilter.updatesRequired = updatesRequired;
 	}
 	
 	/**
 	 * @see org.openmrs.web.filter.StartupFilter#getTemplatePrefix()
 	 */
+	@Override
 	protected String getTemplatePrefix() {
 		return "org/openmrs/web/filter/update/";
 	}
 	
+	/**
+	 * This class controls the final steps and is used by the ajax calls to know what updates have
+	 * been executed.
+	 * 
+	 * TODO: Break this out into a separate (non-inner) class
+	 */
+	private class UpdateFilterCompletion {
+		
+		private Thread thread;
+		
+		private List<String> changesetIds = new ArrayList<String>();
+		
+		private List<String> errors = new ArrayList<String>();
+		
+		private String message = null;
+		
+		private boolean erroneous = false;
+		
+		synchronized public void reportError(String error) {
+			List<String> errors = new ArrayList<String>();
+			errors.add(error);
+			reportErrors(errors);
+		}
+		
+		synchronized public void reportErrors(List<String> errs) {
+			errors.addAll(errs);
+			erroneous = true;
+		}
+		
+		synchronized public boolean hasErrors() {
+			return erroneous;
+		}
+		
+		synchronized public List<String> getErrors() {
+			return errors;
+		}
+		
+		/**
+		 * Start the completion stage. This fires up the thread to do all the work.
+		 */
+		public void start() {
+			setUpdatesRequired(true);
+			thread.start();
+		}
+		
+		public void waitForCompletion() {
+			try {
+				thread.join();
+			}
+			catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				log.error("Error generated", e);
+			}
+		}
+		
+		synchronized public void setMessage(String message) {
+			this.message = message;
+		}
+		
+		synchronized public String getMessage() {
+			return message;
+		}
+		
+		synchronized public void addChangesetId(String changesetid) {
+			this.changesetIds.add(changesetid);
+		}
+		
+		synchronized public List<String> getChangesetIds() {
+			return changesetIds;
+		}
+		
+		/**
+		 * This class does all the work of creating the desired database, user, updates, etc
+		 */
+		public UpdateFilterCompletion() {
+			Runnable r = new Runnable() {
+				
+				/**
+				 * TODO split this up into multiple testable methods
+				 * 
+				 * @see java.lang.Runnable#run()
+				 */
+				public void run() {
+					try {
+						/**
+						 * A callback class that prints out info about liquibase changesets
+						 */
+						class PrintingChangeSetExecutorCallback implements ChangeSetExecutorCallback {
+							
+							private String message;
+							
+							public PrintingChangeSetExecutorCallback(String message) {
+								this.message = message;
+							}
+							
+							/**
+							 * @see org.openmrs.util.DatabaseUpdater.ChangeSetExecutorCallback#executing(liquibase.ChangeSet,
+							 *      int)
+							 */
+							public void executing(ChangeSet changeSet, int numChangeSetsToRun) {
+								addChangesetId(changeSet.getId());
+								setMessage(message);
+							}
+							
+						}
+						
+						try {
+							setMessage("Updating the database to the latest version");
+							DatabaseUpdater.executeChangelog(null, null, new PrintingChangeSetExecutorCallback(
+							        "Updating database tables to latest version "));
+						}
+						catch (InputRequiredException inputRequired) {
+							// the user would be stepped through the questions returned here.
+							log.error("Not implemented", inputRequired);
+							model.updateChanges();
+							reportError("Input during database updates is not yet implemented. "
+							        + inputRequired.getMessage());
+							return;
+						}
+						catch (DatabaseUpdateException e) {
+							log.error("Unable to update the database", e);
+							List<String> errors = new ArrayList<String>();
+							errors.add("Unable to update the database.  See server error logs for the full stacktrace.");
+							errors.addAll(Arrays.asList(e.getMessage().split("\n")));
+							model.updateChanges();
+							reportErrors(errors);
+							return;
+						}
+						
+						setMessage("Starting OpenMRS");
+						try {
+							startOpenmrs(filterConfig.getServletContext());
+						}
+						catch (Throwable t) {
+							log.error("Unable to complete the startup.", t);
+							reportError("Unable to complete the startup.  See the server error log for the complete stacktrace."
+							        + t.getMessage());
+							return;
+						}
+						
+						// set this so that the wizard isn't run again on next page load
+						setUpdatesRequired(false);
+					}
+					finally {
+						if (!hasErrors()) {
+							setUpdatesRequired(false);
+						}
+					}
+				}
+			};
+			
+			thread = new Thread(r);
+		}
+	}
 }
