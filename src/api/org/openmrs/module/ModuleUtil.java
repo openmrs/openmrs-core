bf19d326fdad239d59d13e77ede2f3f537fce4f6
Ben Wolfe <ben@openmrs.org>
2009-06-29 14:47:49 +0000

    Catching errors in module stoppage to make sure a module can be stopped again have a failed module load - #1593
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@8800 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/module/ModuleUtil.java
+++ src/api/org/openmrs/module/ModuleUtil.java
@@ -620,12 +620,17 @@ public class ModuleUtil {
 		
 		List<String> mandatoryModuleIds = new ArrayList<String>();
 		
-		List<GlobalProperty> props = Context.getAdministrationService().getGlobalPropertiesBySuffix(".mandatory");
-		
-		for (GlobalProperty prop : props) {
-			if ("true".equalsIgnoreCase(prop.getPropertyValue())) {
-				mandatoryModuleIds.add(prop.getProperty().replace(".mandatory", ""));
+		try {
+			List<GlobalProperty> props = Context.getAdministrationService().getGlobalPropertiesBySuffix(".mandatory");
+			
+			for (GlobalProperty prop : props) {
+				if ("true".equalsIgnoreCase(prop.getPropertyValue())) {
+					mandatoryModuleIds.add(prop.getProperty().replace(".mandatory", ""));
+				}
 			}
+		}
+		catch (Throwable t) {
+			log.warn("Unable to get the mandatory module list", t);
 		}
 		
 		return mandatoryModuleIds;
