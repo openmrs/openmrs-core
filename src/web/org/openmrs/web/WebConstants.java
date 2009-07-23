c359d3fb91bce3ff5d7a53e5c1ef86e03d7c99f1
Ben Wolfe <ben@openmrs.org>
2009-07-23 19:43:08 +0000

    Making the lockout per IP configurable through a global property - #1486
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@9367 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/web/org/openmrs/web/WebConstants.java
+++ src/web/org/openmrs/web/WebConstants.java
@@ -70,4 +70,11 @@ public class WebConstants {
 	 */
 	public static final String SETUP_PAGE_URL = "initialsetup";
 	
+	/**
+	 * Global property name for the number of times one IP can fail at logging in before being
+	 * locked out. A value of 0 for this property means no IP lockout checks.
+	 * 
+	 * @see org.openmrs.web.servlet.LoginServlet
+	 */
+	public static String GP_ALLOWED_LOGIN_ATTEMPTS_PER_IP = "security.loginAttemptsAllowedPerIP";
 }
