7a798134685ccb9a2930e6295649b06755541b78
Nyoman Ribeka <nyoman@openmrs.org>
2009-09-02 15:59:37 +0000

    Fixed equals method for PatientIdentifier - #1628
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@10173 5bac5841-c719-aa4e-b3fe-cce5062f897a

---
+++ test/api/org/openmrs/PatientIdentifierTest.java
@@ -0,0 +1,40 @@
+package org.openmrs;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openmrs.test.Verifies;
+
+public class PatientIdentifierTest {
+	
+	/**
+	 * @see {@link PatientIdentifier#equals(Object)}
+	 * 
+	 */
+	@Test
+	@Verifies(value = "should should compare when patient and identifier and type is null", method = "equals(Object)")
+	public void equals_shouldShouldCompareWhenPatientAndIdentifierAndTypeIsNull()
+			throws Exception {
+		
+		Patient patient = new Patient();
+		
+		PatientIdentifier second = new PatientIdentifier();
+		second.setIdentifier("EXAMPLE IDENTIFIER");
+		second.setPatient(patient);
+		second.setIdentifierType(new PatientIdentifierType(1));
+		
+		PatientIdentifier first = new PatientIdentifier();
+
+		Assert.assertNull(first.getPatient());
+		Assert.assertNull(first.getIdentifier());
+		Assert.assertNull(first.getIdentifierType());
+		
+		Assert.assertFalse(first + " and " + second + " should not equal." , second.equals(first));
+		
+		first.setIdentifier("EXAMPLE IDENTIFIER");
+		first.setPatient(patient);
+		first.setIdentifierType(new PatientIdentifierType(1));
+		
+		Assert.assertTrue(first + " and " + second + " should be equal." , second.equals(first));
+	}
+}
