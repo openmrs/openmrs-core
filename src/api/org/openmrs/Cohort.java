dd512730e9a49fb7b9568c1e0f2c02c6628acb17
Darius Jazayeri <jazayeri@alum.mit.edu>
2010-03-12 00:32:34 +0000

    1.6.x: fixing an NPE while intersecting a cohort with null
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.6.x@12403 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/Cohort.java
+++ src/api/org/openmrs/Cohort.java
@@ -256,7 +256,7 @@ public class Cohort extends BaseOpenmrsData implements Serializable {
 	 */
 	public static Cohort intersect(Cohort a, Cohort b) {
 		Cohort ret = new Cohort();
-		ret.setName("(" + a.getName() + " * " + b.getName() + ")");
+		ret.setName("(" + (a == null ? "NULL" : a.getName()) + " * " + (b == null ? "NULL" : b.getName()) + ")");
 		if (a != null && b != null) {
 			ret.getMemberIds().addAll(a.getMemberIds());
 			ret.getMemberIds().retainAll(b.getMemberIds());
