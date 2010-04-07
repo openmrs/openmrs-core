6f1abf081d5f81b7dc924202376ce410882ec845
Jeremy Keiper <jeremy@openmrs.org>
2010-04-07 18:28:05 +0000

    for #2212 changed OpenmrsUtil.getDateFormat(Locale) to return a clone instead of the actual cached SimpleDateFormat object
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.6.x@12884 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/util/OpenmrsUtilTest.java
+++ test/api/org/openmrs/util/OpenmrsUtilTest.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
@@ -517,5 +518,40 @@ public class OpenmrsUtilTest extends BaseContextSensitiveTest {
 	public void validatePassword_shouldStillWorkWithoutAnOpenSession() throws Exception {
 		Context.closeSession();
 		OpenmrsUtil.validatePassword("admin", "1234Password", "systemId");
+	}
+
+	/**
+	 * @see {@link OpenmrsUtil#getDateFormat(Locale)}
+	 */
+	@Test
+	@Verifies(value = "should not allow the returned SimpleDateFormat to be modified", method = "getDateFormat(Locale)")
+	public void getDateFormat_shouldNotAllowTheReturnedSimpleDateFormatToBeModified()
+			throws Exception {
+		// start with a locale that is not currently cached by getDateFormat() 
+		Locale locale = new Locale("hk");
+		Assert.assertTrue("default locale is potentially already cached", 
+			!Context.getLocale().equals(locale));
+		
+		// get the initially built dateformat from getDateFormat()
+		SimpleDateFormat sdf = OpenmrsUtil.getDateFormat(locale);
+		Assert.assertNotSame("initial dateFormatCache entry is modifiable", 
+			OpenmrsUtil.getDateFormat(locale), sdf);
+		
+		// verify changing the pattern on our variable does not affect the cache
+		sdf.applyPattern("yyyymmdd");
+		Assert.assertTrue("initial dateFormatCache pattern is modifiable", 
+			!OpenmrsUtil.getDateFormat(locale).toPattern().equals(sdf.toPattern()));
+
+		// the dateformat cache now contains the format for this locale; checking
+		// a second time will guarantee we are looking at cached data and not the
+		// initially built dateformat
+		sdf = OpenmrsUtil.getDateFormat(locale);
+		Assert.assertNotSame("cached dateFormatCache entry is modifiable", 
+			OpenmrsUtil.getDateFormat(locale), sdf);
+		
+		// verify changing the pattern on our variable does not affect the cache
+		sdf.applyPattern("yyyymmdd");
+		Assert.assertTrue("cached dateFormatCache pattern is modifiable", 
+			!OpenmrsUtil.getDateFormat(locale).toPattern().equals(sdf.toPattern()));
 	}
 }
