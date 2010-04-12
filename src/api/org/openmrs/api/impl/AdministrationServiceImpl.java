730626b557e5d9524b3e5aa83be60c13ed276cfb
Ben Wolfe <ben@openmrs.org>
2010-04-12 17:08:24 +0000

    Added a getGlobalPropertyValue convenience method to AdministrationService - #2167 Author: niranjan
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@12920 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/api/impl/AdministrationServiceImpl.java
+++ src/api/org/openmrs/api/impl/AdministrationServiceImpl.java
@@ -15,6 +15,8 @@ package org.openmrs.api.impl;
 
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -1032,8 +1034,39 @@ public class AdministrationServiceImpl extends BaseOpenmrsService implements Adm
 		return propertyName.equals(OpenmrsConstants.GLOBAL_PROPERTY_LOCALE_ALLOWED_LIST);
 	}
 	
+	/**
+	 * @see org.openmrs.api.AdministrationService#getGlobalPropertyByUuid(java.lang.String)
+	 */
 	public GlobalProperty getGlobalPropertyByUuid(String uuid) {
 		return dao.getGlobalPropertyByUuid(uuid);
 	}
 	
+	/**
+	 * @see org.openmrs.api.AdministrationService#getGlobalPropertyValue(java.lang.String,
+	 *      java.lang.Object)
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> T getGlobalPropertyValue(String propertyName, T defaultValue) throws APIException {
+		if (defaultValue == null)
+			throw new IllegalArgumentException("The defaultValue argument cannot be null");
+		
+		String propVal = getGlobalProperty(propertyName);
+		if (!StringUtils.hasLength(propVal))
+			return defaultValue;
+		
+		try {
+			return (T) defaultValue.getClass().getDeclaredConstructor(String.class).newInstance(propVal);
+		}
+		catch (InstantiationException e) {
+			throw new APIException(defaultValue.getClass().getName() + " is not able to be instantiated with value: " + propVal, e);
+		}
+		catch (NoSuchMethodException e) {
+			throw new APIException(defaultValue.getClass().getName() + " does not have a string constructor", e);
+		}
+		catch (Exception e) {
+			log.error("Unable to turn value '" + propVal + "' into type " + defaultValue.getClass().getName(), e);
+			return defaultValue;
+		}
+	}
+
 }
