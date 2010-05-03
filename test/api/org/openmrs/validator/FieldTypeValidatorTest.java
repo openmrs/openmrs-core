d34a485945be8403a6ce33e2e9065c77a7c6920d
Ben Wolfe <ben@openmrs.org>
2010-05-03 15:49:04 +0000

    Fixing utf-8 error in FieldTypeValidatorTest
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@13208 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/validator/FieldTypeValidatorTest.java
+++ test/api/org/openmrs/validator/FieldTypeValidatorTest.java
@@ -21,7 +21,7 @@ public class FieldTypeValidatorTest {
 			throws Exception {
 		FieldType type = new FieldType();
 		type.setName(null);
-		type.setDescription("Humba humba humba t‰tet‰‰‰...");
+		type.setDescription("Humba humba humba ...");
 		
 		Errors errors = new BindException(type, "type");
 		new FieldTypeValidator().validate(type, errors);
