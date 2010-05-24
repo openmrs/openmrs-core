592510de7f89031536e192d377ec5530a6aca1bc
shazin <shazin@gmail.com>
2010-05-24 15:39:15 +0000

    moduleadmin branch: Changing code to allow queuing
    git-svn-id: http://svn.openmrs.org/openmrs/branches/moduleadmin@13445 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/web/org/openmrs/module/web/controller/ModuleListController.java
+++ src/web/org/openmrs/module/web/controller/ModuleListController.java
@@ -34,6 +34,7 @@ import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.APIAuthenticationException;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.Module;
+import org.openmrs.module.ModuleAction;
 import org.openmrs.module.ModuleConstants;
 import org.openmrs.module.ModuleException;
 import org.openmrs.module.ModuleFactory;
@@ -62,7 +63,7 @@ public class ModuleListController extends SimpleFormController {
 	 * Logger for this class and subclasses
 	 */
 	protected static final Log log = LogFactory.getLog(ModuleListController.class);
-	
+
 	/**
 	 * The onSubmit function receives the form/command object that was modified by the input form
 	 * and saves it to the db
@@ -93,8 +94,9 @@ public class ModuleListController extends SimpleFormController {
 		else if (ServletRequestUtils.getStringParameter(request, "unload.x", null) != null)
 			action = "unload";
 		
-		// handle module upload
-		if ("upload".equals(action)) {
+		if ("restartOpenmrs".equals(action)) {
+			WebModuleUtil.restartOpenmrs(getServletContext());
+		} else if ("upload".equals(action)) {// handle module upload
 			// double check upload permissions
 			if (!ModuleUtil.allowAdmin()) {
 				error = msa.getMessage("Module.disallowUploads",
@@ -105,7 +107,6 @@ public class ModuleListController extends SimpleFormController {
 				Module module = null;
 				Boolean updateModule = false;
 				Boolean downloadModule = ServletRequestUtils.getBooleanParameter(request, "download", false);
-				List<Module> dependentModulesStopped = null;
 				try {
 					if (downloadModule) {
 						String downloadURL = request.getParameter("downloadURL");
@@ -128,15 +129,32 @@ public class ModuleListController extends SimpleFormController {
 							Module existingModule = ModuleFactory.getModuleById(tmpModule.getModuleId());
 							if (existingModule != null) {
 								updateModule = true;
-								dependentModulesStopped = ModuleFactory.stopModule(existingModule, false, true); // stop the module with these parameters so that mandatory modules can be upgraded
-								WebModuleUtil.stopModule(existingModule, getServletContext());
-								ModuleFactory.unloadModule(existingModule);
+								Boolean upgrade = (Boolean) httpSession.getAttribute("upgrade");
+								Boolean dntShowUpgConf = (Boolean) httpSession.getAttribute("dntShowUpgConf");
+								//existingModule.setUpgradeModule(tmpModule);
+								if (dntShowUpgConf == null || !dntShowUpgConf) {
+									httpSession.setAttribute("showUpgConf", true);
+									httpSession.setAttribute("module", tmpModule);
+									httpSession.setAttribute("modulename", filename);
+								} else {
+									if (upgrade) {
+										upgradeModule(tmpModule, filename);
+									}
+								}
+							} else {
+								//Adding of module
+								inputStream = new FileInputStream(tmpModule.getFile());
+								moduleFile = ModuleUtil.insertModuleFile(inputStream, filename); // copy the omod over to the repo folder
+								module = ModuleFactory.loadModule(moduleFile);
+								ModuleFactory.queueModuleAction(module.getModuleId(), ModuleAction.PENDING_START);
+								tmpModule.getFile().delete();
+								tmpModule = null;
 							}
-							inputStream = new FileInputStream(tmpModule.getFile());
-							moduleFile = ModuleUtil.insertModuleFile(inputStream, filename); // copy the omod over to the repo folder
 						}
 					}
-					module = ModuleFactory.loadModule(moduleFile);
+					/*if (!updateModule) {
+						module = ModuleFactory.loadModule(moduleFile);
+					}*/
 				}
 				catch (ModuleException me) {
 					log.warn("Unable to load and start module", me);
@@ -154,31 +172,56 @@ public class ModuleListController extends SimpleFormController {
 					
 					if (module == null && moduleFile != null)
 						moduleFile.delete();
+
 				}
 				
 				// if we didn't have trouble loading the module, start it
-				if (module != null) {
+				/*if (module != null && !updateModule) {
 					ModuleFactory.startModule(module);
 					WebModuleUtil.startModule(module, getServletContext(), false);
 					if (module.isStarted()) {
 						success = msa.getMessage("Module.loadedAndStarted", new String[] { module.getName() });
-						
-						if (updateModule && dependentModulesStopped != null) {
-							for (Module depMod : dependentModulesStopped) {
-								ModuleFactory.startModule(depMod);
-								WebModuleUtil.startModule(depMod, getServletContext(), false);
-							}
-						}
-						
 					}
 					else
 						success = msa.getMessage("Module.loaded", new String[] { module.getName() });
-				}
+				}*/
 			}
 		}
 
 		else if (moduleId.equals("")) {
-			ModuleUtil.checkForModuleUpdates();
+			
+			if ("moduleupgrade.yes".equals(action) || "moduleupgrade.no".equals(action)) {
+				try {
+					Boolean dntShowUpgConf = ServletRequestUtils.getBooleanParameter(request, "dontShowMessage", false);
+					httpSession.setAttribute("dntShowUpgConf", dntShowUpgConf);
+					boolean upgrade = false;
+					if ("moduleupgrade.yes".equals(action)) {
+						//Code to Queue Upgrade
+						Module tmpModule = (Module) httpSession.getAttribute("module");
+						String filename = (String) httpSession.getAttribute("modulename");
+						//Module existingModule = ModuleFactory.getModuleById(tmpModule.getModuleId());
+						//existingModule.setUpgradeModule(tmpModule);
+						upgradeModule(tmpModule, filename);
+						upgrade = true;
+					} else {
+						upgrade = false;
+					}
+					if (dntShowUpgConf) {
+						httpSession.setAttribute("upgrade", upgrade);
+					}
+					httpSession.removeAttribute("module");
+					httpSession.removeAttribute("modulename");
+				}
+				catch (ModuleException me) {
+					log.warn("Unable to load and start module", me);
+					error = me.getMessage();
+				}
+				finally {
+					httpSession.removeAttribute("showUpgConf");
+				}
+			} else {
+				ModuleUtil.checkForModuleUpdates();
+			}
 		} else if (action.equals(msa.getMessage("Module.installUpdate"))) {
 			// download and install update
 			if (!ModuleUtil.allowAdmin()) {
@@ -207,24 +250,27 @@ public class ModuleListController extends SimpleFormController {
 					error = msa.getMessage("Module.invalid", args);
 				else {
 					if ("stop".equals(action)) {
-						mod.clearStartupError();
+						/*mod.clearStartupError();
 						ModuleFactory.stopModule(mod);
 						WebModuleUtil.stopModule(mod, getServletContext());
-						success = msa.getMessage("Module.stopped", args);
+						success = msa.getMessage("Module.stopped", args);*/
+						ModuleFactory.queueModuleAction(moduleId, ModuleAction.PENDING_STOP);
 					} else if ("start".equals(action)) {
-						ModuleFactory.startModule(mod);
+						/*ModuleFactory.startModule(mod);
 						WebModuleUtil.startModule(mod, getServletContext(), false);
 						if (mod.isStarted())
 							success = msa.getMessage("Module.started", args);
 						else
-							error = msa.getMessage("Module.not.started", args);
+							error = msa.getMessage("Module.not.started", args);*/
+						ModuleFactory.queueModuleAction(moduleId, ModuleAction.PENDING_START);
 					} else if ("unload".equals(action)) {
-						if (ModuleFactory.isModuleStarted(mod)) {
+						/*if (ModuleFactory.isModuleStarted(mod)) {
 							ModuleFactory.stopModule(mod); // stop the module so that when the web stop is done properly
 							WebModuleUtil.stopModule(mod, getServletContext());
 						}
 						ModuleFactory.unloadModule(mod);
-						success = msa.getMessage("Module.unloaded", args);
+						success = msa.getMessage("Module.unloaded", args);*/
+						ModuleFactory.queueModuleAction(moduleId, ModuleAction.PENDING_UNLOAD);
 					}
 				}
 			}
@@ -271,6 +317,54 @@ public class ModuleListController extends SimpleFormController {
 		
 		map.put("loadedModules", ModuleFactory.getLoadedModules());
 		
+		//Showing the confirmation of upgrade
+		HttpSession session = request.getSession(false);
+		Boolean showUpgradeConfirm = false;
+		if(session != null){
+			showUpgradeConfirm = (Boolean) session.getAttribute("showUpgConf");
+			showUpgradeConfirm = showUpgradeConfirm == null ? false : showUpgradeConfirm;
+		}
+		map.put("showUpgradeConfirm", showUpgradeConfirm);
+		
+		map.put("hasPendingActions", ModuleFactory.hasPendingModuleActions());
+
 		return map;
+	}
+	
+	private void upgradeModule(Module tmpModule, String filename) throws ModuleException {
+		try {
+			List<Module> dependentModulesStopped = null;
+			Module existingModule = ModuleFactory.getModuleById(tmpModule.getModuleId());
+			dependentModulesStopped = ModuleFactory.stopModule(existingModule, false, true); // stop the module with these parameters so that mandatory modules can be upgraded
+			WebModuleUtil.stopModule(existingModule, getServletContext());
+			ModuleFactory.unloadModule(existingModule);
+
+			InputStream inputStream = new FileInputStream(tmpModule.getFile());
+			File moduleFile = ModuleUtil.insertModuleFile(inputStream, filename);
+			Module newModule = ModuleFactory.loadModule(moduleFile);
+		
+			ModuleFactory.startModule(newModule);
+			WebModuleUtil.startModule(newModule, getServletContext(), false);
+		
+			if (newModule.isStarted()) {
+				if (dependentModulesStopped != null) {
+					for (Module mod : dependentModulesStopped) {
+						ModuleFactory.startModule(mod);
+						WebModuleUtil.startModule(mod, getServletContext(), false);
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			throw new ModuleException(e.getMessage());
+		}
+		finally {
+			if (tmpModule != null) {
+				boolean deleted = tmpModule.getFile().delete();
+				if (!deleted) {
+					tmpModule.getFile().deleteOnExit();
+				}
+			}
+		}
 	}
 }
