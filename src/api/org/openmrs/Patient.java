4ae9d2510ca9fd3f9fb49402803e8ff4f25ef8d0
David Thomas <pihdave@gmail.com>
2010-08-14 12:57:00 +0000

    openmrs1.7 branch.  added 2 lines to Patient(Person person) constructor to explicitly set uuid of new patient with uuid of original person. TRUNK-1675
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.7.x@15028 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/Patient.java
+++ src/api/org/openmrs/Patient.java
@@ -51,9 +51,8 @@ public class Patient extends Person implements java.io.Serializable {
 	/**
 	 * This constructor creates a new Patient object from the given {@link Person} object. All
 	 * attributes are copied over to the new object. NOTE! All child collection objects are copied
-	 * as pointers, each individual element is not copied. <br/>
-	 * <br/>
-	 * TODO Should the patient specific attributes be copied? (like identifiers)
+	 * as pointers, each individual element is not copied. <br/> <br/> TODO Should the patient
+	 * specific attributes be copied? (like identifiers)
 	 * 
 	 * @param person the person object to copy onto a new Patient
 	 * @see Person#Person(Person)
@@ -62,6 +61,8 @@ public class Patient extends Person implements java.io.Serializable {
 		super(person);
 		if (person != null)
 			this.patientId = person.getPersonId();
+		if (person.getUuid() != null)
+			this.setUuid(person.getUuid());
 	}
 	
 	/**
@@ -316,8 +317,8 @@ public class Patient extends Person implements java.io.Serializable {
 	}
 	
 	/**
-	 * Returns only the non-voided identifiers for this patient. If you want <u>all</u> identifiers,
-	 * use {@link #getIdentifiers()}
+	 * Returns only the non-voided identifiers for this patient. If you want <u>all</u>
+	 * identifiers, use {@link #getIdentifiers()}
 	 * 
 	 * @return list of non-voided identifiers for this patient
 	 * @see #getIdentifiers()
@@ -334,8 +335,8 @@ public class Patient extends Person implements java.io.Serializable {
 	}
 	
 	/**
-	 * Returns only the non-voided identifiers for this patient. If you want <u>all</u> identifiers,
-	 * use {@link #getIdentifiers()}
+	 * Returns only the non-voided identifiers for this patient. If you want <u>all</u>
+	 * identifiers, use {@link #getIdentifiers()}
 	 * 
 	 * @return list of non-voided identifiers for this patient
 	 * @param pit PatientIdentifierType
