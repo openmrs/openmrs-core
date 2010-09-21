7bbcefefdee7dc77eff7ee835b6fda580958d468
Ben Wolfe <ben@openmrs.org>
2010-09-21 18:20:39 +0000

    Followup fix to add @since to new PersonByNameComparator class
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@15523 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/util/PersonByNameComparator.java
+++ api/src/main/java/org/openmrs/util/PersonByNameComparator.java
@@ -1,3 +1,16 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.util;
 
 import java.util.Comparator;
@@ -8,6 +21,8 @@ import org.openmrs.PersonName;
 /**
  * A simple person comparator for sorting persons by name. Sorts names based on the following
  * precedence: FamilyName, FamilyName2, GivenName, MiddleName, FamilyNamePrefix, FamilyNameSuffix
+ * 
+ * @since 1.8
  */
 public class PersonByNameComparator implements Comparator<Person> {
 	
