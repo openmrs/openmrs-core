1a1e65c8f51aad7a8942e647224618b8cc0ddc63
Sy Haas <syhaas@gmail.com>
2010-09-22 19:44:46 +0000

    1.7.x: fixed datepicker widget when elements have no 'id' TRUNK-1631
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.7.x@15536 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/WEB-INF/view/scripts/calendar/calendar.js
+++ web/WEB-INF/view/scripts/calendar/calendar.js
@@ -1,7 +1,16 @@
 
 function showCalendar(obj) {
 	//set appendText to something so it doesnt automagically pop into the page
-	var dp = new DatePicker(jsDateFormat, obj.id, { appendText: " " });
+	var id = obj.id;
+	if(!id) {
+		obj.id = obj.name;
+		if(!obj.id) {
+			obj.id = "something_random" + (Math.random()*1000);
+		}
+		id = obj.id;
+	}
+	
+	var dp = new DatePicker(jsDateFormat, id, { appendText: " " });
 	$j.datepicker.setDefaults($j.datepicker.regional[jsLocale]);
 
 	obj.onclick = null;
