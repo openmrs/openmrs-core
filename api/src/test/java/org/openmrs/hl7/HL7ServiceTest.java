1c21c656d0d9df4e187c09ad196314866d9e117b
Jeremy Keiper <jeremy@openmrs.org>
2010-09-24 17:25:30 +0000

    TRUNK-1749 - fixed NPE in HL7Service.getUuidFromIdentifiers
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@15571 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/test/java/org/openmrs/hl7/HL7ServiceTest.java
+++ api/src/test/java/org/openmrs/hl7/HL7ServiceTest.java
@@ -588,7 +588,7 @@ public class HL7ServiceTest extends BaseContextSensitiveTest {
 		Message message = hl7service
 		        .parseHL7String("MSH|^~\\&|FORMENTRY|AMRS.ELD|HL7LISTENER|AMRS.ELD|20080226102656||ORU^R01|JqnfhKKtouEz8kzTk6Zo|P|2.5|1||||||||16^AMRS.ELD.FORMID\r"
 		                + "PID|||3^^^^||John3^Doe^||\r"
-		                + "NK1|1|Hornblower^Horatio^L|2B^Sibling^99REL||||||||||||M|19410501||||||||||||||||5^^^L^PN||||\r"
+		                + "NK1|1|Hornblower^Horatio^L|2B^Sibling^99REL||||||||||||M|19410501|||||||||||||||||5^^^L^PN||||\r"
 		                + "PV1||O|1^Unknown Location||||1^Super User (1-8)|||||||||||||||||||||||||||||||||||||20080212|||||||V\r"
 		                + "ORC|RE||||||||20080226102537|1^Super User\r"
 		                + "OBR|1|||1238^MEDICAL RECORD OBSERVATIONS^99DCT\r"
@@ -645,6 +645,29 @@ public class HL7ServiceTest extends BaseContextSensitiveTest {
 		CX[] identifiers = nk1List.get(0).getNextOfKinAssociatedPartySIdentifiers();
 		hl7service.getUuidFromIdentifiers(identifiers);
 		Assert.fail("should have failed");
+	}
+
+	/**
+	 * @see {@link HL7Service#getUuidFromIdentifiers(null)}
+	 */
+	@Test
+	@Verifies(value = "should not fail if no assigning authority is found", method = "getUuidFromIdentifiers(null)")
+	public void getUuidFromIdentifiers_shouldNotFailIfNoAssigningAuthorityIsFound()
+			throws Exception {
+		HL7Service hl7service = Context.getHL7Service();
+		Message message = hl7service
+		        .parseHL7String("MSH|^~\\&|FORMENTRY|AMRS.ELD|HL7LISTENER|AMRS.ELD|20080226102656||ORU^R01|JqnfhKKtouEz8kzTk6Zo|P|2.5|1||||||||16^AMRS.ELD.FORMID\r"
+		                + "PID|||3^^^^||John3^Doe^||\r"
+		                + "NK1|1|Hornblower^Horatio^L|2B^Sibling^99REL||||||||||||M|19410501|||||||||||||||||5^^^^PT||||\r"
+		                + "PV1||O|1^Unknown Location||||1^Super User (1-8)|||||||||||||||||||||||||||||||||||||20080212|||||||V\r"
+		                + "ORC|RE||||||||20080226102537|1^Super User\r"
+		                + "OBR|1|||1238^MEDICAL RECORD OBSERVATIONS^99DCT\r"
+		                + "OBX|1|NM|5497^CD4, BY FACS^99DCT||450|||||||||20080206\r"
+		                + "OBX|2|DT|5096^RETURN VISIT DATE^99DCT||20080229|||||||||20080212");
+		ORU_R01 oru = (ORU_R01) message;
+		List<NK1> nk1List = new ORUR01Handler().getNK1List(oru);
+		CX[] identifiers = nk1List.get(0).getNextOfKinAssociatedPartySIdentifiers();
+		hl7service.getUuidFromIdentifiers(identifiers);
 	}
 	
 }
