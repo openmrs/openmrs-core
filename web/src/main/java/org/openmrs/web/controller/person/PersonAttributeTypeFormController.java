171e6ac72cee9f3aad9a9301cb28afb7632c0581
Darius Jazayeri <jazayeri@alum.mit.edu>
2010-09-30 21:46:10 +0000

    Do not allow creation of PersonAttributeTypes with format=java.util.Date. (Quick fix, of which the bigger problem is TRUNK-1778)
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@15679 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/main/java/org/openmrs/web/controller/person/PersonAttributeTypeFormController.java
+++ web/src/main/java/org/openmrs/web/controller/person/PersonAttributeTypeFormController.java
@@ -174,6 +174,9 @@ public class PersonAttributeTypeFormController extends SimpleFormController {
 		formats.add("java.lang.Float");
 		formats.add("java.lang.Boolean");
 		
+		// java.util.Date doesn't work as a PersonAttributeType since it gets saved in a user-date-format-specific way
+		formats.remove("java.util.Date");
+		
 		map.put("privileges", privileges);
 		map.put("formats", formats);
 		
