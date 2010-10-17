1abd80c1acc454c52e78c62732d35524f1fd880c
Ben Wolfe <ben@openmrs.org>
2010-10-17 01:15:44 +0000

    Adding fix so that all the javascript .withjstl pages can cache - TRUNK-28
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@15981 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/main/java/org/openmrs/web/controller/PseudoStaticContentController.java
+++ web/src/main/java/org/openmrs/web/controller/PseudoStaticContentController.java
@@ -22,46 +22,99 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.GlobalProperty;
+import org.openmrs.api.GlobalPropertyListener;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.mvc.LastModified;
 
 /**
- * This controller basically passes requests straight through to their views. When interpretJstl is
- * enabled, ".withjstl" is appended to the view name. (This allows us to use jstl (such as the
- * spring:message tag) in some javascript files.) If you specify any 'rewrites' then the specified
- * paths are remapped, e.g. /scripts/jquery/jquery-1.3.2.min.js -> /scripts/jquery/jquery.min.js
+ * This controller basically passes requests straight through to their views.
+ * When interpretJstl is enabled, ".withjstl" is appended to the view name.
+ * (This allows us to use jstl (such as the spring:message tag) in some
+ * javascript files.)
+ * 
+ * <br/>
+ * If you specify any 'rewrites' then the specified paths are remapped, e.g:<br/>
+ * /scripts/jquery/jquery-1.3.2.min.js -> /scripts/jquery/jquery.min.js
+ * 
+ * <br/>
+ * All jstl files are cached in the browser until a server restart or a global
+ * property is added/changed/deleted
  */
-public class PseudoStaticContentController implements Controller {
-	
+public class PseudoStaticContentController implements Controller, LastModified,
+		GlobalPropertyListener {
+
 	protected final Log log = LogFactory.getLog(getClass());
-	
+
 	private Boolean interpretJstl = false;
-	
+
 	private Map<String, String> rewrites;
-	
+
+	private static Long lastModified = System.currentTimeMillis();
+
 	public Boolean getInterpretJstl() {
 		return interpretJstl;
 	}
-	
+
 	public void setInterpretJstl(Boolean interpretJstl) {
 		this.interpretJstl = interpretJstl;
 	}
-	
+
 	public Map<String, String> getRewrites() {
 		return rewrites;
 	}
-	
+
 	public void setRewrites(Map<String, String> rewrites) {
 		this.rewrites = rewrites;
 	}
 
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-	                                                                                           IOException {
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String path = request.getServletPath() + request.getPathInfo();
+
 		if (rewrites != null && rewrites.containsKey(path))
 			path = rewrites.get(path);
 		if (interpretJstl)
 			path += ".withjstl";
 		return new ModelAndView(path);
+	}
+
+	@Override
+	public long getLastModified(HttpServletRequest request) {
+
+		// return a mostly constant last modified date for all files passing
+		// through the jsp (.withjstl) servlet
+		// this allows the files to cache until we say so
+		if (interpretJstl) {
+			if (log.isDebugEnabled())
+				log.debug("returning last modified date of : " + lastModified
+						+ " for : " + request.getPathInfo());
+			return lastModified;
+		}
+		
+		// the spring servletdispatcher will try to get the lastModified date
+		// from the actual file in this case
+		return -1;
+
+//		throw new RuntimeException(
+//				"get last modified of non-jstl files elsewhere");
+	}
+
+	@Override
+	public void globalPropertyChanged(GlobalProperty newValue) {
+		// reset for every global property change
+		lastModified = System.currentTimeMillis();
+	}
+
+	@Override
+	public void globalPropertyDeleted(String propertyName) {
+		// reset for every global property change
+		lastModified = System.currentTimeMillis();
+	}
+
+	@Override
+	public boolean supportsPropertyName(String propertyName) {
+		return true;
 	}
 }
