80bebc3d4a41fda7ca1e8c8fe1b3434924714cc3
Wyclif Luyima <wyclif@openmrs.org>
2010-10-18 14:38:17 +0000

    Adding a static method to PersonByNameComparator to compare person objects by name(see CR-TRUNK-141 comments) - TRUNK-417
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@15987 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/test/java/org/openmrs/util/PersonByNameComparatorTest.java
+++ api/src/test/java/org/openmrs/util/PersonByNameComparatorTest.java
@@ -28,13 +28,13 @@ public class PersonByNameComparatorTest {
 	 * @see {@link PersonByNameComparator#compare(Person,Person)}
 	 */
 	@Test
-	@Verifies(value = "should return negative if personName for person1 comes before that of person2", method = "compare(Person,Person)")
-	public void compare_shouldReturnNegativeIfPersonNameForPerson1ComesBeforeThatOfPerson2() throws Exception {
+	@Verifies(value = "should return negative if personName for person1 comes before that of person2", method = "comparePersons(Person,Person)")
+	public void comparePersons_shouldReturnNegativeIfPersonNameForPerson1ComesBeforeThatOfPerson2() throws Exception {
 		Person person1 = new Person();
 		person1.addName(new PersonName("givenName", "middleName", "familyName"));
 		Person person2 = new Person();
 		person2.addName(new PersonName("givenName", "middleNamf", "familyName"));
-		int actualValue = new PersonByNameComparator().compare(person1, person2);
+		int actualValue = PersonByNameComparator.comparePersonsByName(person1, person2);
 		Assert.assertTrue("Expected a negative value but it was: " + actualValue, actualValue < 0);
 	}
 	
@@ -42,13 +42,13 @@ public class PersonByNameComparatorTest {
 	 * @see {@link PersonByNameComparator#compare(Person,Person)}
 	 */
 	@Test
-	@Verifies(value = "should return positive if personName for person1 comes after that of person2", method = "compare(Person,Person)")
-	public void compare_shouldReturnPositiveIfPersonNameForPerson1ComesAfterThatOfPerson2() throws Exception {
+	@Verifies(value = "should return positive if personName for person1 comes after that of person2", method = "comparePersons(Person,Person)")
+	public void comparePersons_shouldReturnPositiveIfPersonNameForPerson1ComesAfterThatOfPerson2() throws Exception {
 		Person person1 = new Person();
 		person1.addName(new PersonName("givenName", "middleNamf", "familyName"));
 		Person person2 = new Person();
 		person2.addName(new PersonName("givenName", "middleName", "familyName"));
-		int actualValue = new PersonByNameComparator().compare(person1, person2);
+		int actualValue = PersonByNameComparator.comparePersonsByName(person1, person2);
 		Assert.assertTrue("Expected a positive value but it was: " + actualValue, actualValue > 0);
 	}
 	
@@ -56,13 +56,13 @@ public class PersonByNameComparatorTest {
 	 * @see {@link PersonByNameComparator#compare(Person,Person)}
 	 */
 	@Test
-	@Verifies(value = "should return zero if the givenName middleName and familyName match", method = "compare(Person,Person)")
-	public void compare_shouldReturnZeroIfTheGivenNameMiddleNameAndFamilyNameMatch() throws Exception {
+	@Verifies(value = "should return zero if the givenName middleName and familyName match", method = "comparePersons(Person,Person)")
+	public void comparePersons_shouldReturnZeroIfTheGivenNameMiddleNameAndFamilyNameMatch() throws Exception {
 		Person person1 = new Person();
 		person1.addName(new PersonName("givenName", "middleName", "familyName"));
 		Person person2 = new Person();
 		person2.addName(new PersonName("givenName", "middleName", "familyName"));
-		int actualValue = new PersonByNameComparator().compare(person1, person2);
+		int actualValue = PersonByNameComparator.comparePersonsByName(person1, person2);
 		Assert.assertTrue("Expected zero but it was: " + actualValue, actualValue == 0);
 	}
 }
