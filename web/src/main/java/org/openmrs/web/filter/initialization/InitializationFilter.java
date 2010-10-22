32eb96043d81a183f9a2ae35546b4dc7b525d7fc
Wyclif Luyima <wyclif@openmrs.org>
2010-10-22 05:28:55 +0000

    New installs do not show initial setup progress/details on Windows platforms - TRUNK-1817
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@16096 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/main/java/org/openmrs/web/filter/initialization/InitializationFilter.java
+++ web/src/main/java/org/openmrs/web/filter/initialization/InitializationFilter.java
@@ -419,16 +419,16 @@ public class InitializationFilter extends StartupFilter {
 				if (appender instanceof MemoryAppender) {
 					MemoryAppender memoryAppender = (MemoryAppender) appender;
 					List<String> logLines = memoryAppender.getLogLines();
-					// truncate the list to the last 3 so we don't overwhelm jquery
-					if (logLines.size() > 3)
-						logLines = logLines.subList(logLines.size() - 3, logLines.size());
+					// truncate the list to the last 5 so we don't overwhelm jquery
+					if (logLines.size() > 5)
+						logLines = logLines.subList(logLines.size() - 5, logLines.size());
 					result.put("logLines", logLines);
 				} else {
 					result.put("logLines", new ArrayList<String>());
 				}
 			}
 			
-			httpResponse.getWriter().write(toJSONString(result));
+			httpResponse.getWriter().write(toJSONString(result, true));
 		}
 	}
 	
