87c7843ae9731ff9e8655c9cc1ed8567e07a5d4f
Ben Wolfe <ben@openmrs.org>
2010-11-02 19:03:38 +0000

    Sped up module loading - TRUNK-1666 Author: mseaton, bwolfe
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@16302 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/module/ModuleUtil.java
+++ api/src/main/java/org/openmrs/module/ModuleUtil.java
@@ -28,6 +28,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -911,6 +912,84 @@ public class ModuleUtil {
 		if (path.startsWith("/"))
 			path = path.substring(1);
 		return path.substring(module.getModuleIdAsPath().length());
+	}
+
+	/**
+	 * This loops over all FILES in this jar to get the package names.  If there is an
+	 * empty directory in this jar it is not returned as a providedPackage. 
+	 * 
+	 * @param file jar file to look into
+	 * @return list of strings of package names in this jar
+	 */
+	public static Collection<String> getPackagesFromFile(File file) {
+		
+		// end early if we're given a non jar file
+		if (!file.getName().endsWith(".jar"))
+			return Collections.EMPTY_SET;
+		
+		Set<String> packagesProvided = new HashSet<String>();
+		
+		JarFile jar = null;
+		try {
+			jar = new JarFile(file);
+			
+			Enumeration<JarEntry> jarEntries = jar.entries();
+			while (jarEntries.hasMoreElements()) {
+				JarEntry jarEntry = jarEntries.nextElement();
+				if (jarEntry.isDirectory()) {
+					// skip over directory entries, we only care about dirs with files in it
+					continue; 
+				}
+				String name = jarEntry.getName();
+				Integer indexOfLastSlash = name.lastIndexOf("/");
+				if (indexOfLastSlash <= 0)
+					continue; 
+				String packageName = name.substring(0, indexOfLastSlash);
+				
+				// skip over some folders in the jar/omod
+				if (packageName.equals("lib") || packageName.equals("META-INF") || packageName.startsWith("web/module")) {
+					continue;
+				}
+				
+				packageName = packageName.replaceAll("/", ".");
+				
+				if (packagesProvided.add(packageName))
+					log.trace("Adding module's jarentry with package: " + packageName);
+			}
+		
+		} catch (IOException e) {
+			log.error("Unable to open jar from file: " + file.getAbsolutePath(), e);
+		} finally {
+			if (jar != null)
+				try {
+					jar.close();
+				} catch (IOException e) {
+					// do nothing
+				}
+			
+		}
+		
+		// clean up packages contained within other packages this is
+		// O(n^2), but its better than putting extra packages into the
+		// set and having the classloader continually loop over them
+		Set<String> packagesProvidedCopy = new HashSet<String>();
+		packagesProvidedCopy.addAll(packagesProvided);
+		
+		for (String packageNameOuter : packagesProvidedCopy) {
+			// add the period so that we don't match to ourselves or to 
+			// similarly named packages. eg. org.pih and org.pihrwanda 
+			// should not match
+			packageNameOuter += ".";  
+			for (String packageNameInner : packagesProvidedCopy) {
+				if (packageNameInner.contains(packageNameOuter)) {
+					packagesProvided.remove(packageNameInner);
+				}
+			}
+			
+		}
+		// end cleanup
+		
+		return packagesProvided;
 	}
 	
 }
