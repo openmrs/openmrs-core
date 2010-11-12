66c325e004dc7d1ffb6b97e8af093a8455756d53
Wyclif Luyima <wyclif@openmrs.org>
2010-11-12 22:38:07 +0000

    Fixing faulty unit tests
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@16493 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/test/java/org/openmrs/validator/ConceptValidatorTest.java
+++ api/src/test/java/org/openmrs/validator/ConceptValidatorTest.java
@@ -116,7 +116,7 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 		ConceptName anotherConceptName = ConceptNameTest.createMockConceptName(3, Context.getLocale(), null, true);
 		anotherConceptName.setName(duplicateName);
 		newConcept.addName(anotherConceptName);
-		Errors errors = new BindException(concept, "concept");
+		Errors errors = new BindException(newConcept, "concept");
 		new ConceptValidator().validate(newConcept, errors);
 	}
 	
@@ -128,7 +128,7 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 	@Test(expected = DuplicateConceptNameException.class)
 	@Verifies(value = "should fail if there is a duplicate unretired preferred name in the same locale", method = "validate(Object,Errors)")
 	public void validate_shouldFailIfThereIsADuplicateUnretiredPreferredNameInTheSameLocale() throws Exception {
-		Context.setLocale(Context.getLocale());
+		Context.setLocale(new Locale("en"));
 		Concept concept = Context.getConceptService().getConcept(5497);
 		ConceptName preferredName = new ConceptName("preferred name", Context.getLocale());
 		concept.setPreferredName(preferredName);
@@ -139,7 +139,7 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 		ConceptName anotherConceptName = ConceptNameTest.createMockConceptName(3, Context.getLocale(), null, true);
 		anotherConceptName.setName("preferred name");
 		newConcept.addName(anotherConceptName);
-		Errors errors = new BindException(concept, "concept");
+		Errors errors = new BindException(newConcept, "concept");
 		new ConceptValidator().validate(newConcept, errors);
 	}
 	
@@ -177,7 +177,7 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 	@Test
 	@Verifies(value = "should pass if the duplicate ConceptName is neither preferred nor fully Specified", method = "validate(Object,Errors)")
 	public void validate_shouldPassIfTheDuplicateConceptNameIsNeitherPreferredNorFullySpecified() throws Exception {
-		Context.setLocale(Context.getLocale());
+		Context.setLocale(new Locale("en"));
 		Concept concept = Context.getConceptService().getConcept(5497);
 		ConceptName duplicateName = new ConceptName("duplicate name", Context.getLocale());
 		//note that the name is neither fully specified nor preferred,
@@ -191,7 +191,7 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 		ConceptName assumedDuplicateName = ConceptNameTest.createMockConceptName(3, Context.getLocale(), null, true);
 		assumedDuplicateName.setName(duplicateName.getName());
 		newConcept.addName(assumedDuplicateName);
-		Errors errors = new BindException(concept, "concept");
+		Errors errors = new BindException(newConcept, "concept");
 		new ConceptValidator().validate(newConcept, errors);
 		Assert.assertEquals(false, errors.hasErrors());
 	}
@@ -212,7 +212,7 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 		ConceptName anotherConceptName = ConceptNameTest.createMockConceptName(3, Context.getLocale(), null, true);
 		anotherConceptName.setName(duplicateName);
 		newConcept.addName(anotherConceptName);
-		Errors errors = new BindException(concept, "concept");
+		Errors errors = new BindException(newConcept, "concept");
 		new ConceptValidator().validate(newConcept, errors);
 		Assert.assertEquals(false, errors.hasErrors());
 	}
