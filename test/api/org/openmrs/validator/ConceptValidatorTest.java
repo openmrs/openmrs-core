d170de9f0632b8100540450bfdfad07768f202fc
Wyclif Luyima <wyclif@openmrs.org>
2010-11-16 03:54:41 +0000

    1.7.x: Backporting fix for failing unit tests
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.7.x@16513 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/validator/ConceptValidatorTest.java
+++ test/api/org/openmrs/validator/ConceptValidatorTest.java
@@ -1,13 +1,12 @@
 package org.openmrs.validator;
 
+import java.util.Calendar;
 import java.util.Locale;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openmrs.Concept;
 import org.openmrs.ConceptName;
-import org.openmrs.ConceptNameTest;
-import org.openmrs.ConceptTest;
 import org.openmrs.api.DuplicateConceptNameException;
 import org.openmrs.api.context.Context;
 import org.openmrs.test.BaseContextSensitiveTest;
@@ -27,12 +26,14 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 	@Test(expected = DuplicateConceptNameException.class)
 	@Verifies(value = "should fail if there is a duplicate unretired concept name in the locale", method = "validate(Concept)")
 	public void validate_shouldFailIfThereIsADuplicateUnretiredConceptNameInTheLocale() throws Exception {
-		Concept concept = ConceptTest.createMockConcept(1, Context.getLocale());
+		Context.setLocale(new Locale("en"));
+		Concept concept = Context.getConceptService().getConcept(5497);
 		String duplicateName = concept.getFullySpecifiedName(Context.getLocale()).getName();
 		
-		ConceptName anotherConceptName = ConceptNameTest.createMockConceptName(3, Context.getLocale(), null, false);
-		anotherConceptName.setName(duplicateName);
-		concept.addName(anotherConceptName);
+		ConceptName newName = new ConceptName(duplicateName, Context.getLocale());
+		newName.setDateCreated(Calendar.getInstance().getTime());
+		newName.setCreator(Context.getAuthenticatedUser());
+		concept.addName(newName);
 		Errors errors = new BindException(concept, "concept");
 		new ConceptValidator().validate(concept, errors);
 	}
@@ -110,14 +111,13 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 	public void validate_shouldFailIfThereIsADuplicateUnretiredFullySpecifiedNameInTheSameLocale() throws Exception {
 		Context.setLocale(new Locale("en"));
 		Concept concept = Context.getConceptService().getConcept(5497);
-		
+		Assert.assertEquals(true, concept.getFullySpecifiedName(Context.getLocale()).isFullySpecifiedName());
 		String duplicateName = concept.getFullySpecifiedName(Context.getLocale()).getName();
-		Concept newConcept = ConceptTest.createMockConcept(2, Context.getLocale());
-		ConceptName anotherConceptName = ConceptNameTest.createMockConceptName(3, Context.getLocale(), null, true);
-		anotherConceptName.setName(duplicateName);
-		newConcept.addName(anotherConceptName);
-		Errors errors = new BindException(concept, "concept");
-		new ConceptValidator().validate(newConcept, errors);
+		
+		Concept anotherConcept = Context.getConceptService().getConcept(5089);
+		anotherConcept.getFullySpecifiedName(Context.getLocale()).setName(duplicateName);
+		Errors errors = new BindException(anotherConcept, "concept");
+		new ConceptValidator().validate(anotherConcept, errors);
 	}
 	
 	/**
@@ -128,19 +128,18 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 	@Test(expected = DuplicateConceptNameException.class)
 	@Verifies(value = "should fail if there is a duplicate unretired preferred name in the same locale", method = "validate(Object,Errors)")
 	public void validate_shouldFailIfThereIsADuplicateUnretiredPreferredNameInTheSameLocale() throws Exception {
-		Context.setLocale(Context.getLocale());
+		Context.setLocale(new Locale("en"));
 		Concept concept = Context.getConceptService().getConcept(5497);
 		ConceptName preferredName = new ConceptName("preferred name", Context.getLocale());
 		concept.setPreferredName(preferredName);
 		Context.getConceptService().saveConcept(concept);
 		Assert.assertEquals("preferred name", concept.getPreferredName(Context.getLocale()).getName());
 		
-		Concept newConcept = ConceptTest.createMockConcept(2, Context.getLocale());
-		ConceptName anotherConceptName = ConceptNameTest.createMockConceptName(3, Context.getLocale(), null, true);
-		anotherConceptName.setName("preferred name");
-		newConcept.addName(anotherConceptName);
-		Errors errors = new BindException(concept, "concept");
-		new ConceptValidator().validate(newConcept, errors);
+		Concept anotherConcept = Context.getConceptService().getConcept(5089);
+		anotherConcept.getFullySpecifiedName(Context.getLocale()).setName("preferred name");
+		
+		Errors errors = new BindException(anotherConcept, "concept");
+		new ConceptValidator().validate(anotherConcept, errors);
 	}
 	
 	/**
@@ -177,22 +176,16 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 	@Test
 	@Verifies(value = "should pass if the duplicate ConceptName is neither preferred nor fully Specified", method = "validate(Object,Errors)")
 	public void validate_shouldPassIfTheDuplicateConceptNameIsNeitherPreferredNorFullySpecified() throws Exception {
-		Context.setLocale(Context.getLocale());
+		Context.setLocale(new Locale("en"));
 		Concept concept = Context.getConceptService().getConcept(5497);
-		ConceptName duplicateName = new ConceptName("duplicate name", Context.getLocale());
-		//note that the name is neither fully specified nor preferred,
-		//test method should fail if the line below is uncommented
-		//duplicateName.setLocalePreferred(true);
-		concept.addName(duplicateName);
-		Context.getConceptService().saveConcept(concept);
-		Assert.assertEquals(true, concept.hasName("duplicate name", Context.getLocale()));
+		//use a synonym as the duplicate name
+		ConceptName duplicateName = concept.getSynonyms(Context.getLocale()).iterator().next();
+		Assert.assertEquals(true, duplicateName.isSynonym());
 		
-		Concept newConcept = ConceptTest.createMockConcept(2, Context.getLocale());
-		ConceptName assumedDuplicateName = ConceptNameTest.createMockConceptName(3, Context.getLocale(), null, true);
-		assumedDuplicateName.setName(duplicateName.getName());
-		newConcept.addName(assumedDuplicateName);
-		Errors errors = new BindException(concept, "concept");
-		new ConceptValidator().validate(newConcept, errors);
+		Concept anotherConcept = Context.getConceptService().getConcept(5089);
+		anotherConcept.getFullySpecifiedName(Context.getLocale()).setName(duplicateName.getName());
+		Errors errors = new BindException(anotherConcept, "concept");
+		new ConceptValidator().validate(anotherConcept, errors);
 		Assert.assertEquals(false, errors.hasErrors());
 	}
 	
@@ -206,14 +199,12 @@ public class ConceptValidatorTest extends BaseContextSensitiveTest {
 		Concept concept = Context.getConceptService().getConcept(5497);
 		concept.setRetired(true);
 		Context.getConceptService().saveConcept(concept);
-		
 		String duplicateName = concept.getFullySpecifiedName(Context.getLocale()).getName();
-		Concept newConcept = ConceptTest.createMockConcept(2, Context.getLocale());
-		ConceptName anotherConceptName = ConceptNameTest.createMockConceptName(3, Context.getLocale(), null, true);
-		anotherConceptName.setName(duplicateName);
-		newConcept.addName(anotherConceptName);
-		Errors errors = new BindException(concept, "concept");
-		new ConceptValidator().validate(newConcept, errors);
+		
+		Concept anotherConcept = Context.getConceptService().getConcept(5089);
+		anotherConcept.getFullySpecifiedName(Context.getLocale()).setName(duplicateName);
+		Errors errors = new BindException(anotherConcept, "concept");
+		new ConceptValidator().validate(anotherConcept, errors);
 		Assert.assertEquals(false, errors.hasErrors());
 	}
 }
