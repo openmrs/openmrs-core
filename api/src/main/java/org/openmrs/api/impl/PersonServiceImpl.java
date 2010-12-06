626699a7db9ebc54ee7b6d39b90763008cc66416
Ben Wolfe <ben@openmrs.org>
2010-12-06 13:57:42 +0000

    Fixed possible NPE when parsing person names with strange user input - TRUNK-1886
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@16831 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/api/impl/PersonServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/PersonServiceImpl.java
@@ -775,28 +775,45 @@ public class PersonServiceImpl extends BaseOpenmrsService implements PersonServi
 	 * @see org.openmrs.api.PersonService#parsePersonName(java.lang.String)
 	 */
 	public PersonName parsePersonName(String name) throws APIException {
+		name = name.trim(); // strip beginning/ending whitespace
+		
 		String firstName = name;
 		String middleName = "";
 		String lastName = "";
 		
 		if (name.contains(",")) {
+			
+			// trim off all trailing commas
+			while (name.endsWith(","))
+				name = name.substring(0, name.length() - 1);
+			
 			String[] names = name.split(", ");
-			String[] firstNames = names[1].split(" ");
-			if (firstNames.length == 2) {
-				lastName = names[0];
-				firstName = firstNames[0];
-				middleName = firstNames[1];
-			} else {
-				firstName = names[1];
-				lastName = names[0];
+			if (names.length > 1) {
+				String[] firstNames = names[1].split(" ");
+				if (firstNames.length == 2) {
+					// user entered "Smith, John Adam"
+					lastName = names[0];
+					firstName = firstNames[0];
+					middleName = firstNames[1];
+				} else {
+					// user entered "Smith, John"
+					firstName = names[1];
+					lastName = names[0];
+				}
+			}
+			else {
+				// user entered something with a trailing comma
+				firstName = name;
 			}
 		} else if (name.contains(" ")) {
 			String[] names = name.split(" ");
 			if (names.length == 3) {
+				// user entered "John Adam Smith"
 				firstName = names[0];
 				middleName = names[1];
 				lastName = names[2];
 			} else {
+				// user entered "John Smith"
 				firstName = names[0];
 				lastName = names[1];
 			}
