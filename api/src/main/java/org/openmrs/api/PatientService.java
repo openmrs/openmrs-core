fa1357ce42867a636e1c4912a725ecd41553736e
Ben Wolfe <ben@openmrs.org>
2010-12-07 21:20:22 +0000

    Fixed problems when updating a person with no addresses - TRUNK-277
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@16856 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/api/PatientService.java
+++ api/src/main/java/org/openmrs/api/PatientService.java
@@ -725,24 +725,28 @@ public interface PatientService extends OpenmrsService {
 	 * @param clazz identifierValidator which validator to get.
 	 * @should return patient identifier validator given class
 	 */
+	@Transactional(readOnly = true)
 	public IdentifierValidator getIdentifierValidator(Class<IdentifierValidator> clazz);
 	
 	/**
 	 * @should return patient identifier validator given class name
 	 * @should treat empty strings like a null entry
 	 */
+	@Transactional(readOnly = true)
 	public IdentifierValidator getIdentifierValidator(String pivClassName);
 	
 	/**
 	 * @return the default IdentifierValidator
 	 * @should return default patient identifier validator
 	 */
+	@Transactional(readOnly = true)
 	public IdentifierValidator getDefaultIdentifierValidator();
 	
 	/**
 	 * @return All registered PatientIdentifierValidators
 	 * @should return all registered patient identifier validators
 	 */
+	@Transactional(readOnly = true)
 	public Collection<IdentifierValidator> getAllIdentifierValidators();
 	
 	/**
