4a191da6fa143e6446a6a39e02f71976d438f1db
Wyclif Luyima <wyclif@openmrs.org>
2010-12-08 20:50:06 +0000

    Fixing test method for checking if deathdate has passed not to fail when the test is run in 59th minute of the hour - TRUNK-1607
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@16882 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/test/java/org/openmrs/web/controller/patient/ShortPatientFormValidatorTest.java
+++ web/src/test/java/org/openmrs/web/controller/patient/ShortPatientFormValidatorTest.java
@@ -177,7 +177,7 @@ public class ShortPatientFormValidatorTest extends BaseWebContextSensitiveTest {
 		p.setCauseOfDeath(new Concept(88));
 		Calendar c = Calendar.getInstance();
 		// put the time into the future by a minute
-		c.roll(Calendar.MINUTE, true);
+		c.add(Calendar.MINUTE, 1);
 		p.setDeathDate(c.getTime());
 		ShortPatientModel model = new ShortPatientModel(p);
 		Errors errors = new BindException(model, "patientModel");
