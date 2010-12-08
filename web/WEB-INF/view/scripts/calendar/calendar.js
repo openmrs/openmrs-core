d32be419ddb8eb8d8bd134588b2052de3c20930b
Jeremy Keiper <jeremy@openmrs.org>
2010-12-08 21:24:27 +0000

    backporting TRUNK-1881 to branches/1.7.x - increased amount of dates in datepicker for birthdays
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.7.x@16884 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/WEB-INF/view/scripts/calendar/calendar.js
+++ web/WEB-INF/view/scripts/calendar/calendar.js
@@ -1,7 +1,11 @@
 
-function showCalendar(obj) {
-	//set appendText to something so it doesnt automagically pop into the page
-	var dp = new DatePicker(jsDateFormat, obj, { appendText: " " });
+function showCalendar(obj, yearsPrevious) {
+	//set appendText to something so it doesn't automagically pop into the page
+	var opts = { appendText: " " };
+	if (yearsPrevious)
+		opts["yearRange"] = "c-" + yearsPrevious + ":c10";
+	
+	var dp = new DatePicker(jsDateFormat, obj, opts);
 	jQuery.datepicker.setDefaults(jQuery.datepicker.regional[jsLocale]);
 
 	obj.onclick = null;
