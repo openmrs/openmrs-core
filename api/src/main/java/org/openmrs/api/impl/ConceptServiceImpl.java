b54f028a63ff9e1c57c70904d567d5a1ffdc5b83
Wyclif Luyima <wyclif@openmrs.org>
2010-12-11 07:08:05 +0000

    Fixed paging when Searching for concepts and restoring the widget back to the latest version before the last revert - TRUNK-1862
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@17019 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/api/impl/ConceptServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/ConceptServiceImpl.java
@@ -1650,11 +1650,28 @@ public class ConceptServiceImpl extends BaseOpenmrsService implements ConceptSer
 	                                             List<ConceptDatatype> requireDatatypes,
 	                                             List<ConceptDatatype> excludeDatatypes, Concept answersToConcept,
 	                                             Integer start, Integer size) throws APIException {
-		
+		//TODO fix DAO layer to filter well and return only concept words associated
+		//to only unique concept for hibernate paging to work and avoid returning sublist
 		List<ConceptWord> conceptWords = getConceptWords(phrase, locales, includeRetired, requireClasses, excludeClasses,
-		    requireDatatypes, excludeDatatypes, answersToConcept, start, size);
+		    requireDatatypes, excludeDatatypes, answersToConcept, null, null);
+		List<ConceptSearchResult> results = createSearchResultsList(conceptWords);
+		int count = results.size();
+		if (start == null && size == null)
+			return results;
 		
-		return createSearchResultsList(conceptWords);
+		if (start == null || start < 0)
+			start = 0;
+		if (size == null || size < 0)
+			size = count;
+		
+		if (count == 0 || (start > (count - 1)) || size < 1)
+			return Collections.emptyList();
+		
+		int lastIndex = start + size;
+		if (lastIndex < count)
+			return results.subList(start, lastIndex);
+		
+		return results.subList(start, count);
 	}
 	
 	/**
