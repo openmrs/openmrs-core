39c0ddab1a57bfb0719c77f020ff0762b36fdbdd
Wyclif Luyima <wyclif@openmrs.org>
2011-01-06 20:04:40 +0000

    Applying code formatting
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.8.x@17471 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/obs/handler/BinaryDataHandler.java
+++ api/src/main/java/org/openmrs/obs/handler/BinaryDataHandler.java
@@ -71,11 +71,10 @@ public class BinaryDataHandler extends AbstractHandler implements ComplexObsHand
 			}
 			
 			if (file.exists()) {
-				FileInputStream fileInputStream = new FileInputStream(file);	
+				FileInputStream fileInputStream = new FileInputStream(file);
 				obs.setComplexData(new ComplexData(originalFilename, new BufferedInputStream(fileInputStream)));
 				fileInputStream.close();
-			}
-			else {
+			} else {
 				log.error("Unable to find file associated with complex obs " + obs.getId());
 			}
 		}
