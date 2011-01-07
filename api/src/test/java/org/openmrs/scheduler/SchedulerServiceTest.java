5dcf3406b28a034f364eed60e2907156ef2bb197
Ben Wolfe <ben@openmrs.org>
2011-01-07 19:47:57 +0000

    Again attempting to fix flakey unit test
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@17504 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/test/java/org/openmrs/scheduler/SchedulerServiceTest.java
+++ api/src/test/java/org/openmrs/scheduler/SchedulerServiceTest.java
@@ -356,11 +356,14 @@ public class SchedulerServiceTest extends BaseContextSensitiveTest {
 		service.saveTask(td);
 		
 		service.scheduleTask(td);
+		
+		// refetch the task
+		td = service.getTaskByName(NAME);
+		
 		// sleep a while until the task has executed, up to 5 times
-		for (int x = 0; x < 5 && actualExecutionTime == null; x++)
+		for (int x = 0; x < 5 && actualExecutionTime == null && td.getLastExecutionTime() == null; x++)
 			Thread.sleep(200);
 		
-		td = service.getTaskByName(NAME);
 		Assert.assertNotNull(actualExecutionTime);
 		assertEquals("Last execution time in seconds is wrong", actualExecutionTime.longValue() / 1000, td
 		        .getLastExecutionTime().getTime() / 1000, 1);
