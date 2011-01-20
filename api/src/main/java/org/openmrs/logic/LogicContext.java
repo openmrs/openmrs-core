0526d449f78c21c4f178a08d0e660b716c8c1d1d
Darius Jazayeri <jazayeri@alum.mit.edu>
2011-01-20 02:42:12 +0000

    Backporting fixes for: TRUNK-1953 - Change method signatures in LogicService, LogicContext and Rule to allow some rules to be faster and more lightweight TRUNK-1968 - LogicService needs methods to evaluate multiple rules at once on a single patient TRUNK-1969 - Change method signatures in LogicDataSource to allow a NAME property on the data source (instead of specifying that in spring xml)
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.8.x@17709 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/logic/LogicContext.java
+++ api/src/main/java/org/openmrs/logic/LogicContext.java
@@ -27,39 +27,47 @@ import org.openmrs.logic.result.Result;
 public interface LogicContext {
 	
 	/**
+	 * Gets the patient object for the given patient id (this patient must be in the cohort of this context)
+	 * 
+	 * @param patientId
+	 * @return
+	 */
+	public Patient getPatient(Integer patientId);
+	
+	/**
 	 * Evaluate a rule for a single patient
 	 * 
-	 * @param patient
+	 * @param patientId
 	 * @param token
 	 * @return <code>Result</code> of the evaluation
 	 * @throws LogicException
 	 * @see org.openmrs.logic.LogicService#eval(Patient, String)
 	 */
-	public Result eval(Patient patient, String token) throws LogicException;
+	public Result eval(Integer patientId, String token) throws LogicException;
 	
 	/**
 	 * Evaluate a rule with parameters for a single patient
 	 * 
-	 * @param patient
+	 * @param patientId
 	 * @param token
 	 * @param parameters
 	 * @return <code>Result</code> of the evaluation
 	 * @throws LogicException
 	 * @see org.openmrs.logic.LogicService#eval(Patient, String, Map)
 	 */
-	public Result eval(Patient patient, String token, Map<String, Object> parameters) throws LogicException;
+	public Result eval(Integer patientId, String token, Map<String, Object> parameters) throws LogicException;
 	
 	/**
 	 * Evaluate a rule with criteria and parameters for a single patient
 	 * 
-	 * @param patient
+	 * @param patientId
 	 * @param criteria
 	 * @param parameters
 	 * @return A <code>Result</code> object with the result of the evaluation
 	 * @throws LogicException
 	 * @see org.openmrs.logic.LogicService#eval(Patient, LogicCriteria, Map)
 	 */
-	public Result eval(Patient patient, LogicCriteria criteria, Map<String, Object> parameters) throws LogicException;
+	public Result eval(Integer patientId, LogicCriteria criteria, Map<String, Object> parameters) throws LogicException;
 	
 	/**
 	 * Fetches a logic data source by name
@@ -72,44 +80,44 @@ public interface LogicContext {
 	/**
 	 * Reads a key from a logic data source
 	 * 
-	 * @param patient
+	 * @param patientId
 	 * @param dataSource
 	 * @param key
 	 * @return <code>Result</code> of the read operation
 	 * @throws LogicException
 	 */
-	public Result read(Patient patient, LogicDataSource dataSource, String key) throws LogicException;
+	public Result read(Integer patientId, LogicDataSource dataSource, String key) throws LogicException;
 	
 	/**
-	 * Auto generated method comment
+	 * Reads a key from a logic data source
 	 * 
-	 * @param patient
+	 * @param patientId
 	 * @param key
 	 * @return <code>Result</code> of the read operation
 	 * @throws LogicException
 	 */
-	public Result read(Patient patient, String key) throws LogicException;
+	public Result read(Integer patientId, String key) throws LogicException;
 	
 	/**
 	 * Reads a key with criteria from a logic data source
 	 * 
-	 * @param patient
+	 * @param patientId
 	 * @param criteria
 	 * @return <code>Result</code> of the read
 	 * @throws LogicException
 	 */
-	public Result read(Patient patient, LogicCriteria criteria) throws LogicException;
+	public Result read(Integer patientId, LogicCriteria criteria) throws LogicException;
 	
 	/**
 	 * Reads a key with criteria from a logic data source
 	 * 
-	 * @param patient
+	 * @param patientId
 	 * @param dataSource
 	 * @param criteria
 	 * @return <code>Result</code> of the read
 	 * @throws LogicException
 	 */
-	public Result read(Patient patient, LogicDataSource dataSource, LogicCriteria criteria) throws LogicException;
+	public Result read(Integer patientId, LogicDataSource dataSource, LogicCriteria criteria) throws LogicException;
 	
 	/**
 	 * Changes the index date for this logic context
