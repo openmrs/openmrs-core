6bb3493fc6b442397dc98a0e9ca1f1b1f4b0932e
Darius Jazayeri <jazayeri@alum.mit.edu>
2011-02-09 01:32:03 +0000

    Followup fix for TRUNK-263 from CR-TRUNK-253
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@18107 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/PersonAddress.java
+++ api/src/main/java/org/openmrs/PersonAddress.java
@@ -125,9 +125,9 @@ public class PersonAddress extends BaseOpenmrsData implements java.io.Serializab
 		boolean returnValue = true;
 		
 		// these are the methods to compare. All are expected to be Strings
-		String[] methods = { "getAddress1", "getAddress2", "getAddress3", "getAddress4", "getAddress5", "getCityVillage",
-		        "getNeighborhoodCell", "getCountyDistrict", "getStateProvince", "getCountry", "getPostalCode",
-		        "getLatitude", "getLongitude" };
+		String[] methods = { "getAddress1", "getAddress2", "getAddress3", "getAddress4", "getAddress5", "getAddress6",
+		        "getCityVillage", "getCountyDistrict", "getStateProvince", "getCountry", "getPostalCode", "getLatitude",
+		        "getLongitude" };
 		
 		Class addressClass = this.getClass();
 		
