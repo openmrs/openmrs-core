dd4211e70c287373b4d1832f7ddee2453974e3b2
Ben Wolfe <ben@openmrs.org>
2011-02-21 20:05:23 +0000

    Adding a new module from within OpenMRS web application reports an error - TRUNK-2032
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.8.x@18332 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/module/ModuleUtil.java
+++ api/src/main/java/org/openmrs/module/ModuleUtil.java
@@ -20,6 +20,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -556,7 +557,7 @@ public class ModuleUtil {
 			
 			log.error("Logging an attempt to connect to: " + url);
 			
-			in = uc.getInputStream();
+			in = openConnectionCheckRedirects(uc);
 		}
 		catch (IOException io) {
 			log.warn("io while reading: " + url, io);
@@ -565,6 +566,53 @@ public class ModuleUtil {
 		return in;
 	}
 	
+	/**
+	 * Convenience method to follow http to https redirects.  Will follow a total of 5 redirects, 
+	 * then fail out due to foolishness on the url's part.
+	 * 
+	 * @param c the {@link URLConnection} to open
+	 * @return an {@link InputStream} that is not necessarily at the same url, possibly at a 403 redirect.
+	 * @throws IOException
+	 * @see {@link #getURLStream(URL)}
+	 */
+	protected static InputStream openConnectionCheckRedirects(URLConnection c) throws IOException {
+		boolean redir;
+		int redirects = 0;
+		InputStream in = null;
+		do {
+			if (c instanceof HttpURLConnection) {
+				((HttpURLConnection) c).setInstanceFollowRedirects(false);
+			}
+			// We want to open the input stream before getting headers
+			// because getHeaderField() et al swallow IOExceptions.
+			in = c.getInputStream();
+			redir = false;
+			if (c instanceof HttpURLConnection) {
+				HttpURLConnection http = (HttpURLConnection) c;
+				int stat = http.getResponseCode();
+				if (stat >= 300 && stat <= 307 && stat != 306 && stat != HttpURLConnection.HTTP_NOT_MODIFIED) {
+					URL base = http.getURL();
+					String loc = http.getHeaderField("Location");
+					URL target = null;
+					if (loc != null) {
+						target = new URL(base, loc);
+					}
+					http.disconnect();
+					// Redirection should be allowed only for HTTP and HTTPS
+					// and should be limited to 5 redirections at most.
+					if (target == null || !(target.getProtocol().equals("http") || target.getProtocol().equals("https"))
+					        || redirects >= 5) {
+						throw new SecurityException("illegal URL redirect");
+					}
+					redir = true;
+					c = target.openConnection();
+					redirects++;
+				}
+			}
+		} while (redir);
+		return in;
+	}
+
 	/**
 	 * Downloads the contents of a URL and copies them to a string (Borrowed from oreilly)
 	 * 
