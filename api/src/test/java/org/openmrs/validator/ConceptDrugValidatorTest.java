4300e08612f2ef220d64ca6eb607958e9849deae
Ben Wolfe <ben@openmrs.org>
2011-02-22 21:37:26 +0000

    Make Concept required on concept drug form - TRUNK-1864 Author: abbas
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@18349 5bac5841-c719-aa4e-b3fe-cce5062f897a

---
+++ api/src/test/java/org/openmrs/validator/ConceptDrugValidatorTest.java
@@ -0,0 +1,44 @@
+package org.openmrs.validator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openmrs.Drug;
+import org.springframework.validation.BindException;
+import org.springframework.validation.Errors;
+
+/**
+ * Tests methods on the {@link ConceptDrugValidator} class.
+ */
+public class ConceptDrugValidatorTest {
+	
+	/**
+	 * @see ConceptDrugValidator#validate(Object,Errors)
+	 * @verifies fail if a concept is not specified
+	 */
+	@Test
+	public void validate_shouldFailIfAConceptIsNotSpecified() throws Exception {
+		Drug drug = new Drug();
+		Errors errors = new BindException(drug, "drug");
+		new ConceptDrugValidator().validate(drug, errors);
+		Assert.assertTrue(errors.hasErrors());
+	}
+
+	/**
+     * @see ConceptDrugValidator#supports(Class)
+     * @verifies reject classes not extending Drug
+     */
+    @Test
+    public void supports_shouldRejectClassesNotExtendingDrug() throws Exception {
+    	Assert.assertFalse(new ConceptDrugValidator().supports(String.class));
+    }
+
+	/**
+     * @see ConceptDrugValidator#supports(Class)
+     * @verifies support Drug class
+     */
+    @Test
+    public void supports_shouldSupportDrug() throws Exception {
+    	Assert.assertTrue(new ConceptDrugValidator().supports(Drug.class));
+    }
+	
+}
