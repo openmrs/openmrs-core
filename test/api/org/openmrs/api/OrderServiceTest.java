5229f95476699d2ddd67d39b5713a255dc4ce62e
Michael Seaton <mseaton@pih.org>
2011-02-23 19:46:35 +0000

    Backporting changes from TRUNK-1943: Add discontinuedReasonNonCoded to Order, to the 1.7.x branch line.
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.7.x@18365 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- test/api/org/openmrs/api/OrderServiceTest.java
+++ test/api/org/openmrs/api/OrderServiceTest.java
@@ -17,6 +17,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openmrs.Order;
 import org.openmrs.OrderType;
+import org.openmrs.api.OrderService;
 import org.openmrs.api.context.Context;
 import org.openmrs.test.BaseContextSensitiveTest;
 import org.openmrs.test.Verifies;
@@ -84,5 +85,24 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 	public void getOrderTypeByUuid_shouldReturnNullIfNoObjectFoundWithGivenUuid()
 			throws Exception {
 		Assert.assertNull(Context.getOrderService().getOrderTypeByUuid("some invalid uuid"));
+	}
+	
+	/**
+	 * @see {@link OrderService#saveOrder(Order)}
+	 */
+	@Test
+	@Verifies(value = "when saving a discontinuedReasonNonCoded parameter the value is correctly stored to the database", method = "saveOrder(Order)")
+	public void saveOrder_shouldSaveDiscontinuedReasonNonCoded() throws Exception {
+		String uuid = "921de0a3-05c4-444a-be03-e01b4c4b9142";
+		Order order = Context.getOrderService().getOrderByUuid(uuid);
+		String discontinuedReasonNonCoded = "Non coded discontinued reason";
+		
+		order.setDiscontinuedReasonNonCoded(discontinuedReasonNonCoded);
+		OrderService orderService = Context.getOrderService();
+		orderService.saveOrder(order);
+		
+		order = Context.getOrderService().getOrderByUuid(uuid);
+		
+		Assert.assertEquals(discontinuedReasonNonCoded, order.getDiscontinuedReasonNonCoded());
 	}
 }
