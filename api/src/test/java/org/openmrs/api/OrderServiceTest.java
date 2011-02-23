983287dcc59646da32aa073f4629abca5dec893a
Michael Seaton <mseaton@pih.org>
2011-02-23 19:25:03 +0000

    committing missing unit test which was meant to be added as a part of TRUNK-1943
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@18363 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -81,4 +81,23 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 	public void getOrderTypeByUuid_shouldReturnNullIfNoObjectFoundWithGivenUuid() throws Exception {
 		Assert.assertNull(Context.getOrderService().getOrderTypeByUuid("some invalid uuid"));
 	}
+	
+	/**
+	 * @see {@link OrderService#saveOrder(Order)}
+	 */
+	@Test
+	@Verifies(value = "when saving a discontinuedReasonNonCoded parameter the value is correctly stored to the database", method = "saveOrder(Order)")
+	public void saveOrder_shouldSaveDiscontinuedReasonNonCoded() throws Exception {
+		String uuid = "921de0a3-05c4-444a-be03-e01b4c4b9142";
+		Order order = Context.getOrderService().getOrderByUuid(uuid);
+		String discontinuedReasonNonCoded = "Non coded discontinued reason";
+		
+		order.setDiscontinuedReasonNonCoded(discontinuedReasonNonCoded);
+		OrderService orderService = Context.getOrderService();
+		orderService.saveOrder(order);
+		
+		order = Context.getOrderService().getOrderByUuid(uuid);
+		
+		Assert.assertEquals(discontinuedReasonNonCoded, order.getDiscontinuedReasonNonCoded());
+	}
 }
