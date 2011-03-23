f8d278fe37db3f41735b89ef557d42da47e95228
Mark Goodrich <mgoodrich@pih.org>
2011-03-23 17:59:00 +0000

    fix for TRUNK-2129: When saving voided Person PersonSaveHandler should make sure the personVoidReason field is not empty, *not* the (patient) voidReason
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@18850 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/api/handler/PersonSaveHandler.java
+++ api/src/main/java/org/openmrs/api/handler/PersonSaveHandler.java
@@ -93,7 +93,7 @@ public class PersonSaveHandler implements SaveHandler<Person> {
 		// do the checks for voided attributes (also in PersonVoidHandler)
 		if (person.isPersonVoided()) {
 			
-			if (!StringUtils.hasLength(person.getVoidReason()))
+			if (!StringUtils.hasLength(person.getPersonVoidReason()))
 				throw new APIException(
 				        "The voided bit was set to true, so a void reason is required at save time for person: " + person);
 			
