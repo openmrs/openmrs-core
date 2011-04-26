52ce93c4e404ee3da865b980dc48991850c614b7
Daniel Kayiwa <dkayiwa@openmrs.org>
2011-04-26 17:26:32 +0000

    Adding Managing Visit Attribute Types pages - TRUNK-2249
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@19649 5bac5841-c719-aa4e-b3fe-cce5062f897a

---
+++ web/src/main/java/org/openmrs/web/controller/visit/VisitAttributeTypeFormController.java
@@ -0,0 +1,129 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.web.controller.visit;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.VisitAttributeType;
+import org.openmrs.api.APIException;
+import org.openmrs.api.VisitService;
+import org.openmrs.api.context.Context;
+import org.openmrs.web.WebConstants;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+import org.springframework.web.servlet.view.RedirectView;
+
+/**
+ * Controller for editing visit attribute types.
+ * 
+ * @since 1.9
+ */
+public class VisitAttributeTypeFormController extends SimpleFormController {
+	
+	/** Logger for this class and subclasses */
+	protected final Log log = LogFactory.getLog(getClass());
+	
+	/**
+	 * The onSubmit function receives the form/command object that was modified by the input form
+	 * and saves it to the db
+	 * 
+	 * @see org.springframework.web.servlet.mvc.SimpleFormController#onSubmit(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse, java.lang.Object,
+	 *      org.springframework.validation.BindException)
+	 */
+	protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object obj,
+	        BindException errors) throws Exception {
+		
+		HttpSession httpSession = request.getSession();
+		
+		String view = getFormView();
+		
+		if (Context.isAuthenticated()) {
+			VisitAttributeType visitAttributeType = (VisitAttributeType) obj;
+			VisitService es = Context.getVisitService();
+			
+			if (request.getParameter("save") != null) {
+				es.saveVisitAttributeType(visitAttributeType);
+				view = getSuccessView();
+				httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "VisitAttributeType.saved");
+			}
+
+			// if the user is retiring out the VisitAttributeType
+			else if (request.getParameter("retire") != null) {
+				String retireReason = request.getParameter("retireReason");
+				if (visitAttributeType.getVisitAttributeTypeId() != null && !(StringUtils.hasText(retireReason))) {
+					errors.reject("retireReason", "general.retiredReason.empty");
+					return showForm(request, response, errors);
+				}
+				
+				es.retireVisitAttributeType(visitAttributeType, retireReason);
+				httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "VisitAttributeType.retiredSuccessfully");
+				
+				view = getSuccessView();
+			}
+
+			// if the user is purging the visitAttributeType
+			else if (request.getParameter("purge") != null) {
+				
+				try {
+					es.purgeVisitAttributeType(visitAttributeType);
+					httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "VisitAttributeType.purgedSuccessfully");
+					view = getSuccessView();
+				}
+				catch (DataIntegrityViolationException e) {
+					httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, "error.object.inuse.cannot.purge");
+					view = "visitAttributeType.form?visitAttributeTypeId=" + visitAttributeType.getVisitAttributeTypeId();
+				}
+				catch (APIException e) {
+					httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, "error.general: " + e.getLocalizedMessage());
+					view = "visitAttributeType.form?visitAttributeTypeId=" + visitAttributeType.getVisitAttributeTypeId();
+				}
+			}
+			
+		}
+		
+		return new ModelAndView(new RedirectView(view));
+	}
+	
+	/**
+	 * This is called prior to displaying a form for the first time. It tells Spring the
+	 * form/command object to load into the request
+	 * 
+	 * @see org.springframework.web.servlet.mvc.AbstractFormController#formBackingObject(javax.servlet.http.HttpServletRequest)
+	 */
+	protected Object formBackingObject(HttpServletRequest request) throws ServletException {
+		
+		VisitAttributeType visitAttributeType = null;
+		
+		if (Context.isAuthenticated()) {
+			VisitService os = Context.getVisitService();
+			String visitAttributeTypeId = request.getParameter("visitAttributeTypeId");
+			if (visitAttributeTypeId != null)
+				visitAttributeType = os.getVisitAttributeType(Integer.valueOf(visitAttributeTypeId));
+		}
+		
+		if (visitAttributeType == null)
+			visitAttributeType = new VisitAttributeType();
+		
+		return visitAttributeType;
+	}
+}
