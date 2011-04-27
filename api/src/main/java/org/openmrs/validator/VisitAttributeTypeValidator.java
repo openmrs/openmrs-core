0a7c99b51a19b3dae87506b1e066b4262fb0e1d5
Daniel Kayiwa <dkayiwa@openmrs.org>
2011-04-27 11:35:53 +0000

    Adding more minOccurs and maxOccurs validation to visit attribute types - TRUNK-2249
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@19677 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/validator/VisitAttributeTypeValidator.java
+++ api/src/main/java/org/openmrs/validator/VisitAttributeTypeValidator.java
@@ -54,11 +54,27 @@ public class VisitAttributeTypeValidator implements Validator {
 	public void validate(Object obj, Errors errors) {
 		VisitAttributeType visitAttributeType = (VisitAttributeType) obj;
 		if (visitAttributeType == null) {
-			errors.rejectValue("visitAttributeType", "error.general");
+			errors.reject("error.general");
 		} else {
 			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "error.name");
 			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "minOccurs", "error.null");
-			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "maxOccurs", "error.null");
+			
+			Integer minOccurs = visitAttributeType.getMinOccurs();
+			Integer maxOccurs = visitAttributeType.getMaxOccurs();
+			
+			if (minOccurs != null) {
+				if (minOccurs < 0) {
+					errors.rejectValue("minOccurs", "AttributeType.minOccursShouldNotBeLessThanZero");
+				}
+			}
+			
+			if (maxOccurs != null) {
+				if (maxOccurs < 0) {
+					errors.rejectValue("maxOccurs", "AttributeType.maxOccursShouldNotBeLessThanZero");
+				} else if (maxOccurs < minOccurs) {
+					errors.rejectValue("maxOccurs", "AttributeType.maxOccursShouldNotBeLessThanMinOccurs");
+				}
+			}
 		}
 	}
 }
