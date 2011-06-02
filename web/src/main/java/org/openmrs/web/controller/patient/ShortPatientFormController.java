c1551559fa2a92b86f0816267c755ef00ec80c31
Wyclif Luyima <wyclif@openmrs.org>
2011-06-02 04:59:20 +0000

    Backporting, Cannot edit 2 patient records in separate tabs on the short patient form - TRUNK-2325
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.8.x@20440 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/main/java/org/openmrs/web/controller/patient/ShortPatientFormController.java
+++ web/src/main/java/org/openmrs/web/controller/patient/ShortPatientFormController.java
@@ -23,9 +23,11 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Concept;
+import org.openmrs.Location;
 import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.PatientIdentifier;
+import org.openmrs.PatientIdentifierType;
 import org.openmrs.Person;
 import org.openmrs.PersonAddress;
 import org.openmrs.PersonAttribute;
@@ -50,8 +52,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.context.request.WebRequest;
 
 /**
@@ -62,8 +62,6 @@ import org.springframework.web.context.request.WebRequest;
  */
 
 @Controller
-@SessionAttributes( { "patientModel", "relationshipsMap", "identifierTypes", "locations", "personNameCache",
-        "personAddressCache" })
 public class ShortPatientFormController {
 	
 	private static final Log log = LogFactory.getLog(ShortPatientFormController.class);
@@ -78,93 +76,99 @@ public class ShortPatientFormController {
 	PatientValidator patientValidator;
 	
 	/**
-	 * Sets up the modelObject and data to be displayed in the the short patient form
-	 * 
-	 * @param patientId the patientId of the patient to edited
-	 * @param model modelObject to be used
-	 * @param request
-	 * @return the view to forward to
-	 * @should redirect to the shortPatientForm
+	 * @return
 	 */
 	@RequestMapping(method = RequestMethod.GET, value = SHORT_PATIENT_FORM_URL)
-	public String showForm(@RequestParam(value = "patientId", required = false) Integer patientId, ModelMap model,
-	                       WebRequest request) {
-		
-		if (Context.isAuthenticated()) {
-			Patient patient = null;
-			if (patientId != null) {
-				try {
-					patient = Context.getPatientService().getPatient(patientId);
-				}
-				catch (ClassCastException ex) {
-					// we're promoting an existing Person to a full Patient
-					// this will be handled in the next lines
-				}
-				if (patient == null) {
-					Person toPromote = Context.getPersonService().getPerson(patientId);
-					if (toPromote == null)
-						throw new IllegalArgumentException("No patient or person with the given id");
-					patient = new Patient(toPromote);
-				}
-			} else {
-				// we may have some details to add to a blank patient
-				patient = new Patient();
-				String name = request.getParameter("addName");
-				if (!StringUtils.isBlank(name)) {
-					String gender = request.getParameter("addGender");
-					String date = request.getParameter("addBirthdate");
-					String age = request.getParameter("addAge");
-					PersonFormController.getMiniPerson(patient, name, gender, date, age);
-				}
+	public void showForm() {
+	}
+	
+	@ModelAttribute("patientModel")
+	public ShortPatientModel getPatientModel(@RequestParam(value = "patientId", required = false) Integer patientId,
+	                                         ModelMap model, WebRequest request) {
+		Patient patient = null;
+		if (patientId != null) {
+			try {
+				patient = Context.getPatientService().getPatient(patientId);
 			}
-			
-			ShortPatientModel patientModel = new ShortPatientModel(patient);
-			model.addAttribute("patientModel", patientModel);
-			model.addAttribute("relationshipsMap", getRelationshipsMap(patient, request));
-			model.addAttribute("identifierTypes", Context.getPatientService().getAllPatientIdentifierTypes());
-			model.addAttribute("locations", Context.getLocationService().getAllLocations());
-			model.addAttribute("defaultLocation", (LocationUtility.getUserDefaultLocation() != null) ? LocationUtility
-			        .getUserDefaultLocation() : LocationUtility.getDefaultLocation());
-			
-			//if we have an existing personName, cache it in the session so that we can use it to 
-			//track changes in givenName, middleName, familyName, will also use it to restore the original values
-			if (patient.getPersonName() != null && patient.getPersonName().getId() != null)
-				model.addAttribute("personNameCache", PersonName.newInstance(patient.getPersonName()));
-			else
-				model.addAttribute("personNameCache", new PersonName());
-			
-			//store the person address copy in the session
-			if (patient.getPersonAddress() != null && patient.getPersonAddress().getId() != null)
-				model.addAttribute("personAddressCache", patient.getPersonAddress().clone());
-			else
-				model.addAttribute("personAddressCache", new PersonAddress());
-			
-			String propCause = Context.getAdministrationService().getGlobalProperty("concept.causeOfDeath");
-			Concept conceptCause = Context.getConceptService().getConcept(propCause);
-			String causeOfDeathOther = "";
-			if (conceptCause != null && patient.getPatientId() != null) {
-				List<Obs> obssDeath = Context.getObsService().getObservationsByPersonAndConcept(patient, conceptCause);
-				
-				if (obssDeath.size() == 1) {
-					Obs obsDeath = obssDeath.iterator().next();
-					causeOfDeathOther = obsDeath.getValueText();
-					if (causeOfDeathOther == null) {
-						log.debug("cod is null, so setting to empty string");
-						causeOfDeathOther = "";
-					} else {
-						log.debug("cod is valid: " + causeOfDeathOther);
-					}
-				} else {
-					log.debug("obssDeath is wrong size: " + obssDeath.size());
-				}
-			} else {
-				log.debug("No concept cause found");
+			catch (ClassCastException ex) {
+				// we're promoting an existing Person to a full Patient
+				// this will be handled in the next lines
 			}
-			// end get 'other' cause of death
-			model.addAttribute("causeOfDeathOther", causeOfDeathOther);
+			if (patient == null) {
+				Person toPromote = Context.getPersonService().getPerson(patientId);
+				if (toPromote == null)
+					throw new IllegalArgumentException("No patient or person with the given id");
+				patient = new Patient(toPromote);
+			}
+		} else {
+			// we may have some details to add to a blank patient
+			patient = new Patient();
+			String name = request.getParameter("addName");
+			if (!StringUtils.isBlank(name)) {
+				String gender = request.getParameter("addGender");
+				String date = request.getParameter("addBirthdate");
+				String age = request.getParameter("addAge");
+				PersonFormController.getMiniPerson(patient, name, gender, date, age);
+			}
 		}
 		
-		return SHORT_PATIENT_FORM_URL;
+		// if we have an existing personName, cache the original name so that we
+		// can use it to
+		// track changes in givenName, middleName, familyName, will also use
+		// it to restore the original values
+		if (patient.getPersonName() != null && patient.getPersonName().getId() != null)
+			model.addAttribute("personNameCache", PersonName.newInstance(patient.getPersonName()));
+		else
+			model.addAttribute("personNameCache", new PersonName());
+		
+		// cache a copy of the person address for comparison in case the name is
+		// edited
+		if (patient.getPersonAddress() != null && patient.getPersonAddress().getId() != null)
+			model.addAttribute("personAddressCache", patient.getPersonAddress().clone());
+		else
+			model.addAttribute("personAddressCache", new PersonAddress());
+		
+		String propCause = Context.getAdministrationService().getGlobalProperty("concept.causeOfDeath");
+		Concept conceptCause = Context.getConceptService().getConcept(propCause);
+		String causeOfDeathOther = "";
+		if (conceptCause != null && patient.getPatientId() != null) {
+			List<Obs> obssDeath = Context.getObsService().getObservationsByPersonAndConcept(patient, conceptCause);
+			
+			if (obssDeath.size() == 1) {
+				Obs obsDeath = obssDeath.iterator().next();
+				causeOfDeathOther = obsDeath.getValueText();
+				if (causeOfDeathOther == null) {
+					log.debug("cod is null, so setting to empty string");
+					causeOfDeathOther = "";
+				} else {
+					log.debug("cod is valid: " + causeOfDeathOther);
+				}
+			} else {
+				log.debug("obssDeath is wrong size: " + obssDeath.size());
+			}
+		} else {
+			log.debug("No concept cause found");
+		}
+		// end get 'other' cause of death
+		model.addAttribute("causeOfDeathOther", causeOfDeathOther);
+		
+		return new ShortPatientModel(patient);
+	}
+	
+	@ModelAttribute("locations")
+	public List<Location> getLocations() {
+		return Context.getLocationService().getAllLocations();
+	}
+	
+	@ModelAttribute("defaultLocation")
+	public Location getDefaultLocation() {
+		return (LocationUtility.getUserDefaultLocation() != null) ? LocationUtility.getUserDefaultLocation()
+		        : LocationUtility.getDefaultLocation();
+	}
+	
+	@ModelAttribute("identifierTypes")
+	public List<PatientIdentifierType> getIdentifierTypes() {
+		return Context.getPatientService().getAllPatientIdentifierTypes();
 	}
 	
 	/**
@@ -184,12 +188,14 @@ public class ShortPatientFormController {
 	 * @should add a new name if the person had no names
 	 * @should add a new address if the person had none
 	 * @should ignore a new address that was added and voided at same time
+	 * @should set the cause of death as none a coded concept
+	 * @should set the cause of death as a none coded concept
+	 * @should void the cause of death obs that is none coded
 	 */
 	@RequestMapping(method = RequestMethod.POST, value = SHORT_PATIENT_FORM_URL)
 	public String saveShortPatient(WebRequest request, @ModelAttribute("personNameCache") PersonName personNameCache,
 	                               @ModelAttribute("personAddressCache") PersonAddress personAddressCache,
-	                               @ModelAttribute("patientModel") ShortPatientModel patientModel, BindingResult result,
-	                               SessionStatus status) {
+	                               @ModelAttribute("patientModel") ShortPatientModel patientModel, BindingResult result) {
 		
 		if (Context.isAuthenticated()) {
 			// First do form validation so that we can easily bind errors to
@@ -204,15 +210,17 @@ public class ShortPatientFormController {
 			Errors patientErrors = new BindException(patient, "patient");
 			patientValidator.validate(patient, patientErrors);
 			if (patientErrors.hasErrors()) {
-				//bind the errors to the patientModel object by adding them to result since this is not a patient object
-				//so that spring doesn't try to look for getters/setters for Patient in ShortPatientModel
+				// bind the errors to the patientModel object by adding them to
+				// result since this is not a patient object
+				// so that spring doesn't try to look for getters/setters for
+				// Patient in ShortPatientModel
 				for (ObjectError error : patientErrors.getAllErrors())
 					result.reject(error.getCode(), error.getArguments(), "Validation errors found");
 				
 				return SHORT_PATIENT_FORM_URL;
 			}
 			
-			//check if name/address were edited, void them and replace them
+			// check if name/address were edited, void them and replace them
 			boolean foundChanges = hasPersonNameOrAddressChanged(patient, personNameCache, personAddressCache);
 			
 			try {
@@ -227,9 +235,10 @@ public class ShortPatientFormController {
 				
 				if (!patient.getVoided()) {
 					// save the relationships to the database
-					Map<String, Relationship> relationships = getRelationshipsMap(patient, request);
+					Map<String, Relationship> relationships = getRelationshipsMap(patientModel, request);
 					for (Relationship relationship : relationships.values()) {
-						// if the user added a person to this relationship, save it
+						// if the user added a person to this relationship, save
+						// it
 						if (relationship.getPersonA() != null && relationship.getPersonB() != null)
 							Context.getPersonService().saveRelationship(relationship);
 					}
@@ -239,21 +248,15 @@ public class ShortPatientFormController {
 				log.error("Error occurred while attempting to save patient", e);
 				request.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, Context.getMessageSourceService().getMessage(
 				    "Patient.save.error"), WebRequest.SCOPE_SESSION);
-				if (foundChanges) {
-					//TODO revert the changes and send them back to the form
-					
-					//Clear the session attributes and don't send the user back to the form
-					//because the created person name/addresses will be recreated over again
-					//if the user attempts to resubmit
-					status.setComplete();
-					return "redirect:" + PATIENT_DASHBOARD_URL + "?patientId=" + patient.getPatientId();
-				}
+				// TODO revert the changes and send them back to the form
 				
-				return SHORT_PATIENT_FORM_URL;
+				// don't send the user back to the form because the created
+				// person name/addresses
+				// will be recreated over again if the user attempts to resubmit
+				if (!foundChanges)
+					return SHORT_PATIENT_FORM_URL;
 			}
 			
-			// clear the session attributes
-			status.setComplete();
 			return "redirect:" + PATIENT_DASHBOARD_URL + "?patientId=" + patient.getPatientId();
 			
 		}
@@ -292,8 +295,10 @@ public class ShortPatientFormController {
 		
 		// add all the existing identifiers and any new ones.
 		for (PatientIdentifier id : patientModel.getIdentifiers()) {
-			//skip past the new ones removed from the user interface(may be they were invalid
-			//and the user changed their mind about adding them and they removed them)
+			// skip past the new ones removed from the user interface(may be
+			// they were invalid
+			// and the user changed their mind about adding them and they
+			// removed them)
 			if (id.getPatientIdentifierId() == null && id.isVoided())
 				continue;
 			
@@ -314,7 +319,10 @@ public class ShortPatientFormController {
 	 * @param request the webRequest Object
 	 * @return map of strings matched against actual relationships
 	 */
-	private Map<String, Relationship> getRelationshipsMap(Person person, WebRequest request) {
+	@ModelAttribute("relationshipsMap")
+	private Map<String, Relationship> getRelationshipsMap(@ModelAttribute("patientModel") ShortPatientModel patientModel,
+	                                                      WebRequest request) {
+		Person person = patientModel.getPatient();
 		Map<String, Relationship> relationshipMap = new LinkedHashMap<String, Relationship>();
 		
 		// gp is in the form "3a, 7b, 4a"
@@ -397,7 +405,6 @@ public class ShortPatientFormController {
 	 * @param request webRequest object
 	 */
 	private void saveDeathInfo(ShortPatientModel patientModel, WebRequest request) {
-		
 		// update the death reason
 		if (patientModel.getPatient().getDead()) {
 			log.debug("Patient is dead, so let's make sure there's an Obs for it");
@@ -506,22 +513,23 @@ public class ShortPatientFormController {
 		boolean foundChanges = false;
 		PersonName personName = patient.getPersonName();
 		if (personNameCache.getId() != null) {
-			//if the existing persoName has been edited
+			// if the existing persoName has been edited
 			if (!getPersonNameString(personName).equalsIgnoreCase(getPersonNameString(personNameCache))) {
 				if (log.isDebugEnabled())
 					log.debug("Voiding person name with id: " + personName.getId() + " and replacing it with a new one: "
 					        + personName.toString());
 				foundChanges = true;
-				//create a new one and copy the changes to it
+				// create a new one and copy the changes to it
 				PersonName newName = PersonName.newInstance(personName);
 				newName.setPersonNameId(null);
 				newName.setUuid(null);
-				newName.setChangedBy(null);//just in case it had a value
+				newName.setChangedBy(null);// just in case it had a value
 				newName.setDateChanged(null);
 				newName.setCreator(Context.getAuthenticatedUser());
 				newName.setDateCreated(new Date());
 				
-				//restore the given,middle and familyName, then void the old name
+				// restore the given,middle and familyName, then void the old
+				// name
 				personName.setGivenName(personNameCache.getGivenName());
 				personName.setMiddleName(personNameCache.getMiddleName());
 				personName.setFamilyName(personNameCache.getFamilyName());
@@ -531,7 +539,7 @@ public class ShortPatientFormController {
 				    new Object[] { newName.toString() }, "Voided because it was edited to: " + newName.toString(),
 				    Context.getLocale()));
 				
-				//add the created name
+				// add the created name
 				patient.addName(newName);
 			}
 		}
@@ -539,7 +547,7 @@ public class ShortPatientFormController {
 		PersonAddress personAddress = patient.getPersonAddress();
 		if (personAddress != null) {
 			if (personAddressCache.getId() != null) {
-				//if the existing personAddress has been edited
+				// if the existing personAddress has been edited
 				if (!personAddress.isBlank() && !personAddressCache.isBlank()
 				        && !personAddress.toString().equalsIgnoreCase(personAddressCache.toString())) {
 					if (log.isDebugEnabled())
@@ -547,16 +555,17 @@ public class ShortPatientFormController {
 						        + " and replacing it with a new one: " + personAddress.toString());
 					
 					foundChanges = true;
-					//create a new one and copy the changes to it
+					// create a new one and copy the changes to it
 					PersonAddress newAddress = (PersonAddress) personAddress.clone();
 					newAddress.setPersonAddressId(null);
 					newAddress.setUuid(null);
-					newAddress.setChangedBy(null);//just in case it had a value
+					newAddress.setChangedBy(null);// just in case it had a value
 					newAddress.setDateChanged(null);
 					newAddress.setCreator(Context.getAuthenticatedUser());
 					newAddress.setDateCreated(new Date());
 					
-					//restore address fields that are checked for changes and void the address
+					// restore address fields that are checked for changes and
+					// void the address
 					personAddress.setAddress1(personAddressCache.getAddress1());
 					personAddress.setAddress2(personAddressCache.getAddress2());
 					personAddress.setAddress3(personAddressCache.getAddress3());
@@ -574,7 +583,7 @@ public class ShortPatientFormController {
 					    "general.voidReasonWithArgument", new Object[] { newAddress.toString() },
 					    "Voided because it was edited to: " + newAddress.toString(), Context.getLocale()));
 					
-					//Add the created one
+					// Add the created one
 					patient.addAddress(newAddress);
 				}
 			}
