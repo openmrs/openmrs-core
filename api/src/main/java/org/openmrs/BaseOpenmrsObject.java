cba527d89f5b8941f045efadf3afc73aa977e7b5
Rafal Korytkowski <rafal@openmrs.org>
2011-06-16 11:32:39 +0000

    IN PROGRESS - issue TRUNK-1993: UUID used by equals and hashCode in OpenmrsObject https://tickets.openmrs.org/browse/TRUNK-1993
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@20756 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/BaseOpenmrsObject.java
+++ api/src/main/java/org/openmrs/BaseOpenmrsObject.java
@@ -13,13 +13,15 @@
  */
 package org.openmrs;
 
+import java.util.UUID;
+
 /**
  * This is the base implementation of the {@link OpenmrsObject} interface.<br/>
  * It implements the uuid variable that all objects are expected to have.
  */
 public abstract class BaseOpenmrsObject implements OpenmrsObject {
 	
-	private String uuid;
+	private String uuid = UUID.randomUUID().toString();
 	
 	/**
 	 * @see org.openmrs.OpenmrsObject#getUuid()
@@ -35,4 +37,63 @@ public abstract class BaseOpenmrsObject implements OpenmrsObject {
 		this.uuid = uuid;
 	}
 	
+	/**
+	 * Returns a hash code based on the <code>uuid</code> field.
+	 * <p>
+	 * If the <code>uuid</code> field is <code>null</code>, it delegates to
+	 * {@link Object#hashCode()}.
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		if (uuid == null)
+			return super.hashCode();
+		return uuid.hashCode();
+	}
+	
+	/**
+	 * Returns <code>true</code> if and only if <code>x</code> and <code>y</code> refer to the same
+	 * object (<code>x == y</code> has the value <code>true</code>) or both have the same
+	 * <code>uuid</code> (<code>((x.uuid != null) && x.uuid.equals(y.uuid))</code> has the value
+	 * <code>true</code>).
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof BaseOpenmrsObject))
+			return false;
+		BaseOpenmrsObject other = (BaseOpenmrsObject) obj;
+		if (uuid == null)
+			return false;
+		return uuid.equals(other.uuid);
+	}
+	
+	/**
+	 * Returns a string consisting of the name of the class of which the object is an instance and
+	 * the <code>uuid</code> field surrounded by <code>[</code> and <code>]</code>. In other words,
+	 * this method returns a string equal to the value of: <blockquote>
+	 * 
+	 * <pre>
+	 * getClass().getName() + '[' + uuid + ']'
+	 * </pre>
+	 * 
+	 * </blockquote>
+	 * <p>
+	 * If the <code>uuid</code> field is <code>null</code>, it delegates to
+	 * {@link Object#toString()}
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		if (uuid != null) {
+			return getClass().getName() + "[" + uuid + "]";
+		} else {
+			return super.toString();
+		}
+	}
 }
