e7bc41edefc645ed5314fb19c64a5ad2bcf935f1
Rafal Korytkowski <rafal@openmrs.org>
2011-07-18 09:51:24 +0000

    IN PROGRESS - issue TRUNK-2433: AdministrationService.saveGlobalProperties is dangerous, and should be changed https://tickets.openmrs.org/browse/TRUNK-2433
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@21675 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/main/java/org/openmrs/web/controller/maintenance/GlobalPropertyController.java
+++ web/src/main/java/org/openmrs/web/controller/maintenance/GlobalPropertyController.java
@@ -56,6 +56,8 @@ public class GlobalPropertyController extends SimpleFormController {
 	 * @see org.springframework.web.servlet.mvc.SimpleFormController#onSubmit(javax.servlet.http.HttpServletRequest,
 	 *      javax.servlet.http.HttpServletResponse, java.lang.Object,
 	 *      org.springframework.validation.BindException)
+	 * @should save or update included properties
+	 * @should purge not included properties
 	 */
 	@SuppressWarnings("unchecked")
 	protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object obj,
@@ -106,6 +108,11 @@ public class GlobalPropertyController extends SimpleFormController {
 				}
 				
 				try {
+					// delete all properties not in this new list
+					List<GlobalProperty> purgeGlobalPropList = new ArrayList<GlobalProperty>(as.getAllGlobalProperties());
+					purgeGlobalPropList.removeAll(globalPropList);
+					as.purgeGlobalProperties(purgeGlobalPropList);
+					
 					as.saveGlobalProperties(globalPropList);
 					httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "GlobalProperty.saved");
 					
