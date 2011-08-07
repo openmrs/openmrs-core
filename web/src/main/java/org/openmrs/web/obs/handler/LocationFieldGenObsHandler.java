26e8685ac8a8f52679bfe252d4bbedd16511b937
Suranga Nath Kasthurirathne <surangakas@gmail.com>
2011-08-07 17:56:42 +0000

    Meaningful comments for LocationFieldGenObsHandler (overlooked earlier)
    git-svn-id: http://svn.openmrs.org/openmrs/branches/obs-codes-expanded@22247 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/main/java/org/openmrs/web/obs/handler/LocationFieldGenObsHandler.java
+++ web/src/main/java/org/openmrs/web/obs/handler/LocationFieldGenObsHandler.java
@@ -18,12 +18,29 @@ import org.openmrs.obs.handler.LocationHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.core.annotation.Order;
 
+/**
+ * Handler for storing Location objects as the value for Complex Observations. 
+ * The Location Id of each Location object is stored in the value_complex column of the 
+ * Obs table. This value may be preceded by an optional displayable value. 
+ * 
+ * This class extends org.openmrs.obs.handler.LocationHandler
+ * for LocationHandlers. You may also consider extending
+ * Locationhandler class to meet your requirements. 
+ * 
+ * There may be several classes which extend
+ * LocationHandler. Out of these, only one will be loaded by Spring. The class to be loaded will be
+ * decided based on the @Order annotation value. 
+ * 
+ * As default, LocationHandler will have the lowest possible
+ * priority, and will be overridden by LocationFieldGenObsHandler, which lets us use fieldGen tags
+ * for data entry.
+ */
 @Component
 @Order(0)
 public class LocationFieldGenObsHandler extends LocationHandler implements FieldGenObsHandler {
 	
-	/* (non-Javadoc)
-	 * @see org.openmrs.web.obs.handler.FieldGenObsHandler#getWidgetName()
+	/**
+	 * Method returns widgetName to be used by fieldGen tags
 	 */
 	@Override
 	public String getWidgetName() {
