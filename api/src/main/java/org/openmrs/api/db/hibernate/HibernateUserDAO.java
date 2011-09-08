f293c93a434d4e4f701074ae4f072160796bb5cb
Saptarshi Purkayastha <sunbiz@gmail.com>
2011-09-08 20:10:07 +0000

    TRUNK-1925: Changes to DAO to be db-independent
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@23228 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/api/db/hibernate/HibernateUserDAO.java
+++ api/src/main/java/org/openmrs/api/db/hibernate/HibernateUserDAO.java
@@ -97,7 +97,7 @@ public class HibernateUserDAO implements UserDAO {
 	@SuppressWarnings("unchecked")
 	public User getUserByUsername(String username) {
 		Query query = sessionFactory.getCurrentSession().createQuery(
-		    "from User u where u.retired = 0 and (u.username = ? or u.systemId = ?)");
+		    "from User u where u.retired = '0' and (u.username = ? or u.systemId = ?)");
 		query.setString(0, username);
 		query.setString(1, username);
 		List<User> users = query.list();
