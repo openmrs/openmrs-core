650171da6be75ab0b638e8b11261c0249a609c5c
Rafal Korytkowski <rafal@openmrs.org>
2011-09-27 12:14:05 +0000

    IN PROGRESS - issue TRUNK-2295: Voiding a visit should ask for confirmation, and show how many dependent object will be deleted https://tickets.openmrs.org/browse/TRUNK-2295
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@23666 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/main/java/org/openmrs/web/controller/visit/VisitFormController.java
+++ web/src/main/java/org/openmrs/web/controller/visit/VisitFormController.java
@@ -71,7 +71,7 @@ public class VisitFormController {
 	
 	@ModelAttribute("visit")
 	public Visit getVisit(@RequestParam(value = "visitId", required = false) Integer visitId,
-	        @RequestParam(value = "patientId", required = false) Integer patientId, ModelMap model) {
+	                      @RequestParam(value = "patientId", required = false) Integer patientId, ModelMap model) {
 		Visit visit = null;
 		if (visitId != null)
 			visit = Context.getVisitService().getVisit(visitId);
@@ -82,6 +82,28 @@ public class VisitFormController {
 		}
 		
 		return visit;
+	}
+	
+	@ModelAttribute("encountersByVisitCount")
+	public Integer getEncountersByVisitCount(@ModelAttribute("visit") Visit visit) {
+		if (visit != null && visit.getId() != null) {
+			List<Encounter> encountersByVisit = Context.getEncounterService().getEncountersByVisit(visit, false);
+			return encountersByVisit.size();
+		}
+		return null;
+	}
+	
+	@ModelAttribute("obsByVisitCount")
+	public Integer getObsByVisitCount(@ModelAttribute("visit") Visit visit) {
+		if (visit != null && visit.getId() != null) {
+			List<Encounter> encountersByVisit = Context.getEncounterService().getEncountersByVisit(visit, false);
+			int obsByVisitCount = 0;
+			for (Encounter encounter : encountersByVisit) {
+	            obsByVisitCount += encounter.getObs().size();
+            }
+			return obsByVisitCount;
+		}
+		return null;
 	}
 	
 	/**
@@ -96,7 +118,7 @@ public class VisitFormController {
 	@SuppressWarnings("unchecked")
 	@RequestMapping(method = RequestMethod.POST, value = VISIT_FORM_URL)
 	public String saveVisit(HttpServletRequest request, @ModelAttribute("visit") Visit visit, BindingResult result,
-	        ModelMap model) {
+	                        ModelMap model) {
 		
 		String[] ids = ServletRequestUtils.getStringParameters(request, "encounterIds");
 		List<Integer> encounterIds = new ArrayList<Integer>();
@@ -176,7 +198,8 @@ public class VisitFormController {
 	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/admin/visits/voidVisit")
 	public String voidVisit(WebRequest request, @ModelAttribute(value = "visit") Visit visit,
-	        @RequestParam(required = false, value = "voidReason") String voidReason, SessionStatus status, ModelMap model) {
+	                        @RequestParam(required = false, value = "voidReason") String voidReason, SessionStatus status,
+	                        ModelMap model) {
 		
 		if (!StringUtils.hasText(voidReason))
 			voidReason = Context.getMessageSourceService().getMessage("general.default.voidReason");
@@ -191,8 +214,8 @@ public class VisitFormController {
 		}
 		catch (APIException e) {
 			log.warn("Error occurred while attempting to void visit", e);
-			request.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, Context.getMessageSourceService().getMessage(
-			    "Visit.void.error"), WebRequest.SCOPE_SESSION);
+			request.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
+			    Context.getMessageSourceService().getMessage("Visit.void.error"), WebRequest.SCOPE_SESSION);
 		}
 		
 		return VISIT_FORM;
@@ -209,20 +232,20 @@ public class VisitFormController {
 	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/admin/visits/unvoidVisit")
 	public String unvoidVisit(WebRequest request, @ModelAttribute(value = "visit") Visit visit, SessionStatus status,
-	        ModelMap model) {
+	                          ModelMap model) {
 		
 		try {
 			Context.getVisitService().unvoidVisit(visit);
 			if (log.isDebugEnabled())
 				log.debug("Unvoided visit with id: " + visit.getId());
-			request.setAttribute(WebConstants.OPENMRS_MSG_ATTR, Context.getMessageSourceService().getMessage(
-			    "Visit.unvoided"), WebRequest.SCOPE_SESSION);
+			request.setAttribute(WebConstants.OPENMRS_MSG_ATTR,
+			    Context.getMessageSourceService().getMessage("Visit.unvoided"), WebRequest.SCOPE_SESSION);
 			return "redirect:" + VISIT_FORM_URL + ".form?visitId=" + visit.getVisitId();
 		}
 		catch (APIException e) {
 			log.warn("Error occurred while attempting to unvoid visit", e);
-			request.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, Context.getMessageSourceService().getMessage(
-			    "Visit.unvoid.error"), WebRequest.SCOPE_SESSION);
+			request.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
+			    Context.getMessageSourceService().getMessage("Visit.unvoid.error"), WebRequest.SCOPE_SESSION);
 		}
 		
 		return VISIT_FORM;
@@ -239,7 +262,7 @@ public class VisitFormController {
 	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/admin/visits/purgeVisit")
 	public String purgeVisit(WebRequest request, @ModelAttribute(value = "visit") Visit visit, SessionStatus status,
-	        ModelMap model) {
+	                         ModelMap model) {
 		try {
 			Integer patientId = visit.getPatient().getPatientId();
 			Context.getVisitService().purgeVisit(visit);
@@ -251,8 +274,8 @@ public class VisitFormController {
 		}
 		catch (APIException e) {
 			log.warn("Error occurred while attempting to purge visit", e);
-			request.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, Context.getMessageSourceService().getMessage(
-			    "Visit.purge.error"), WebRequest.SCOPE_SESSION);
+			request.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
+			    Context.getMessageSourceService().getMessage("Visit.purge.error"), WebRequest.SCOPE_SESSION);
 		}
 		//there was an exception thrown
 		return "redirect:" + VISIT_FORM_URL + ".form?visitId=" + visit.getVisitId();
