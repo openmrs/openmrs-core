3a05214e5af7dfcea97fd9d17e1364af50fd4db4
Wyclif Luyima <wyclif@openmrs.org>
2011-09-27 07:59:30 +0000

    Fix tests with @Ignore in ConceptServiceTest - TRUNK-1956
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@23658 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/api/db/hibernate/HibernateConceptDAO.java
+++ api/src/main/java/org/openmrs/api/db/hibernate/HibernateConceptDAO.java
@@ -1549,11 +1549,14 @@ public class HibernateConceptDAO implements ConceptDAO {
 			pl.add(Projections.distinct(Projections.groupProperty("cw1.concept")));
 			pl.add(Projections.property("cw1.word"));
 			//if we have multiple words for the same concept, get the one with a highest weight
-			pl.add(Projections.max("cw1.weight"));
-			pl.add(Projections.property("cw1.conceptName"));
-			
+			pl.add(Projections.max("cw1.weight"), "maxWeight");
+			//TODO In case a concept has multiple names that contains words that match the search phrase, 
+			//setting this to min or max will select the concept name that was added first or last,
+			//but it should actually be the one that contains the word with the highest weight.
+			//see ConceptServiceTest.getConcepts_shouldReturnASearchResultWhoseConceptNameContainsAWordWithMoreWeight()
+			pl.add(Projections.min("cw1.conceptName"));
 			searchCriteria.setProjection(pl);
-			searchCriteria.addOrder(Order.desc("cw1.weight"));
+			searchCriteria.addOrder(Order.desc("maxWeight"));
 			
 			if (start != null)
 				searchCriteria.setFirstResult(start);
