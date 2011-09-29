213595b12770344d198de62741a736ef0987bfe1
Wyclif Luyima <wyclif@openmrs.org>
2011-09-29 01:00:24 +0000

    applying auto formatting
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@23705 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/annotation/AllowEmptyStrings.java
+++ api/src/main/java/org/openmrs/annotation/AllowEmptyStrings.java
@@ -20,7 +20,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
 /**
  * Annotation used to indicate that a method allows empty strings.
  * 
