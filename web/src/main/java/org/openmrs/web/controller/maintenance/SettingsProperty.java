dcdff3b3c46a3ef8227de8ce94261e52acd82e84
Rafal Korytkowski <rafal@openmrs.org>
2011-09-29 13:30:13 +0000

    IN PROGRESS - issue TRUNK-438: Add a "Settings" page and Rename Manage Global Properties page https://tickets.openmrs.org/browse/TRUNK-438
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@23715 5bac5841-c719-aa4e-b3fe-cce5062f897a

---
+++ web/src/main/java/org/openmrs/web/controller/maintenance/SettingsProperty.java
@@ -0,0 +1,136 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.web.controller.maintenance;
+
+import org.apache.commons.lang.StringUtils;
+import org.openmrs.GlobalProperty;
+
+/**
+ * Wrapper for {@link GlobalProperty} used in {@link SettingsForm}.
+ */
+public class SettingsProperty implements Comparable<SettingsProperty> {
+	
+	public static final String GENERAL = "...";
+	
+	private GlobalProperty globalProperty;
+	
+	public SettingsProperty(GlobalProperty globalProperty) {
+		this.globalProperty = globalProperty;
+	}
+	
+	/**
+	 * @return the section
+	 */
+	public String getSection() {
+		String section = GENERAL;
+		int sectionEnd = globalProperty.getProperty().indexOf(".");
+		if (sectionEnd > 0) {
+			section = globalProperty.getProperty().substring(0, sectionEnd);
+			section = beautify(section);
+		}
+		
+		return section;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		String name = globalProperty.getProperty();
+		int sectionEnd = globalProperty.getProperty().indexOf(".");
+		if (sectionEnd > 0) {
+			name = globalProperty.getProperty().substring(sectionEnd + 1);
+		}
+		
+		name = beautify(name);
+		
+		return name;
+	}
+	
+	/**
+	 * @return the globalProperty
+	 */
+	public GlobalProperty getGlobalProperty() {
+		return globalProperty;
+	}
+	
+	/**
+	 * @param globalProperty the globalProperty to set
+	 */
+	public void setGlobalProperty(GlobalProperty globalProperty) {
+		this.globalProperty = globalProperty;
+	}
+	
+	/**
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	@Override
+	public int compareTo(SettingsProperty o) {
+		return globalProperty.getProperty().compareTo(o.globalProperty.getProperty());
+	}
+	
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((globalProperty.getProperty() == null) ? 0 : globalProperty.getProperty().hashCode());
+		return result;
+	}
+	
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SettingsProperty other = (SettingsProperty) obj;
+		if (globalProperty.getProperty() == null) {
+			if (other.globalProperty.getProperty() != null)
+				return false;
+		} else if (!globalProperty.getProperty().equals(other.globalProperty.getProperty()))
+			return false;
+		return true;
+	}
+	
+	/**
+	 * Beautifies string
+	 * 
+	 * @param section
+	 * @return
+	 */
+	private String beautify(String section) {
+		section = section.replace("_", " ");
+		section = section.replace(".", " ");
+		
+		String[] sections = StringUtils.splitByCharacterTypeCamelCase(section);
+		section = StringUtils.join(sections, " ");
+		
+		sections = StringUtils.split(section);
+		for (int i = 0; i < sections.length; i++) {
+			sections[i] = StringUtils.capitalize(sections[i]);
+		}
+		section = StringUtils.join(sections, " ");
+		
+		return section;
+	}
+	
+}
