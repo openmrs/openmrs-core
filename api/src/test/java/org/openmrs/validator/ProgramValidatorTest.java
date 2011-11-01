16e3eedc0d3d1848eb44a937160c5e9d8efdf457
Kishore Kumar Yekkanti <kishoreyekkanti@gmail.com>
2011-11-01 07:33:40 +0000

    TRUNK-2799 - Missing description when saving program
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@24209 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/test/java/org/openmrs/validator/ProgramValidatorTest.java
+++ api/src/test/java/org/openmrs/validator/ProgramValidatorTest.java
@@ -53,6 +53,21 @@ public class ProgramValidatorTest extends BaseContextSensitiveTest {
 		new ProgramValidator().validate(prog, errors);
 		Assert.assertTrue(errors.hasFieldErrors("name"));
 	}
+
+    /**
+	 * @see {@link ProgramValidator#validate(Object,Errors)}
+	 */
+	@Test
+	@Verifies(value = "should fail validation if description is null or empty or whitespace", method = "validate(Object,Errors)")
+	public void validate_shouldFailValidationIfDescriptionIsNullOrEmptyOrWhitespace() throws Exception {
+		Program prog = new Program();
+		prog.setName("Hypochondriasis program");
+		prog.setConcept(Context.getConceptService().getConcept(3));
+
+		Errors errors = new BindException(prog, "prog");
+		new ProgramValidator().validate(prog, errors);
+		Assert.assertTrue(errors.hasFieldErrors("description"));
+	}
 	
 	/**
 	 * @see {@link ProgramValidator#validate(Object,Errors)}
@@ -77,6 +92,7 @@ public class ProgramValidatorTest extends BaseContextSensitiveTest {
 	public void validate_shouldPassValidationIfAllRequiredFieldsHaveProperValues() throws Exception {
 		Program prog = new Program();
 		prog.setName("Hypochondriasis program");
+        prog.setDescription("This is Hypochondriasis program");
 		prog.setConcept(Context.getConceptService().getConcept(3));
 		
 		Errors errors = new BindException(prog, "prog");
