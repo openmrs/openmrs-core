8dba5edf9929087a4245e70a005b962b5fac58b4
Ben Wolfe <ben@openmrs.org>
2011-11-08 12:08:23 +0000

    Patient is unintentionally deleted while replacing his/her drug regimen from the regimen tab of the dashboard - TRUNK-2674
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.6.x@24341 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/aop/RequiredDataAdvice.java
+++ src/api/org/openmrs/aop/RequiredDataAdvice.java
@@ -101,20 +101,21 @@ public class RequiredDataAdvice implements MethodBeforeAdvice {
 	public void before(Method method, Object[] args, Object target) throws Throwable {
 		String methodName = method.getName();
 		
+		// skip out early if there are no arguments
+		if (args == null || args.length == 0)
+			return;
+		
+		Object mainArgument = args[0];
+		
+		// fail early on a null parameter
+		if (mainArgument == null)
+			return;
+		
 		// the "create" is there to cover old deprecated methods since AOP doesn't occur
 		// on method calls within a class, only on calls to methods from external classes to methods
 		// "update" is not an option here because there are multiple methods that start with "update" but is
 		// not updating the primary argument. eg: ConceptService.updateConceptWord(Concept)
 		if (methodName.startsWith("save") || methodName.startsWith("create")) {
-			// skip out early if there are no arguments
-			if (args == null || args.length == 0)
-				return;
-			
-			Object mainArgument = args[0];
-			
-			// fail early on a null parameter
-			if (mainArgument == null)
-				return;
 			
 			Class<?> argClass = mainArgument.getClass();
 			
@@ -139,28 +140,34 @@ public class RequiredDataAdvice implements MethodBeforeAdvice {
 				
 			}
 			
-		} else if (methodName.startsWith("void")) {
-			Voidable voidable = (Voidable) args[0];
-			String voidReason = (String) args[1];
-			recursivelyHandle(VoidHandler.class, voidable, voidReason);
+		} else {
+			// fail early if the method name is not like retirePatient or voidConcept when dealing 
+			// with Patients or Concepts as the first argument
+			if (!method.getName().endsWith(mainArgument.getClass().getSimpleName()))
+				return;
 			
-		} else if (methodName.startsWith("unvoid")) {
-			Voidable voidable = (Voidable) args[0];
-			Date originalDateVoided = voidable.getDateVoided();
-			recursivelyHandle(UnvoidHandler.class, voidable, Context.getAuthenticatedUser(), originalDateVoided, null, null);
-			
-		} else if (methodName.startsWith("retire")) {
-			Retireable retirable = (Retireable) args[0];
-			String retireReason = (String) args[1];
-			recursivelyHandle(RetireHandler.class, retirable, retireReason);
-			
-		} else if (methodName.startsWith("unretire")) {
-			Retireable retirable = (Retireable) args[0];
-			Date originalDateRetired = retirable.getDateRetired();
-			recursivelyHandle(UnretireHandler.class, retirable, Context.getAuthenticatedUser(), originalDateRetired, null, null);
-			
+			if (methodName.startsWith("void")) {
+				Voidable voidable = (Voidable) args[0];
+				String voidReason = (String) args[1];
+				recursivelyHandle(VoidHandler.class, voidable, voidReason);
+				
+			} else if (methodName.startsWith("unvoid")) {
+				Voidable voidable = (Voidable) args[0];
+				Date originalDateVoided = voidable.getDateVoided();
+				recursivelyHandle(UnvoidHandler.class, voidable, Context.getAuthenticatedUser(), originalDateVoided, null, null);
+				
+			} else if (methodName.startsWith("retire")) {
+				Retireable retirable = (Retireable) args[0];
+				String retireReason = (String) args[1];
+				recursivelyHandle(RetireHandler.class, retirable, retireReason);
+				
+			} else if (methodName.startsWith("unretire")) {
+				Retireable retirable = (Retireable) args[0];
+				Date originalDateRetired = retirable.getDateRetired();
+				recursivelyHandle(UnretireHandler.class, retirable, Context.getAuthenticatedUser(), originalDateRetired, null, null);
+				
+			}
 		}
-		
 	}
 	
 	/**
