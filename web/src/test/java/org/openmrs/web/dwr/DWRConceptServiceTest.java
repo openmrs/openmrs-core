5ef074c039d0341ccd5442df3f44ab09d5f34320
Darius Jazayeri <jazayeri@alum.mit.edu>
2011-11-22 18:53:34 +0000

    Followup fix for TRUNK-2807 (unit test data was in the wrong folder), and formatting
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@24563 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/test/java/org/openmrs/web/dwr/DWRConceptServiceTest.java
+++ web/src/test/java/org/openmrs/web/dwr/DWRConceptServiceTest.java
@@ -10,13 +10,14 @@ public class DWRConceptServiceTest extends BaseContextSensitiveTest {
 	public void before() throws Exception {
 		executeDataSet("org/openmrs/web/dwr/include/DWRConceptServiceTest-coded-concept-with-no-answers.xml");
 	}
-
+	
 	/**
 	 * @see DWRConceptService#findConceptAnswers(String,Integer,boolean,boolean)
 	 * @verifies not fail if the specified concept has no answers (regression test for TRUNK-2807)
 	 */
 	@Test
-	public void findConceptAnswers_shouldNotFailIfTheSpecifiedConceptHasNoAnswersRegressionTestForTRUNK2807() throws Exception {
+	public void findConceptAnswers_shouldNotFailIfTheSpecifiedConceptHasNoAnswersRegressionTestForTRUNK2807()
+	        throws Exception {
 		new DWRConceptService().findConceptAnswers("", 1000, false, true);
 		// if we got here, we've passed, because we didn't get a NullPointerException
 	}
