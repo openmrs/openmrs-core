92a09b811e47ca8f452dbdbe1ed89266c380c4f3
Wyclif Luyima <wyclif@openmrs.org>
2011-11-22 22:47:55 +0000

    Back porting, Delete Forever error should have better language - TRUNK-2847
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.9.x@24566 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/api/impl/VisitServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/VisitServiceImpl.java
@@ -182,7 +182,6 @@ public class VisitServiceImpl extends BaseOpenmrsService implements VisitService
 	public void purgeVisit(Visit visit) throws APIException {
 		if (visit.getVisitId() == null)
 			return;
-		//TODO there is a ticket for adding includeVoided argument to getEncountersByVisit for this not to fail
 		if (Context.getEncounterService().getEncountersByVisit(visit, true).size() > 0)
 			throw new APIException(Context.getMessageSourceService().getMessage("Visit.purge.inUse", null,
 			    "Cannot purge a visit that has encounters associated to it", Context.getLocale()));
