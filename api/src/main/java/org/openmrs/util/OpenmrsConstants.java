b1b81c9a04ecb81b238bc30f60e9d560820d41a4
Wyclif Luyima <wyclif@openmrs.org>
2011-12-01 03:44:49 +0000

    Back porting, Fixing description of concept_map_type_management.enable global property
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.9.x@24672 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/util/OpenmrsConstants.java
+++ api/src/main/java/org/openmrs/util/OpenmrsConstants.java
@@ -1309,7 +1309,7 @@ public final class OpenmrsConstants {
 		                "Specifies how patient names are matched while searching patient. Valid values are 'ANYWHERE' or 'START'. Defaults to start if missing or invalid value is present."));
 		
 		props.add(new GlobalProperty(GP_ENABLE_CONCEPT_MAP_TYPE_MANAGEMENT, "false",
-		        "Enable or disables management of concept map types", Boolean.class, null));
+		        "Enables or disables management of concept map types", Boolean.class, null));
 		
 		props
 		        .add(new GlobalProperty(
