26b7bef77a0c21ee3de26860a2d1dc71870449a5
Ben Wolfe <ben@openmrs.org>
2011-12-05 07:50:12 +0000

    Backporting HibernateEnumType not finding module methods - TRUNK-2907
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.7.x@24720 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- src/api/org/openmrs/util/HibernateEnumType.java
+++ src/api/org/openmrs/util/HibernateEnumType.java
@@ -25,11 +25,14 @@ import org.hibernate.HibernateException;
 import org.hibernate.MappingException;
 import org.hibernate.usertype.ParameterizedType;
 import org.hibernate.usertype.UserType;
+import org.openmrs.api.context.Context;
 
 /**
  * This is intended to be a temporary utility class until enums are handled properly in Hibernate:
  * http://community.jboss.org/wiki/Java5EnumUserType
  * http://docs.jboss.org/hibernate/stable/core/reference/en/html_single/#inheritance
+ * 
+ * @since 1.7
  */
 public class HibernateEnumType implements UserType, ParameterizedType {
 	
@@ -44,7 +47,7 @@ public class HibernateEnumType implements UserType, ParameterizedType {
 		}
   
 	  	try {
-			this.clazz = Class.forName(enumClassName);
+			this.clazz = Context.loadClass(enumClassName);
 		}
 		catch (ClassNotFoundException e) {
 			throw new MappingException("enumClass " + enumClassName + " not found", e);
