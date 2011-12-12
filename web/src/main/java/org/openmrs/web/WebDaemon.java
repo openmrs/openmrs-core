ec690cd619935347855950e7568b79a77bbafd5e
Daniel Kayiwa <dkayiwa@openmrs.org>
2011-12-12 10:12:29 +0000

    Back porting to 1.9.x: AuditableInterceptor throws an NPE at startup, preventing OpenMRS from running - TRUNK-2917
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.9.x@24873 5bac5841-c719-aa4e-b3fe-cce5062f897a

---
+++ web/src/main/java/org/openmrs/web/WebDaemon.java
@@ -0,0 +1,69 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.web;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.api.context.Daemon;
+import org.openmrs.module.ModuleException;
+
+/**
+ * This class provides {@link Daemon} functionality in a web context.
+ * 
+ * @since 1.9
+ */
+public class WebDaemon extends Daemon {
+	
+	/**
+	 * Start openmrs in a new thread that is authenticated as the daemon user.
+	 * 
+	 * @param servletContext the servlet context.
+	 */
+	public static void startOpenmrs(final ServletContext servletContext) throws ServletException {
+		
+		// create a new thread and execute that task in it
+		DaemonThread startOpenmrsThread = new DaemonThread() {
+			
+			@Override
+			public void run() {
+				isDaemonThread.set(true);
+				try {
+					Listener.startOpenmrs(servletContext);
+				}
+				catch (Throwable t) {
+					exceptionThrown = t;
+				}
+				finally {
+					Context.closeSession();
+				}
+			}
+		};
+		
+		startOpenmrsThread.start();
+		
+		// wait for the "startOpenmrs" thread to finish
+		try {
+			startOpenmrsThread.join();
+		}
+		catch (InterruptedException e) {
+			// ignore
+		}
+		
+		if (startOpenmrsThread.getExceptionThrown() != null) {
+			throw new ModuleException("Unable to start openmrs as Daemon", startOpenmrsThread.getExceptionThrown());
+		}
+	}
+}
