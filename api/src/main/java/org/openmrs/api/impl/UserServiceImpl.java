70527d0f5da99095664aad5e85c7d96e1ed8d51e
Jeremy Keiper <jeremy@openmrs.org>
2011-12-14 15:10:15 +0000

    backporting TRUNK-2503 to branches/1.9.x - Include inherited roles when searching for users
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.9.x@24938 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/api/impl/UserServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/UserServiceImpl.java
@@ -13,9 +13,12 @@
  */
 package org.openmrs.api.impl;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Vector;
 
 import org.apache.commons.logging.Log;
@@ -600,13 +603,20 @@ public class UserServiceImpl extends BaseOpenmrsService implements UserService {
 		if (roles == null)
 			roles = new Vector<Role>();
 		
+		// add the requested roles and all child roles for consideration
+		Set<Role> allRoles = new HashSet<Role>();
+		for (Role r : roles) {
+			allRoles.add(r);
+			allRoles.addAll(r.getAllChildRoles());
+		}
+		
 		// if the authenticated role is in the list of searched roles, then all
 		// persons should be searched
 		Role auth_role = getRole(RoleConstants.AUTHENTICATED);
 		if (roles.contains(auth_role))
 			return dao.getUsers(name, new Vector<Role>(), includeRetired, start, length);
 		
-		return dao.getUsers(name, roles, includeRetired, start, length);
+		return dao.getUsers(name, new ArrayList<Role>(allRoles), includeRetired, start, length);
 	}
 	
 }
--- api/src/test/java/org/openmrs/RoleTest.java
+++ api/src/test/java/org/openmrs/RoleTest.java
@@ -13,10 +13,14 @@
  */
 package org.openmrs;
 
+import java.util.HashSet;
+import java.util.Set;
+import junit.framework.Assert;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.openmrs.api.context.Context;
 import org.openmrs.test.Verifies;
 import org.openmrs.util.RoleConstants;
 import static org.junit.Assert.assertNotNull;
@@ -121,22 +125,91 @@ public class RoleTest {
 	}
 	
 	/**
-	 * Tests how roles inherit other roles
-	 * 
-	 * @throws Exception
+	 * @see {@link Role#getAllParentRoles()}
 	 */
 	@Test
-	public void shouldInheritingRoles() throws Exception {
+	@Verifies(value = "should only return parent roles", method = "getAllParentRoles()")
+	public void getAllParentRoles_shouldOnlyReturnParentRoles() throws Exception {
+		Role grandparent = new Role("Grandparent");
+		Role aunt = new Role("Aunt");
+		Role uncle = new Role("Uncle");
+		Role parent = new Role("Parent");
+		Role child1 = new Role("Child 1");
+		Role child2 = new Role("Child 2");
+		Role niece = new Role("Niece");
 		
-		// TODO finish test method
+		Set<Role> inheritedRoles = new HashSet<Role>();
 		
-		// test the isInheriting method
+		// grandparent -> aunt, uncle, parent
+		inheritedRoles.add(grandparent);
+		parent.setInheritedRoles(new HashSet(inheritedRoles));
+		aunt.setInheritedRoles(new HashSet(inheritedRoles));
+		uncle.setInheritedRoles(new HashSet(inheritedRoles));
 		
-		// test the getting of parent roles
+		// aunt, uncle -> niece
+		inheritedRoles.clear();
+		inheritedRoles.add(uncle);
+		inheritedRoles.add(aunt);
+		niece.setInheritedRoles(new HashSet(inheritedRoles));
 		
-		// test the recursing over parent roles
+		// parent -> child1, child2
+		inheritedRoles.clear();
+		inheritedRoles.add(parent);
+		child1.setInheritedRoles(new HashSet(inheritedRoles));
+		child2.setInheritedRoles(new HashSet(inheritedRoles));
 		
-		// test the possible infinite loop condition of recursing over parent roles
+		// ensure only inherited roles are found
+		Assert.assertEquals(3, niece.getAllParentRoles().size());
+		Assert.assertEquals(2, child1.getAllParentRoles().size());
+		Assert.assertEquals(2, child2.getAllParentRoles().size());
+		Assert.assertEquals(1, parent.getAllParentRoles().size());
+		Assert.assertEquals(1, aunt.getAllParentRoles().size());
+		Assert.assertEquals(1, uncle.getAllParentRoles().size());
+		Assert.assertEquals(0, grandparent.getAllParentRoles().size());
+	}
+	
+	/**
+	 * @see {@link Role#getAllChildRoles()}
+	 */
+	@Test
+	@Verifies(value = "should only return child roles", method = "getAllChildRoles()")
+	public void getAllChildRoles_shouldOnlyReturnChildRoles() throws Exception {
+		Role grandparent = new Role("Grandparent");
+		Role aunt = new Role("Aunt");
+		Role uncle = new Role("Uncle");
+		Role parent = new Role("Parent");
+		Role child1 = new Role("Child 1");
+		Role child2 = new Role("Child 2");
+		Role niece = new Role("Niece");
+		
+		Set<Role> childRoles = new HashSet<Role>();
+		
+		// grandparent -> aunt, uncle, parent
+		childRoles.add(aunt);
+		childRoles.add(uncle);
+		childRoles.add(parent);
+		grandparent.setChildRoles(new HashSet(childRoles));
+		
+		// aunt, uncle -> niece
+		childRoles.clear();
+		childRoles.add(niece);
+		aunt.setChildRoles(new HashSet(childRoles));
+		uncle.setChildRoles(new HashSet(childRoles));
+		
+		// parent -> child1, child2
+		childRoles.clear();
+		childRoles.add(child1);
+		childRoles.add(child2);
+		parent.setChildRoles(new HashSet(childRoles));
+		
+		// ensure only child roles are found
+		Assert.assertEquals(0, niece.getAllChildRoles().size());
+		Assert.assertEquals(0, child1.getAllChildRoles().size());
+		Assert.assertEquals(0, child2.getAllChildRoles().size());
+		Assert.assertEquals(2, parent.getAllChildRoles().size());
+		Assert.assertEquals(1, aunt.getAllChildRoles().size());
+		Assert.assertEquals(1, uncle.getAllChildRoles().size());
+		Assert.assertEquals(6, grandparent.getAllChildRoles().size());
 	}
 	
 }
