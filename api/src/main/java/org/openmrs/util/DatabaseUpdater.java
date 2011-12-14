7cf9621caa5ca111747d298c35c91807280e1023
Ben Wolfe <ben@openmrs.org>
2011-12-14 11:57:10 +0000

    Followup to not show maintenance wizard if the db is locked but there are no db changes TRUNK-1637 Author: cta
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@24931 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/util/DatabaseUpdater.java
+++ api/src/main/java/org/openmrs/util/DatabaseUpdater.java
@@ -252,33 +252,36 @@ public class DatabaseUpdater {
 	 */
 	public static boolean updatesRequired() throws Exception {
 		log.debug("checking for updates");
-		
+		List<OpenMRSChangeSet> changesets = getUnrunDatabaseChanges();
 		Database database = null;
 		try {
-			
+
 			Liquibase liquibase = getLiquibase(null, null);
 			database = liquibase.getDatabase();
-			
+
 			// if the db is locked, it means there was a crash
 			// or someone is executing db updates right now. either way
 			// returning true here stops the openmrs startup and shows
 			// the user the maintenance wizard for updates
 			if (isLocked()) {
-				return true;
+				// if there is a db lock but there are no db changes we undo the
+				// lock
+				if (changesets.size() == 0) {
+					DatabaseUpdater.releaseDatabaseLock();
+					return false;
+				} else {
+					return true;
+				}
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			// do nothing
-		}
-		finally {
+		} finally {
 			try {
 				database.getConnection().close();
-			}
-			catch (Throwable t) {
+			} catch (Throwable t) {
 				// pass
 			}
 		}
-		List<OpenMRSChangeSet> changesets = getUnrunDatabaseChanges();
 		return changesets.size() > 0;
 	}
 	
