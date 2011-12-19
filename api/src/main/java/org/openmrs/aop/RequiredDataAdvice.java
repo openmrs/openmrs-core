a01a268f73e971978060321830e01d7b7d9f52c3
Suranga Nath Kasthurirathne <surangakas@gmail.com>
2011-12-19 05:10:21 +0000

    Merging branch to trunk - Step 2
    git-svn-id: http://svn.openmrs.org/openmrs/branches/obs-codes-expanded@25000 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/aop/RequiredDataAdvice.java
+++ api/src/main/java/org/openmrs/aop/RequiredDataAdvice.java
@@ -88,6 +88,7 @@ public class RequiredDataAdvice implements MethodBeforeAdvice {
 		fieldAccess.add("Concept.answers");
 		fieldAccess.add("Concept.names");
 		fieldAccess.add("Encounter.obs");
+		fieldAccess.add("Encounter.encounterProviders");
 		fieldAccess.add("Program.allWorkflows");
 		fieldAccess.add("Obs.groupMembers");
 	}
@@ -101,35 +102,46 @@ public class RequiredDataAdvice implements MethodBeforeAdvice {
 	public void before(Method method, Object[] args, Object target) throws Throwable {
 		String methodName = method.getName();
 		
+		// skip out early if there are no arguments
+		if (args == null || args.length == 0)
+			return;
+		
+		Object mainArgument = args[0];
+		
+		// fail early on a null parameter
+		if (mainArgument == null)
+			return;
+		
 		// the "create" is there to cover old deprecated methods since AOP doesn't occur
 		// on method calls within a class, only on calls to methods from external classes to methods
 		// "update" is not an option here because there are multiple methods that start with "update" but is
 		// not updating the primary argument. eg: ConceptService.updateConceptWord(Concept)
 		if (methodName.startsWith("save") || methodName.startsWith("create")) {
-			// skip out early if there are no arguments
-			if (args == null || args.length == 0)
-				return;
-			
-			Object mainArgument = args[0];
-			
-			// fail early on a null parameter
-			if (mainArgument == null)
-				return;
 			
 			// if the first argument is an OpenmrsObject, handle it now
 			Reflect reflect = new Reflect(OpenmrsObject.class);
 			
 			if (reflect.isSuperClass(mainArgument)) {
+				// fail early if the method name is not like saveXyz(Xyz)
+				if (!methodNameEndsWithClassName(method, mainArgument.getClass()))
+					return;
+				
 				// if a second argument exists, pass that to the save handler as well
 				// (with current code, it means we're either in an obs save or a user save)				
 				String other = null;
 				if (args.length > 1 && args[1] instanceof String)
 					other = (String) args[1];
 				
+				Context.getAdministrationService().validateInManualFlushMode(mainArgument);
+				
 				recursivelyHandle(SaveHandler.class, (OpenmrsObject) mainArgument, other);
 			}
 			// if the first argument is a list of openmrs objects, handle them all now
 			else if (Reflect.isCollection(mainArgument) && isOpenmrsObjectCollection(mainArgument)) {
+				// ideally we would fail early if the method name is not like savePluralOfXyz(Collection<Xyz>)
+				// but this only occurs once in the API (AdministrationService.saveGlobalProperties
+				// so it is not worth handling this case
+				
 				// if a second argument exists, pass that to the save handler as well
 				// (with current code, it means we're either in an obs save or a user save)				
 				String other = null;
@@ -139,35 +151,67 @@ public class RequiredDataAdvice implements MethodBeforeAdvice {
 				Collection<OpenmrsObject> openmrsObjects = (Collection<OpenmrsObject>) mainArgument;
 				
 				for (OpenmrsObject object : openmrsObjects) {
+					Context.getAdministrationService().validateInManualFlushMode(mainArgument);
+					
 					recursivelyHandle(SaveHandler.class, object, other);
 				}
 				
 			}
+		} else {
+			// fail early if the method name is not like retirePatient or retireConcept when dealing
+			// with Patients or Concepts as the first argument
+			if (!methodNameEndsWithClassName(method, mainArgument.getClass()))
+				return;
 			
-		} else if (methodName.startsWith("void")) {
-			Voidable voidable = (Voidable) args[0];
-			String voidReason = (String) args[1];
-			recursivelyHandle(VoidHandler.class, voidable, voidReason);
-			
-		} else if (methodName.startsWith("unvoid")) {
-			Voidable voidable = (Voidable) args[0];
-			Date originalDateVoided = voidable.getDateVoided();
-			User originalVoidingUser = voidable.getVoidedBy();
-			recursivelyHandle(UnvoidHandler.class, voidable, originalVoidingUser, originalDateVoided, null, null);
-			
-		} else if (methodName.startsWith("retire")) {
-			Retireable retirable = (Retireable) args[0];
-			String retireReason = (String) args[1];
-			recursivelyHandle(RetireHandler.class, retirable, retireReason);
-			
-		} else if (methodName.startsWith("unretire")) {
-			Retireable retirable = (Retireable) args[0];
-			Date originalDateRetired = retirable.getDateRetired();
-			recursivelyHandle(UnretireHandler.class, retirable, Context.getAuthenticatedUser(), originalDateRetired, null,
-			    null);
-			
+			if (methodName.startsWith("void")) {
+				Voidable voidable = (Voidable) args[0];
+				String voidReason = (String) args[1];
+				recursivelyHandle(VoidHandler.class, voidable, voidReason);
+				
+			} else if (methodName.startsWith("unvoid")) {
+				Voidable voidable = (Voidable) args[0];
+				Date originalDateVoided = voidable.getDateVoided();
+				User originalVoidingUser = voidable.getVoidedBy();
+				recursivelyHandle(UnvoidHandler.class, voidable, originalVoidingUser, originalDateVoided, null, null);
+				
+			} else if (methodName.startsWith("retire")) {
+				Retireable retirable = (Retireable) args[0];
+				String retireReason = (String) args[1];
+				recursivelyHandle(RetireHandler.class, retirable, retireReason);
+				
+			} else if (methodName.startsWith("unretire")) {
+				Retireable retirable = (Retireable) args[0];
+				Date originalDateRetired = retirable.getDateRetired();
+				recursivelyHandle(UnretireHandler.class, retirable, Context.getAuthenticatedUser(), originalDateRetired,
+				    null, null);
+			}
+		}
+	}
+	
+	/**
+	 * Convenience method to change the given method to make sure it ends with
+	 * the given class name. <br/>
+	 * This will recurse to the super class to check that as well.
+	 * 
+	 * @param method
+	 *            the method name (like savePatient, voidEncounter,
+	 *            retireConcept)
+	 * @param mainArgumentClass
+	 *            class to compare
+	 * @return true if method's name ends with the mainArgumentClasses simple
+	 *         name
+	 */
+	private boolean methodNameEndsWithClassName(Method method, Class mainArgumentClass) {
+		if (method.getName().endsWith(mainArgumentClass.getSimpleName()))
+			return true;
+		else {
+			mainArgumentClass = mainArgumentClass.getSuperclass();
+			// stop recursing if no super class
+			if (mainArgumentClass != null)
+				return methodNameEndsWithClassName(method, mainArgumentClass);
 		}
 		
+		return false;
 	}
 	
 	/**
