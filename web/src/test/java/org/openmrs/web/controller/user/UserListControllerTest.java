25190b566c6d6f046b2dec6b8d0997f9fd25f00e
Suranga Nath Kasthurirathne <surangakas@gmail.com>
2011-12-19 09:48:05 +0000

    Merging branch to trunk - Step 7
    git-svn-id: http://svn.openmrs.org/openmrs/branches/obs-codes-expanded@25008 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/test/java/org/openmrs/web/controller/user/UserListControllerTest.java
+++ web/src/test/java/org/openmrs/web/controller/user/UserListControllerTest.java
@@ -19,7 +19,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openmrs.Role;
 import org.openmrs.User;
-import org.openmrs.test.TestUtil;
 import org.openmrs.web.test.BaseWebContextSensitiveTest;
 import org.springframework.ui.ModelMap;
 
@@ -36,7 +35,7 @@ public class UserListControllerTest extends BaseWebContextSensitiveTest {
 	public void displayUsers_shouldGetAllUsersIfNoNameGiven() throws Exception {
 		UserListController controller = new UserListController();
 		List<User> users = controller.getUsers("Search", "", null, false);
-		Assert.assertEquals(2, users.size());
+		Assert.assertEquals(3, users.size());
 	}
 	
 	/**
@@ -47,7 +46,7 @@ public class UserListControllerTest extends BaseWebContextSensitiveTest {
 	public void displayUsers_shouldGetUsersJustGivenActionParameter() throws Exception {
 		UserListController controller = new UserListController();
 		List<User> users = controller.getUsers("Search", null, null, null);
-		Assert.assertEquals(2, users.size());
+		Assert.assertEquals(3, users.size());
 	}
 	
 	/**
@@ -69,6 +68,6 @@ public class UserListControllerTest extends BaseWebContextSensitiveTest {
 	public void displayUsers_shouldIncludeDisabledUsersIfRequested() throws Exception {
 		UserListController controller = new UserListController();
 		List<User> users = controller.getUsers("Search", "", new Role(""), true);
-		Assert.assertEquals(3, users.size());
+		Assert.assertEquals(4, users.size());
 	}
 }
---
+++ web/src/test/java/org/openmrs/web/dwr/DWRProviderServiceTest.java
@@ -0,0 +1,102 @@
+package org.openmrs.web.dwr;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Vector;
+
+import org.apache.commons.collections.Closure;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openmrs.web.test.BaseWebContextSensitiveTest;
+
+public class DWRProviderServiceTest extends BaseWebContextSensitiveTest {
+	
+	private static final String PROVIDERS_INITIAL_XML = "org/openmrs/api/include/ProviderServiceTest-initial.xml";
+	
+	private static final String PROVIDER_ATTRIBUTE_TYPES_XML = "org/openmrs/api/include/ProviderServiceTest-providerAttributes.xml";
+	
+	private DWRProviderService service;
+	
+	@Before
+	public void setup() throws Exception {
+		service = new DWRProviderService();
+		
+		executeDataSet(PROVIDERS_INITIAL_XML);
+		executeDataSet(PROVIDER_ATTRIBUTE_TYPES_XML);
+	}
+	
+	/**
+	 * @see DWRProviderService#findProvider(String,boolean,Integer,Integer)
+	 * @verifies return a message with no matches found when no providers are found
+	 */
+	@Test
+	public void findProvider_shouldReturnAMessageWithNoMatchesFoundWhenNoProvidersAreFound() throws Exception {
+		Vector<Object> providers = service.findProvider("noProvider", false, 0, 1);
+		
+		Assert.assertEquals("Provider.noMatchesFound", ((String) providers.get(0)));
+	}
+	
+	/**
+	 * @see DWRProviderService#findProvider(String,boolean,Integer,Integer)
+	 * @verifies return the list of providers including voided providers for the matching search
+	 *           name
+	 */
+	@Test
+	public void findProvider_shouldReturnTheListOfProvidersIncludingVoidedProvidersForTheMatchingSearchName()
+	        throws Exception {
+		
+		Vector<Object> providers = service.findProvider("provider", true, 0, 10);
+		Assert.assertEquals(3, providers.size());
+		
+		Assert.assertTrue(CollectionUtils.exists(providers, new Predicate() {
+			
+			@Override
+			public boolean evaluate(Object object) {
+				return ((ProviderListItem) object).getDisplayName().equals("Jimmy Manana Chemalit");
+			}
+		}));
+	}
+	
+	/**
+	 * @see DWRProviderService#findProvider(String,boolean,Integer,Integer)
+	 * @verifies return the list of providers matching the search name
+	 */
+	@Test
+	public void findProvider_shouldReturnTheListOfProvidersMatchingTheSearchName() throws Exception {
+		
+		Vector<Object> providers = service.findProvider("provider", false, 0, 10);
+		Assert.assertEquals(2, providers.size());
+		
+		final ArrayList<String> providerNames = new ArrayList<String>();
+		
+		CollectionUtils.forAllDo(providers, new Closure() {
+			
+			@Override
+			public void execute(Object input) {
+				providerNames.add(((ProviderListItem) input).getDisplayName());
+			}
+		});
+		
+		Assert.assertTrue(providerNames.containsAll(Arrays.asList("Bruno Otterbourg", "Hippocrates of Cos")));
+	}
+	
+	/**
+	 * @see DWRProviderService#findProviderCountAndProvider(String,boolean,Integer,Integer)
+	 * @verifies return the count of all providers matching the searched name along with provider
+	 *           list
+	 */
+	@Test
+	@Ignore("This test fails because we have the order by for person names mentioned in the person.hbm.xml for the names set. "
+	        + "H2 is expecting a group by clause for all the columns mentioned in the order by which is not needed to execute a query in mysql."
+	        + "Keeping the test case here because this might be a problem in other databases too")
+	public void findProviderCountAndProvider_shouldReturnTheCountOfAllProvidersMatchingTheSearchedNameAlongWithProviderList()
+	        throws Exception {
+		Map<String, Object> countAndProviders = service.findProviderCountAndProvider("provider", true, 0, 2);
+		Assert.assertEquals(3, countAndProviders.get("count"));
+	}
+}
