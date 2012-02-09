07f6b38cb4faaad23fc9d0c5cd96bff65b1e4846
Daniel Kayiwa <dkayiwa@openmrs.org>
2012-02-09 07:57:04 +0000

    Fixing: ProviderService.getProviderByUuid(String) has a typo - TRUNK-3053
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@25770 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/test/java/org/openmrs/api/ProviderServiceTest.java
+++ api/src/test/java/org/openmrs/api/ProviderServiceTest.java
@@ -191,12 +191,12 @@ public class ProviderServiceTest extends BaseContextSensitiveTest {
 	}
 	
 	/**
-	 * @see ProviderService#getProviderbyUuid(String)
+	 * @see ProviderService#getProviderByUuid(String)
 	 * @verifies get provider given Uuid
 	 */
 	@Test
-	public void getProviderbyUuid_shouldGetProviderGivenUuid() throws Exception {
-		Provider provider = service.getProviderbyUuid("a2c3868a-6b90-11e0-93c3-18a905e044dc");
+	public void getProviderByUuid_shouldGetProviderGivenUuid() throws Exception {
+		Provider provider = service.getProviderByUuid("a2c3868a-6b90-11e0-93c3-18a905e044dc");
 		Assert.assertNotNull(provider);
 		assertEquals("RobertClive", provider.getName());
 	}
