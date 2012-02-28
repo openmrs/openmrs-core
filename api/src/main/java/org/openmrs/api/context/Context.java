91d58d8c4251921861098f85e19f09a7d2fb5b79
Ben Wolfe <ben@openmrs.org>
2012-02-28 17:59:10 +0000

    Locale does not work property when using "Become This User" - TRUNK-1800 Author: mvorobey
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@26152 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/api/context/Context.java
+++ api/src/main/java/org/openmrs/api/context/Context.java
@@ -317,12 +317,25 @@ public class Context {
 	 * 
 	 * @param systemId
 	 * @throws ContextAuthenticationException
+	 * @should change locale when become another user
 	 */
 	public static void becomeUser(String systemId) throws ContextAuthenticationException {
 		if (log.isInfoEnabled())
 			log.info("systemId: " + systemId);
 		
-		getUserContext().becomeUser(systemId);
+		User user = getUserContext().becomeUser(systemId);
+		
+		// if assuming identity procedure finished successfully, we should change context locale parameter
+		Locale locale = null;
+		if (user.getUserProperties().containsKey(OpenmrsConstants.USER_PROPERTY_DEFAULT_LOCALE)) {
+			String localeString = user.getUserProperty(OpenmrsConstants.USER_PROPERTY_DEFAULT_LOCALE);
+			locale = LocaleUtility.fromSpecification(localeString);
+		}
+		// when locale parameter is not valid or does not exist
+		if (locale == null) {
+			locale = LocaleUtility.getDefaultLocale();
+		}
+		Context.setLocale(locale);
 	}
 	
 	/**
