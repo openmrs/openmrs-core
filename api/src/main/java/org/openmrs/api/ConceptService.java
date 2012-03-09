18a44da774214f0fec0190f49222fedf4a001780
Rafal Korytkowski <rafal@openmrs.org>
2012-03-09 13:19:17 +0000

    Fixing since annotation for TRUNK-3005.
    git-svn-id: http://svn.openmrs.org/openmrs/branches/1.9.x@26377 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/api/ConceptService.java
+++ api/src/main/java/org/openmrs/api/ConceptService.java
@@ -2010,7 +2010,7 @@ public interface ConceptService extends OpenmrsService {
 	 * @param exactLocale <code>false</code> if search for both global and country specific, <code>true</code> if <code>null</code>
 	 * @return the list of concepts
 	 * @throws APIException
-	 * @since 1.10
+	 * @since 1.9
 	 * @should return concepts for all countries and global language given language only locale
 	 * @should return concepts for specific country and global language given language and country
 	 *         locale
