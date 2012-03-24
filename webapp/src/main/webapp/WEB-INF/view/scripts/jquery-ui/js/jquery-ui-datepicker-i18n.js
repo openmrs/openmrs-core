3688c54bfb372143fd9c9e48b4eccbdc1a73ffe5
Wyclif Luyima <wyclif@openmrs.org>
2012-03-24 16:15:31 +0000

    Foreign characters corrupted within datepicker - TRUNK-3127
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@26682 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- webapp/src/main/webapp/WEB-INF/view/scripts/jquery-ui/js/jquery-ui-datepicker-i18n.js
+++ webapp/src/main/webapp/WEB-INF/view/scripts/jquery-ui/js/jquery-ui-datepicker-i18n.js
@@ -37,13 +37,12 @@ jQuery(function($){
 		yearSuffix: ''};
 	$j.datepicker.regional['fr'] = {
 		closeText: 'Fermer',
-		prevText: '&#x3c;Pr�c',
+		prevText: '&#x3c;Préc',
 		nextText: 'Suiv&#x3e;',
 		currentText: 'Courant',
-		monthNames: ['Janvier','F�vrier','Mars','Avril','Mai','Juin',
-		    'Juillet','Ao�t','Septembre','Octobre','Novembre','D�cembre'],
-		monthNamesShort: ['Jan','F�v','Mar','Avr','Mai','Jun',
-		    'Jul','Ao�','Sep','Oct','Nov','D�c'],
+		monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin',
+			'Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
+		monthNamesShort: ['Jan','Fév','Mar','Avr','Mai','Jun','Jul','Aoû','Sep','Oct','Nov','Déc'],
 		dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
 		dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
 		dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
