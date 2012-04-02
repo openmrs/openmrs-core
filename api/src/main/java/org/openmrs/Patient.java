a7faf4addcb312d4b47344eb89d6bf7f14f2aaf0
Ben Wolfe <ben@openmrs.org>
2012-04-02 13:13:33 +0000

    Cannot add an identifier to a patient if they previously had that same identifier, and it was voided. - TRUNK-3118 Author: maurya
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@26763 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/Patient.java
+++ api/src/main/java/org/openmrs/Patient.java
@@ -181,7 +181,7 @@ public class Patient extends Person implements java.io.Serializable {
 		if (patientIdentifier != null) {
 			// make sure the set doesn't already contain an identifier with the same
 			// identifier, identifierType
-			for (PatientIdentifier currentId : getIdentifiers()) {
+			for (PatientIdentifier currentId : getActiveIdentifiers()) {
 				if (currentId.equalsContent(patientIdentifier)) {
 					return; // fail silently if someone tries to add a duplicate
 				}
