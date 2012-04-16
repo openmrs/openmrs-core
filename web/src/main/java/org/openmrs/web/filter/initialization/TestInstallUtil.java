7f447de1b7c76e70d14378cb0374b09b865f86f6
Daniel Kayiwa <dkayiwa@openmrs.org>
2012-04-16 15:48:54 +0000

    Adding a wiki link to the error message when importing test data: Unable to use the the new release testing feature to upgrade a 1.6.4 database - TRUNK-3022
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@26901 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/main/java/org/openmrs/web/filter/initialization/TestInstallUtil.java
+++ web/src/main/java/org/openmrs/web/filter/initialization/TestInstallUtil.java
@@ -114,7 +114,7 @@ public class TestInstallUtil {
 				return true;
 			}
 			
-			log.error("The process terminated abnormally while adding test data");
+			log.error("The process terminated abnormally while adding test data. Please look under the Configuration section at: https://wiki.openmrs.org/display/docs/Release+Testing+Helper+Module");
 			
 		}
 		catch (IOException e) {
