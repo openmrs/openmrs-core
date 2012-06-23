c7fb281678ef8605d32c186512e912544ce6f755
Mykola Vorobey <vorobeymykola@gmail.com>
2012-06-23 11:24:36 +0000

    TRUNK-3245 - Added functionality for respecting include/exclude lists when searching for concepts using DWRConceptService
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@28188 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- web/src/test/java/org/openmrs/web/dwr/DWRConceptServiceTest.java
+++ web/src/test/java/org/openmrs/web/dwr/DWRConceptServiceTest.java
@@ -1,10 +1,20 @@
 package org.openmrs.web.dwr;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openmrs.Concept;
+import org.openmrs.api.context.Context;
 import org.openmrs.test.BaseContextSensitiveTest;
+import org.openmrs.util.OpenmrsUtil;
 
 public class DWRConceptServiceTest extends BaseContextSensitiveTest {
+	
+	private DWRConceptService dwrConceptService = new DWRConceptService();
 	
 	@Before
 	public void before() throws Exception {
@@ -18,7 +28,156 @@ public class DWRConceptServiceTest extends BaseContextSensitiveTest {
 	@Test
 	public void findConceptAnswers_shouldNotFailIfTheSpecifiedConceptHasNoAnswersRegressionTestForTRUNK2807()
 	        throws Exception {
-		new DWRConceptService().findConceptAnswers("", 1000, false, true);
+		dwrConceptService.findConceptAnswers("", 1000, false, true);
 		// if we got here, we've passed, because we didn't get a NullPointerException
+	}
+	
+	/**
+	 * @see DWRConceptService#findBatchOfConcepts(String, boolean, java.util.List, java.util.List,
+	 *      java.util.List, java.util.List, Integer, Integer)
+	 * @verifies return concept by given id if exclude and include lists are empty
+	 */
+	@Test
+	public void findBatchOfConcepts_shouldReturnConceptByGivenIdIfExclusionAndInclusionListsAreEmpty() throws Exception {
+		String phrase = "1000";
+		Concept expected = Context.getConceptService().getConcept(phrase);
+		List<Object> result = dwrConceptService.findBatchOfConcepts(phrase, Boolean.FALSE, null, null, null, null, null,
+		    null);
+		Assert.assertNotNull(result);
+		Assert.assertTrue(isConceptFound(expected, result));
+	}
+	
+	/**
+	 * @see DWRConceptService#findBatchOfConcepts(String, boolean, java.util.List, java.util.List,
+	 *      java.util.List, java.util.List, Integer, Integer)
+	 * @verifies return concept by given id if classname is included
+	 */
+	@Test
+	public void findBatchOfConcepts_shouldReturnConceptByGivenIdIfClassnameIsIncluded() throws Exception {
+		String phrase = "1000";
+		Concept expected = Context.getConceptService().getConcept(phrase);
+		// prepare include concept classnames list
+		List<String> includeClassNames = new ArrayList<String>();
+		includeClassNames.add("Question");
+		List<Object> result = dwrConceptService.findBatchOfConcepts(phrase, Boolean.FALSE, includeClassNames, null, null,
+		    null, null, null);
+		Assert.assertNotNull(result);
+		Assert.assertTrue(isConceptFound(expected, result));
+	}
+	
+	/**
+	 * @see DWRConceptService#findBatchOfConcepts(String, boolean, java.util.List, java.util.List,
+	 *      java.util.List, java.util.List, Integer, Integer)
+	 * @verifies not return concept by given id if classname is not included
+	 */
+	@Test
+	public void findBatchOfConcepts_shouldNotReturnConceptByGivenIdIfClassnameIsNotIncluded() throws Exception {
+		String phrase = "1000";
+		Concept expected = Context.getConceptService().getConcept(phrase);
+		// prepare include concept classnames list and
+		// intentionally do not add expected concept class name
+		List<String> includeClassNames = new ArrayList<String>();
+		includeClassNames.add("test");
+		List<Object> result = dwrConceptService.findBatchOfConcepts(phrase, Boolean.FALSE, includeClassNames, null, null,
+		    null, null, null);
+		Assert.assertNotNull(result);
+		Assert.assertFalse(isConceptFound(expected, result));
+	}
+	
+	/**
+	 * @see DWRConceptService#findBatchOfConcepts(String, boolean, java.util.List, java.util.List,
+	 *      java.util.List, java.util.List, Integer, Integer)
+	 * @verifies not return concept by given id if classname is excluded
+	 */
+	@Test
+	public void findBatchOfConcepts_shouldNotReturnConceptByGivenIdIfClassnameIsExcluded() throws Exception {
+		String phrase = "1000";
+		Concept expected = Context.getConceptService().getConcept(phrase);
+		// prepare exclude concept classnames list
+		List<String> excludeClassNames = new ArrayList<String>();
+		excludeClassNames.add("Question");
+		List<Object> result = dwrConceptService.findBatchOfConcepts(phrase, Boolean.FALSE, null, excludeClassNames, null,
+		    null, null, null);
+		Assert.assertNotNull(result);
+		Assert.assertFalse(isConceptFound(expected, result));
+	}
+	
+	/**
+	 * @see DWRConceptService#findBatchOfConcepts(String, boolean, java.util.List, java.util.List,
+	 *      java.util.List, java.util.List, Integer, Integer)
+	 * @verifies return concept by given id if datatype is included
+	 */
+	@Test
+	public void findBatchOfConcepts_shouldReturnConceptByGivenIdIfDatatypeIsIncluded() throws Exception {
+		String phrase = "1000";
+		Concept expected = Context.getConceptService().getConcept(phrase);
+		// prepare include concept datatypes list
+		List<String> includeDatatypes = new ArrayList<String>();
+		includeDatatypes.add("Coded");
+		List<Object> result = dwrConceptService.findBatchOfConcepts(phrase, Boolean.FALSE, null, null, includeDatatypes,
+		    null, null, null);
+		Assert.assertNotNull(result);
+		Assert.assertTrue(isConceptFound(expected, result));
+	}
+	
+	/**
+	 * @see DWRConceptService#findBatchOfConcepts(String, boolean, java.util.List, java.util.List,
+	 *      java.util.List, java.util.List, Integer, Integer)
+	 * @verifies not return concept by given id if datatype is not included
+	 */
+	@Test
+	public void findBatchOfConcepts_shouldNotReturnConceptByGivenIdIfDatatypeIsNotIncluded() throws Exception {
+		String phrase = "1000";
+		Concept expected = Context.getConceptService().getConcept(phrase);
+		// prepare include concept datatypes list and
+		// intentionally do not add expected concept data type
+		List<String> includeDatatypes = new ArrayList<String>();
+		includeDatatypes.add("test");
+		List<Object> result = dwrConceptService.findBatchOfConcepts(phrase, Boolean.FALSE, null, null, includeDatatypes,
+		    null, null, null);
+		Assert.assertNotNull(result);
+		Assert.assertFalse(isConceptFound(expected, result));
+	}
+	
+	/**
+	 * @see DWRConceptService#findBatchOfConcepts(String, boolean, java.util.List, java.util.List,
+	 *      java.util.List, java.util.List, Integer, Integer)
+	 * @verifies not return concept by given id if datatype is excluded
+	 */
+	@Test
+	public void findBatchOfConcepts_shouldNotReturnConceptByGivenIdIfDatatypeIsExcluded() throws Exception {
+		String phrase = "1000";
+		Concept expected = Context.getConceptService().getConcept(phrase);
+		// prepare exclude concept datatypes list
+		List<String> excludeDatatypes = new ArrayList<String>();
+		excludeDatatypes.add("Coded");
+		List<Object> result = dwrConceptService.findBatchOfConcepts(phrase, Boolean.FALSE, null, null, null,
+		    excludeDatatypes, null, null);
+		Assert.assertNotNull(result);
+		Assert.assertFalse(isConceptFound(expected, result));
+	}
+	
+	/**
+	 * Convenient method that determines whether given concept is present in result list or not
+	 * 
+	 * @param expected the concept to be checked
+	 * @param result the list of concept lookup result items
+	 * @return true if given concept is present among result items
+	 */
+	private boolean isConceptFound(Concept expected, List<Object> result) {
+		boolean found = Boolean.FALSE;
+		if (result != null) {
+			for (Iterator<?> iterator = result.iterator(); iterator.hasNext();) {
+				Object item = iterator.next();
+				if (item instanceof ConceptListItem) {
+					ConceptListItem resultItem = (ConceptListItem) item;
+					if (resultItem != null && OpenmrsUtil.nullSafeEquals(resultItem.getConceptId(), expected.getConceptId())) {
+						found = Boolean.TRUE;
+						break;
+					}
+				}
+			}
+		}
+		return found;
 	}
 }
