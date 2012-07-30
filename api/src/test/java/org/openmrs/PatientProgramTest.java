fe1b283d2441bb2b78b9ab8144490d42b8eeb663
Mark Goodrich <mgoodrich@pih.org>
2012-07-30 22:41:47 +0000

    TRUNK-3645: Problem with using SortedSet to sort patient states
    git-svn-id: http://svn.openmrs.org/openmrs/trunk@28719 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/test/java/org/openmrs/PatientProgramTest.java
+++ api/src/test/java/org/openmrs/PatientProgramTest.java
@@ -13,12 +13,23 @@
  */
 package org.openmrs;
 
+import java.lang.reflect.Method;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class PatientProgramTest {
+	
+	private Date date = new Date();
+	
+	private Date earlierDate = new Date(date.getTime() - 10000);
+	
+	private Date laterDate = new Date(date.getTime() + 10000);
 	
 	/**
 	 * @see PatientProgram#voidLastState(ProgramWorkflow,User,Date,String)
@@ -53,5 +64,43 @@ public class PatientProgramTest {
 		//then
 		Assert.assertTrue(state1.isVoided());
 		Assert.assertFalse(state2.isVoided());
+	}
+	
+	@Test
+	public void getSortedStates_shouldReturnAllStatesEvenIfTwoHaveIdenticalStartAndEndDates() throws Exception {
+		
+		// this test written specifically to verify fix for https://tickets.openmrs.org/browse/TRUNK-3645
+		Method getSortedStates = PatientProgram.class.getDeclaredMethod("getSortedStates");
+		getSortedStates.setAccessible(true);
+		Assert.assertNotNull(getSortedStates);
+		
+		Set<PatientState> patientStates = new HashSet<PatientState>();
+		PatientState patientState = new PatientState();
+		patientState.setStartDate(date);
+		patientState.setEndDate(laterDate);
+		patientState.setVoided(false);
+		patientStates.add(patientState);
+		
+		PatientState patientState2 = new PatientState();
+		patientState2.setStartDate(date);
+		patientState2.setEndDate(laterDate);
+		patientState2.setVoided(false);
+		patientStates.add(patientState2);
+		
+		PatientState patientState3 = new PatientState();
+		patientState3.setStartDate(earlierDate);
+		patientState3.setEndDate(date);
+		patientState3.setVoided(false);
+		patientStates.add(patientState3);
+		
+		PatientProgram program = new PatientProgram();
+		program.setStates(patientStates);
+		
+		// when
+		List<PatientState> sortedStates = (List<PatientState>) getSortedStates.invoke(program);
+		
+		// then
+		Assert.assertEquals(3, sortedStates.size());
+		
 	}
 }
