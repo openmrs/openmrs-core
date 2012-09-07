f18f4831b11cc6a05284930c1b424f946133b5b4
Ben Wolfe <benwolfe@gmail.com>
2012-09-07 17:04:02 -0400

    Yet another attempt at fixing the randomly failing scheduler service tests

--- api/src/test/java/org/openmrs/test/BaseContextSensitiveTest.java
+++ api/src/test/java/org/openmrs/test/BaseContextSensitiveTest.java
@@ -217,7 +217,7 @@ public abstract class BaseContextSensitiveTest extends AbstractJUnit4SpringConte
 		// properties
 		if (useInMemoryDatabase() == true) {
 			runtimeProperties.setProperty(Environment.DIALECT, H2Dialect.class.getName());
-			runtimeProperties.setProperty(Environment.URL, "jdbc:h2:mem:openmrs;DB_CLOSE_DELAY=30");
+			runtimeProperties.setProperty(Environment.URL, "jdbc:h2:mem:openmrs;DB_CLOSE_DELAY=30;LOCK_TIMEOUT=10000");
 			runtimeProperties.setProperty(Environment.DRIVER, "org.h2.Driver");
 			runtimeProperties.setProperty(Environment.USER, "sa");
 			runtimeProperties.setProperty(Environment.PASS, "");
