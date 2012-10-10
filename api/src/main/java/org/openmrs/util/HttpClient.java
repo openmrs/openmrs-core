6be824b12bb94670748d3d5236d44455f3a7eb43
djazayeri <jazayeri@alum.mit.edu>
2012-10-10 17:50:04 +0800

    Applying Ben's code review comments to Zabil's changes from a2e350f8f3291e854cd1e198ef593c109e213047

--- api/src/main/java/org/openmrs/util/HttpClient.java
+++ api/src/main/java/org/openmrs/util/HttpClient.java
@@ -1,3 +1,17 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
 package org.openmrs.util;
 
 import org.apache.commons.logging.Log;
@@ -11,6 +25,10 @@ import java.net.MalformedURLException;
 import java.net.URLEncoder;
 import java.util.Map;
 
+/**
+ * This class supports doing an HTTP post to a URL. (It replaces the OpenmrsUtil.postToUrl method, allowing us to
+ * mock http calls in unit tests.)
+ */
 public class HttpClient {
 	
 	private static Log log = LogFactory.getLog(HttpClient.class);
--- api/src/test/java/org/openmrs/util/HttpClientTest.java
+++ api/src/test/java/org/openmrs/util/HttpClientTest.java
@@ -1,3 +1,17 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
 package org.openmrs.util;
 
 import org.junit.Before;
@@ -11,7 +25,9 @@ import java.util.HashMap;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class HttpClientTest {
 	
