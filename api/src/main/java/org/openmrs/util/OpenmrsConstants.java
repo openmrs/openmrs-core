249360d9a4e9a1a0dcd5483e59a8883980ed7f22
lluismf <lluismf@gmail.com>
2012-11-01 17:05:22 +0100

    TRUNK-1632: Strange allergies allowed in the Allergies list on the patient dashboard

--- api/src/main/java/org/openmrs/util/OpenmrsConstants.java
+++ api/src/main/java/org/openmrs/util/OpenmrsConstants.java
@@ -993,6 +993,18 @@ public final class OpenmrsConstants {
 	 * management form.
 	 */
 	public static final String GP_CONCEPT_DRUG_ROUTE_CONCEPT_CLASSES = "conceptDrug.route.conceptClasses";
+
+	/**
+	 * Global property name of the allowed concept classes for the allergen field of the allergy
+	 * management form.
+	 */
+	public static final String GP_ALLERGY_ALLERGEN_CONCEPT_CLASSES = "allergy.allergen.ConceptClasses";
+
+	/**
+	 * Global property name of the allowed concept classes for the reaction field of the allergy
+	 * management form.
+	 */
+	public static final String GP_ALLERGY_REACTION_CONCEPT_CLASSES = "allergy.reaction.ConceptClasses";
 	
 	/**
 	 * Encryption properties; both vector and key are required to utilize a two-way encryption
@@ -1458,6 +1470,12 @@ public final class OpenmrsConstants {
 		                GP_DASHBOARD_METADATA_CASE_CONVERSION,
 		                "",
 		                "Indicates which type automatic case conversion is applied to program/workflow/state in the patient dashboard. Valid values: lowercase, uppercase, capitalize. If empty no conversion is applied."));
+
+		props.add(new GlobalProperty(GP_ALLERGY_ALLERGEN_CONCEPT_CLASSES, "Drug,MedSet",
+		        "A comma-separated list of the allowed concept classes for the allergen field of the allergy dialog"));
+
+		props.add(new GlobalProperty(GP_ALLERGY_REACTION_CONCEPT_CLASSES, "Symptom",
+		        "A comma-separated list of the allowed concept classes for the reaction field of the allergy dialog"));
 		
 		for (GlobalProperty gp : ModuleFactory.getGlobalProperties()) {
 			props.add(gp);
