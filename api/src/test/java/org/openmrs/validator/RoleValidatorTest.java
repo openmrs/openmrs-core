67662ec248395af2ac05654140a8c936eb429ad9
andreapat <apattson@earthlink.net>
2012-11-14 15:40:25 -0800

    TRUNK-3474 Remove not null constraint on the description column/property on relationship_type table. Remove not null constraint on the description column/property on role table. Modified RoleValidator to not reject null/empty descriptions. Modified RoleValidatorTest to pass if null/empty decription found.

--- api/src/test/java/org/openmrs/validator/RoleValidatorTest.java
+++ api/src/test/java/org/openmrs/validator/RoleValidatorTest.java
@@ -41,25 +41,25 @@ public class RoleValidatorTest {
 	 * @see {@link RoleValidator#validate(Object,Errors)}
 	 */
 	@Test
-	@Verifies(value = "should fail validation if description is null or empty or whitespace", method = "validate(Object,Errors)")
-	public void validate_shouldFailValidationIfDescriptionIsNullOrEmptyOrWhitespace() throws Exception {
+	@Verifies(value = "should pass validation if description is null or empty or whitespace", method = "validate(Object,Errors)")
+	public void validate_shouldPassValidationIfDescriptionIsNullOrEmptyOrWhitespace() throws Exception {
 		Role role = new Role();
 		role.setRole("Bowling race car driver");
 		role.setDescription(null);
 		
 		Errors errors = new BindException(role, "type");
 		new RoleValidator().validate(role, errors);
-		Assert.assertTrue(errors.hasFieldErrors("description"));
+		Assert.assertFalse(errors.hasFieldErrors("description"));
 		
 		role.setDescription("");
 		errors = new BindException(role, "role");
 		new RoleValidator().validate(role, errors);
-		Assert.assertTrue(errors.hasFieldErrors("description"));
+		Assert.assertFalse(errors.hasFieldErrors("description"));
 		
 		role.setDescription(" ");
 		errors = new BindException(role, "role");
 		new RoleValidator().validate(role, errors);
-		Assert.assertTrue(errors.hasFieldErrors("description"));
+		Assert.assertFalse(errors.hasFieldErrors("description"));
 	}
 	
 	/**
