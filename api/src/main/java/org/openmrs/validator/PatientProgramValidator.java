f3d7a71f8d06a71f3f957e14dd828ce8ada6615d
rkorytkowski <rafal.korytkowski@gmail.com>
2012-11-16 12:12:17 +0100

    Revert "Removing accidentally commented out code for: Provide configuration"
    This reverts commit 903bbfacfcea7e7009cf17ca26c29f47721d48c1.

--- api/src/main/java/org/openmrs/validator/PatientProgramValidator.java
+++ api/src/main/java/org/openmrs/validator/PatientProgramValidator.java
@@ -71,9 +71,12 @@ public class PatientProgramValidator implements Validator {
 	 * @should pass for patient states that have the same start dates in the same work flow
 	 */
 	public void validate(Object obj, Errors errors) {
-		if (!ValidateUtil.isValidationOn()) {
+		//if (!ValidateUtil.isValidationOn()) {
+		//	return;
+		//}
+		if ("true".equals(Context.getAdministrationService().getGlobalProperty(
+		    OpenmrsConstants.GP_TURN_OFF_SAVE_HANDLER_VALIDATION, "false")))
 			return;
-		}
 		
 		if (log.isDebugEnabled())
 			log.debug(this.getClass().getName() + ".validate...");
