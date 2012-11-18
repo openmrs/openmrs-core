cf3133a6d6f44acc9951980266cf96cf79d4a275
wluyima <wyclif@openmrs.com>
2012-11-18 13:52:08 -0500

    Back ported - Made small fix to search widgets not look up the selected item from the array of results on click

--- webapp/src/main/webapp/WEB-INF/view/scripts/jquery-ui/js/openmrsSearch.js
+++ webapp/src/main/webapp/WEB-INF/view/scripts/jquery-ui/js/openmrsSearch.js
@@ -423,7 +423,7 @@ function OpenmrsSearch(div, showIncludeVoided, searchHandler, selectionHandler, 
 		    		"sLengthMenu": omsgs.showNumberofEntries
 		    	},
 		    	
-		    	/* Called to toggle the verobse output */
+		    	/* Called to toggle the verbose output */
 		    	fnDrawCallback : function(oSettings){
 		    		//we have nothing to hide
 		    		if(!self.options.showIncludeVerbose || !self._table || self._table.fnGetNodes().length == 0)
@@ -521,7 +521,7 @@ function OpenmrsSearch(div, showIncludeVoided, searchHandler, selectionHandler, 
 		    		if(self.options.selectionHandler) {
 		    			$j(nRow).unbind('click').bind('click', function() {
 		    				//Register onclick handlers to each row
-		    				self._doSelected(iDisplayIndexFull, self._results[iDisplayIndexFull]);
+		    				self._doSelected(iDisplayIndexFull, currItem);
 		    			});
 		    		}
 		    		
