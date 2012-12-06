7a7d4b55ceac6c8b8e13e67d5357e3e49c772b33
wluyima <wyclif@openmrs.com>
2012-12-05 21:21:20 -0500

    Modules folder should be configured via a runtime property, not a global property, and it should default to a folder that depends on the webapp name - TRUNK-3754

--- api/src/main/java/org/openmrs/module/ModuleConstants.java
+++ api/src/main/java/org/openmrs/module/ModuleConstants.java
@@ -77,4 +77,10 @@ public class ModuleConstants {
 	 */
 	public static final String RUNTIMEPROPERTY_MODULE_LIST_TO_LOAD = "module.list_to_load";
 	
+	/**
+	 * Name of the runtime property that will tell the system where to look for modules to load. Can
+	 * be either relative or absolute
+	 */
+	public static final String REPOSITORY_FOLDER_RUNTIME_PROPERTY = "module.repository_folder";
+	
 }
