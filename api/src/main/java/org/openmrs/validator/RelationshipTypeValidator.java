dc7864ee686932958f985b1690499b620a33ada3
k-joseph <joseph.kaweesi2012@gmail.com>
2012-12-27 18:56:54 +0300

    added RelationshipTypeValidator.java, added rejectIfDuplicateMetadataName() to ValidateUtil.java, and finally add

--- api/src/main/java/org/openmrs/validator/RelationshipTypeValidator.java
+++ api/src/main/java/org/openmrs/validator/RelationshipTypeValidator.java
@@ -2,33 +2,28 @@
  * The contents of this file are subject to the OpenMRS Public License
  * Version 1.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
  *
- * Software distributed under the License is distributed on an "AS IS"
+* Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
  * License for the specific language governing rights and limitations
  * under the License.
  *
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
-
 package org.openmrs.validator;
+
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.RelationshipType;
-import org.openmrs.annotation.Handler;
+import org.openmrs.api.context.Context;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 /**
- * Validates attributes on the {@link RelationshipType} object.
- * 
- * @since 1.10
+ *
  */
-@Handler(supports = { RelationshipType.class }, order = 50)
-
 public class RelationshipTypeValidator implements Validator {
 	
 	/** Log for this class and subclasses */
@@ -45,37 +40,33 @@ public class RelationshipTypeValidator implements Validator {
 	}
 	
 	/**
-	 * Checks the form object for any inconsistencies/errors
-	 * 
-	 * @see org.springframework.validation.Validator#validate(java.lang.Object,
-	 *      org.springframework.validation.Errors)
-	 * @should fail validation if name is null or empty or whitespace
-	 * @should fail validation if nothing is passed into getIsToB() and getIsToA() thus Relationship type cannot be null
-	 * @should pass validation if all required fields have proper values
+	 * @see org.springframework.validation.Validator#validate(java.lang.Object, org.springframework.validation.Errors)
 	 */
-	 public void validate(Object obj, Errors errors) {
-		RelationshipType RelationshipType = (RelationshipType) obj;
-		
-		if (RelationshipType == null) {
-			errors.rejectValue("RelationhipType", "error.general");
+	public void validate(Object obj, Errors errors) {
+		RelationshipType relationshipType = (RelationshipType) obj;
+		if (relationshipType == null) {
+			errors.rejectValue("relationshipType", "error.general");
 		} else {
-			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "error.name");
-		}
 		
 		/**
-		 *       NOTE:
-		 *RelationshipType.aIsToB = A is to B
-		 *RelationshipType.bIsToA = B is to A
-		 *RelationshipType.aIsToB.required = A is to B name is required
-		 *RelationshipType.bIsToA.required = B is to A name is required
-		*/
- 		if (RelationshipType.getaIsToB() == null || RelationshipType.getaIsToB().equals(""))
-			errors.rejectValue("aIsToB", "RelationshipType.aIsToB.required");
-			errors.rejectValue("localizedAIsToB.unlocalizedValue", "RelationshipType.aIsToB.required");
- 		
- 		if (RelationshipType.getbIsToA() == null || RelationshipType.getbIsToA().equals(""))
-			errors.rejectValue("bIsToA", "RelationshipType.bIsToA.required");
-			errors.rejectValue("localizedBIsToA.unlocalizedValue", "RelationshipType.bIsToA.required");
- 		
-	 }
+		*check duplicate name 		
+		*/		
+			List<RelationshipType> rts = Context.getPersonService().getRelationshipTypes(relationshipType.toString());
+			
+			/**
+			*set name fields for comparison
+			*/
+			relationshipType.setName(relationshipType.toString());
+			for(org.openmrs.BaseOpenmrsMetadata dupobj : rts) {
+				dupobj.setName(dupobj.toString());
+			}	
+			org.openmrs.validator.ValidateUtil.rejectIfDuplicateMetadataName(relationshipType, rts.toArray(
+			new org.openmrs.BaseOpenmrsMetadata[rts.size()]), errors, "aIsToB", "general.error.nameAlreadyInUse");
+			
+			/**
+			*unset name field
+			*/
+			relationshipType.setName(null);
+		}
+	}
 }
