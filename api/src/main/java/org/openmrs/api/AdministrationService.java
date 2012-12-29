b8ade276b99728ee85bc549cbabe3978ebeabc09
suranga <surangakas@gmail.com>
2012-12-29 10:14:19 +0530

    TRUNK-3773

--- api/src/main/java/org/openmrs/api/AdministrationService.java
+++ api/src/main/java/org/openmrs/api/AdministrationService.java
@@ -552,15 +552,31 @@ public interface AdministrationService extends OpenmrsService {
 	public void deleteGlobalProperty(String propertyName);
 	
 	/**
-	 * Use
+	 * Save the given global property to the database. If the global property already exists,
+	 * then it will be overwritten
 	 * 
-	 * <pre>
-	 * saveGlobalProperty(new GlobalProperty(propertyName, propertyValue));
-	 * </pre>
-	 * 
-	 * @deprecated use #saveGlobalProperty(GlobalProperty)
+	 * @param propertyName the name of the global property to save
+	 * @param propertyValue the value of the global property to save
+	 * @should create global property in database
+	 * @should overwrite global property if exists
+	 * @should not allow different properties to have the same string with different case
+	 * @should save a global property whose typed value is handled by a custom datatype
 	 */
 	public void setGlobalProperty(String propertyName, String propertyValue);
+	
+	/**
+	 * Overwrites the value of the global property if it already exists. If the global property does
+	 * not exist, an exception will be thrown
+	 * 
+	 * @param propertyName  the name of the global property to overwrite
+	 * @param propertyValue  the value of the global property to overwrite
+	 * @throws IllegalStateException
+	 * @should update global property in database
+	 * @should overwrite global property if exists
+	 * @should not allow different properties to have the same string with different case
+	 * @should save a global property whose typed value is handled by a custom datatype
+	 */
+	public void updateGlobalProperty(String propertyName, String propertyValue) throws IllegalStateException;
 	
 	/**
 	 * Save the given global property to the database
