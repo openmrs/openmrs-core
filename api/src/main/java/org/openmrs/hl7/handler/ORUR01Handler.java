c81a72c2f5d1f96d6058b1d36568de0ce37134b1
wluyima <wyclif@openmrs.com>
2013-02-19 12:06:25 +0300

    Back porting to 1.9.x: Handle complex observations (ED) in ORUR01Handler - TRUNK-3887

--- api/src/main/java/org/openmrs/hl7/handler/ORUR01Handler.java
+++ api/src/main/java/org/openmrs/hl7/handler/ORUR01Handler.java
@@ -47,6 +47,7 @@ import org.openmrs.api.context.Context;
 import org.openmrs.hl7.HL7Constants;
 import org.openmrs.hl7.HL7InQueueProcessor;
 import org.openmrs.hl7.HL7Service;
+import org.openmrs.obs.ComplexData;
 import org.openmrs.util.OpenmrsConstants;
 import org.openmrs.util.OpenmrsUtil;
 import org.springframework.util.StringUtils;
@@ -64,6 +65,7 @@ import ca.uhn.hl7v2.model.v25.datatype.CX;
 import ca.uhn.hl7v2.model.v25.datatype.DLD;
 import ca.uhn.hl7v2.model.v25.datatype.DT;
 import ca.uhn.hl7v2.model.v25.datatype.DTM;
+import ca.uhn.hl7v2.model.v25.datatype.ED;
 import ca.uhn.hl7v2.model.v25.datatype.FT;
 import ca.uhn.hl7v2.model.v25.datatype.ID;
 import ca.uhn.hl7v2.model.v25.datatype.IS;
@@ -136,6 +138,7 @@ public class ORUR01Handler implements Application {
 	 * @should create an encounter and find the provider by uuid
 	 * @should create an encounter and find the provider by providerId
 	 * @should fail if the provider name type code is not specified and is not a personId
+	 * @should set complex data for obs with complex concepts
 	 */
 	public Message processMessage(Message message) throws ApplicationException {
 		
@@ -767,6 +770,22 @@ public class ORUR01Handler implements Application {
 				return null;
 			}
 			obs.setValueText(value.getValue());
+		} else if ("ED".equals(hl7Datatype)) {
+			ED value = (ED) obx5;
+			if (value == null || value.getData() == null || !StringUtils.hasText(value.getData().getValue())) {
+				log.warn("Not creating null valued obs for concept " + concept);
+				return null;
+			}
+			//we need to hydrate the concept so that the EncounterSaveHandler
+			//doesn't fail since it needs to check if it is a concept numeric
+			Concept c = Context.getConceptService().getConcept(obs.getConcept().getConceptId());
+			obs.setConcept(c);
+			String title = null;
+			if (obs.getValueCodedName() != null)
+				title = obs.getValueCodedName().getName();
+			if (!StringUtils.hasText(title))
+				title = c.getName().getName();
+			obs.setComplexData(new ComplexData(title, value.getData().getValue()));
 		} else {
 			// unsupported data type
 			// TODO: support RP (report), SN (structured numeric)
@@ -1063,6 +1082,13 @@ public class ORUR01Handler implements Application {
 		return location;
 	}
 	
+	/**
+	 * needs to find a Form based on information in MSH-21. example: 16^AMRS.ELD.FORMID
+	 * 
+	 * @param msh
+	 * @return
+	 * @throws HL7Exception
+	 */
 	private Form getForm(MSH msh) throws HL7Exception {
 		Integer formId = null;
 		try {
