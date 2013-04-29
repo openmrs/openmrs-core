98c22c0aaa14259cfa7c07e1ef1d9b39083f335e
arekp <arek.pien@gmail.com>
2013-04-30 00:03:56 +0200

    TRUNK-3567 Add Observation without Question Concept throws NullPointerException
    TRUNK-3567 Add Observation without Question Concept throws NullPointerException
    
    Applying auto format changes

--- web/src/main/java/org/openmrs/web/controller/observation/ObsFormController.java
+++ web/src/main/java/org/openmrs/web/controller/observation/ObsFormController.java
@@ -106,8 +106,7 @@ public class ObsFormController extends SimpleFormController {
 			obs.setValueCoded(obs.getValueCodedName().getConcept());
 		}
 		
-		// Set the complex obs value before we validate. See TRUNK-3353
-		if (obs.getConcept().isComplex()) {
+		if (obs.getConcept() != null && obs.getConcept().isComplex()) {
 			InputStream complexDataInputStream = setComplexData(obs, request);
 			if (complexDataInputStream != null) {
 				complexDataInputStream.close();
@@ -180,10 +179,12 @@ public class ObsFormController extends SimpleFormController {
 					httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "Obs.unvoidedSuccessfully");
 				}
 				
-			} catch (IllegalArgumentException e) {
+			}
+			catch (IllegalArgumentException e) {
 				errors.reject("invalidImage", "Obs.invalidImage");
 				return showForm(request, response, errors);
-			} catch (APIException e) {
+			}
+			catch (APIException e) {
 				httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, e.getMessage());
 				return showForm(request, response, errors);
 			}
