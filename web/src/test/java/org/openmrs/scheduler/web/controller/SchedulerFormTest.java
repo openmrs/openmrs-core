a7289b412773bf9e4f8d8fa1eb0dbcc5dc314dde
Lee Breisacher <leebreisacher@gmail.com>
2013-06-28 08:04:04 -0700

    move unit test to separate class to get away from @Before failure

---
+++ web/src/test/java/org/openmrs/scheduler/web/controller/SchedulerFormTest.java
@@ -0,0 +1,31 @@
+package org.openmrs.scheduler.web.controller;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Test;
+import org.openmrs.web.test.BaseWebContextSensitiveTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+
+public class SchedulerFormTest extends BaseWebContextSensitiveTest {
+	
+	@Autowired
+	private SchedulerFormController controller;
+	
+	/**
+	 * See TRUNK-3970: Error when adding a task in version 1.9.3
+	 * https://tickets.openmrs.org/browse/TRUNK-3970
+	 */
+	//	@Ignore
+	@Test
+	public void addANewTaskShouldNotError() throws Exception {
+		HttpServletRequest request = new MockHttpServletRequest("GET", "/openmrs/admin/scheduler/scheduler.form");
+		ModelAndView mav = controller.handleRequest(request, new MockHttpServletResponse());
+		assertNotNull(mav);
+	}
+	
+}
