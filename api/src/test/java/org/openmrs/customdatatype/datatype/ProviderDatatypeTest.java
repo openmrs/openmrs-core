fb15422e83a62ddf1365946425f992e9138c45f2
k-joseph <kaweesi.joseph2012@gmail.com>
2013-07-28 13:46:34 +0300

    [TRUNK-3623] Create a custom data type and its associated handler for providers

---
+++ api/src/test/java/org/openmrs/customdatatype/datatype/ProviderDatatypeTest.java
@@ -0,0 +1,54 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.customdatatype.datatype;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.Provider;
+import org.openmrs.api.context.Context;
+import org.openmrs.test.BaseContextSensitiveTest;
+
+/**
+ * test class for the org.openmrs.customdatatype.datatype.ProviderDatatype
+ */
+public class ProviderDatatypeTest extends BaseContextSensitiveTest {
+	
+	ProviderDatatype datatype;
+	
+	@Before
+	public void before() {
+		datatype = new ProviderDatatype();
+	}
+	
+	/**
+	 * @see Provider#deserialize(String)
+	 * @verifies reconstruct a provider serialized by this handler
+	 */
+	@Test
+	public void deserialize_shouldReconstructAProviderSerializedByThisHandler() throws Exception {
+		Provider provider = Context.getProviderService().getProviderByUuid("9bc5693a-f558-40c9-8177-145a4b119ca7");
+		Assert.assertEquals(provider, datatype.deserialize(datatype.serialize(provider)));
+	}
+	
+	/**
+	 * @see Provider#serialize(provider)
+	 * @verifies return a Provider uuid during serialization
+	 */
+	@Test
+	public void serialize_shouldReturnAProviderUuidDuringSerialization() throws Exception {
+		Provider provider = new Provider();
+		Assert.assertEquals(provider.getUuid(), datatype.serialize(provider));
+	}
+}
