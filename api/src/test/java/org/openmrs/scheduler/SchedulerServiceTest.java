936c85ba548269af46cc7bafc1107a01ec6fde6b
dkayiwa <dkayiwa@openmrs.org>
2013-10-03 12:12:32 +0300

    SchedulerServiceTest.saveTask_shouldSaveTaskToTheDatabase fails randomly - TRUNK-3831

--- api/src/test/java/org/openmrs/scheduler/SchedulerServiceTest.java
+++ api/src/test/java/org/openmrs/scheduler/SchedulerServiceTest.java
@@ -19,11 +19,13 @@ import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.api.context.Context;
 import org.openmrs.scheduler.tasks.AbstractTask;
@@ -36,6 +38,19 @@ import org.openmrs.util.OpenmrsClassLoader;
 public class SchedulerServiceTest extends BaseContextSensitiveTest {
 	
 	private static Log log = LogFactory.getLog(SchedulerServiceTest.class);
+	
+	@Before
+	public void setUp() throws Exception {
+		Context.flushSession();
+		
+		Collection<TaskDefinition> tasks = Context.getSchedulerService().getRegisteredTasks();
+		for (TaskDefinition task : tasks) {
+			Context.getSchedulerService().shutdownTask(task);
+			Context.getSchedulerService().deleteTask(task.getId());
+		}
+		
+		Context.flushSession();
+	}
 	
 	@Test
 	public void shouldResolveValidTaskClass() throws Exception {
