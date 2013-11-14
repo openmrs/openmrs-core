abb2abef5072621f7b33636cba1553562471030e
pamcdm <pmori@pmori.local>
2013-11-14 14:34:05 -0200

    delegate getDrugs to the larger method getDrugs(phrase,null,true, false, false, null, null)

--- api/src/main/java/org/openmrs/api/db/hibernate/HibernateConceptDAO.java
+++ api/src/main/java/org/openmrs/api/db/hibernate/HibernateConceptDAO.java
@@ -409,20 +409,7 @@ public class HibernateConceptDAO implements ConceptDAO {
 		List<String> words = ConceptWord.getUniqueWords(phrase);
 		List<Drug> conceptDrugs = new Vector<Drug>();
 		
-		if (words.size() > 0) {
-			
-			Criteria searchCriteria = sessionFactory.getCurrentSession().createCriteria(Drug.class, "drug");
-			
-			Iterator<String> word = words.iterator();
-			searchCriteria.add(Restrictions.ilike("name", word.next(), MatchMode.ANYWHERE));
-			while (word.hasNext()) {
-				String w = word.next();
-				log.debug(w);
-				searchCriteria.add(Restrictions.ilike("name", w, MatchMode.ANYWHERE));
-			}
-			searchCriteria.addOrder(Order.asc("drug.concept"));
-			conceptDrugs = searchCriteria.list();
-		}
+        conceptDrugs = getDrugs(phrase,null,true, false, false, null, null);
 		
 		return conceptDrugs;
 	}
@@ -1432,7 +1419,7 @@ public class HibernateConceptDAO implements ConceptDAO {
 	@Override
 	public List<Drug> getDrugs(String drugName, Concept concept, boolean searchOnPhrase, boolean searchDrugConceptNames,
 	        boolean includeRetired, Integer start, Integer length) throws DAOException {
-
+		
 		Criteria searchCriteria = sessionFactory.getCurrentSession().createCriteria(Drug.class, "drug");
 		if (StringUtils.isBlank(drugName) && concept == null)
 			return Collections.emptyList();
