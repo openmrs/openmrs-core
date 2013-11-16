d748fb73ba1e896d5a05f222b9a50311449ac841
Patrick Turley [pturley] <patrick.turley@gmail.com>
2013-11-15 23:09:37 -0600

    adding vagrant setup with mysql.  Needs more work for actual setup, but this is a start

---
+++ puppet/modules/mysql/manifests/server/root_password.pp
@@ -0,0 +1,19 @@
+#
+class mysql::server::root_password {
+
+  $options = $mysql::server::options
+
+  # manage root password if it is set
+  if $mysql::server::root_password != 'UNSET' {
+    mysql_user { 'root@localhost':
+      ensure        => present,
+      password_hash => mysql_password($mysql::server::root_password),
+    }
+
+    file { "${::root_home}/.my.cnf":
+      content => template('mysql/my.cnf.pass.erb'),
+      require => Mysql_user['root@localhost'],
+    }
+  }
+
+}
