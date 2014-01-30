f4ec499d0c4dd296c4cbc1708716f15fc48a24c6
ujwala <contact.uju@gmail.com>
2014-01-30 09:30:11 -0600

    Trunk 4140

--- api/src/main/java/org/openmrs/api/db/hibernate/HibernateContextDAO.java
+++ api/src/main/java/org/openmrs/api/db/hibernate/HibernateContextDAO.java
@@ -13,6 +13,7 @@
  */
 package org.openmrs.api.db.hibernate;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.Properties;
@@ -377,12 +378,11 @@ public class HibernateContextDAO implements ContextDAO {
 		}
 		
 		// load in the default hibernate properties from hibernate.default.properties
-		InputStream propertyStream = null;
+
 		try {
 			Properties props = new Properties();
-			propertyStream = ConfigHelper.getResourceAsStream("/hibernate.default.properties");
-			OpenmrsUtil.loadProperties(props, propertyStream);
-			
+			File file= new File(getClass().getClassLoader().getResource("/hibernate.default.properties").getFile());
+			OpenmrsUtil.loadProperties(props,file);
 			// add in all default properties that don't exist in the runtime
 			// properties yet
 			for (Map.Entry<Object, Object> entry : props.entrySet()) {
@@ -390,14 +390,12 @@ public class HibernateContextDAO implements ContextDAO {
 					runtimeProperties.put(entry.getKey(), entry.getValue());
 			}
 		}
-		finally {
-			try {
-				propertyStream.close();
-			}
-			catch (Throwable t) {
+		
+			
+		catch (Throwable t) {
 				// pass
 			}
-		}
+		
 		
 	}
 	
