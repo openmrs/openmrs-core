e570c145f6882c7127bc7c4c2c61f80c98c96e39
wyclif <wyclif@openmrs.org>
2014-02-05 20:06:23 -0500

    Removed failing tests since it is no longer relevant

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -102,25 +102,6 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 		Assert.assertNull(Context.getOrderService().getOrderByUuid("some invalid uuid"));
 	}
 	
-	/**
-	 * @see {@link OrderService#saveOrder(Order)}
-	 */
-	@Test
-	@Verifies(value = "when saving a discontinuedReasonNonCoded parameter the value is correctly stored to the database", method = "saveOrder(Order)")
-	public void saveOrder_shouldSaveDiscontinuedReasonNonCoded() throws Exception {
-		String uuid = "921de0a3-05c4-444a-be03-e01b4c4b9142";
-		Order order = Context.getOrderService().getOrderByUuid(uuid);
-		String discontinuedReasonNonCoded = "Non coded discontinued reason";
-		
-		order.setOrderReasonNonCoded(discontinuedReasonNonCoded);
-		OrderService orderService = Context.getOrderService();
-		orderService.saveOrder(order);
-		
-		order = Context.getOrderService().getOrderByUuid(uuid);
-		
-		Assert.assertEquals(discontinuedReasonNonCoded, order.getOrderReasonNonCoded());
-	}
-	
 	@Test
 	public void purgeOrder_shouldDeleteObsThatReference() throws Exception {
 		executeDataSet("org/openmrs/api/include/OrderServiceTest-deleteObsThatReference.xml");
