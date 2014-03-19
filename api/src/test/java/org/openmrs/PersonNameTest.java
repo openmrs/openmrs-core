bf5c4fe5b3eca0f62e5273a177864432bbd074ee
kushal8 <kmit.kushal@gmail.com>
2014-03-19 20:17:55 +0530

    TRUNK-344 PersonName.getFullName() should use the configured NameLayout

--- api/src/test/java/org/openmrs/PersonNameTest.java
+++ api/src/test/java/org/openmrs/PersonNameTest.java
@@ -466,4 +466,34 @@ public class PersonNameTest {
 		Assert.assertEquals("Bob Jones", pn.getFullName());
 	}
 	
+	/**
+	 * @see {@link PersonName#getFullName()}
+	 */
+	@Test
+	@Verifies(value = "should Not return Long If Person Name Format Is Short", method = "getFullName()")
+	public void getFullName_shouldNotReturnLongIfPersonNameFormatIsShort() throws Exception {
+		PersonName pn = new PersonName();
+		pn.setGivenName("Taylor");
+		pn.setMiddleName("Bob");
+		pn.setFamilyName("Jones");
+		pn.setFamilyNamePrefix("jr.");
+		PersonName.setFormat(OpenmrsConstants.PERSON_NAME_FORMAT_SHORT);
+		Assert.assertEquals(pn.getFullName(), "Taylor Bob Jones");
+	}
+	
+	@Test
+	@Verifies(value = "should Not return Short If Person Name Format Is Long", method = "getFullName()")
+	public void getFullName_shouldNotReturnShortIfPersonNameFormatIsLong() throws Exception {
+		PersonName pn = new PersonName();
+		pn.setGivenName("Taylor");
+		pn.setMiddleName("Bob");
+		pn.setFamilyNamePrefix("Wilson");
+		pn.setFamilyName("Mark");
+		pn.setFamilyName2("Jones");
+		pn.setFamilyNameSuffix("jr.");
+		pn.setDegree("3");
+		PersonName.setFormat(OpenmrsConstants.PERSON_NAME_FORMAT_LONG);
+		Assert.assertEquals(pn.getFullName(), "Taylor Bob Wilson Mark Jones jr. 3");
+	}
+	
 }
