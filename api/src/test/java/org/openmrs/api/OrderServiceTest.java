f3836a4414361d00837c1ba684b8bc3e33738794
harsha89 <harsz89@gmail.com>
2014-04-19 02:39:24 +0530

    added getDrugRoutes function as specified in TRUNK-4347

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -1927,4 +1927,26 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 		orderService.saveOrder(order, null);
 		assertNotNull(order.getOrderId());
 	}
+
+    /**
+     * @see org.openmrs.api.OrderService#getDrugRoutes()
+     * @verifies get drug routes associated with concept uuid provided in global properties
+     */
+    @Test
+    public void getDrugRoutes_shouldGetDrugRoutesAssociatedConceptPrividedInGlobalProperties() throws Exception {
+        Concept drugRoutes = new Concept();
+        ConceptName cn = new ConceptName("Test concept for drug routes", Locale.US);
+        drugRoutes.addName(cn);
+        drugRoutes.setSet(true);
+        drugRoutes.addSetMember(conceptService.getConcept(3));
+        drugRoutes.addSetMember(conceptService.getConcept(4));
+        drugRoutes.addSetMember(conceptService.getConcept(5));
+        drugRoutes=conceptService.saveConcept(drugRoutes);
+        AdministrationService as = Context.getAdministrationService();
+        List<GlobalProperty> globalProperties = Context.getAdministrationService().getAllGlobalProperties();
+        globalProperties.add(new GlobalProperty(OpenmrsConstants.DRUG_ROUTE_CONCEPT_UUID, drugRoutes.getUuid(), "test"));
+        as.saveGlobalProperties(globalProperties);
+        List<Concept> drugRoutesList=orderService.getDrugRoutes();
+        assertEquals(3,drugRoutesList.size());
+    }
 }
