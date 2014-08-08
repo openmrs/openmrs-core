4717ec6d3e91a3a10a2a41f8b81981df78749a88
Miss Beens <bhartin@thoughtworks.com>
2014-08-08 19:05:22 +0530

    Trunk-4444 Validate order overlap with scheduled date
    Trunk-4444 Implementing feedback on earlier commit
    
    Trunk-4444 Adding void order check while checking overlapping schedule of orders
    
    Trunk-4444 Moving checkoverlap method to OrderUtil
    
    Follow up to ensure that orders that start or end on dates equal to other orders' start and end are considered to be overlapping  - TRUNK-4444
    
    Follow up to ensure that orders that start or end on dates equal to other orders' start and end are considered to be overlapping  - TRUNK-4444

--- api/src/main/java/org/openmrs/order/OrderUtil.java
+++ api/src/main/java/org/openmrs/order/OrderUtil.java
@@ -15,7 +15,9 @@ package org.openmrs.order;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Order;
 import org.openmrs.OrderType;
+import org.openmrs.util.OpenmrsUtil;
 
 /**
  * Contains convenience methods for working with Orders.
@@ -50,6 +52,44 @@ public class OrderUtil {
 			}
 		}
 		return false;
+	}
+	
+	/**
+	 * Checks if the schedules of the specified orders overlap, Note this only makes a check that is
+	 * purely based on dates ignoring other properties like patient, voided, careSetting and the
+	 * orderable
+	 * 
+	 * @since 1.10
+	 * @param order1, order2 orders to match
+	 * @return true if the schedules overlap otherwise false
+	 * @should return true if order1 and order2 do not have end date
+	 * @should return true if order1 and order2 have same start dates
+	 * @should return false if order1 ends before order2 starts
+	 * @should return false if order1 starts after order2
+	 * @should return true if order1 stops after the order2 has already been activated
+	 * @should return true if order1 starts when the order2 is active
+	 * @should return true if order1 starts before order2 and ends after order2
+	 * @should return true if order2 starts before order1 and ends after order1
+	 * @should return true if order1 starts on the stop date of order2
+	 * @should return true if order1 ends on the start date of order2
+	 * @should return true if both orders start and end on same dates
+	 */
+	public static boolean checkScheduleOverlap(Order order1, Order order2) {
+		if (order2.getEffectiveStopDate() == null && order1.getEffectiveStopDate() == null) {
+			return true;
+		}
+		
+		if (order2.getEffectiveStopDate() == null) {
+			return OpenmrsUtil.compare(order1.getEffectiveStopDate(), order2.getEffectiveStartDate()) > -1;
+		}
+		
+		if (order1.getEffectiveStopDate() == null) {
+			return (OpenmrsUtil.compare(order1.getEffectiveStartDate(), order2.getEffectiveStartDate()) > -1)
+			        && (OpenmrsUtil.compare(order1.getEffectiveStartDate(), order2.getEffectiveStopDate()) < 1);
+		}
+		
+		return (OpenmrsUtil.compare(order1.getEffectiveStartDate(), order2.getEffectiveStopDate()) < 1)
+		        && (OpenmrsUtil.compare(order1.getEffectiveStopDate(), order2.getEffectiveStartDate()) > -1);
 	}
 	
 }
