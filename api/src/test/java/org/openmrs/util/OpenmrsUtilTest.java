96e61a8d884ba0b0418689ef342ee15c63e53554
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2014-09-12 14:00:50 +0200

    Fixed formatting

--- api/src/test/java/org/openmrs/util/OpenmrsUtilTest.java
+++ api/src/test/java/org/openmrs/util/OpenmrsUtilTest.java
@@ -796,9 +796,9 @@ public class OpenmrsUtilTest extends BaseContextSensitiveTest {
 		OpenmrsUtil.storeProperties(properties, actual, null);
 		
 		// Java's underlying implementation correctly writes:
-        // blacklistRegex=[^\\p{InBasicLatin}\\p{InLatin1Supplement}]
-        // This method didn't exist in Java 5, which is why we wrote a utility method in the first place, so we should
-        // just get rid of our own implementation, and use the underlying java one.
+		// blacklistRegex=[^\\p{InBasicLatin}\\p{InLatin1Supplement}]
+		// This method didn't exist in Java 5, which is why we wrote a utility method in the first place, so we should
+		// just get rid of our own implementation, and use the underlying java one.
 		properties.store(new OutputStreamWriter(expected, utf8), null);
 		
 		assertThat(actual.toByteArray(), is(expected.toByteArray()));
