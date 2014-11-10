6104473f4499d6125c55551e83455f0d7b030f36
Deepak N <endeep123@gmail.com>
2014-11-10 10:46:32 +0530

    TRUNK-4532 | Chethan, Deepak| Fix validation while saving revised order overlapping with active order

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -1401,7 +1401,7 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 		assertTrue(duplicateOrder.isCurrent());
 		assertEquals(cd4Count, duplicateOrder.getConcept());
 		
-		Order order = new Order();
+		Order order = new TestOrder();
 		order.setPatient(patient);
 		order.setCareSetting(orderService.getCareSetting(2));
 		order.setConcept(cd4Count);
@@ -1412,6 +1412,48 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 		expectedException.expect(APIException.class);
 		expectedException.expectMessage("Cannot have more than one active order for the same orderable and care setting");
 		orderService.saveOrder(order, null);
+	}
+	
+	/**
+	 * @verifies fail if an active order for the same concept and care setting exists
+	 * @see OrderService#saveOrder(org.openmrs.Order, OrderContext)
+	 */
+	@Test
+	public void saveOrder_shouldFailForRevisionOrderIfAnActiveOrderForTheSameConceptAndCareSettingExists() throws Exception {
+		//sanity check that we have an active order
+		final Patient patient = patientService.getPatient(2);
+		final Concept cd4Count = conceptService.getConcept(5497);
+		TestOrder activeOrder = new TestOrder();
+		activeOrder.setPatient(patient);
+		activeOrder.setConcept(cd4Count);
+		activeOrder.setEncounter(encounterService.getEncounter(6));
+		activeOrder.setOrderer(providerService.getProvider(1));
+		activeOrder.setCareSetting(orderService.getCareSetting(2));
+		activeOrder.setDateActivated(new Date());
+		activeOrder.setAutoExpireDate(DateUtils.addDays(new Date(), 10));
+		orderService.saveOrder(activeOrder, null);
+		
+		//New Drug order in future for same concept
+		TestOrder secondOrder = new TestOrder();
+		secondOrder.setPatient(activeOrder.getPatient());
+		secondOrder.setConcept(activeOrder.getConcept());
+		secondOrder.setEncounter(encounterService.getEncounter(6));
+		secondOrder.setOrderer(providerService.getProvider(1));
+		secondOrder.setCareSetting(activeOrder.getCareSetting());
+		secondOrder.setDateActivated(new Date());
+		secondOrder.setScheduledDate(DateUtils.addDays(activeOrder.getEffectiveStopDate(), 1));
+		secondOrder.setUrgency(Order.Urgency.ON_SCHEDULED_DATE);
+		orderService.saveOrder(secondOrder, null);
+		
+		//Revise Second Order to have date activated to overlap with active order
+		TestOrder revision = secondOrder.cloneForRevision();
+		revision.setScheduledDate(DateUtils.addDays(activeOrder.getEffectiveStartDate(), 2));
+		revision.setEncounter(encounterService.getEncounter(6));
+		revision.setOrderer(providerService.getProvider(1));
+		
+		expectedException.expect(APIException.class);
+		expectedException.expectMessage("Cannot have more than one active order for the same orderable and care setting");
+		orderService.saveOrder(revision, null);
 	}
 	
 	/**
