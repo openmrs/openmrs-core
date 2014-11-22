f88c8a5a2dc1076d3259db9347768b7a7e0b429a
harsha89 <harsz89@gmail.com>
2014-11-23 01:34:55 +0530

    Fix for coded concept broken with Most Recent Observation on dashboard TRUNK-4131
    Follow up to clean unit tests - TRUNK-4131

--- web/src/main/java/org/openmrs/web/dwr/DWRObsService.java
+++ web/src/main/java/org/openmrs/web/dwr/DWRObsService.java
@@ -26,10 +26,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.directwebremoting.WebContextFactory;
 import org.openmrs.Concept;
+import org.openmrs.ConceptAnswer;
 import org.openmrs.Encounter;
 import org.openmrs.Location;
 import org.openmrs.Obs;
 import org.openmrs.Person;
+import org.openmrs.api.AdministrationService;
 import org.openmrs.api.context.Context;
 import org.openmrs.util.OpenmrsUtil;
 
@@ -42,7 +44,7 @@ public class DWRObsService {
 	
 	/**
 	 * Void the given observation
-	 *
+	 * 
 	 * @param obsId
 	 * @param reason
 	 */
@@ -60,7 +62,7 @@ public class DWRObsService {
 	
 	/**
 	 * Get all observations for the given encounter TODO: rename to getObservationsByEncounter
-	 *
+	 * 
 	 * @param encounterId
 	 * @return
 	 */
@@ -92,7 +94,7 @@ public class DWRObsService {
 	
 	/**
 	 * Auto generated method comment
-	 *
+	 * 
 	 * @param personId
 	 * @param encounterId
 	 * @param conceptId
@@ -100,13 +102,13 @@ public class DWRObsService {
 	 * @param obsDateStr
 	 */
 	public void createObs(Integer personId, Integer encounterId, Integer conceptId, String valueText, String obsDateStr)
-	        throws Exception {
+	    throws Exception {
 		createNewObs(personId, encounterId, null, conceptId, valueText, obsDateStr);
 	}
 	
 	/**
 	 * Auto generated method comment
-	 *
+	 * 
 	 * @param personId
 	 * @param encounterId
 	 * @param locationId
@@ -115,7 +117,7 @@ public class DWRObsService {
 	 * @param obsDateStr
 	 */
 	public void createNewObs(Integer personId, Integer encounterId, Integer locationId, Integer conceptId, String valueText,
-	        String obsDateStr) throws Exception {
+	                         String obsDateStr) throws Exception {
 		
 		log.info("Create new observation ");
 		
@@ -178,6 +180,42 @@ public class DWRObsService {
 				}
 			}
 			obs.setValueDatetime(obsDateValue);
+		} else if ("CWE".equals(hl7DataType)) {
+			if (valueText != null) {
+				Collection<ConceptAnswer> conceptAnswers = concept.getAnswers(false);
+				int conceptIdFromValueTest;
+				try {
+					conceptIdFromValueTest = Integer.parseInt(valueText);
+				}
+				catch (NumberFormatException e) {
+					log.error("Unable to parse given value text to integer while resolving concept id" + valueText, e);
+					throw new Exception("Can't resolve concept id. Please specify valid id" + valueText);
+				}
+				for (ConceptAnswer answer : conceptAnswers) {
+					if (answer.getAnswerConcept().getId() == conceptIdFromValueTest) {
+						obs.setValueCoded(answer.getAnswerConcept());
+					}
+				}
+			}
+		} else if ("BIT".equals(hl7DataType)) {
+			String booleanConceptId = null;
+			AdministrationService administrationService = Context.getAdministrationService();
+			if ("Yes".equalsIgnoreCase(valueText) || "True".equalsIgnoreCase(valueText) || "1".equals(valueText)) {
+				booleanConceptId = administrationService.getGlobalProperty("concept.true");
+			} else if ("No".equalsIgnoreCase(valueText) || "False".equalsIgnoreCase(valueText) || "0".equals(valueText)) {
+				booleanConceptId = administrationService.getGlobalProperty("concept.false");
+			}
+			Concept booleanConcept = null;
+			if (booleanConceptId != null) {
+				try {
+					booleanConcept = Context.getConceptService().getConcept(Integer.parseInt(booleanConceptId));
+				}
+				catch (NumberFormatException e) {
+					log.error("Unable to parse concept id string to integer to resolve concept" + booleanConceptId, e);
+					throw new Exception("No boolean concept found in the system");
+				}
+			}
+			obs.setValueCoded(booleanConcept);
 		} else {
 			obs.setValueText(valueText);
 		}
@@ -230,10 +268,10 @@ public class DWRObsService {
 		return objectList;
 	}
 	*/
-
+	
 	/**
 	 * Auto generated method comment
-	 *
+	 * 
 	 * @param personId
 	 * @param conceptId
 	 * @param encounterId
@@ -300,7 +338,7 @@ public class DWRObsService {
 	
 	/**
 	 * Auto generated method comment
-	 *
+	 * 
 	 * @param obsId
 	 * @return
 	 */
