29278cb691a3f8ac43f69e8b8537e3c3e128db98
Izaron <izarizar@mail.ru>
2014-12-05 14:48:37 +0300

    Collections.emptyList(),emptyMap() and emptySet() should be used instead of Collections.EMPTY_LIST, EMPTY_MAP and EMPTY_SET
    GCI-9
    Collections.emptyList(),emptyMap() and emptySet() should be used instead
    of Collections.EMPTY_LIST, EMPTY_MAP and EMPTY_SET

--- api/src/main/java/org/openmrs/api/impl/VisitServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/VisitServiceImpl.java
@@ -246,7 +246,7 @@ public class VisitServiceImpl extends BaseOpenmrsService implements VisitService
 	public List<Visit> getVisitsByPatient(Patient patient) throws APIException {
 		//Don't bother to hit the database
 		if (patient == null || patient.getId() == null) {
-			return Collections.EMPTY_LIST;
+			return Collections.emptyList();
 		}
 		
 		return Context.getVisitService().getVisits(null, Collections.singletonList(patient), null, null, null, null, null,
@@ -271,7 +271,7 @@ public class VisitServiceImpl extends BaseOpenmrsService implements VisitService
 	public List<Visit> getVisitsByPatient(Patient patient, boolean includeInactive, boolean includeVoided)
 	        throws APIException {
 		if (patient == null || patient.getId() == null) {
-			return Collections.EMPTY_LIST;
+			return Collections.emptyList();
 		}
 		
 		return dao.getVisits(null, Collections.singletonList(patient), null, null, null, null, null, null, null,
