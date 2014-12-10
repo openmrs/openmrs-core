d974937c5357520b7ce23b6d243be9462a767db6
Anish Basu <anishbasu.pr@gmail.com>
2014-12-10 20:50:12 +0530

    Fixed the static setter to comply with code guidelines

--- api/src/main/java/org/openmrs/api/impl/OrderServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/OrderServiceImpl.java
@@ -753,7 +753,7 @@ public class OrderServiceImpl extends BaseOpenmrsService implements OrderService
 	 */
 	@Override
 	public void globalPropertyChanged(GlobalProperty newValue) {
-		setOrderNumberGeneratorNull();
+		setOrderNumberGenerator(null);
 	}
 	
 	/**
@@ -761,14 +761,14 @@ public class OrderServiceImpl extends BaseOpenmrsService implements OrderService
 	 */
 	@Override
 	public void globalPropertyDeleted(String propertyName) {
-		setOrderNumberGeneratorNull();
+		setOrderNumberGenerator(null);
 	}
 	
 	/**
 	 * Helper method to deter instance methods from setting static fields
 	 */
-	private static void setOrderNumberGeneratorNull() {
-		orderNumberGenerator = null;
+	private static void setOrderNumberGenerator(OrderNumberGenerator orderNumberGenerator) {
+		OrderServiceImpl.orderNumberGenerator = orderNumberGenerator;
 	}
 	
 	/**
