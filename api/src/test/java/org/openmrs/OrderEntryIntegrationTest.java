c99841ee6c45f482bf1baa9d04c18f69bb48807e
Darius Jazayeri <jazayeri@alum.mit.edu>
2015-01-21 16:06:13 -0800

    TRUNK-4580 - Fixing compile error (because I didn't verify the build after a git cherry-pick)

--- api/src/test/java/org/openmrs/OrderEntryIntegrationTest.java
+++ api/src/test/java/org/openmrs/OrderEntryIntegrationTest.java
@@ -27,6 +27,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
