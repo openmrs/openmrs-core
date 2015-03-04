8b928fed53690b856d02eab5393846761f3fedaf
Burke Mamlin <burke@openmrs.org>
2015-03-04 13:00:20 -0500

    Converted to MPL 2.0 HD and standard line endings

--- web/src/main/java/org/springframework/http/converter/json/MappingJacksonHttpMessageConverter.java
+++ web/src/main/java/org/springframework/http/converter/json/MappingJacksonHttpMessageConverter.java
@@ -1,19 +1,12 @@
-/*
- * Copyright 2002-2013 the original author or authors.
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
  */
-
 package org.springframework.http.converter.json;
 
 import java.io.IOException;
