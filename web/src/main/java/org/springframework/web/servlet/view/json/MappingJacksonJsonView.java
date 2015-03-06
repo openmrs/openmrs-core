fc5c31adf4d177b6b415f771a5df73df0816ea98
Kristopher Schmidt <kris.schmidt7@gmail.com>
2015-03-06 00:32:42 -0500

    TRUNK-4607: fix springframework license files and @since, ignore springframework license files from license check
    TRUNK-4607: fix license check paths in pom.xml

--- web/src/main/java/org/springframework/web/servlet/view/json/MappingJacksonJsonView.java
+++ web/src/main/java/org/springframework/web/servlet/view/json/MappingJacksonJsonView.java
@@ -1,11 +1,22 @@
-/**
- * This Source Code Form is subject to the terms of the Mozilla Public License,
- * v. 2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
- * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+/** 
+ * NOTE: this class, which has been removed in Spring 4, has been copied over from Spring 3.2.7 to support old style Jackson 1 serialization,
+ * which is still the default as of the Spring 4 upgrade. Spring suggests an upgrade to Jackson 2.
+ **/
+
+/*
+ * Copyright 2002-2012 the original author or authors.
  *
- * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
- * graphic logo is a trademark of OpenMRS Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.springframework.web.servlet.view.json;
 
@@ -42,7 +53,7 @@ import org.springframework.web.servlet.view.AbstractView;
  * @author Arjen Poutsma
  * @author Rossen Stoyanchev
  * @author Juergen Hoeller
- * @since 3.0
+ * @since Spring 3.0
  * @see org.springframework.http.converter.json.MappingJacksonHttpMessageConverter
  */
 public class MappingJacksonJsonView extends AbstractView {
