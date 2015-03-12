622bc39ab67bfa0376fac3d777ee2bf3d22a4e39
mogoodrich <mgoodrich@pih.org>
2015-03-12 12:02:54 -0400

    TRUNK-4636: NameTemplate should provide a format method

--- api/src/main/java/org/openmrs/layout/name/NameTemplate.java
+++ api/src/main/java/org/openmrs/layout/name/NameTemplate.java
@@ -9,8 +9,16 @@
  */
 package org.openmrs.layout.name;
 
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang.StringUtils;
+import org.openmrs.PersonName;
+import org.openmrs.api.APIException;
 import org.openmrs.layout.LayoutSupport;
 import org.openmrs.layout.LayoutTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @since 1.12
@@ -23,6 +31,40 @@ public class NameTemplate extends LayoutTemplate {
 	
 	public String getNonLayoutToken() {
 		return "IS_NOT_NAME_TOKEN";
+	}
+	
+	public String format(PersonName personName) {
+		
+		List<String> personNameLines = new ArrayList<String>();
+		List<List<Map<String, String>>> lines = getLines();
+		String layoutToken = getLayoutToken();
+		
+		try {
+			for (List<Map<String, String>> line : lines) {
+				String nameLine = "";
+				Boolean hasToken = false;
+				for (Map<String, String> lineToken : line) {
+					if (lineToken.get("isToken").equals(layoutToken)) {
+						String tokenValue = BeanUtils.getProperty(personName, lineToken.get("codeName"));
+						if (StringUtils.isNotBlank(tokenValue)) {
+							hasToken = true;
+							nameLine += tokenValue;
+						}
+					} else {
+						nameLine += lineToken.get("displayText");
+					}
+				}
+				// only display a line if there's at least one token within it we've been able to resolve
+				if (StringUtils.isNotBlank(nameLine) && hasToken) {
+					personNameLines.add(nameLine);
+				}
+			}
+			// bit of hack, but we ignore the "line-by-line" format and just delimit a "line" with blank space
+			return StringUtils.join(personNameLines, " ");
+		}
+		catch (Exception e) {
+			throw new APIException("Unable to format personName " + personName.getId() + " using name template", e);
+		}
 	}
 	
 	@Override
--- web/src/main/java/org/openmrs/layout/web/name/NameTemplate.java
+++ web/src/main/java/org/openmrs/layout/web/name/NameTemplate.java
@@ -9,8 +9,16 @@
  */
 package org.openmrs.layout.web.name;
 
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang.StringUtils;
+import org.openmrs.PersonName;
+import org.openmrs.api.APIException;
 import org.openmrs.layout.web.LayoutSupport;
 import org.openmrs.layout.web.LayoutTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @deprecated
@@ -25,6 +33,40 @@ public class NameTemplate extends LayoutTemplate {
 	
 	public String getNonLayoutToken() {
 		return "IS_NOT_NAME_TOKEN";
+	}
+	
+	public String format(PersonName personName) {
+		
+		List<String> personNameLines = new ArrayList<String>();
+		List<List<Map<String, String>>> lines = getLines();
+		String layoutToken = getLayoutToken();
+		
+		try {
+			for (List<Map<String, String>> line : lines) {
+				String nameLine = "";
+				Boolean hasToken = false;
+				for (Map<String, String> lineToken : line) {
+					if (lineToken.get("isToken").equals(layoutToken)) {
+						String tokenValue = BeanUtils.getProperty(personName, lineToken.get("codeName"));
+						if (StringUtils.isNotBlank(tokenValue)) {
+							hasToken = true;
+							nameLine += tokenValue;
+						}
+					} else {
+						nameLine += lineToken.get("displayText");
+					}
+				}
+				// only display a line if there's at least one token within it we've been able to resolve
+				if (StringUtils.isNotBlank(nameLine) && hasToken) {
+					personNameLines.add(nameLine);
+				}
+			}
+			// bit of hack, but we ignore the "line-by-line" format and just delimit a "line" with blank space
+			return StringUtils.join(personNameLines, " ");
+		}
+		catch (Exception e) {
+			throw new APIException("Unable to format personName " + personName.getId() + " using name template", e);
+		}
 	}
 	
 	@Override
