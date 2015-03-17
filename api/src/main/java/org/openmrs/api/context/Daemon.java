be7d2ef1986121d92d7b543582ca4a039f453cd1
Tomasz Mueller <tmueller@soldevelo.com>
2015-03-17 15:44:16 +0100

    TRUNK-4645 "Thread.run() and Runnable.run() should not be called directly" refix

--- api/src/main/java/org/openmrs/api/context/Daemon.java
+++ api/src/main/java/org/openmrs/api/context/Daemon.java
@@ -164,6 +164,8 @@ public class Daemon {
 	 * @should throw error if called from a non daemon thread
 	 * @should not throw error if called from a daemon thread
 	 */
+	@SuppressWarnings("squid:3AS1217")
+	//We intentionally do not start a new thread yet, rather wrap the run call in a session.
 	public static Thread runInNewDaemonThread(final Runnable runnable) {
 		// make sure we're already in a daemon thread
 		if (!isDaemonThread()) {
@@ -178,7 +180,7 @@ public class Daemon {
 				isDaemonThread.set(true);
 				try {
 					Context.openSession();
-                    new Thread(runnable).start();
+					runnable.run();
 				}
 				finally {
 					Context.closeSession();
@@ -259,6 +261,8 @@ public class Daemon {
 	 * @return the newly spawned {@link Thread}
 	 * @since 1.9.2
 	 */
+	@SuppressWarnings("squid:3AS1217")
+	//We intentionally do not start a new thread yet, rather wrap the run call in a session.
 	public static Thread runInDaemonThread(final Runnable runnable, DaemonToken token) {
 		if (!ModuleFactory.isTokenValid(token)) {
 			throw new ContextAuthenticationException("Invalid token " + token);
@@ -271,7 +275,7 @@ public class Daemon {
 				isDaemonThread.set(true);
 				try {
 					Context.openSession();
-                    new Thread(runnable).start();
+					runnable.run();
 				}
 				finally {
 					Context.closeSession();
