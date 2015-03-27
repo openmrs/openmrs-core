a1754bd3836127323226eae69d5940533b54aa20
Tomasz Mueller <tmueller@soldevelo.com>
2015-03-27 16:46:09 +0100

    TRUNK 4385 "Annotated BaseOpenmrs... classes"
    (cherry picked from commit e5f145769a118b779b61761fa098ac7639092422)

--- api/src/main/java/org/openmrs/BaseOpenmrsMetadata.java
+++ api/src/main/java/org/openmrs/BaseOpenmrsMetadata.java
@@ -13,6 +13,9 @@ import java.util.Date;
 
 import org.hibernate.search.annotations.Field;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /**
  * In OpenMRS, we distinguish between data and metadata within our data model. Metadata represent
  * system and descriptive data such as data types &mdash; a relationship type or encounter type.
@@ -22,29 +25,40 @@ import org.hibernate.search.annotations.Field;
  * @since 1.5
  * @see OpenmrsMetadata
  */
+@MappedSuperclass
 public abstract class BaseOpenmrsMetadata extends BaseOpenmrsObject implements OpenmrsMetadata {
 	
 	//***** Properties *****
+	@Column(name = "name", nullable = false, length = 255)
 	@Field
 	private String name;
 	
+	@Column(name = "description", length = 255)
 	private String description;
 	
+	@Column(name = "creator", nullable = false)
 	private User creator;
 	
+	@Column(name = "date_created", nullable = false)
 	private Date dateCreated;
 	
+	@Column(name = "changed_by")
 	private User changedBy;
 	
+	@Column(name = "date_changed")
 	private Date dateChanged;
 	
+	@Column(name = "retired", nullable = false)
 	@Field
 	private Boolean retired = Boolean.FALSE;
 	
+	@Column(name = "date_retired")
 	private Date dateRetired;
 	
+	@Column(name = "retired_by")
 	private User retiredBy;
 	
+	@Column(name = "retire_reason", length = 255)
 	private String retireReason;
 	
 	//***** Constructors *****
