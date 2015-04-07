e7c3cdafc1a5aec0c006ce0b9acd92e871fd643a
Justin Cohler <justincohler@gmail.com>
2015-04-07 17:40:32 -0400

    updated ForgotPasswordFormController to return dummy questions for invalid usernames

--- web/src/main/java/org/openmrs/web/controller/ForgotPasswordFormController.java
+++ web/src/main/java/org/openmrs/web/controller/ForgotPasswordFormController.java
@@ -9,18 +9,21 @@
  */
 package org.openmrs.web.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.commons.lang.StringUtils;
 import org.openmrs.User;
 import org.openmrs.api.context.Context;
 import org.openmrs.util.PrivilegeConstants;
@@ -50,6 +53,16 @@ public class ForgotPasswordFormController extends SimpleFormController {
 	}
 	
 	/**
+	 * The mapping from a user's IP address to the number of security question attempts from that IP
+	 */
+	private Map<String, Integer> securityQuestionAttemptsByIP = new HashMap<String, Integer>();
+	
+	/**
+	 * The mapping from a user's IP address to the time their reset was lockedOut
+	 */
+	private Map<String, Date> securityQuestionLockoutDateByIP = new HashMap<String, Date>();
+	
+	/**
 	 * The mapping from user's IP address to the number of attempts at logging in from that IP
 	 */
 	private Map<String, Integer> loginAttemptsByIP = new HashMap<String, Integer>();
@@ -75,6 +88,12 @@ public class ForgotPasswordFormController extends SimpleFormController {
 		String username = request.getParameter("uname");
 		
 		String ipAddress = request.getRemoteAddr();
+		
+		Integer securityQuestionAttempts = securityQuestionAttemptsByIP.get(ipAddress);
+		if (securityQuestionAttempts == null) {
+			securityQuestionAttempts = 1;
+		}
+		
 		Integer forgotPasswordAttempts = loginAttemptsByIP.get(ipAddress);
 		if (forgotPasswordAttempts == null) {
 			forgotPasswordAttempts = 1;
@@ -82,7 +101,7 @@ public class ForgotPasswordFormController extends SimpleFormController {
 		
 		boolean lockedOut = false;
 		
-		if (forgotPasswordAttempts > 5) {
+		if (forgotPasswordAttempts > 5 || securityQuestionAttempts > 5) {
 			lockedOut = true;
 			
 			Date lockedOutTime = lockoutDateByIP.get(ipAddress);
@@ -125,7 +144,10 @@ public class ForgotPasswordFormController extends SimpleFormController {
 				}
 				
 				if (user == null || StringUtils.isEmpty(user.getSecretQuestion())) {
-					httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, "auth.question.empty");
+					securityQuestionAttempts++;
+					httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "auth.question.fill");
+					request.setAttribute("secretQuestion", getDummySecurityQuestion(ipAddress));
+					
 				} else {
 					httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "auth.question.fill");
 					request.setAttribute("secretQuestion", user.getSecretQuestion());
@@ -150,7 +172,9 @@ public class ForgotPasswordFormController extends SimpleFormController {
 				
 				// check the secret question again in case the user got here "illegally"
 				if (user == null || StringUtils.isEmpty(user.getSecretQuestion())) {
-					httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, "auth.question.empty");
+					securityQuestionAttempts++;
+					httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "auth.question.fill");
+					request.setAttribute("secretQuestion", getDummySecurityQuestion(ipAddress));
 				} else if (user.getSecretQuestion() != null && Context.getUserService().isSecretAnswer(user, secretAnswer)) {
 					
 					StringBuilder randomPassword = new StringBuilder();
@@ -172,6 +196,7 @@ public class ForgotPasswordFormController extends SimpleFormController {
 					httpSession.setAttribute("loginAttempts", 0);
 					return new ModelAndView(new RedirectView(request.getContextPath() + "/options.form#Change Login Info"));
 				} else {
+					securityQuestionAttempts++;
 					httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, "auth.answer.invalid");
 					httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "auth.question.fill");
 					request.setAttribute("secretQuestion", user.getSecretQuestion());
@@ -182,6 +207,50 @@ public class ForgotPasswordFormController extends SimpleFormController {
 		loginAttemptsByIP.put(ipAddress, forgotPasswordAttempts);
 		request.setAttribute("uname", username);
 		return showForm(request, response, errors);
+	}
+	
+	/**
+	 * Method gets a dummy question from a list of dummy questions for the event where a 
+	 * user enters an invalid username in forgotPasswordForm or no security questions exist.
+	 * 
+	 * @param ipAddress
+	 * @return dummy question text
+	 */
+	public String getDummySecurityQuestion(String ipAddress) {
+		String question = null;
+		List<String> questions = getSecurityQuestions();
+		
+		if (questions == null || questions.isEmpty()) {
+			return null;
+		}
+		Random rand = new Random();
+		int randomNum = rand.nextInt(questions.size());
+		try {
+			Integer ipNumber = Integer.parseInt(ipAddress.replace(".", ""));
+			question = questions.get(ipNumber % questions.size());
+		}
+		catch (Exception e) {
+			log.error(e.getMessage());
+			question = questions.get(randomNum);
+		}
+		
+		return question;
+	}
+	
+	/**
+	 *  Method returns a list of dummy questions to return for security puposes. 
+	 *  
+	 * @return List of dummy security questions
+	 */
+	public List<String> getSecurityQuestions() {
+		List<String> questions = new ArrayList<String>();
+		questions.add(Context.getMessageSourceService().getMessage("auth.question.mothersMaidenName"));
+		questions.add(Context.getMessageSourceService().getMessage("auth.question.cityBorn"));
+		questions.add(Context.getMessageSourceService().getMessage("auth.question.fathersMiddleName"));
+		questions.add(Context.getMessageSourceService().getMessage("auth.question.favoriteTeacher"));
+		questions.add(Context.getMessageSourceService().getMessage("auth.question.firstPet"));
+		
+		return questions;
 	}
 	
 }
