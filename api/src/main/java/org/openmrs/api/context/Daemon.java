1372f1ba11c612f7497ec53864229e3db792b83f
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-04-10 12:29:32 +0200

    TRUNK-4645 Fixed suppress rule id

--- api/src/main/java/org/openmrs/api/context/Daemon.java
+++ api/src/main/java/org/openmrs/api/context/Daemon.java
@@ -168,7 +168,7 @@ public class Daemon {
 	 * @should throw error if called from a non daemon thread
 	 * @should not throw error if called from a daemon thread
 	 */
-	@SuppressWarnings("squid:3AS1217")
+	@SuppressWarnings("squid:S1217")
 	public static Thread runInNewDaemonThread(final Runnable runnable) {
 		// make sure we're already in a daemon thread
 		if (!isDaemonThread()) {
@@ -183,8 +183,9 @@ public class Daemon {
 				isDaemonThread.set(true);
 				try {
 					Context.openSession();
+					//Suppressing sonar issue "squid:S1217"
 					//We intentionally do not start a new thread yet, rather wrap the run call in a session.
-					runnable.run();
+					runnable.run(); 
 				}
 				finally {
 					Context.closeSession();
@@ -265,7 +266,7 @@ public class Daemon {
 	 * @return the newly spawned {@link Thread}
 	 * @since 1.9.2
 	 */
-	@SuppressWarnings("squid:3AS1217")
+	@SuppressWarnings("squid:S1217")
 	public static Thread runInDaemonThread(final Runnable runnable, DaemonToken token) {
 		if (!ModuleFactory.isTokenValid(token)) {
 			throw new ContextAuthenticationException("Invalid token " + token);
@@ -278,6 +279,7 @@ public class Daemon {
 				isDaemonThread.set(true);
 				try {
 					Context.openSession();
+					//Suppressing sonar issue "squid:S1217"
 					//We intentionally do not start a new thread yet, rather wrap the run call in a session.
 					runnable.run();
 				}
