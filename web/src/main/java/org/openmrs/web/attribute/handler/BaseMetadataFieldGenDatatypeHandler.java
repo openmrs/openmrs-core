5e645fc95bf1a7d2031665c56af1c3b05e9f29d8
wyclif <wyclif@openmrs.org>
2015-04-17 22:44:04 -0400

    Refactored existing base classes for custom datatypes and added more - TRUNK-3499
    Added missing license - TRUNK-3499
    
    Renamed test classes that contain no test methods

--- web/src/main/java/org/openmrs/web/attribute/handler/BaseMetadataFieldGenDatatypeHandler.java
+++ web/src/main/java/org/openmrs/web/attribute/handler/BaseMetadataFieldGenDatatypeHandler.java
@@ -1,48 +1,42 @@
 /**
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- *
- * Copyright (C) OpenMRS, LLC. All Rights Reserved.
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
  */
 package org.openmrs.web.attribute.handler;
 
-import org.apache.commons.lang.StringUtils;
 import org.openmrs.OpenmrsMetadata;
 import org.openmrs.customdatatype.CustomDatatype;
-import org.openmrs.customdatatype.SerializingCustomDatatype;
+import org.openmrs.customdatatype.datatype.BaseMetadataDatatype;
 
 /**
- * This is a superclass for custom datatypes for metadata
+ * This is a superclass for FieldGenDatatypeHandlers for OpenmrsMetadata
  * 
- * @since 1.10
+ * @since 1.12
  */
-public abstract class BaseMetadataFieldGenDatatypeHandler<T extends BaseOpenmrsFieldGenDatatypeHandler> implements FieldGenDatatypeHandler<SerializingCustomDatatype<T>, T> {
+public abstract class BaseMetadataFieldGenDatatypeHandler<DT extends BaseMetadataDatatype<T>, T extends OpenmrsMetadata> extends SerializingFieldGenDatatypeHandler<DT, T> {
 	
 	/**
-	 * @see org.openmrs.web.attribute.handler.HtmlDisplayableDatatypeHandler#toHtmlSummary(org.openmrs.customdatatype.CustomDatatype,
-	 *      java.lang.String)
-	 */
-	@Override
-	public CustomDatatype.Summary toHtmlSummary(CustomDatatype<T> datatype, String valueReference) {
-		return toHtmlSummary(datatype, valueReference);
-	}
-	
-	/**
-	 * @see org.openmrs.web.attribute.handler.HtmlDisplayableDatatypeHandler#toHtml(org.openmrs.customdatatype.CustomDatatype,
-	 *      java.lang.String)
+	 * @see SerializingFieldGenDatatypeHandler#toHtml(org.openmrs.customdatatype.CustomDatatype,
+	 *      String)
+	 * @should return the name
 	 */
 	@Override
 	public String toHtml(CustomDatatype<T> datatype, String valueReference) {
-		SerializingCustomDatatype<T> dt = (SerializingCustomDatatype<T>) datatype;
-		String result = ((OpenmrsMetadata) dt.deserialize(valueReference)).getName();
-		
-		return result;
+		return toHtmlSummary(datatype, valueReference).getSummary();
+	}
+	
+	/**
+	 * @see SerializingFieldGenDatatypeHandler#toHtmlSummary(org.openmrs.customdatatype.CustomDatatype,
+	 *      String)
+	 * @should use the name in the html summary instance
+	 */
+	@Override
+	public CustomDatatype.Summary toHtmlSummary(CustomDatatype<T> datatype, String valueReference) {
+		return datatype.getTextSummary(valueReference);
 	}
 }
