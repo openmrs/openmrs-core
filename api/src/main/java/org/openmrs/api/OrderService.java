513a40676dd2368dd43f0bf2b2f7af650f314581
dkayiwa <dkayiwa@openmrs.org>
2015-06-22 15:27:59 +0300

    TRUNK-4702. change attay of overlapping DrugOrders to array of UIDs
    Fixed failing tests - TRUNK-4702

--- api/src/main/java/org/openmrs/api/OrderService.java
+++ api/src/main/java/org/openmrs/api/OrderService.java
@@ -32,6 +32,8 @@ import org.openmrs.util.PrivilegeConstants;
  */
 public interface OrderService extends OpenmrsService {
 	
+	public static final String PARALLEL_ORDERS = "PARALLEL_ORDERS";
+	
 	/**
 	 * Setter for the Order data access object. The dao is used for saving and getting orders
 	 * to/from the database
@@ -85,7 +87,7 @@ public interface OrderService extends OpenmrsService {
 	 * @should pass if an active order for the same concept exists in a different care setting
 	 * @should set Order type of Drug Order to drug order if not set and concept not mapped
 	 * @should set Order type of Test Order to test order if not set and concept not mapped
-	 * @should fail if an active drug order for the same drug formulation exists
+	 * @should throw AmbiguousOrderException if an active drug order for the same drug formulation exists
 	 * @should pass if an active order for the same concept exists in a different care setting
 	 * @should fail for revision order if an active drug order for the same concept and care settings exists
 	 * @should pass for revision order if an active test order for the same concept and care settings exists
