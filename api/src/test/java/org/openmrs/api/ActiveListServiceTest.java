d46be75832570fa3ef93700a425a1a794483b1cb
Lluis Martinez Ferrando <lluismf@gmail.com>
2015-09-20 22:42:03 +0200

    Replace deprecated Assert class

--- api/src/test/java/org/openmrs/api/ActiveListServiceTest.java
+++ api/src/test/java/org/openmrs/api/ActiveListServiceTest.java
@@ -12,8 +12,7 @@ package org.openmrs.api;
 import java.util.Date;
 import java.util.List;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.Concept;
