5c7ce70b67104bd306040a9756e9d0be003af9f6
dkayiwa <dkayiwa@openmrs.org>
2015-09-21 21:03:37 +0300

    Modules without a required_version element in config.xml should be loaded (cherry picked from commit 6b61aeb06e76c4beb2af2d325acfc36f822635aa)

--- api/src/main/java/org/openmrs/module/ModuleUtil.java
+++ api/src/main/java/org/openmrs/module/ModuleUtil.java
@@ -251,6 +251,7 @@ public class ModuleUtil {
 	 * @should not match when version has wild card plus qualifier and is outside boundary
 	 * @should match when version has wild card and is within boundary
 	 * @should not match when version has wild card and is outside boundary
+	 * @should return true when required version is empty
 	 */
 	public static boolean matchRequiredVersions(String version, String versionRange) {
 		if (versionRange != null && !versionRange.equals("")) {
@@ -308,6 +309,11 @@ public class ModuleUtil {
 				}
 			}
 		}
+		else {
+			//no version checking if required version is not specified
+			return true;
+		}
+		
 		return false;
 	}
 	
