88d0d7a20fafcfcb9c2f7373b51b0e7ff8a8f7cd
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-10-06 15:42:03 +0200

    TRUNK-4771 Authorization checks are slow when running as Daemon
    (cherry picked from commit f2ea1e64a40707cad4a7480e5713e8ef4e872314)

--- api/src/main/java/org/openmrs/api/context/Context.java
+++ api/src/main/java/org/openmrs/api/context/Context.java
@@ -648,7 +648,11 @@ public class Context {
 	 * @return true if user has been authenticated in this context
 	 */
 	public static boolean isAuthenticated() {
-		return getAuthenticatedUser() != null;
+		if (Daemon.isDaemonThread()) {
+			return true;
+		} else {
+			return getAuthenticatedUser() != null;
+		}
 	}
 	
 	/**
@@ -686,7 +690,6 @@ public class Context {
 	 * @should give daemon user full privileges
 	 */
 	public static boolean hasPrivilege(String privilege) {
-		
 		// the daemon threads have access to all things
 		if (Daemon.isDaemonThread()) {
 			return true;
