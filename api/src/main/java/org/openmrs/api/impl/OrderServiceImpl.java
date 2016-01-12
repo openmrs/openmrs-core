6f1768af215c93d291de86e64963063ac5138e1e
Venkata Jaswanth <jaswanth@thoughtworks.com>
2016-01-12 15:31:55 +0530

    TRUNK-4805 | Retrospective stop of orders

--- api/src/main/java/org/openmrs/api/impl/OrderServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/OrderServiceImpl.java
@@ -337,9 +337,14 @@ public class OrderServiceImpl extends BaseOpenmrsService implements OrderService
 			return;
 		}
 
+		Date asOfDate = null;
+		if (isRetrospective) {
+			asOfDate = order.getDateActivated();
+		}
+
 		//Mark first order found corresponding to this DC order as discontinued.
 		List<? extends Order> orders = getActiveOrders(order.getPatient(), order.getOrderType(), order.getCareSetting(),
-		    null);
+		    asOfDate);
 		boolean isDrugOrderAndHasADrug = DrugOrder.class.isAssignableFrom(getActualType(order))
 		        && (((DrugOrder) order).getDrug() != null || ((DrugOrder) order).isNonCodedDrug());
 		Order orderToBeDiscontinued = null;
@@ -719,10 +724,13 @@ public class OrderServiceImpl extends BaseOpenmrsService implements OrderService
 		if (DISCONTINUE == orderToStop.getAction()) {
 			throw new APIException("An order with action " + DISCONTINUE + " cannot be discontinued.");
 		}
+		if (isRetrospective && orderToStop.getDateStopped() != null) {
+			throw new APIException("Order.retrospective.stopped.cannot.discontinued");
+		}
 		if (!isRetrospective && !orderToStop.isActive()) {
 			throw new APIException("Cannot discontinue an order that is already stopped, expired or voided");
 		}
-		else if (isRetrospective && !orderToStop.isActive(orderToStop.getDateActivated())) {
+		else if (isRetrospective && !orderToStop.isActive(discontinueDate)) {
 			throw new APIException("Order.retrospective.stopped.cannot.discontinued");
 		}
 
