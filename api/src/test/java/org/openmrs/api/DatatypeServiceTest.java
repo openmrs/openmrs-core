5fdeff9f0955758d1444110a1c1992a5ed1f559c
wyclif <wyclif@openmrs.org>
2016-01-21 14:54:58 -0500

    Fixed bug where a custom datatype handler that extends a generic abstract class can't be found  -TRUNK-3499

---
+++ api/src/test/java/org/openmrs/api/DatatypeServiceTest.java
@@ -0,0 +1,46 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.api;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openmrs.api.context.Context;
+import org.openmrs.attribute.handler.DateDatatypeHandler;
+import org.openmrs.attribute.handler.LocationDatatypeHandler;
+import org.openmrs.customdatatype.CustomDatatype;
+import org.openmrs.customdatatype.CustomDatatypeUtil;
+import org.openmrs.customdatatype.datatype.DateDatatype;
+import org.openmrs.customdatatype.datatype.LocationDatatype;
+import org.openmrs.test.BaseContextSensitiveTest;
+
+public class DatatypeServiceTest extends BaseContextSensitiveTest {
+	
+	/**
+	 * @see DatatypeService#getHandler(CustomDatatype,String)
+	 * @verifies return a handler for the specified datatype
+	 */
+	@Test
+	public void getHandler_shouldReturnAHandlerForTheSpecifiedDatatype() throws Exception {
+		DatatypeService service = Context.getDatatypeService();
+		CustomDatatype dateDatatype = CustomDatatypeUtil.getDatatype(DateDatatype.class.getName(), null);
+		Assert.assertEquals(DateDatatypeHandler.class, service.getHandler(dateDatatype, null).getClass());
+	}
+	
+	/**
+	 * @see DatatypeService#getHandler(CustomDatatype,String)
+	 * @verifies return a handler for a datatype that extends a generic superclass
+	 */
+	@Test
+	public void getHandler_shouldReturnAHandlerForADatatypeThatExtendsAGenericSuperclass() throws Exception {
+		DatatypeService service = Context.getDatatypeService();
+		CustomDatatype locationDatatype = CustomDatatypeUtil.getDatatype(LocationDatatype.class.getName(), null);
+		Assert.assertEquals(LocationDatatypeHandler.class, service.getHandler(locationDatatype, null).getClass());
+	}
+}
---
+++ api/src/test/java/org/openmrs/attribute/handler/BaseDatatypeHandler.java
@@ -0,0 +1,21 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.attribute.handler;
+
+import org.openmrs.customdatatype.CustomDatatype;
+import org.openmrs.customdatatype.CustomDatatypeHandler;
+
+public abstract class BaseDatatypeHandler<DT extends CustomDatatype<T>, T> implements CustomDatatypeHandler<DT, T> {
+	
+	@Override
+	public void setHandlerConfiguration(String handlerConfig) {
+		
+	}
+}
