8457aa36654a92a6bd98ea8812df1cba69d0d775
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-01-29 22:06:50 +0530

    MessageSourceService.getMessage methods that take a MessageSourceResolvable should return the proper default message

--- api/src/main/java/org/openmrs/messagesource/impl/MessageSourceServiceImpl.java
+++ api/src/main/java/org/openmrs/messagesource/impl/MessageSourceServiceImpl.java
@@ -96,7 +96,12 @@ public class MessageSourceServiceImpl implements MessageSourceService {
 	 *      java.util.Locale)
 	 */
 	public String getMessage(MessageSourceResolvable resolvable, Locale locale) {
-		return activeMessageSource.getMessage(resolvable, locale);
+		if((resolvable.getCodes()[0]).equals((activeMessageSource.getMessage(resolvable, locale)))){
+			return (resolvable.getCodes()[(resolvable.getCodes().length) - 1]);
+		}
+		else{
+			return activeMessageSource.getMessage(resolvable, locale);
+		}
 	}
 	
 	/**
