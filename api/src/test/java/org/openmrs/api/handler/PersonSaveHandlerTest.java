1208bea8dad456229eb8ebafc3f1ed75d26aaab0
vishnurao <vishnumechineni@gmail.com>
2016-02-05 02:16:07 +0530

    TRUNK-2004:Ignore blank person addresses on the patient forms

---
+++ api/src/test/java/org/openmrs/api/handler/PersonSaveHandlerTest.java
@@ -0,0 +1,46 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.api.handler;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openmrs.test.BaseContextSensitiveTest;
+import org.openmrs.Person;
+import org.openmrs.PersonAddress;
+import org.openmrs.PersonName;
+import org.openmrs.User;
+import org.openmrs.test.Verifies;
+
+import java.util.Date;
+
+/**
+ * Tests the {@link PersonSaveHandler} class
+ */
+public class PersonSaveHandlerTest extends BaseContextSensitiveTest {
+
+    /**
+     * @see PersonSaveHandler#handle(Person,User, Date,String)
+     */
+    @Test
+    @Verifies(value = "should ignore blank addresses", method = "handle(Person,User,Date,String)")
+    public void handle_shouldIgnoreBlankAddresses() throws Exception {
+        PersonSaveHandler handler = new PersonSaveHandler();
+        Person person = new Person();
+        PersonName personName = new PersonName("John","","Smith");
+        person.addName(personName);
+        person.setGender("M");
+        PersonAddress personAddress = new PersonAddress();
+        personAddress.setAddress1("     ");
+        person.addAddress(personAddress);
+
+        handler.handle(person,null,null,null);
+        Assert.assertEquals(0,person.getAddresses().size());
+    }
+}
