0bfc4ec3ae0ce677a11e1fee251442e69da669c5
Ivange Larry <ivangelarry@gmail.com>
2016-02-25 13:06:00 +0100

    TRUNK-4668: when a module is started, all its dependencies should be started

--- api/src/main/java/org/openmrs/module/ModuleFactory.java
+++ api/src/main/java/org/openmrs/module/ModuleFactory.java
@@ -234,16 +234,7 @@ public class ModuleFactory {
 					if (mod.isStarted()) {
 						continue; // skip over modules that are already started
 					}
-					
-					// Skip module if required ones are not started
-					if (!requiredModulesStarted(mod)) {
-						String message = getFailedToStartModuleMessage(mod);
-						log.error(message);
-						mod.setStartupErrorMessage(message);
-						notifySuperUsersAboutModuleFailure(mod);
-						continue;
-					}
-					
+										
 					try {
 						if (log.isDebugEnabled()) {
 							log.debug("starting module: " + mod.getModuleId());
@@ -579,7 +570,7 @@ public class ModuleFactory {
 	 * @see Daemon#startModule(Module)
 	 */
 	public static Module startModule(Module module) throws ModuleException {
-		return Daemon.startModule(module);
+		return startModule(module, false, null);
 	}
 	
 	/**
@@ -598,6 +589,31 @@ public class ModuleFactory {
 	 */
 	public static Module startModule(Module module, boolean isOpenmrsStartup,
 	        AbstractRefreshableApplicationContext applicationContext) throws ModuleException {
+		
+		if (!requiredModulesStarted(module)) {
+			int missingModules = 0;
+			
+			for (String packageName : module.getRequiredModulesMap().keySet()) {
+				Module mod = getModuleByPackage(packageName);
+				
+				// mod not installed
+				if (mod == null) {
+					missingModules++;
+					continue;
+				}
+				
+				if (!mod.isStarted())
+					startModule(mod);
+			}
+			
+			if (missingModules > 0) {
+				String message = getFailedToStartModuleMessage(module);
+				log.error(message);
+				module.setStartupErrorMessage(message);
+				notifySuperUsersAboutModuleFailure(module);
+				return module; // instead of return null, i realized that Daemon.startModule() always returns a Module object,irrespective of whether the startup succeeded  
+			}
+		}
 		return Daemon.startModule(module, isOpenmrsStartup, applicationContext);
 	}
 	
@@ -636,9 +652,10 @@ public class ModuleFactory {
 	public static Module startModuleInternal(Module module, boolean isOpenmrsStartup,
 	        AbstractRefreshableApplicationContext applicationContext) throws ModuleException {
 		
+		
 		if (module != null) {
-			
 			String moduleId = module.getModuleId();
+			
 			try {
 				
 				// check to be sure this module can run with our current version
@@ -794,7 +811,6 @@ public class ModuleFactory {
 				log.warn("Error while trying to start module: " + moduleId, e);
 				module.setStartupErrorMessage("Error while trying to start module", e);
 				notifySuperUsersAboutModuleFailure(module);
-				
 				// undo all of the actions in startup
 				try {
 					boolean skipOverStartedProperty = false;
