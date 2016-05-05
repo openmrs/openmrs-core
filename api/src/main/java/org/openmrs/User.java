cbe14310b0f3580491279aabbe910607d48fb682
dkayiwa <kayiwadaniel@gmail.com>
2016-05-05 22:48:08 +0300

    Fixing compiler error (cherry picked from commit e17493ab236ee8730a3220354d65de7f2ae49455)

--- api/src/main/java/org/openmrs/User.java
+++ api/src/main/java/org/openmrs/User.java
@@ -20,6 +20,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.APIException;
