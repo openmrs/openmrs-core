874c8ddc15a36ff08256d91210668fd5d5bdbb5a
Bradley LaVigne <lavigb23@uwosh.edu>
2016-06-26 09:53:26 -0500

    merge

--- api/src/test/java/org/openmrs/api/EncounterServiceTest.java
+++ api/src/test/java/org/openmrs/api/EncounterServiceTest.java
@@ -2958,14 +2958,14 @@ public class EncounterServiceTest extends BaseContextSensitiveTest {
 		}
 		
 		assertEquals("Two New Order Groups Get Saved", 2, orderGroups.size());
-	}	
+	}
 	/**
 	 * @see EncounterService#getEncountersByPatient(String,boolean)
 	 */
 	@Test
 	@Verifies(value = "should get all unvoided encounters for the given substring of patient names", method = "getEncountersByPatient(String,boolean)")
 	public void getEncountersByPatient_shouldGetAllUnvoidedEncountersForTheGivenSubstringOfPatientNames() throws Exception {
-		EncounterService encounterService = Context.getEncounterService();		
+		EncounterService encounterService = Context.getEncounterService();
 		List<Encounter> encounters = encounterService.getEncountersByPatient("Joh", false);
 		assertEquals(3, encounters.size());
 	}
