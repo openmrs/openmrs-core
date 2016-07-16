7f31f62ccc1de1f5fc3eadd6206a02aecffc4252
Felix Hammerl <fhammerl@thoughtworks.com>
2016-07-16 16:44:52 +0200

    TRUNK-2136: Add unit tests
    Add unit tests for ComplexObsHandler.getSupportedViews() and
    ComplexObsHandler.supportsView(), as discussed in review notes

---
+++ api/src/test/java/org/openmrs/obs/ImageHandlerTest.java
@@ -0,0 +1,50 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.obs;
+
+import org.junit.Test;
+import org.openmrs.obs.handler.ImageHandler;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+
+public class ImageHandlerTest {
+
+    @Test
+    public void shouldReturnSupportedViews() {
+        ImageHandler handler = new ImageHandler();
+        String[] actualViews = handler.getSupportedViews();
+        String[] expectedViews = { ComplexObsHandler.RAW_VIEW };
+
+        assertArrayEquals(actualViews, expectedViews);
+    }
+
+    @Test
+    public void shouldSupportRawView() {
+        ImageHandler handler = new ImageHandler();
+
+        assertTrue(handler.supportsView(ComplexObsHandler.RAW_VIEW));
+    }
+
+    @Test
+    public void shouldNotSupportOtherViews() {
+        ImageHandler handler = new ImageHandler();
+
+        assertFalse(handler.supportsView(ComplexObsHandler.HTML_VIEW));
+        assertFalse(handler.supportsView(ComplexObsHandler.PREVIEW_VIEW));
+        assertFalse(handler.supportsView(ComplexObsHandler.TEXT_VIEW));
+        assertFalse(handler.supportsView(ComplexObsHandler.TITLE_VIEW));
+        assertFalse(handler.supportsView(ComplexObsHandler.URI_VIEW));
+        assertFalse(handler.supportsView(""));
+        assertFalse(handler.supportsView((String) null));
+    }
+}
