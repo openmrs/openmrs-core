c8f42aef0d49e1c1d891713a271f0bb5a5349071
dkayiwa <kayiwadaniel@gmail.com>
2016-10-10 20:20:54 +0300

    Fixing failing tests

--- api/src/test/java/org/openmrs/api/db/hibernate/ImmutableEntityInterceptorTest.java
+++ api/src/test/java/org/openmrs/api/db/hibernate/ImmutableEntityInterceptorTest.java
@@ -70,7 +70,7 @@ public class ImmutableEntityInterceptorTest extends BaseContextSensitiveTest {
 		String[] currentState = new String[] { "new" };
 		ImmutableEntityInterceptor interceptor = new SomeImmutableEntityInterceptor();
 		expectedException.expect(APIException.class);
-		expectedException.expectMessage(is(Context.getMessageSourceService().getMessage("editing.fields.not.allowed", new Object[] { Order.class.getSimpleName() },  null)));
+		expectedException.expectMessage(is(Context.getMessageSourceService().getMessage("editing.fields.not.allowed", new Object[] { "[immutable]", Order.class.getSimpleName() },  null)));
 		interceptor.onFlushDirty(new Order(), null, currentState, previousState, propertyNames, null);
 	}
 	
@@ -100,7 +100,7 @@ public class ImmutableEntityInterceptorTest extends BaseContextSensitiveTest {
 		String[] currentState = new String[] { "new" };
 		ImmutableEntityInterceptor interceptor = new SomeImmutableEntityInterceptor();
 		expectedException.expect(APIException.class);
-		expectedException.expectMessage(is(Context.getMessageSourceService().getMessage("editing.fields.not.allowed", new Object[] { Order.class.getSimpleName() },  null)));
+		expectedException.expectMessage(is(Context.getMessageSourceService().getMessage("editing.fields.not.allowed", new Object[] { "[immutable]", Order.class.getSimpleName() },  null)));
 		Order order = new Order();
 		order.setVoided(true);
 		interceptor.onFlushDirty(order, null, currentState, previousState, propertyNames, null);
