627b40cbecbd40ca6ed49a9d2a94ad7516ac11b8
ykarim <ykarim@pioneeracademy.org>
2017-01-01 12:24:13 -0500

    TRUNK-5005: ModuleFactoryTest Error

--- api/src/main/java/org/openmrs/module/ModuleFactory.java
+++ api/src/main/java/org/openmrs/module/ModuleFactory.java
@@ -187,15 +187,18 @@ public class ModuleFactory {
 	public static void loadModules(List<File> modulesToLoad) {
 		// loop over the modules and load all the modules that we can
 		for (File f : modulesToLoad) {
-			// ignore .svn folder and the like
-			if (!f.getName().startsWith(".")) {
-				try {
-					Module mod = loadModule(f, true); // last module loaded wins
-					log.debug("Loaded module: " + mod + " successfully");
+			if (f.exists()) {
+				// ignore .svn folder and the like
+				if (!f.getName().startsWith(".")) {
+					try {
+						Module mod = loadModule(f, true); // last module loaded wins
+						log.debug("Loaded module: " + mod + " successfully");
+					} catch (Exception e) {
+						log.debug("Unable to load file in module directory: " + f + ". Skipping file.", e);
+					}
 				}
-				catch (Exception e) {
-					log.debug("Unable to load file in module directory: " + f + ". Skipping file.", e);
-				}
+			} else {
+				log.debug("Could not find file in module directory: " + f);
 			}
 		}
 		
