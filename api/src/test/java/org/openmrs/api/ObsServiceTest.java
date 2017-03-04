6253e05c2f042075b1183db21ec4dfa469e5284e
mks-d <dimitri@mekomsolutions.com>
2017-03-04 08:56:05 +0700

    TRUNK-5077: Aligning ObsServiceTest with branch 2.0.x.
      modified:   api/src/test/java/org/openmrs/api/ObsServiceTest.java
    
    (cherry picked from commit 4b628f1429f8c732f1bd967d258c8948b764b032)

--- api/src/test/java/org/openmrs/api/ObsServiceTest.java
+++ api/src/test/java/org/openmrs/api/ObsServiceTest.java
@@ -1331,6 +1331,22 @@ public class ObsServiceTest extends BaseContextSensitiveTest {
 		obsService.purgeObs(obs);
 		
 		Assert.assertNull(obsService.getObs(7));
+		
+		executeDataSet(COMPLEX_OBS_XML);
+		Obs complexObs = obsService.getComplexObs(44, OpenmrsConstants.RAW_VIEW);
+		// obs #44 is coded by the concept complex #8473 pointing to ImageHandler
+		// ImageHandler inherits AbstractHandler which handles complex data files on disk
+		assertNotNull(complexObs.getComplexData());
+		AdministrationService as = Context.getAdministrationService();
+		File complexObsDir = OpenmrsUtil.getDirectoryInApplicationDataDirectory(as
+		        .getGlobalProperty(OpenmrsConstants.GLOBAL_PROPERTY_COMPLEX_OBS_DIR));
+		for (File file : complexObsDir.listFiles()) {
+			file.delete();
+		}
+		
+		obsService.purgeObs(complexObs);
+		
+		assertNull(obsService.getObs(obs.getObsId()));
 	}
 	
 	/**
