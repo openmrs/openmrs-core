a9440426fa49e74fcd340360256639e547378192
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2017-03-23 15:13:39 +0100

    TRUNK-5071 Fixed tests
    (cherry picked from commit 055277682117e69fcd49aef1ebbcc72a4511b18a)

--- api/src/test/java/org/openmrs/api/AdministrationServiceTest.java
+++ api/src/test/java/org/openmrs/api/AdministrationServiceTest.java
@@ -474,7 +474,7 @@ public class AdministrationServiceTest extends BaseContextSensitiveTest {
 	@Verifies(value = "should return all global properties in the database", method = "getAllGlobalProperties()")
 	public void getAllGlobalProperties_shouldReturnAllGlobalPropertiesInTheDatabase() throws Exception {
 		executeDataSet(ADMIN_INITIAL_DATA_XML);
-		Assert.assertEquals(20, Context.getAdministrationService().getAllGlobalProperties().size());
+		Assert.assertEquals(21, Context.getAdministrationService().getAllGlobalProperties().size());
 	}
 	
 	/**
@@ -546,9 +546,9 @@ public class AdministrationServiceTest extends BaseContextSensitiveTest {
 		executeDataSet(ADMIN_INITIAL_DATA_XML);
 		AdministrationService as = Context.getAdministrationService();
 		
-		Assert.assertEquals(20, as.getAllGlobalProperties().size());
+		Assert.assertEquals(21, as.getAllGlobalProperties().size());
 		as.purgeGlobalProperty(as.getGlobalPropertyObject("a_valid_gp_key"));
-		Assert.assertEquals(19, as.getAllGlobalProperties().size());
+		Assert.assertEquals(20, as.getAllGlobalProperties().size());
 	}
 	
 	/**
