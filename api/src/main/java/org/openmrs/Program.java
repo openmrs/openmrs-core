6b305db75a5b15275c9ebbafe9b0d5d8966fb9c5
sumanmaity112 <suman.maity112@gmail.com>
2017-03-28 14:10:09 +0430

    TRUNK-5120 : Fixed hibernate exceptions occurs during update program name

--- api/src/main/java/org/openmrs/Program.java
+++ api/src/main/java/org/openmrs/Program.java
@@ -192,7 +192,8 @@ public class Program extends BaseOpenmrsMetadata {
 	}
 	
 	public void setAllWorkflows(Set<ProgramWorkflow> allWorkflows) {
-		this.allWorkflows = new HashSet<ProgramWorkflow>(allWorkflows);
+		this.allWorkflows.clear();
+		this.allWorkflows.addAll(allWorkflows);
 	}
 	
 	/**
