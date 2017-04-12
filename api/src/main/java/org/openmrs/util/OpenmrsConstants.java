2311ede63b0164cfdb8bfeaf75075326f29454fc
abarnett <celius377@gmail.com>
2017-04-11 22:30:07 -0400

    Changed public static variables to final in Security, SchedulerConstants, and OpenmrsConstants. Added .get and .set methods for Listener.java and OpenmrsUtil.java to access on the variables changed.

--- api/src/main/java/org/openmrs/util/OpenmrsConstants.java
+++ api/src/main/java/org/openmrs/util/OpenmrsConstants.java
@@ -42,7 +42,16 @@ public final class OpenmrsConstants {
 	
 	private static final Logger log = LoggerFactory.getLogger(OpenmrsConstants.class);
 	
-	public static String KEY_OPENMRS_APPLICATION_DATA_DIRECTORY = "OPENMRS_APPLICATION_DATA_DIRECTORY";
+	
+	private static String KEY_OPENMRS_APPLICATION_DATA_DIRECTORY = "OPENMRS_APPLICATION_DATA_DIRECTORY";
+	
+	public static void SET_KEY_OPENMRS_APPLICATION_DATA_DIRECTORY(String name){
+		KEY_OPENMRS_APPLICATION_DATA_DIRECTORY = name;
+	}
+	public static String GET_KEY_OPENMRS_APPLICATION_DATA_DIRECTORY(){
+		return KEY_OPENMRS_APPLICATION_DATA_DIRECTORY;
+	}
+	
 	
 	/**
 	 * This is the hard coded primary key of the concept class for DRUG. This has to be done because
@@ -154,9 +163,9 @@ public final class OpenmrsConstants {
 	 * @see #APPLICATION_DATA_DIRECTORY_RUNTIME_PROPERTY
 	 * @see OpenmrsUtil#getApplicationDataDirectory()
 	 */
-	public static String APPLICATION_DATA_DIRECTORY_FALLBACK_UNIX = "/var/lib";
+	public static final String APPLICATION_DATA_DIRECTORY_FALLBACK_UNIX = "/var/lib";
 	
-	public static String APPLICATION_DATA_DIRECTORY_FALLBACK_WIN = System.getenv("appdata");
+	public static final String APPLICATION_DATA_DIRECTORY_FALLBACK_WIN = System.getenv("appdata");
 	
 	/**
 	 * The name of the runtime property that a user can set that will specify where openmrs's
@@ -164,13 +173,13 @@ public final class OpenmrsConstants {
 	 * 
 	 * @see #APPLICATION_DATA_DIRECTORY
 	 */
-	public static String APPLICATION_DATA_DIRECTORY_RUNTIME_PROPERTY = "application_data_directory";
+	public static final String APPLICATION_DATA_DIRECTORY_RUNTIME_PROPERTY = "application_data_directory";
 	
 	/**
 	 * The name of the runtime property that a user can set that will specify whether the database
 	 * is automatically updated on startup
 	 */
-	public static String AUTO_UPDATE_DATABASE_RUNTIME_PROPERTY = "auto_update_database";
+	public static final String AUTO_UPDATE_DATABASE_RUNTIME_PROPERTY = "auto_update_database";
 	
 	/**
 	 * These words are ignored in concept and patient searches
@@ -375,37 +384,37 @@ public final class OpenmrsConstants {
 	 * Global property name that allows specification of whether user passwords must contain both
 	 * upper and lower case characters. Allowable values are "true", "false", and null
 	 */
-	public static String GP_PASSWORD_REQUIRES_UPPER_AND_LOWER_CASE = "security.passwordRequiresUpperAndLowerCase";
+	public static final String GP_PASSWORD_REQUIRES_UPPER_AND_LOWER_CASE = "security.passwordRequiresUpperAndLowerCase";
 	
 	/**
 	 * Global property name that allows specification of whether user passwords require non-digits.
 	 * Allowable values are "true", "false", and null
 	 */
-	public static String GP_PASSWORD_REQUIRES_NON_DIGIT = "security.passwordRequiresNonDigit";
+	public static final String GP_PASSWORD_REQUIRES_NON_DIGIT = "security.passwordRequiresNonDigit";
 	
 	/**
 	 * Global property name that allows specification of whether user passwords must contain digits.
 	 * Allowable values are "true", "false", and null
 	 */
-	public static String GP_PASSWORD_REQUIRES_DIGIT = "security.passwordRequiresDigit";
+	public static final String GP_PASSWORD_REQUIRES_DIGIT = "security.passwordRequiresDigit";
 	
 	/**
 	 * Global property name that allows specification of whether user passwords can match username
 	 * or system id. Allowable values are "true", "false", and null
 	 */
-	public static String GP_PASSWORD_CANNOT_MATCH_USERNAME_OR_SYSTEMID = "security.passwordCannotMatchUsername";
+	public static final String GP_PASSWORD_CANNOT_MATCH_USERNAME_OR_SYSTEMID = "security.passwordCannotMatchUsername";
 	
 	/**
 	 * Global property name that allows specification of whether user passwords have a minimum
 	 * length requirement Allowable values are any integer
 	 */
-	public static String GP_PASSWORD_MINIMUM_LENGTH = "security.passwordMinimumLength";
+	public static final String GP_PASSWORD_MINIMUM_LENGTH = "security.passwordMinimumLength";
 	
 	/**
 	 * Global property name that allows specification of a regular expression that passwords must
 	 * adhere to
 	 */
-	public static String GP_PASSWORD_CUSTOM_REGEX = "security.passwordCustomRegex";
+	public static final String GP_PASSWORD_CUSTOM_REGEX = "security.passwordCustomRegex";
 	
 	/**
 	 * Global property name for absolute color for patient graphs.
