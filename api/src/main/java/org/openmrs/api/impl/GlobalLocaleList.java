7232e6dfa2e43d8c4c3d6b58a7862bee712d7b29
sam <samuelsmalek@gmail.com>
2017-06-06 22:06:53 +0300

    I have removed the try and catch block

--- api/src/main/java/org/openmrs/api/impl/GlobalLocaleList.java
+++ api/src/main/java/org/openmrs/api/impl/GlobalLocaleList.java
@@ -33,17 +33,10 @@ public class GlobalLocaleList implements GlobalPropertyListener {
 	public void globalPropertyChanged(GlobalProperty newValue) {
 		allowedLocales = new LinkedHashSet<Locale>();
 		for (String allowedLocaleString : newValue.getPropertyValue().split(",")) {
-			try {
+		//The try catch blocked has been removed.
 				Locale allowedLocale = LocaleUtility.fromSpecification(allowedLocaleString.trim());
 				if (allowedLocale != null) {
-					allowedLocales.add(allowedLocale);
-				}
-			}
-			catch (Exception e) {
-				// bad locale spec? just ignore it. the UI should take care of
-				// guiding the user.
-			}
-		}
+					allowedLocales.add(allowedLocale);}
 	}
 	
 	/**
