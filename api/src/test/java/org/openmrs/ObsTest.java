ad55a4e5aebf3215f8490f22214c06624d04c4b3
dkayiwa <kayiwadaniel@gmail.com>
2017-08-10 11:20:36 +0300

    Obs.setValueBoolean should not set value for non boolean concept - TRUNK-5202

--- api/src/test/java/org/openmrs/ObsTest.java
+++ api/src/test/java/org/openmrs/ObsTest.java
@@ -1048,4 +1048,14 @@ public class ObsTest {
 		assertFalse(obs.isDirty());
 	}
 
+	@Test
+	public void setValueBoolean_shouldNotSetValueForNonBooleanConcept() throws Exception {
+		Obs obs = createObs(2);
+		ConceptDatatype dataType = new ConceptDatatype();
+		dataType.setUuid(ConceptDatatype.CODED_UUID);
+		obs.getConcept().setDatatype(dataType);
+		assertNotNull(obs.getValueCoded());
+		obs.setValueBoolean(null);
+		assertNotNull(obs.getValueCoded());
+	}
 }
