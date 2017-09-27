c4fa843ee92653ba45e20e05f2ab0ef1c371a62e
Nishara Kavindi <kavindi183@gmail.com>
2017-09-28 02:25:54 +0530

    Update LocationServiceImpl.java

--- api/src/main/java/org/openmrs/api/impl/LocationServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/LocationServiceImpl.java
@@ -121,7 +121,7 @@ public class LocationServiceImpl extends BaseOpenmrsService implements LocationS
 		}
 		
 		//Try to look up 'Unknown Location' in case the global property is something else
-		setLocationToGivenLocationOrDefault( location, locationGP,"Unknown Location");
+		setLocationToGivenLocationOrDefault ( location, locationGP,"Unknown Location");
 		
 		// If Unknown Location does not exist, try Unknown if the global property was different
 		setLocationToGivenLocationOrDefault(location, locationGP,"Unknown");
@@ -133,6 +133,7 @@ public class LocationServiceImpl extends BaseOpenmrsService implements LocationS
 		
 		return location;
 	}
+	
 	private void setLocationToGivenLocationOrDefault(Location location, String locationGP, String defaultLocation) {
 		if (location == null && (!StringUtils.hasText(locationGP) || !defaultLocation.equalsIgnoreCase(locationGP))) {
 			location = Context.getLocationService().getLocation(defaultLocation);
