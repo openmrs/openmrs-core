7be1aa8e8f513791e5e3da16d39cb8cb5d8bc268
payalchheda <chheda.pc@gmail.com>
2017-11-09 04:28:46 +0000

    Add bug fix 1 for sql injection

--- api/src/main/java/org/openmrs/util/databasechange/MigrateAllergiesChangeSet.java
+++ api/src/main/java/org/openmrs/util/databasechange/MigrateAllergiesChangeSet.java
@@ -157,11 +157,15 @@ public class MigrateAllergiesChangeSet implements CustomTaskChange {
 	private Integer getConceptByGlobalProperty(Database database, String globalPropertyName) throws Exception {
 		JdbcConnection connection = (JdbcConnection) database.getConnection();
 		Statement stmt = connection.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT property_value FROM global_property WHERE property = '" + globalPropertyName + "'");
+                // add openmrs security bug fix for sql injection attack
+                PreparedStatement globalPropertyQuery = connection.prepareStatement("SELECT property_value FROM global_property WHERE property = ?");
+                globalPropertyQuery.setString(1, globalPropertyName);
+		ResultSet rs = globalPropertyQuery.executeQuery();
 		if (rs.next()) {
 			String uuid = rs.getString("property_value");
-			
-			rs = stmt.executeQuery("SELECT concept_id FROM concept WHERE uuid = '" + uuid + "'");
+			PreparedStatement getUIDQuery = connection.prepareStatement("SELECT concept_id FROM concept WHERE uuid = ?");
+                        getUIDQuery.setString(1, uuid);
+			rs = getUIDQuery.executeQuery();
 			if (rs.next()) {
 				return rs.getInt("concept_id");
 			}
