174d788beaaf1fa1ee70b4394585694606fa41b3
JyothsnaAshok <34093139+JyothsnaAshok@users.noreply.github.com>
2017-12-03 13:27:24 +0530

    Update HttpClient.java

--- api/src/main/java/org/openmrs/util/HttpClient.java
+++ api/src/main/java/org/openmrs/util/HttpClient.java
@@ -75,7 +75,7 @@ public class HttpClient {
 			wr.close();
 			
 			// Get the response
-			rd = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+			rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			String line;
 			while ((line = rd.readLine()) != null) {
 				response = String.format("%s%s%n", response, line);
