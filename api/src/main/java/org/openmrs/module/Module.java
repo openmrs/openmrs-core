60eb596e5a2d15b1951e704f8ac79a4d20098baa
Said Yaka <saidyakabjk@gmail.com>
2017-12-05 02:01:12 -0500

    GCI-204:extensionNames should be a Map interface rather than the implementation IdentityHashMap (#2373)
    https://codein.withgoogle.com/dashboard/task-instances/6542227721420800/

--- api/src/main/java/org/openmrs/module/Module.java
+++ api/src/main/java/org/openmrs/module/Module.java
@@ -69,7 +69,7 @@ public final class Module {
 	
 	private List<AdvicePoint> advicePoints = new ArrayList<AdvicePoint>();
 	
-	private IdentityHashMap<String, String> extensionNames = new IdentityHashMap<String, String>();
+	private Map<String, String> extensionNames = new IdentityHashMap<String, String>();
 	
 	private List<Extension> extensions = new ArrayList<Extension>();
 	
@@ -511,7 +511,7 @@ public final class Module {
 	 * @param map from pointid to classname
 	 * @see ModuleFileParser
 	 */
-	public void setExtensionNames(IdentityHashMap<String, String> map) {
+	public void setExtensionNames(Map<String, String> map) {
 		if (log.isDebugEnabled()) {
 			for (Map.Entry<String, String> entry : extensionNames.entrySet()) {
 				log.debug("Setting extension names: " + entry.getKey() + " : " + entry.getValue());
