b90b7a4c8e0826e78613eeb542a64037bfc3085f
teleivo <mrteleivo@gmail.com>
2017-12-31 16:08:13 +0100

    TRUNK-5147 Remove wrong test javadocs
    removed javadocs above tests which were wrong since the method signature
    of the method they are testing changed.
    
    these javadocs do not provide more information anyway than the test
    itself, are just taking up space and are never updated as can be seen.

--- api/src/test/java/org/openmrs/api/VisitServiceTest.java
+++ api/src/test/java/org/openmrs/api/VisitServiceTest.java
@@ -437,18 +437,12 @@ public class VisitServiceTest extends BaseContextSensitiveTest {
 		assertEquals(8, visitService.getVisitsByPatient(new Patient(2), true, false).size());
 	}
 	
-	/**
-	 * @see VisitService#getVisits(java.util.Collection, java.util.Collection, java.util.Collection, java.util.Collection, Date, Date, Date, Date, boolean)
-	 */
 	@Test
 	public void getVisits_shouldGetVisitsByIndications() {
 		assertEquals(1, visitService.getVisits(null, null, null, Collections.singletonList(new Concept(5497)),
 		    null, null, null, null, null, true, false).size());
 	}
 	
-	/**
-	 * @see VisitService#getVisits(java.util.Collection, java.util.Collection, java.util.Collection, java.util.Collection, Date, Date, Date, Date, boolean)
-	 */
 	@Test
 	public void getVisits_shouldGetVisitsByLocations() {
 		List<Location> locations = new ArrayList<>();
@@ -457,9 +451,6 @@ public class VisitServiceTest extends BaseContextSensitiveTest {
 		    false).size());
 	}
 	
-	/**
-	 * @see VisitService#getVisits(java.util.Collection, java.util.Collection, java.util.Collection, java.util.Collection, Date, Date, Date, Date, boolean)
-	 */
 	@Test
 	public void getVisits_shouldGetVisitsByVisitType() {
 		List<VisitType> visitTypes = new ArrayList<>();
@@ -468,9 +459,6 @@ public class VisitServiceTest extends BaseContextSensitiveTest {
 		    true, false).size());
 	}
 	
-	/**
-	 * @see VisitService#getVisits(java.util.Collection, java.util.Collection, java.util.Collection, java.util.Collection, Date, Date, Date, Date, boolean)
-	 */
 	@Test
 	public void getVisits_shouldGetVisitsEndedBetweenTheGivenEndDates() {
 		executeDataSet(VISITS_WITH_DATES_XML);
@@ -503,9 +491,6 @@ public class VisitServiceTest extends BaseContextSensitiveTest {
 		assertTrue(TestUtil.containsId(visits, 5));
 	}
 	
-	/**
-	 * @see VisitService#getVisits(java.util.Collection, java.util.Collection, java.util.Collection, java.util.Collection, Date, Date, Date, Date, boolean)
-	 */
 	@Test
 	public void getVisits_shouldGetVisitsStartedBetweenTheGivenStartDates() {
 		executeDataSet(VISITS_WITH_DATES_XML);
@@ -518,9 +503,6 @@ public class VisitServiceTest extends BaseContextSensitiveTest {
 		    null, true, false).size());
 	}
 	
-	/**
-	 * @see VisitService#getVisits(java.util.Collection, java.util.Collection, java.util.Collection, java.util.Collection, Date, Date, Date, Date, boolean)
-	 */
 	@Test
 	public void getVisits_shouldReturnAllVisitsIfIncludeVoidedIsSetToTrue() {
 		assertEquals(6, getNumberOfAllVisitsIncludingVoided());
@@ -676,10 +658,6 @@ public class VisitServiceTest extends BaseContextSensitiveTest {
 		assertNull(visitService.getVisitAttributeByUuid("not-a-uuid"));
 	}
 	
-	/**
-	 * @throws ParseException
-	 * @see VisitService#getVisits(Collection,Collection,Collection,Collection,Date,Date,Date,Date,Map,boolean)
-	 */
 	@Test
 	public void getVisits_shouldGetAllVisitsWithGivenAttributeValues() throws ParseException {
 		executeDataSet(VISITS_ATTRIBUTES_XML);
@@ -690,10 +668,6 @@ public class VisitServiceTest extends BaseContextSensitiveTest {
 		assertEquals(Integer.valueOf(1), visits.get(0).getVisitId());
 	}
 	
-	/**
-	 * @throws ParseException
-	 * @see VisitService#getVisits(Collection,Collection,Collection,Collection,Date,Date,Date,Date,Map,boolean)
-	 */
 	@Test
 	public void getVisits_shouldNotFindAnyVisitsIfNoneHaveGivenAttributeValues() throws ParseException {
 		executeDataSet(VISITS_ATTRIBUTES_XML);
@@ -842,9 +816,6 @@ public class VisitServiceTest extends BaseContextSensitiveTest {
 		assertNotNull(attribute.getDateVoided());
 	}
 	
-	/**
-	 * @see VisitService#stopVisits()
-	 */
 	@Test
 	public void stopVisits_shouldCloseAllUnvoidedActiveVisitMatchingTheSpecifiedVisitTypes() {
 		executeDataSet("org/openmrs/api/include/VisitServiceTest-includeVisitsAndTypeToAutoClose.xml");
