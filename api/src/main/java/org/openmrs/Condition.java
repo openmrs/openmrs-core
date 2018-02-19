6a49188f21bc6e2efb6edf774016727b2af41fd8
Paradise Kelechi <paradisekelechi@gmail.com>
2018-02-19 12:19:23 +0100

    TRUNK-5338 add foreign key constraints to liquibase changeset

--- api/src/main/java/org/openmrs/Condition.java
+++ api/src/main/java/org/openmrs/Condition.java
@@ -26,7 +26,7 @@ import java.util.Date;
  */
 public class Condition extends BaseChangeableOpenmrsData {
 	
-	public static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	private Integer conditionId;
 	
