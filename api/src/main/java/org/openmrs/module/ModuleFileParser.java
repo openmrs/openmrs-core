883dbca33d01c705843a5d4b43f6f109e23c1aff
teleivo <mrteleivo@gmail.com>
2018-03-29 07:51:38 +0200

    TRUNK-5385 Extract creation of module from document
    method is concerned with creating an instance of Module
    with values from the config xml Document

--- api/src/main/java/org/openmrs/module/ModuleFileParser.java
+++ api/src/main/java/org/openmrs/module/ModuleFileParser.java
@@ -155,65 +155,7 @@ public class ModuleFileParser {
 				    "Module.error.cannotGetConfigFileStream"), moduleFile.getName(), e);
 			}
 			Document configDoc = parseConfig(configStream);
-
-			Element rootNode = configDoc.getDocumentElement();
-			
-			String configVersion = rootNode.getAttribute("configVersion").trim();
-			
-			if (!validConfigVersions.contains(configVersion)) {
-				throw new ModuleException(Context.getMessageSourceService().getMessage("Module.error.invalidConfigVersion",
-				    new Object[] { configVersion, String.join(", ", validConfigVersions) }, Context.getLocale()), moduleFile.getName());
-			}
-			
-			String name = getElement(rootNode, "name").trim();
-			String moduleId = getElement(rootNode,"id").trim();
-			String packageName = getElement(rootNode,"package").trim();
-			String author = getElement(rootNode,"author").trim();
-			String desc = getElement(rootNode, "description").trim();
-			String version = getElement(rootNode, "version").trim();
-			
-			// do some validation
-			if (name == null || name.length() == 0) {
-				throw new ModuleException(Context.getMessageSourceService().getMessage("Module.error.nameCannotBeEmpty"),
-				        moduleFile.getName());
-			}
-			if (moduleId == null || moduleId.length() == 0) {
-				throw new ModuleException(Context.getMessageSourceService().getMessage("Module.error.idCannotBeEmpty"), name);
-			}
-			if (packageName == null || packageName.length() == 0) {
-				throw new ModuleException(Context.getMessageSourceService().getMessage("Module.error.packageCannotBeEmpty"),
-				        name);
-			}
-			
-			// create the module object
-			module = new Module(name, moduleId, packageName, author, desc, version);
-			
-			// find and load the activator class
-			module.setActivatorName(getElement(rootNode, "activator").trim());
-			
-			module.setRequireDatabaseVersion(getElement(rootNode, "require_database_version").trim());
-			module.setRequireOpenmrsVersion(getElement(rootNode, "require_version").trim());
-			module.setUpdateURL(getElement(rootNode, "updateURL").trim());
-			module.setRequiredModulesMap(getRequiredModules(rootNode));
-			module.setAwareOfModulesMap(getAwareOfModules(rootNode));
-			module.setStartBeforeModulesMap(getStartBeforeModules(rootNode));
-			
-			module.setAdvicePoints(getAdvice(rootNode,  module));
-			module.setExtensionNames(getExtensions(rootNode));
-			
-			module.setPrivileges(getPrivileges(rootNode));
-			module.setGlobalProperties(getGlobalProperties(rootNode));
-			
-			module.setMappingFiles(getMappingFiles(rootNode));
-			module.setPackagesWithMappedClasses(getPackagesWithMappedClasses(rootNode));
-			
-			module.setConfig(configDoc);
-			
-			module.setMandatory(getMandatory(rootNode, configVersion));
-			
-			module.setFile(moduleFile);
-			
-			module.setConditionalResources(getConditionalResources(rootNode));
+			module = createModule(configDoc);
 		}
 		finally {
 			try {
@@ -231,6 +173,68 @@ public class ModuleFileParser {
 				}
 			}
 		}
+		return module;
+	}
+
+	private Module createModule(Document configDoc) {
+		Module module;Element rootNode = configDoc.getDocumentElement();
+
+		String configVersion = rootNode.getAttribute("configVersion").trim();
+
+		if (!validConfigVersions.contains(configVersion)) {
+			throw new ModuleException(Context.getMessageSourceService().getMessage("Module.error.invalidConfigVersion",
+			    new Object[] { configVersion, String.join(", ", validConfigVersions) }, Context.getLocale()), moduleFile.getName());
+		}
+
+		String name = getElement(rootNode, "name").trim();
+		String moduleId = getElement(rootNode,"id").trim();
+		String packageName = getElement(rootNode,"package").trim();
+		String author = getElement(rootNode,"author").trim();
+		String desc = getElement(rootNode, "description").trim();
+		String version = getElement(rootNode, "version").trim();
+
+		// do some validation
+		if (name == null || name.length() == 0) {
+			throw new ModuleException(Context.getMessageSourceService().getMessage("Module.error.nameCannotBeEmpty"),
+			        moduleFile.getName());
+		}
+		if (moduleId == null || moduleId.length() == 0) {
+			throw new ModuleException(Context.getMessageSourceService().getMessage("Module.error.idCannotBeEmpty"), name);
+		}
+		if (packageName == null || packageName.length() == 0) {
+			throw new ModuleException(Context.getMessageSourceService().getMessage("Module.error.packageCannotBeEmpty"),
+			        name);
+		}
+
+		// create the module object
+		module = new Module(name, moduleId, packageName, author, desc, version);
+
+		// find and load the activator class
+		module.setActivatorName(getElement(rootNode, "activator").trim());
+
+		module.setRequireDatabaseVersion(getElement(rootNode, "require_database_version").trim());
+		module.setRequireOpenmrsVersion(getElement(rootNode, "require_version").trim());
+		module.setUpdateURL(getElement(rootNode, "updateURL").trim());
+		module.setRequiredModulesMap(getRequiredModules(rootNode));
+		module.setAwareOfModulesMap(getAwareOfModules(rootNode));
+		module.setStartBeforeModulesMap(getStartBeforeModules(rootNode));
+
+		module.setAdvicePoints(getAdvice(rootNode,  module));
+		module.setExtensionNames(getExtensions(rootNode));
+
+		module.setPrivileges(getPrivileges(rootNode));
+		module.setGlobalProperties(getGlobalProperties(rootNode));
+
+		module.setMappingFiles(getMappingFiles(rootNode));
+		module.setPackagesWithMappedClasses(getPackagesWithMappedClasses(rootNode));
+
+		module.setConfig(configDoc);
+
+		module.setMandatory(getMandatory(rootNode, configVersion));
+
+		module.setFile(moduleFile);
+
+		module.setConditionalResources(getConditionalResources(rootNode));
 		return module;
 	}
 
