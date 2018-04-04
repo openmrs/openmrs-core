bbca156dd92abb85332f83f7d16d1ffc359e0f97
teleivo <mrteleivo@gmail.com>
2018-04-04 22:04:20 +0200

    Test AdministrationService.addGlobalPropertyListener
    by adding a unit test to the AdministrationServiceUnitTest
    mocking EventListeners since
    AdministrationServiceImpl.addGlobalPropertyListener
    simply delegates to the EventListeners which serves its
    List<GlobalPropertyListeners> to the public

--- api/src/test/java/org/openmrs/api/AdministrationServiceUnitTest.java
+++ api/src/test/java/org/openmrs/api/AdministrationServiceUnitTest.java
@@ -9,16 +9,23 @@
  */
 package org.openmrs.api;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.openmrs.api.db.AdministrationDAO;
-import org.openmrs.api.impl.AdministrationServiceImpl;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.api.db.AdministrationDAO;
+import org.openmrs.api.impl.AdministrationServiceImpl;
 
 /**
  * Unit tests for {@link AdministrationService}.
@@ -26,6 +33,8 @@ import static org.mockito.Mockito.verify;
 public class AdministrationServiceUnitTest {
 	
 	private AdministrationDAO adminDAO;
+	
+	private EventListeners eventListeners;
 	
 	private AdministrationService adminService;
 	
@@ -35,6 +44,8 @@ public class AdministrationServiceUnitTest {
 		adminService = new AdministrationServiceImpl();
 		adminDAO = mock(AdministrationDAO.class);
 		adminService.setAdministrationDAO(adminDAO);
+		eventListeners = mock(EventListeners.class);
+		((AdministrationServiceImpl) adminService).setEventListeners(eventListeners);
 	}
 	
 	@Test
@@ -58,5 +69,18 @@ public class AdministrationServiceUnitTest {
 
 		adminService.getGlobalPropertyValue("valid.double", null);
 	}
-	
+
+	@Test
+	public void addGlobalPropertyListener_shouldAddListener() {
+
+		List<GlobalPropertyListener> listeners = new ArrayList<>();
+		when(eventListeners.getGlobalPropertyListeners()).thenReturn(listeners);
+
+		GlobalPropertyListener listener = mock(GlobalPropertyListener.class);
+
+		adminService.addGlobalPropertyListener(listener);
+
+		assertThat(listeners.size(), is(1));
+		assertThat(listeners, contains(listener));
+	}
 }
