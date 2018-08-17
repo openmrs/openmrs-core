06b3f675cdcf85cf875abb2886dc5a98311c9da9
fanyui <fanyuiharisu@gmail.com>
2018-08-17 13:02:59 +0100

    modified context to set starttls.enable value for session to be used for authenticated email sending

--- api/src/main/java/org/openmrs/api/context/Context.java
+++ api/src/main/java/org/openmrs/api/context/Context.java
@@ -567,6 +567,7 @@ public class Context {
 					props.setProperty("mail.from", adminService.getGlobalProperty("mail.from"));
 					props.setProperty("mail.debug", adminService.getGlobalProperty("mail.debug"));
 					props.setProperty("mail.smtp.auth", adminService.getGlobalProperty("mail.smtp_auth"));
+					props.setProperty(OpenmrsConstants.GP_MAIL_SMTP_STARTTLS_ENABLE, adminService.getGlobalProperty(OpenmrsConstants.GP_MAIL_SMTP_STARTTLS_ENABLE));
 
 					Authenticator auth = new Authenticator() {
 
