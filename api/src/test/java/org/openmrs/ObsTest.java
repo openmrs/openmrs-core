a2beae3e9cc090a137ce857e2c7a547b57f84ebb
jwnasambu <33891016+jwnasambu@users.noreply.github.com>
2019-02-22 13:22:33 +0300

    TRUNK-248 - Added tests for junit TODOs (#2811)

--- api/src/test/java/org/openmrs/ObsTest.java
+++ api/src/test/java/org/openmrs/ObsTest.java
@@ -1015,6 +1015,24 @@ public class ObsTest {
 		obs.setInterpretation(Obs.Interpretation.LOW);
 		obs.setConcept(new Concept());
 		obs.setValueNumeric(1.2);
+		obs.setObsGroup(new  Obs());        
+        obs.setAccessionNumber("4849RDD");          
+        obs.setValueCoded(new Concept());                  
+        obs.setValueDrug(new Drug());                      
+        obs.setValueGroupId(new  Integer(23));
+        obs.setValueDatetime(new Date());
+        obs.setValueModifier("djfsihdihd");              
+        obs.setValueText("xyzABC");
+        obs.setComment("This is an example");
+        obs.setEncounter(new Encounter(3));  
+        obs.setCreator( new  User(1));                    
+        obs.setDateCreated(new Date());        
+        obs.setVoided(false);            
+        obs.setVoidedBy(new  User(2));      
+        obs.setDateVoided(new Date());                                
+        obs.setVoidReason("Some reason");    
+        obs.setValueComplex("SDIODF7980IDSISF");
+        obs.setComplexData(new ComplexData("Complex data", "Complex data".getBytes()));
 		
 		Obs copy = Obs.newInstance(obs);
 		
@@ -1027,7 +1045,25 @@ public class ObsTest {
 		assertThat(copy.getValueNumeric(), is(obs.getValueNumeric()));
 		assertThat(copy.getStatus(), is(obs.getStatus()));
 		assertThat(copy.getInterpretation(), is(obs.getInterpretation()));
-		// TODO test that the rest of the fields are set
+		assertThat(copy.getConcept(), is(obs.getConcept()));
+		assertThat(copy.getAccessionNumber(),  is(obs.getAccessionNumber()));                          
+		assertThat(copy.getValueDrug(),  is(obs.getValueDrug()));                      
+		assertThat(copy.getValueGroupId(),  is(obs.getValueGroupId()));
+		assertThat(copy.getValueDatetime(),  is(obs.getValueDatetime()));
+		assertThat(copy.getValueNumeric(),  is(obs.getValueNumeric()));      
+		assertThat(copy.getValueModifier(),  is(obs.getValueModifier()));              
+		assertThat(copy.getValueText(),  is(obs.getValueText()));
+		assertThat(copy.getComment(),  is(obs.getComment()));
+		assertThat(copy.getEncounter(),  is(obs.getEncounter()));
+		assertThat(copy.getCreator(),  is(obs.getCreator()));                  
+		assertThat(copy.getDateCreated(),  is(obs.getDateCreated()));        
+		assertThat(copy.getVoided(),  is(obs.getVoided()));          
+		assertThat(copy.getVoidedBy(),  is(obs.getVoidedBy()));    
+		assertThat(copy.getDateVoided(),  is(obs.getDateVoided()));                              
+		assertThat(copy.getVoidReason(),  is(obs.getVoidReason()));    
+		assertThat(copy.getValueComplex(),  is(obs.getValueComplex()));
+		assertThat(copy.getComplexData(),  is(obs.getComplexData()));
+
 	}
 	
 	@Test
