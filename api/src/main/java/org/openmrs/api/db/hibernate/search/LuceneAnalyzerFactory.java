1936ca5fdb53f3270ff5c622512a35527f015bbc
Brandon Istenes <bistenes@gmail.com>
2019-11-12 12:56:35 -0600

    Try a bunch of crazy stuff

--- api/src/main/java/org/openmrs/api/db/hibernate/search/LuceneAnalyzerFactory.java
+++ api/src/main/java/org/openmrs/api/db/hibernate/search/LuceneAnalyzerFactory.java
@@ -14,15 +14,22 @@ import org.apache.lucene.analysis.core.WhitespaceTokenizerFactory;
 import org.apache.lucene.analysis.ngram.EdgeNGramFilterFactory;
 import org.apache.lucene.analysis.ngram.NGramFilterFactory;
 import org.apache.lucene.analysis.util.TokenFilterFactory;
+import org.hibernate.SessionFactory;
 import org.hibernate.search.annotations.Factory;
 import org.hibernate.search.cfg.AnalyzerDefMapping;
 import org.hibernate.search.cfg.SearchMapping;
+import org.openmrs.api.AdministrationService;
 import org.openmrs.api.context.Context;
 import org.openmrs.propertyeditor.ConceptMapTypeEditor;
 import org.openmrs.util.OpenmrsConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -42,16 +49,35 @@ import java.util.stream.Collectors;
  * @since 2.4.0
  */
 public class LuceneAnalyzerFactory {
-	
+	// @bistenes: I tried three approaches to getting the global properties.
+	//   1. Context.getAdministrationService().getGlobalProperty(gpName) directly in getSearchMapping
+	//   2. Autowired AdministrationService
+	//   3. Initializing the value in a PostConstruct method
+	// All of these approaches produced a circular dependency with sessionFactory.
+	//
 	private static final Logger log = LoggerFactory.getLogger(ConceptMapTypeEditor.class);
 	
-	private static final String gp_name = OpenmrsConstants.GP_LUCENE_SEARCH_FILTERS;
+	private static final String gpName = OpenmrsConstants.GP_LUCENE_SEARCH_FILTERS;
+	
+	private String filters;
+
+	//	@Autowired
+//	private AdministrationService adminService;
+	
+//	@PostConstruct
+//	public void setup() {
+//		AdministrationService adminService = Context.getAdministrationService();
+//		filters = adminService.getGlobalProperty(gpName);
+//	}
 
 	@Factory
 	public SearchMapping getSearchMapping() {
-		
-		String filters = Context.getAdministrationService().getGlobalProperty(gp_name);
 
+//		AdministrationService adminService = Context.getAdministrationService();
+//		filters = adminService.getGlobalProperty(gpName);
+		
+//		filters = "org.apache.lucene.analysis.core.LowerCaseFilterFactory,org.apache.lucene.analysis.standard.ClassicFilterFactory,org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory";
+		
 		Set<Class<? extends TokenFilterFactory>> filterFactories = Arrays.stream(filters.split(","))
 			.map(this::getFilterFactory)
 			.collect(Collectors.toSet());
@@ -84,7 +110,7 @@ public class LuceneAnalyzerFactory {
 		try {
 			clz = Class.forName(name);
 		} catch (Exception e) {
-			log.error("Unable to get Lucene filter factory named '" + name + "'. Please check the value of the setting " + gp_name);
+			log.error("Unable to get Lucene filter factory named '" + name + "'. Please check the value of the setting " + gpName);
 			throw new RuntimeException(e);
 		}
 		Class<? extends TokenFilterFactory> tff;
@@ -92,7 +118,7 @@ public class LuceneAnalyzerFactory {
 			tff = clz.asSubclass(TokenFilterFactory.class);
 		} catch (ClassCastException e) {
 			log.error("The class '" + name + "' provided by the setting does not appear to be a TokenFilterFactory. " +
-				"Filters listed in the setting " + gp_name + " must extend TokenFilterFactory.");
+				"Filters listed in the setting " + gpName + " must extend TokenFilterFactory.");
 			throw e;
 		}
 		return tff;
