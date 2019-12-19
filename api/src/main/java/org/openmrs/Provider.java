e82475435d04db514f30f022fbc2626517105fc6
Wyclif Luyima <wyclif@openmrs.org>
2019-12-18 23:18:42 -0600

    Revert "TRUNK-5671 Replace Provider hbm mapping file with annotations (#3031)"
    This reverts commit 3a66cab70f182205cabfad7cd623b07e3af59bb5.

--- api/src/main/java/org/openmrs/Provider.java
+++ api/src/main/java/org/openmrs/Provider.java
@@ -9,32 +9,6 @@
  */
 package org.openmrs;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
-import javax.persistence.Access;
-import javax.persistence.AccessType;
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
-import javax.persistence.SecondaryTable;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.BatchSize;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-import org.hibernate.annotations.DiscriminatorOptions;
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,49 +17,19 @@ import org.slf4j.LoggerFactory;
  *
  * @since 1.9
  */
-@Entity
-@Table(name = "provider")
-@DiscriminatorColumn(name = "provider_id", discriminatorType = DiscriminatorType.INTEGER)
-@DiscriminatorOptions(insert = false)
-/*
- * Provider inherits name and description properties from a superclass, name is optional but marked as required in a
- * superclass so we need to override that. Description is not used but exists in a superclass and is marked as
- * persistent, so hibernate generates a query containing the column which of course fails to execute because
- * of the missing column, so we introduce this dummy table containing the column to 'please' hibernate.
- */
-@SecondaryTable(name = "provider_unused_fields")
-@AttributeOverrides({ @AttributeOverride(name = "name", column = @Column),
-        @AttributeOverride(name = "description", column = @Column(table = "provider_unused_fields", name = "description", insertable = false, updatable = false)) })
 public class Provider extends BaseCustomizableMetadata<ProviderAttribute> {
 	
 	private static final Logger log = LoggerFactory.getLogger(Provider.class);
 	
-	@Id
-	@GeneratedValue
-	@Column(name = "provider_id")
 	private Integer providerId;
 	
-	@ManyToOne
-	@JoinColumn(name = "person_id")
-	@Cascade(CascadeType.SAVE_UPDATE)
 	private Person person;
 	
 	private String identifier;
 	
-	@ManyToOne
-	@JoinColumn(name = "role_id")
 	private Concept role;
 	
-	@ManyToOne
-	@JoinColumn(name = "speciality_id")
 	private Concept speciality;
-	
-	@Access(AccessType.PROPERTY)
-	@OneToMany(mappedBy = "provider", cascade = javax.persistence.CascadeType.ALL, orphanRemoval = true)
-	@LazyCollection(LazyCollectionOption.TRUE)
-	@OrderBy("voided asc")
-	@BatchSize(size = 100)
-	private Set<ProviderAttribute> attributes = new LinkedHashSet<>();
 	
 	public Provider() {
 	}
