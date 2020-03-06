7d2c11f859b1655a2bd50443c52803c9974b4145
kelvin3094 <kelvin309413243@hotmail.com>
2020-03-06 17:13:33 -0500

    Completed tests for dealing with duplicate identifiers. Fixed formatting

--- api/src/main/java/org/openmrs/api/db/hibernate/HibernatePatientIdentifierMergeDAO.java
+++ api/src/main/java/org/openmrs/api/db/hibernate/HibernatePatientIdentifierMergeDAO.java
@@ -1,3 +1,12 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.api.db.hibernate;
 
 import org.hibernate.SQLQuery;
@@ -30,6 +39,16 @@ public class HibernatePatientIdentifierMergeDAO implements PatientIdentifierMerg
 	public void mergePatientIdentifier(PatientIdentifierType main, PatientIdentifierType toBeMerged) throws DAOException {
 		String nameOfMain = main.getName();
 		String nameOfToBeMerged = toBeMerged.getName();
+		
+		// Check if there are duplicate patient identifiers. Throw DAOException if there is
+		String checkDuplicates = "SELECT  COUNT(identifier) FROM patient_identifier WHERE identifier_type = :mainName or identifier_type = :mergedName GROUP BY identifier HAVING COUNT(identifier) > 1";
+		SQLQuery queryDuplicates = sessionFactory.getCurrentSession().createSQLQuery(checkDuplicates);
+		queryDuplicates.setParameter("mainName", main.getPatientIdentifierTypeId());
+		queryDuplicates.setParameter("mergedName", toBeMerged.getPatientIdentifierTypeId());
+		if (queryDuplicates.list().size() >= 1) {
+			throw new DAOException("Can't automatically merge. Duplicate patient identifiers");
+		}
+		
 		// Merge the patient identifiers of nameOfMain and nameOfToBeMerged
 		String queryString = "UPDATE patient_identifier SET identifier_type = (SELECT patient_identifier_type_id FROM patient_identifier_type WHERE NAME = :mainName) " + 
 				"WHERE identifier_type = (SELECT patient_identifier_type_id FROM patient_identifier_type WHERE NAME = :mergedName)";
--- api/src/test/java/org/openmrs/api/db/hibernate/HibernatePatientIdentifierMergeDAOTest.java
+++ api/src/test/java/org/openmrs/api/db/hibernate/HibernatePatientIdentifierMergeDAOTest.java
@@ -1,13 +1,25 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.api.db.hibernate;
 
 
 import static org.junit.Assert.assertEquals;
 import org.hibernate.SQLQuery;
 import org.hibernate.SessionFactory;
+import org.hibernate.exception.ConstraintViolationException;
 import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.Patient;
+import org.openmrs.PatientIdentifier;
 import org.openmrs.PatientIdentifierType;
+import org.openmrs.api.db.DAOException;
 import org.openmrs.test.BaseContextSensitiveTest;
 
 public class HibernatePatientIdentifierMergeDAOTest extends BaseContextSensitiveTest {
@@ -16,14 +28,13 @@ public class HibernatePatientIdentifierMergeDAOTest extends BaseContextSensitive
 	private SessionFactory sessionFactory;
 	
 	@Before
-	public void getPersonDAO() {
+	public void getMergePatientIdentifierTypeDAO() {
 		sessionFactory = (SessionFactory) applicationContext.getBean("sessionFactory");
 		hibernatePatientIdentifierMergeDao.setSessionFactory(sessionFactory);
 	}
 	
 	@Test
-	public void mergePatientIdentifierTypes() {
-		
+	public void getPatientIdentifer_shouldMergeThePatientIdentiferTypes() {
 		// Getting the PatientIdentifierType objects in the mock database
 		// OpenMRS Identification Number id is 1
 		// Old Identification Number id is 2
@@ -33,11 +44,6 @@ public class HibernatePatientIdentifierMergeDAOTest extends BaseContextSensitive
 		// Get patients that are already in the database
 		Patient patient1 = (Patient) sessionFactory.getCurrentSession().get(Patient.class, 2);
 		Patient patient2 = (Patient) sessionFactory.getCurrentSession().get(Patient.class, 6);
-		
-		// What the patient identifier types were before merging
-		String beforeMerge = "SELECT identifier_type FROM patient_identifier";
-		SQLQuery queryBefore = sessionFactory.getCurrentSession().createSQLQuery(beforeMerge);
-		System.out.println(queryBefore.list());
 
 		// Calling the merge function for merging duplicate patient identifier types
 		hibernatePatientIdentifierMergeDao.mergePatientIdentifier(patientIdentifierType1, patientIdentifierType2);
@@ -45,7 +51,6 @@ public class HibernatePatientIdentifierMergeDAOTest extends BaseContextSensitive
 		// Get the list of patient identifiers for all patients. They should all be 1 now. Was previously a mix of 1 and 2.
 		String getAllPatientIdentifierUsed = "SELECT identifier_type FROM patient_identifier";
 		SQLQuery query = sessionFactory.getCurrentSession().createSQLQuery(getAllPatientIdentifierUsed);
-		System.out.println(query.list());
 		for(int i = 0; i < query.list().size(); i++) {
 			assertEquals(1, query.list().get(i));
 		}
@@ -58,4 +63,36 @@ public class HibernatePatientIdentifierMergeDAOTest extends BaseContextSensitive
 		// patient1 and patient2 should now have the same patient identifier type, which is "OpenMRS Identification Number id"
 	    assertEquals(updatedPatient1.getPatientIdentifier().getIdentifierType(), updatedPatient2.getPatientIdentifier().getIdentifierType());
 	}	
+	
+	@Test
+	public void getPatientIdentifer_shouldGiveEverWhenThereAreDuplicateIdentifiers() {
+		
+		// Getting the PatientIdentifierType objects in the mock database
+		PatientIdentifierType patientIdentifierType1 = (PatientIdentifierType) sessionFactory.getCurrentSession().get(PatientIdentifierType.class, 1);
+		PatientIdentifierType patientIdentifierType2 = (PatientIdentifierType) sessionFactory.getCurrentSession().get(PatientIdentifierType.class, 2);
+				
+		// Get patients that are already in the database and chnage their patient identifiers to be the same (should give error)
+		Patient patient1 = (Patient) sessionFactory.getCurrentSession().get(Patient.class, 2);
+		Patient patient2 = (Patient) sessionFactory.getCurrentSession().get(Patient.class, 6);
+		// Delete patient then readd to database with the new attributes (duplicate identifiers)
+		sessionFactory.getCurrentSession().delete(patient2);
+		PatientIdentifier p2 = patient2.getPatientIdentifier();
+		p2.setIdentifier(patient1.getPatientIdentifier().getIdentifier());
+		patient2.removeIdentifier(patient2.getPatientIdentifier());
+		patient2.addIdentifier(p2);
+
+		try {
+			sessionFactory.getCurrentSession().save(patient2);
+			// Should give exception because you're not allowed to have duplicate identifiers
+			} catch (ConstraintViolationException e){
+			}
+		hibernatePatientIdentifierMergeDao.mergePatientIdentifier(patientIdentifierType1, patientIdentifierType2);
+		
+		try {
+			// Calling the merge duplicate id function. Will give DAOException
+			hibernatePatientIdentifierMergeDao.mergePatientIdentifier(patientIdentifierType1, patientIdentifierType2);
+			}
+			catch(DAOException e) {
+			}
+	}
 }
