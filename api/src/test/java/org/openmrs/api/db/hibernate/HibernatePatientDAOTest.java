16fc4d623b676158fa4a9fb3672f1d6d1d3cf012
kelvin3094 <kelvin309413243@hotmail.com>
2020-03-09 13:32:01 -0400

    Fixed formatting.

--- api/src/test/java/org/openmrs/api/db/hibernate/HibernatePatientDAOTest.java
+++ api/src/test/java/org/openmrs/api/db/hibernate/HibernatePatientDAOTest.java
@@ -31,40 +31,36 @@ import org.openmrs.api.db.DAOException;
 import org.openmrs.test.BaseContextSensitiveTest;
 
 public class HibernatePatientDAOTest extends BaseContextSensitiveTest {
-
+	
 	private HibernatePatientDAO hibernatePatientDao;
+	
 	private SessionFactory sessionFactory;
-
+	
 	@Before
 	public void beforeEach() {
 		sessionFactory = (SessionFactory) applicationContext.getBean("sessionFactory");
 		updateSearchIndex();
 		hibernatePatientDao = (HibernatePatientDAO) applicationContext.getBean("patientDAO");
 	}
-
+	
 	@Test
 	public void getPatientIdentifiers_shouldGetByIdentifierType() {
 		List<PatientIdentifierType> identifierTypes = singletonList(new PatientIdentifierType(2));
-		List<PatientIdentifier> identifiers = hibernatePatientDao
-				.getPatientIdentifiers(null, identifierTypes, emptyList(), emptyList(), null);
-		List<Integer> identifierIds = identifiers.stream().map(PatientIdentifier::getId)
-				.collect(Collectors.toList());
-
+		List<PatientIdentifier> identifiers = hibernatePatientDao.getPatientIdentifiers(null, identifierTypes, emptyList(),
+		    emptyList(), null);
+		List<Integer> identifierIds = identifiers.stream().map(PatientIdentifier::getId).collect(Collectors.toList());
+		
 		Assert.assertEquals(2, identifiers.size());
 		Assert.assertThat(identifierIds, hasItems(1, 3));
 	}
-
+	
 	@Test
 	public void getPatientIdentifiers_shouldGetByPatients() {
-		List<Patient> patients = Arrays.asList(
-				hibernatePatientDao.getPatient(6),
-				hibernatePatientDao.getPatient(7)
-		);
-		List<PatientIdentifier> identifiers = hibernatePatientDao
-				.getPatientIdentifiers(null, emptyList(), emptyList(), patients, null);
-		List<Integer> identifierIds = identifiers.stream().map(PatientIdentifier::getId)
-				.collect(Collectors.toList());
-
+		List<Patient> patients = Arrays.asList(hibernatePatientDao.getPatient(6), hibernatePatientDao.getPatient(7));
+		List<PatientIdentifier> identifiers = hibernatePatientDao.getPatientIdentifiers(null, emptyList(), emptyList(),
+		    patients, null);
+		List<Integer> identifierIds = identifiers.stream().map(PatientIdentifier::getId).collect(Collectors.toList());
+		
 		Assert.assertEquals(2, identifiers.size());
 		Assert.assertThat(identifierIds, hasItems(3, 4));
 	}
@@ -74,20 +70,22 @@ public class HibernatePatientDAOTest extends BaseContextSensitiveTest {
 		// Getting the PatientIdentifierType objects in the mock database
 		// OpenMRS Identification Number id is 1
 		// Old Identification Number id is 2
-		PatientIdentifierType patientIdentifierType1 = (PatientIdentifierType) sessionFactory.getCurrentSession().get(PatientIdentifierType.class, 1);
-		PatientIdentifierType patientIdentifierType2 = (PatientIdentifierType) sessionFactory.getCurrentSession().get(PatientIdentifierType.class, 2);
-				
+		PatientIdentifierType patientIdentifierType1 = (PatientIdentifierType) sessionFactory.getCurrentSession()
+		        .get(PatientIdentifierType.class, 1);
+		PatientIdentifierType patientIdentifierType2 = (PatientIdentifierType) sessionFactory.getCurrentSession()
+		        .get(PatientIdentifierType.class, 2);
+		
 		// Get patients that are already in the database
 		Patient patient1 = (Patient) sessionFactory.getCurrentSession().get(Patient.class, 2);
 		Patient patient2 = (Patient) sessionFactory.getCurrentSession().get(Patient.class, 6);
-
+		
 		// Calling the merge function for merging duplicate patient identifier types
 		hibernatePatientDao.mergePatientIdentifier(patientIdentifierType1, patientIdentifierType2);
 		
 		// Get the list of patient identifiers for all patients. They should all be 1 now. Was previously a mix of 1 and 2.
 		String getAllPatientIdentifierUsed = "SELECT identifier_type FROM patient_identifier";
 		SQLQuery query = sessionFactory.getCurrentSession().createSQLQuery(getAllPatientIdentifierUsed);
-		for(int i = 0; i < query.list().size(); i++) {
+		for (int i = 0; i < query.list().size(); i++) {
 			assertEquals(1, query.list().get(i));
 		}
 		
@@ -95,18 +93,21 @@ public class HibernatePatientDAOTest extends BaseContextSensitiveTest {
 		sessionFactory.getCurrentSession().update(patient2);
 		Patient updatedPatient1 = (Patient) sessionFactory.getCurrentSession().get(Patient.class, 2);
 		Patient updatedPatient2 = (Patient) sessionFactory.getCurrentSession().get(Patient.class, 6);
-
+		
 		// patient1 and patient2 should now have the same patient identifier type, which is "OpenMRS Identification Number id"
-	    assertEquals(updatedPatient1.getPatientIdentifier().getIdentifierType(), updatedPatient2.getPatientIdentifier().getIdentifierType());
-	}	
+		assertEquals(updatedPatient1.getPatientIdentifier().getIdentifierType(),
+		    updatedPatient2.getPatientIdentifier().getIdentifierType());
+	}
 	
 	@Test
 	public void getPatientIdentifer_shouldGiveEverWhenThereAreDuplicateIdentifiers() {
 		
 		// Getting the PatientIdentifierType objects in the mock database
-		PatientIdentifierType patientIdentifierType1 = (PatientIdentifierType) sessionFactory.getCurrentSession().get(PatientIdentifierType.class, 1);
-		PatientIdentifierType patientIdentifierType2 = (PatientIdentifierType) sessionFactory.getCurrentSession().get(PatientIdentifierType.class, 2);
-				
+		PatientIdentifierType patientIdentifierType1 = (PatientIdentifierType) sessionFactory.getCurrentSession()
+		        .get(PatientIdentifierType.class, 1);
+		PatientIdentifierType patientIdentifierType2 = (PatientIdentifierType) sessionFactory.getCurrentSession()
+		        .get(PatientIdentifierType.class, 2);
+		
 		// Get patients that are already in the database and chnage their patient identifiers to be the same (should give error)
 		Patient patient1 = (Patient) sessionFactory.getCurrentSession().get(Patient.class, 2);
 		Patient patient2 = (Patient) sessionFactory.getCurrentSession().get(Patient.class, 6);
@@ -116,19 +117,16 @@ public class HibernatePatientDAOTest extends BaseContextSensitiveTest {
 		p2.setIdentifier(patient1.getPatientIdentifier().getIdentifier());
 		patient2.removeIdentifier(patient2.getPatientIdentifier());
 		patient2.addIdentifier(p2);
-
+		
 		try {
-			sessionFactory.getCurrentSession().save(patient2);
 			// Should give exception because you're not allowed to have duplicate identifiers
-			} catch (ConstraintViolationException e){
-			}
-
-		try {
+			sessionFactory.getCurrentSession().save(patient2);
 			// Calling the merge duplicate id function. Will give DAOException
 			hibernatePatientDao.mergePatientIdentifier(patientIdentifierType1, patientIdentifierType2);
-			}
-			catch(DAOException e) {
-			}
+			Assert.fail("Either ConstraintViolationException or DAOException was expected!");
+		}
+		catch (ConstraintViolationException | DAOException e) {
+			// should have an exception
+		}
 	}
-
 }
