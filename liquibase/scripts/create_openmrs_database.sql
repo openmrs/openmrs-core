118b8116a4b99df3f570efe482e71b8832b00abe
Wolf Schlegel <wolf.schlegel@thoughtworks.com>
2020-05-17 14:13:46 +0200

    TRUNK-4830 add creation of liquibase tables to helper script

--- liquibase/scripts/create_openmrs_database.sql
+++ liquibase/scripts/create_openmrs_database.sql
@@ -9,3 +9,34 @@
 --
 create database openmrs;
 show databases;
+
+use openmrs;
+
+DROP TABLE IF EXISTS `liquibasechangelog`;
+CREATE TABLE `liquibasechangelog` (
+  `ID` varchar(255) NOT NULL,
+  `AUTHOR` varchar(255) NOT NULL,
+  `FILENAME` varchar(255) NOT NULL,
+  `DATEEXECUTED` datetime NOT NULL,
+  `ORDEREXECUTED` int(11) NOT NULL,
+  `EXECTYPE` varchar(10) NOT NULL,
+  `MD5SUM` varchar(35) DEFAULT NULL,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `COMMENTS` varchar(255) DEFAULT NULL,
+  `TAG` varchar(255) DEFAULT NULL,
+  `LIQUIBASE` varchar(20) DEFAULT NULL,
+  `CONTEXTS` varchar(255) DEFAULT NULL,
+  `LABELS` varchar(255) DEFAULT NULL,
+  `DEPLOYMENT_ID` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `liquibasechangeloglock`;
+CREATE TABLE `liquibasechangeloglock` (
+  `ID` int(11) NOT NULL,
+  `LOCKED` tinyint(1) NOT NULL,
+  `LOCKGRANTED` datetime DEFAULT NULL,
+  `LOCKEDBY` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+show tables;
