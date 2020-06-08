485daa08f6a2a1391a056c85c507b7251a009cd5
gita cliff <gitacliff48@gmail.com>
2020-06-08 23:00:15 +0300

    TRUNK-5678:Classloading issues on Openmrs installation and DB Upgrades

--- web/src/main/java/org/openmrs/web/filter/update/UpdateFilter.java
+++ web/src/main/java/org/openmrs/web/filter/update/UpdateFilter.java
@@ -23,6 +23,7 @@ import org.openmrs.util.DatabaseUpdaterLiquibaseProvider;
 import org.openmrs.util.InputRequiredException;
 import org.openmrs.liquibase.ChangeLogVersionFinder;
 import org.openmrs.util.MemoryAppender;
+import org.openmrs.util.OpenmrsClassLoader;
 import org.openmrs.util.OpenmrsConstants;
 import org.openmrs.util.OpenmrsUtil;
 import org.openmrs.util.RoleConstants;
@@ -481,6 +482,13 @@ public class UpdateFilter extends StartupFilter {
 	 * @see Listener#startOpenmrs(ServletContext)
 	 */
 	private void startOpenmrs(ServletContext servletContext) throws Exception {
+		
+		// Update the filter to set the Openmrs classloader on the current thread before
+		// starting OpenMRS
+		// and the spring web application context that way it is inherited by the daemon
+		// thread.
+		Thread.currentThread().setContextClassLoader(OpenmrsClassLoader.getInstance());
+
 		// start spring
 		// after this point, all errors need to also call: contextLoader.closeWebApplicationContext(event.getServletContext())
 		// logic copied from org.springframework.web.context.ContextLoaderListener
