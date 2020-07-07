7b297a254cb092d2531b329277cd29c4005fe4b2
achilep <you@example.com>
2020-07-07 15:59:35 +0100

    TRUNK-5816: Migrate unit tests to Junit5

--- api/src/test/java/org/openmrs/UserTest.java
+++ api/src/test/java/org/openmrs/UserTest.java
@@ -9,11 +9,12 @@
  */
 package org.openmrs;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.openmrs.util.RoleConstants;
 
 public class UserTest {
@@ -26,7 +27,7 @@ public class UserTest {
 	
 	private final String ROLE_WHICH_DOES_NOT_EXIT = "Role Which Does Not Exist";
 	
-	@Before
+	@BeforeEach
 	public void setUp() {
 		user = new User();
 		user.addRole(new Role("Some Role", "This is a test role"));
