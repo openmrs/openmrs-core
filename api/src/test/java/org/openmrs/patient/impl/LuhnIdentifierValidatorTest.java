b3d2fab879ab757f8c1798fe3fce0041b10fc5d6
achilep <you@example.com>
2020-07-12 23:38:03 +0100

    TRUNK-5816: Migrate unit tests to Junit5

--- api/src/test/java/org/openmrs/patient/impl/LuhnIdentifierValidatorTest.java
+++ api/src/test/java/org/openmrs/patient/impl/LuhnIdentifierValidatorTest.java
@@ -9,12 +9,12 @@
  */
 package org.openmrs.patient.impl;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.openmrs.patient.UnallowedIdentifierException;
 
 /**
