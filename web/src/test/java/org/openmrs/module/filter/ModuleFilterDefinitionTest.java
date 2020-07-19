cd9f10b4fba8297c3f90d77f86d12f5a0b965948
achilep <you@example.com>
2020-07-19 20:41:46 +0100

    TRUNK-5817: Migrate web and webapp package tests to Junit 5

--- web/src/test/java/org/openmrs/module/filter/ModuleFilterDefinitionTest.java
+++ web/src/test/java/org/openmrs/module/filter/ModuleFilterDefinitionTest.java
@@ -12,23 +12,20 @@ package org.openmrs.module.filter;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.StringReader;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.openmrs.module.Module;
 import org.openmrs.module.ModuleException;
 import org.openmrs.module.web.filter.ModuleFilterDefinition;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 
-@PrepareForTest(ModuleFilterDefinition.class)
 public class ModuleFilterDefinitionTest {
 	
 	public static Document getDocument(String xmlString) {
