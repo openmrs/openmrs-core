e1106ca02ff6dd738b6caf52d568bf666b143410
alinmihaila99 <mihaila.alin99@yahoo.com>
2020-07-21 13:43:33 +0300

    mesaj

--- api/src/main/java/org/openmrs/OrderType.java
+++ api/src/main/java/org/openmrs/OrderType.java
@@ -29,8 +29,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapKeyColumn;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 /**
  * OrderTypes are used to classify different types of Orders e.g to distinguish between Serology and
