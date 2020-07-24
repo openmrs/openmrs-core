07e84edd47af4af87b0f08a589cdb505062e9ffc
Ivo <teleivo@users.noreply.github.com>
2020-07-24 08:28:56 +0200

    TRUNK-5816 Merge custom TestExecutionlisteners with defaults
    so we do not have to keep track of what the defaults are and how they change in spring updates. See https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/testing.html#testcontext-tel-config-merging

--- api/src/test/java/org/openmrs/test/jupiter/BaseContextSensitiveTest.java
+++ api/src/test/java/org/openmrs/test/jupiter/BaseContextSensitiveTest.java
@@ -88,13 +88,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.event.EventPublishingTestExecutionListener;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextBeforeModesTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import org.springframework.test.context.web.ServletTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 import org.xml.sax.InputSource;
 
@@ -112,10 +106,10 @@ import org.xml.sax.InputSource;
 @ContextConfiguration(locations = { "classpath:applicationContext-service.xml", "classpath*:openmrs-servlet.xml",
         "classpath*:moduleApplicationContext.xml", "classpath*:TestingApplicationContext.xml" })
 @TestExecutionListeners(
-	{ TransactionalTestExecutionListener.class, SkipBaseSetupAnnotationExecutionListener.class,
-		StartModuleExecutionListener.class, ServletTestExecutionListener.class, DirtiesContextBeforeModesTestExecutionListener.class,
-	DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
-	EventPublishingTestExecutionListener.class })
+	listeners = { SkipBaseSetupAnnotationExecutionListener.class,
+		StartModuleExecutionListener.class },
+        mergeMode = MERGE_WITH_DEFAULTS
+)
 @Transactional
 @Rollback
 @ExtendWith(SpringExtension.class)
