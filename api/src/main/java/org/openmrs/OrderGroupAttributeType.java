5a7a0879e4d9fc53e6068ed7b455ef323c280d00
tendomart <tmkmart@gmail.com>
2020-07-26 18:49:51 +0300

    TRUNK-5410 Added hbm Mapping Files and added them to cfg file

--- api/src/main/java/org/openmrs/OrderGroupAttributeType.java
+++ api/src/main/java/org/openmrs/OrderGroupAttributeType.java
@@ -9,7 +9,6 @@
  */
 package org.openmrs;
 
-import org.openmrs.api.context.Context;
 import org.openmrs.attribute.AttributeType;
 import org.openmrs.attribute.BaseAttributeType;
 
@@ -17,122 +16,21 @@ import java.util.Date;
 
 public class OrderGroupAttributeType extends BaseAttributeType<OrderGroup> implements AttributeType<OrderGroup> {
 	private Integer orderGroupAttributeTypeId;
-	public String name;
-	public String description;
-	public User user;
-	@Override
-	public String getName() {
-		return name;
-	}
 
-	@Override
-	public void setName(String name) {
-         this.name=name;
-	}
-
-	@Override
-	public String getDescription() {
-		return description;
-	}
-
-	@Override
-	public void setDescription(String description) {
-       this.description=description;
-	}
-
-	@Override
-	public User getChangedBy() {
-		return user ;
-	}
-
-	@Override
-	public void setChangedBy(User changedBy) {
-        this.user=changedBy;
-	}
-
-	@Override
-	public Date getDateChanged() {
-		return new Date();
-	}
-
-	@Override
-	public void setDateChanged(Date dateChanged) {
-         
-	}
-
-	@Override
-	public User getCreator() {
-		return null;
-	}
-
-	@Override
-	public void setCreator(User creator) {
-
-	}
-
-	@Override
-	public Date getDateCreated() {
-		return null;
-	}
-
-	@Override
-	public void setDateCreated(Date dateCreated) {
-
-	}
-
-	@Override
-	public Boolean isRetired() {
-		return null;
-	}
-
-	@Override
-	public Boolean getRetired() {
-		return null;
-	}
-
-	@Override
-	public void setRetired(Boolean retired) {
-
-	}
-
-	@Override
-	public User getRetiredBy() {
-		return null;
-	}
-
-	@Override
-	public void setRetiredBy(User retiredBy) {
-
-	}
-
-	@Override
-	public Date getDateRetired() {
-		return null;
-	}
-
-	@Override
-	public void setDateRetired(Date dateRetired) {
-
-	}
-
-	@Override
-	public String getRetireReason() {
-		return null;
-	}
-
-	@Override
-	public void setRetireReason(String retireReason) {
-
-	}
-
+	/**
+	 * @see org.openmrs.OpenmrsObject#getId()
+	 */
 	@Override
 	public Integer getId() {
-		return orderGroupAttributeTypeId;
+		return getOrderGroupAttributeTypeId();
 	}
 
+	/**
+	 * @see org.openmrs.OpenmrsObject#setId(java.lang.Integer)
+	 */
 	@Override
 	public void setId(Integer id) {
-         setOrderGroupAttributeTypeId(id);
+		setOrderGroupAttributeTypeId(id);
 	}
 
 	/**
@@ -147,45 +45,5 @@ public class OrderGroupAttributeType extends BaseAttributeType<OrderGroup> imple
 	 */
 	public void setOrderGroupAttributeTypeId(Integer orderGroupAttributeTypeId) {
 		this.orderGroupAttributeTypeId = orderGroupAttributeTypeId;
-	}
-
-	@Override
-	public String getUuid() {
-		return null;
-	}
-
-	@Override
-	public void setUuid(String uuid) {
-
-	}
-
-	@Override
-	public Integer getMinOccurs() {
-		return null;
-	}
-
-	@Override
-	public Integer getMaxOccurs() {
-		return null;
-	}
-
-	@Override
-	public String getDatatypeClassname() {
-		return null;
-	}
-
-	@Override
-	public String getDatatypeConfig() {
-		return null;
-	}
-
-	@Override
-	public String getPreferredHandlerClassname() {
-		return null;
-	}
-
-	@Override
-	public String getHandlerConfig() {
-		return null;
 	}
 }
