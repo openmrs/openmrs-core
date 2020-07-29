71d3dfad3bec05b7477199a4dbff00432686d3b5
alinmihaila99 <mihaila.alin99@yahoo.com>
2020-07-29 11:44:50 +0300

    TRUNK-5891: ProgramWorkflowState Domain - Switching from Hibernate Mappings to Annotations

--- api/src/main/java/org/openmrs/ProgramWorkflowState.java
+++ api/src/main/java/org/openmrs/ProgramWorkflowState.java
@@ -17,7 +17,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 /**
@@ -36,7 +35,7 @@ public class ProgramWorkflowState extends BaseChangeableOpenmrsMetadata {
 	
 	@Id
 	@Column(name = "program_workflow_state_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer programWorkflowStateId;
 	
 	@ManyToOne
