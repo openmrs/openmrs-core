2a3991cd48d551b6997ba4cf7b58a6bc5208139b
Luis Oliveira <luis.oliveira@icrc.org>
2020-11-09 10:25:19 +0000

    Replaced wildcard imports.

--- api/src/main/java/org/openmrs/Encounter.java
+++ api/src/main/java/org/openmrs/Encounter.java
@@ -9,7 +9,16 @@
  */
 package org.openmrs;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.persistence.Access;
