7676950a8dfff2cbb4344f6b2b6477be1b2e37f0
dkayiwa <kayiwadaniel@gmail.com>
2021-02-04 20:35:30 +0300

    TRUNK-5991 Fixing compiler errors

--- api/src/test/java/org/openmrs/validator/DrugOrderValidatorTest.java
+++ api/src/test/java/org/openmrs/validator/DrugOrderValidatorTest.java
@@ -22,6 +22,7 @@ import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openmrs.CareSetting;
 import org.openmrs.Concept;
