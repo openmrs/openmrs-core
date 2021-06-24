8114fe6ff28061da51fdac3dff73fe3446cf20b8
Ian <52504170+ibacher@users.noreply.github.com>
2021-06-24 14:00:10 -0400

    Update api/src/main/java/org/openmrs/util/CurrentUsers.java

--- api/src/main/java/org/openmrs/util/CurrentUsers.java
+++ api/src/main/java/org/openmrs/util/CurrentUsers.java
@@ -24,7 +24,7 @@ public class CurrentUsers implements UserSessionListener {
 
 	@Override
 	public void loggedInOrOut(User user, Event event, Status status) {
-		if(!(status == Status.SUCCESS)) {
+		if (!(status == Status.SUCCESS)) {
 			return;
 		}
 		
