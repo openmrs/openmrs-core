1d26067a70a38839a399010b1b6c5d0bc985a5d5
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-06-26 00:24:00 +0200

    TRUNK-6008: Fix HibernatePatientDAO.getDuplicatePatientsByAttributes

--- api/src/test/java/org/openmrs/api/db/hibernate/HibernatePatientDAOTest.java
+++ api/src/test/java/org/openmrs/api/db/hibernate/HibernatePatientDAOTest.java
@@ -12,9 +12,11 @@ package org.openmrs.api.db.hibernate;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.sql.Date;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,16 +26,21 @@ import org.junit.jupiter.api.Test;
 import org.openmrs.Patient;
 import org.openmrs.PatientIdentifier;
 import org.openmrs.PatientIdentifierType;
+import org.openmrs.Person;
+import org.openmrs.PersonName;
 import org.openmrs.test.jupiter.BaseContextSensitiveTest;
 
 public class HibernatePatientDAOTest extends BaseContextSensitiveTest {
 
 	private HibernatePatientDAO hibernatePatientDao;
+	
+	private HibernatePersonDAO hibernatePersonDAO;
 
 	@BeforeEach
 	public void beforeEach() {
 		updateSearchIndex();
 		hibernatePatientDao = (HibernatePatientDAO) applicationContext.getBean("patientDAO");
+		hibernatePersonDAO = (HibernatePersonDAO) applicationContext.getBean("personDAO");
 	}
 
 	@Test
@@ -63,4 +70,116 @@ public class HibernatePatientDAOTest extends BaseContextSensitiveTest {
 		assertThat(identifierIds, hasItems(3, 4));
 	}
 
+	@Test
+	public void getDuplicatePatientsByAttributes_shouldNotReturnPatientsWithUniqueNames() {
+		// given
+		Person person1 = new Person();
+		person1.addName(new PersonName("Ioan", "Theo", "Fletcher"));
+		person1 = hibernatePersonDAO.savePerson(person1);
+		hibernatePatientDao.savePatient(new Patient(person1));
+
+		Person person2 = new Person();
+		person2.addName(new PersonName("Ioan1", "Theo1", "Fletcher1"));
+		person2 = hibernatePersonDAO.savePerson(person2);
+		hibernatePatientDao.savePatient(new Patient(person2));
+
+		// when
+		List<Patient> duplicatePatients = hibernatePatientDao
+			.getDuplicatePatientsByAttributes(Arrays.asList("givenName", "middleName", "familyName"));
+
+		// then
+		assertThat(duplicatePatients.size(), equalTo(0));
+	}
+
+	@Test
+	public void getDuplicatePatientsByAttributes_shouldReturnPatientsWithDuplicatedAllNames() {
+		// given
+		Person person1 = new Person();
+		person1.addName(new PersonName("Ioan", "Theo", "Fletcher"));
+		person1 = hibernatePersonDAO.savePerson(person1);
+		hibernatePatientDao.savePatient(new Patient(person1));
+
+		Person person2 = new Person();
+		person2.addName(new PersonName("Ioan", "Theo", "Fletcher"));
+		person2 = hibernatePersonDAO.savePerson(person2);
+		hibernatePatientDao.savePatient(new Patient(person2));
+
+		// when
+		List<Patient> duplicatePatients = hibernatePatientDao
+			.getDuplicatePatientsByAttributes(Arrays.asList("givenName", "middleName", "familyName"));
+		
+		// then
+		assertThat(duplicatePatients.size(), equalTo(2));
+	}
+
+	@Test
+	public void getDuplicatePatientsByAttributes_shouldReturnPatientsWithDuplicatedFamilyNames() {
+		// given
+		Person person1 = new Person();
+		person1.addName(new PersonName("Ioan", "Theo", "Fletcher"));
+		person1 = hibernatePersonDAO.savePerson(person1);
+		hibernatePatientDao.savePatient(new Patient(person1));
+
+		Person person2 = new Person();
+		person2.addName(new PersonName("Ioan1", "Theo1", "Fletcher"));
+		person2 = hibernatePersonDAO.savePerson(person2);
+		hibernatePatientDao.savePatient(new Patient(person2));
+
+		// when
+		List<Patient> duplicatePatients = hibernatePatientDao
+			.getDuplicatePatientsByAttributes(singletonList("familyName"));
+
+		// then
+		assertThat(duplicatePatients.size(), equalTo(2));
+	}
+
+	@Test
+	public void getDuplicatePatientsByAttributes_shouldReturnPatientsWithDuplicatedAllFields() {
+		// given
+		Person person1 = new Person();
+		person1.addName(new PersonName("Ioan", "Theo", "Fletcher"));
+		person1.setGender("M");
+		person1.setBirthdate(Date.valueOf("2021-06-26"));
+		person1 = hibernatePersonDAO.savePerson(person1);
+		hibernatePatientDao.savePatient(new Patient(person1));
+
+		Person person2 = new Person();
+		person2.addName(new PersonName("Ioan", "Theo", "Fletcher"));
+		person2.setGender("M");
+		person2.setBirthdate(Date.valueOf("2021-06-26"));
+		person2 = hibernatePersonDAO.savePerson(person2);
+		hibernatePatientDao.savePatient(new Patient(person2));
+
+		// when
+		List<Patient> duplicatePatients = hibernatePatientDao
+			.getDuplicatePatientsByAttributes(Arrays.asList("gender", "birthdate", "givenName", "middleName", "familyName"));
+
+		// then
+		assertThat(duplicatePatients.size(), equalTo(2));
+	}
+
+	@Test
+	public void getDuplicatePatientsByAttributes_shouldNotReturnPatientsWithDuplicatedAllFieldsBesidesGender() {
+		// given
+		Person person1 = new Person();
+		person1.addName(new PersonName("Ioan", "Theo", "Fletcher"));
+		person1.setGender("M");
+		person1.setBirthdate(Date.valueOf("2021-06-26"));
+		person1 = hibernatePersonDAO.savePerson(person1);
+		hibernatePatientDao.savePatient(new Patient(person1));
+
+		Person person2 = new Person();
+		person2.addName(new PersonName("Ioan", "Theo", "Fletcher"));
+		person2.setGender("F");
+		person2.setBirthdate(Date.valueOf("2021-06-26"));
+		person2 = hibernatePersonDAO.savePerson(person2);
+		hibernatePatientDao.savePatient(new Patient(person2));
+
+		// when
+		List<Patient> duplicatePatients = hibernatePatientDao
+			.getDuplicatePatientsByAttributes(Arrays.asList("gender", "birthdate", "givenName", "middleName", "familyName"));
+
+		// then
+		assertThat(duplicatePatients.size(), equalTo(0));
+	}
 }
