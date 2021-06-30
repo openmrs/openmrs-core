e679877103a89937a867d1fdf3aa48f8caaddd73
Ian <ian_bacher@brown.edu>
2021-06-30 08:03:09 -0400

    Revert "TRUNK-6009: Remove use of Class.forName() to load db drivers (#3813)"
    This reverts commit a49b0cf4960f08a92b29bd8fdecd542034aed625.

--- api/src/main/java/org/openmrs/util/DatabaseUpdater.java
+++ api/src/main/java/org/openmrs/util/DatabaseUpdater.java
@@ -447,6 +447,7 @@ public class DatabaseUpdater {
 		Properties props = Context.getRuntimeProperties();
 		mergeDefaultRuntimeProperties(props);
 		
+		String driver = props.getProperty("hibernate.connection.driver_class");
 		String username = props.getProperty("hibernate.connection.username");
 		String password = props.getProperty("hibernate.connection.password");
 		String url = props.getProperty("hibernate.connection.url");
@@ -456,6 +457,7 @@ public class DatabaseUpdater {
 			url = url + "&sessionVariables=default_storage_engine=InnoDB";
 		}
 		
+		Class.forName(driver);
 		return DriverManager.getConnection(url, username, password);
 	}
 	
