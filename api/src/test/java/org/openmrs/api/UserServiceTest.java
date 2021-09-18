667908fe6e1163c4419d3edc4978bf53d3a6c66f
tendomart <tmkmart@gmail.com>
2021-09-18 22:14:38 +0300

    removing unnecessary loop

--- api/src/test/java/org/openmrs/api/UserServiceTest.java
+++ api/src/test/java/org/openmrs/api/UserServiceTest.java
@@ -1654,13 +1654,9 @@ public class UserServiceTest extends BaseContextSensitiveTest {
 	@Test
 	public void saveUserProperty_shouldAddANewPropertyWithAVeryLargeStringWithoutRunningIntoError() {
 		final String USER_PROPERTY_KEY = liquibase.util.StringUtil.repeat("emrapi.lastViewedPatientIds,",10);
-		final String USER_PROPERTY_VALUE = liquibase.util.StringUtil.repeat("1234",500);
+		final String USER_PROPERTY_VALUE = liquibase.util.StringUtil.repeat("52345",9899);
 		User updatedUser = userService.saveUserProperty(USER_PROPERTY_KEY, USER_PROPERTY_VALUE);
-		String keyList = "";
-		for (String key : updatedUser.getUserProperties().keySet()) {
-			 keyList = key;
-		}
-		assertEquals(280, keyList.length());
-		assertEquals(2000, updatedUser.getUserProperties().get(USER_PROPERTY_KEY).length());
+		assertEquals(280, updatedUser.getUserProperties().keySet().stream().findFirst().get().length());
+		assertEquals(49495, updatedUser.getUserProperties().get(USER_PROPERTY_KEY).length());
 	}
 }
