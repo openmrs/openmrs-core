629b9a08a23e894953d2aecb934811e9efbec7b3
Christopher Miiro <chrismiiroslav@gmail.com>
2021-10-12 00:28:29 +0300

    TRUNK-6027:Make Orders Attributable (#3865)
    * TRUNK-6027:Make Orders Attributable
    
    * TRUNK-6027:Make Orders Attributable; correcting JavaDoc comments, to @Since 2.5.0 in OrderAttribute and OrderAttributeType classes
    
    * TRUNK-6027:Make Orders Attributable; correcting JavaDoc comments, to 2.5.0-SNAPSHOT in OrderAttribute and OrderAttributeType classes
    
    * TRUNK-6027:Make Orders Attributable; correcting JavaDoc comments, to 2.5.0-SNAPSHOT in OrderAttribute and OrderAttributeType classes
    
    * TRUNK-6027:Make Orders Attributable; correcting JavaDoc comments to 2.5.0 in OrderAttribute and OrderAttributeType classes and adding license-headers
    
    * TRUNK-6027:Make Orders Attributable, adding implementation and resolving merge conflicts.
    
    * TRUNK-6027:Make Orders Attributable, fixing Liquibase integration test.
    
    * TRUNK-6027:Make Orders Attributable, adding precoditions for the order_attribute and order_attribute_type tables.
    
    * TRUNK-6027:Make Orders Attributable, modifying tests.
    
    * TRUNK-6027:Make Orders Attributable, reverting Indentation changes in the HibernateOrderDAO class
    
    * TRUNK-6027:Make Orders Attributable, adding unimplemented methods in the HibernateOrderDAO class after reverting Indentation changes to commit 8e597d7
    
    * TRUNK-6027:Make Orders Attributable, adding attributes relationship to Order.hbm.xml and improving tests
    
    * TRUNK-6027:Make Orders Attributtable,cleaning up fileds from Order class that are already in base class
    
    * TRUNK-6027:Make Orders Attributable, replacing literal with constants in tests
    
    * TURNK-6027:Make Orders Attributable, removing redundant assertions
    
    * TURNK-6027:Make Orders Attributable, removing redundant constants
    
    * TRUNK-6027:Make Orders Attributable, modifying tests.
    
    * TRUNK-6027:Make Orders Attributable, removing duplicate tests and adding since annotations to OrderService Javadocs

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -11,6 +11,7 @@ package org.openmrs.api;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -56,6 +57,8 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import org.openmrs.Order.Action;
+import org.openmrs.OrderAttribute;
+import org.openmrs.OrderAttributeType;
 import org.openmrs.TestOrder;
 import org.openmrs.Patient;
 import org.openmrs.DosingInstructions;
@@ -118,6 +121,8 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 
 	private static final String ORDER_GROUP_ATTRIBUTES = "org/openmrs/api/include/OrderServiceTest-createOrderGroupAttributes.xml";
 
+	private static final String ORDER_ATTRIBUTES = "org/openmrs/api/include/OrderServiceTest-createOrderAttributes.xml";
+
 	@Autowired
 	private ConceptService conceptService;
 
@@ -144,6 +149,7 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 	
 	@BeforeEach
 	public void setUp(){
+		executeDataSet(ORDER_ATTRIBUTES);
 		executeDataSet(ORDER_GROUP_ATTRIBUTES);
 	}
 
@@ -3976,5 +3982,84 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 		
 		order = orderService.saveOrder(order, null);
 		assertEquals(NAMESPACE + "^" + FORMFIELD_PATH, order.getFormNamespaceAndPath());
+	}
+
+	@Test
+	public void getAllOrderAttributeTypes_shouldReturnAllOrderAttributeTypes() {
+		assertThat(orderService.getAllOrderAttributeTypes(), hasSize(4));
+	}
+
+	@Test
+	public void getOrderAttributeTypeById_shouldReturnNullIfNoOrderAttributeTypeHasTheProvidedId() {
+		assertNull(orderService.getOrderAttributeTypeById(15));
+	}
+
+	@Test
+	public void getOrderAttributeTypeById_shouldReturnOrderAttributeTypeUsingProvidedId() {
+		assertThat(orderService.getOrderAttributeTypeById(2).getId(), is(2));
+	}
+
+	@Test
+	public void getOrderAttributeTypeByUuid_shouldReturnOrderAttributeTypeUsingProvidedUuid() {
+		assertEquals("Referral", orderService.getOrderAttributeTypeByUuid(
+				"9758d106-79b0-4f45-8d8c-ae8b3f25d72a").getName());
+	}
+	
+	@Test
+	public void saveOrderAttributeType_shouldEditTheExistingOrderAttributeType() {
+		OrderAttributeType orderAttributeType = orderService.getOrderAttributeTypeById(4);
+		assertEquals("Drug", orderAttributeType.getName());
+		orderAttributeType.setName("Drug Dispense");
+		orderService.saveOrderAttributeType(orderAttributeType);
+		assertThat(orderService.getOrderAttributeTypeById(orderAttributeType.getId()).getName(), is("Drug Dispense"));
+	}
+
+	@Test
+	public void retireOrderAttributeType_shouldRetireTheProvidedOrderAttributeType() throws ParseException {
+		OrderAttributeType orderAttributeType = orderService.getOrderAttributeTypeById(2);
+		assertFalse(orderAttributeType.getRetired());
+		assertNull(orderAttributeType.getRetiredBy());
+		assertNull(orderAttributeType.getRetireReason());
+		assertNull(orderAttributeType.getDateRetired());
+		orderService.retireOrderAttributeType(orderAttributeType, "Test Retire");
+		orderAttributeType = orderService.getOrderAttributeTypeById(orderAttributeType.getId());
+		assertTrue(orderAttributeType.getRetired());
+		assertNotNull(orderAttributeType.getRetiredBy());
+		assertEquals("Test Retire", orderAttributeType.getRetireReason());
+		assertNotNull(orderAttributeType.getDateRetired());
+	}
+
+	@Test
+	public void unretireOrderAttributeType_shouldUnretireTheProvidedOrderAttributeType() {
+		OrderAttributeType orderAttributeType = orderService.getOrderAttributeTypeById(4);
+		assertTrue(orderAttributeType.getRetired());
+		assertNotNull(orderAttributeType.getRetiredBy());
+		assertNotNull(orderAttributeType.getDateRetired());
+		assertNotNull(orderAttributeType.getRetireReason());
+		orderService.unretireOrderAttributeType(orderAttributeType);
+		assertFalse(orderAttributeType.getRetired());
+		assertNull(orderAttributeType.getRetiredBy());
+		assertNull(orderAttributeType.getDateRetired());
+		assertNull(orderAttributeType.getRetireReason());
+	}
+	
+	@Test
+	public void purgeOrderAttributeType_shouldPurgeTheProvidedOrderAttributeType() {
+		final int ORIGINAL_COUNT = orderService.getAllOrderAttributeTypes().size();
+		orderService.purgeOrderAttributeType(orderService.getOrderAttributeTypeById(3));
+		assertNull(orderService.getOrderAttributeTypeById(3));
+		assertEquals(ORIGINAL_COUNT - 1, orderService.getAllOrderAttributeTypes().size());
+	}
+
+	@Test
+	public void getOrderAttributeByUuid_shouldReturnNullIfNonExistingUuidIsProvided() {
+		assertNull(orderService.getOrderAttributeByUuid("26bbdf73-4268-4e65-aa72-54cb928870d6"));
+	}
+
+	@Test
+	public void getOrderAttributeByUuid_shouldReturnOrderAttributeUsingProvidedUuid() {
+		OrderAttribute orderAttribute = orderService.getOrderAttributeByUuid("8c3c27e4-030f-410e-86de-a5743b0b3361");
+		assertEquals("Testing Reference", orderAttribute.getValueReference());
+		assertEquals(1, orderAttribute.getId());
 	}
 }
