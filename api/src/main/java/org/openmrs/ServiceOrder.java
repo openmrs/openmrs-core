d6aa58221acca5a445e3f5e2f9d2a3bd3f9c44b6
tendomart <tmkmart@gmail.com>
2021-10-12 02:21:26 +0300

    Refactoring cloneForRevisionHelper

--- api/src/main/java/org/openmrs/ServiceOrder.java
+++ api/src/main/java/org/openmrs/ServiceOrder.java
@@ -174,11 +174,12 @@ public abstract class ServiceOrder extends Order {
 	 * @return Returns the Order that was passed in, with state copied into it
 	 */
 	protected ServiceOrder cloneForDiscontinuingHelper(ServiceOrder target) {
-		target.setSpecimenSource(getSpecimenSource());
-		target.setLaterality(getLaterality());
-		target.setClinicalHistory(getClinicalHistory());
-		target.setFrequency(getFrequency());
-		target.setNumberOfRepeats(getNumberOfRepeats());
+		target.setCareSetting(getCareSetting());
+		target.setConcept(getConcept());
+		target.setAction(Action.DISCONTINUE);
+		target.setPreviousOrder(getPreviousOrder());
+		target.setPatient(getPatient());
+		target.setOrderType(getOrderType());
 		return target;
 	}
 }
