3f7c7f346cbb2d66d896073857909a4b06fa690b
Ian <ian_bacher@brown.edu>
2022-01-05 19:24:58 -0500

    TRUNK-6055: Restore liquibase logging messages to the log

---
+++ api/src/main/java/liquibase/ext/logging/slf4j/Slf4JLogService.java
@@ -0,0 +1,31 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package liquibase.ext.logging.slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import liquibase.logging.Logger;
+import liquibase.logging.core.AbstractLogService;
+
+public class Slf4JLogService extends AbstractLogService {
+	
+	private final Map<Class<?>, Slf4JLogger> loggers = new HashMap<>();
+	
+	@Override
+	public int getPriority() {
+		return PRIORITY_SPECIALIZED;
+	}
+	
+	@Override
+	public Logger getLog(Class clazz) {
+		return loggers.computeIfAbsent(clazz, (c) -> new Slf4JLogger(c, getFilter()));
+	}
+}
