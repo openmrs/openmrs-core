36c6a4ddba81086f2c8edd9708a2200c054b6466
HerbertYiga <herbertyiga24@gmail.com>
2022-01-14 14:28:32 +0300

    cleanup

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -119,7 +119,7 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 	private static final String ORDER_GROUP_ATTRIBUTES = "org/openmrs/api/include/OrderServiceTest-createOrderGroupAttributes.xml";
     
 	private static final String ORDER_OTHERS = "org/openmrs/api/include/OrderServiceTest-others.xml";
-	  
+	
 	private static final String UNVOID_ORDERS= "org/openmrs/api/include/OrderTest_unVoidOrderTest.xml";
 	
 	@Autowired
