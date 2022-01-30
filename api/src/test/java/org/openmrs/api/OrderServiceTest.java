0f18ea1386482529a2e665718ad3d98fdf7fb507
Saurabh kumar <saurabhofficial@hotmail.com>
2022-01-31 00:43:01 +0530

    TRUNK-5905 : Replaced xml mapping with annotations (#3924)

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -56,6 +56,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import org.openmrs.ConceptMapType;
 import org.openmrs.Order.Action;
 import org.openmrs.OrderAttribute;
 import org.openmrs.OrderAttributeType;
@@ -2647,7 +2648,7 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 			.addAnnotatedClass(Encounter.class).addAnnotatedClass(SomeTestOrder.class)
 			.addAnnotatedClass(Diagnosis.class).addAnnotatedClass(Condition.class)
 			.addAnnotatedClass(Visit.class).addAnnotatedClass(VisitAttributeType.class)
-			.addAnnotatedClass(ProviderAttributeType.class).getMetadataBuilder().build();
+			.addAnnotatedClass(ProviderAttributeType.class).addAnnotatedClass(ConceptMapType.class).getMetadataBuilder().build();
 
 
 		Field field = adminDAO.getClass().getDeclaredField("metadata");
