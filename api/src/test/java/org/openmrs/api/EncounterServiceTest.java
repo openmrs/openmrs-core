4fb725aa6ab09ac762035ca37c9b1950b44be44c
kdaud <dkakumirizii@gmail.com>
2022-05-11 15:15:27 +0300

    Remove duplicate patient identifier in the xml file

--- api/src/test/java/org/openmrs/api/EncounterServiceTest.java
+++ api/src/test/java/org/openmrs/api/EncounterServiceTest.java
@@ -899,7 +899,7 @@ public class EncounterServiceTest extends BaseContextSensitiveTest {
 	public void getEncountersByPatientIdentifier_shouldNotGetVoidedEncounters() {
 		EncounterService encounterService = Context.getEncounterService();
 		
-		List<Encounter> encounters = encounterService.getEncountersByPatientIdentifier("4321");
+		List<Encounter> encounters = encounterService.getEncountersByPatientIdentifier("12345");
 		assertEquals(2, encounters.size());
 	}
 	
