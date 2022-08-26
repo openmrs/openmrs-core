55671d35b29c2ea96827c6010d63b521f1bf9c7f
Wikum Chamith <wikum@linuxdeveloper.space>
2022-08-26 13:42:56 +0530

    TRUNK-6066 Adding a logic to assign a value to OpenmrsConstants.GP_PASSWORD_RESET_URL
    For backwards compatibility, Adding a logic to assign the value of OpenmrsConstants.GP_HOST_URL to OpenmrsConstants.GP_PASSWORD_RESET_URL when there is value to OpenmrsConstants.GP_HOST_URL and OpenmrsConstants.GP_PASSWORD_RESET_URL is empty.

--- api/src/main/java/org/openmrs/api/impl/UserServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/UserServiceImpl.java
@@ -731,6 +731,9 @@ public class UserServiceImpl extends BaseOpenmrsService implements UserService {
 		AdministrationService adminService = Context.getAdministrationService();
 		Locale locale = getDefaultLocaleForUser(user);
 		
+//		Delete this method call when removing {@link OpenmrsConstants#GP_HOST_URL}
+		copyHostURLGlobalPropertyToPasswordResetGlobalProperty(adminService);
+
 		String link = adminService.getGlobalProperty(OpenmrsConstants.GP_PASSWORD_RESET_URL)
 		        .replace("{activationKey}", token);
 		
@@ -746,6 +749,17 @@ public class UserServiceImpl extends BaseOpenmrsService implements UserService {
 		Context.getMessageService().sendMessage(user.getEmail(), sender, subject, msg);
 		
 		return user;
+	}
+
+	/**
+	 * Delete this method when deleting {@link OpenmrsConstants#GP_HOST_URL}
+	 */
+	private void copyHostURLGlobalPropertyToPasswordResetGlobalProperty(AdministrationService adminService) {
+		String hostURLGP = adminService.getGlobalProperty(OpenmrsConstants.GP_HOST_URL);
+		String passwordResetGP = adminService.getGlobalProperty(OpenmrsConstants.GP_PASSWORD_RESET_URL);
+		if(StringUtils.isNotBlank(hostURLGP) && StringUtils.isBlank(passwordResetGP)) {
+			adminService.setGlobalProperty(OpenmrsConstants.GP_PASSWORD_RESET_URL,  hostURLGP);
+		}
 	}
 
 	/**
