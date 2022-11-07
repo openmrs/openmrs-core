fbc74902ffdc3ff969cce2141e8ae74ef3d34afd
sekiranda joel <sekirandaj@gmail.com>
2022-11-07 16:20:59 +0300

    TRUNK-5810:replacing Relationship class hibernate mappings with annotations

--- api/src/main/java/org/openmrs/Relationship.java
+++ api/src/main/java/org/openmrs/Relationship.java
@@ -11,25 +11,48 @@ package org.openmrs;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+
 /**
  * Relationship
  */
+   @Entity
+   @Table(name = "relationship")
 public class Relationship extends BaseChangeableOpenmrsData {
 	
 	public static final long serialVersionUID = 323423L;
 	
 	// Fields
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="relationship_id",nullable=false)
 	private Integer relationshipId;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "person_a", nullable=false)
 	private Person personA;
-	
+
+	@ManyToOne
+	@JoinColumn(name="relationship", nullable=false)
 	private RelationshipType relationshipType;
-	
+
+	@ManyToOne
+	@JoinColumn(name="person_b",nullable = false)
 	private Person personB;
-	
+
+	@Column(name= "start_date",length=19)
 	private Date startDate;
-	
+
+	@Column(name="end_date",length= 19)
 	private Date endDate;
 	
 	// Constructors
