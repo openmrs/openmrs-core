4e4eb6f2586880e766caa73c855b163a32494824
sekiranda joel <sekirandaj@gmail.com>
2022-11-10 07:22:01 +0300

    TRUNK-5810:replacing Relationship class hibernate mappings with annotations

--- api/src/main/java/org/openmrs/Relationship.java
+++ api/src/main/java/org/openmrs/Relationship.java
@@ -20,203 +20,198 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-
-
 /**
  * Relationship
  */
-   @Entity
-   @Table(name = "relationship")
+@Entity
+@Table(name = "relationship")
 public class Relationship extends BaseChangeableOpenmrsData {
-	
-	public static final long serialVersionUID = 323423L;
-	
-	// Fields
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="relationship_id",nullable=false)
-	private Integer relationshipId;
+  public static final long serialVersionUID = 323423L;
 
-	@ManyToOne
-	@JoinColumn(name = "person_a", nullable=false)
-	private Person personA;
+  // Fields
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "relationship_id", nullable = false)
+  private Integer relationshipId;
 
-	@ManyToOne
-	@JoinColumn(name="relationship", nullable=false)
-	private RelationshipType relationshipType;
+  @ManyToOne
+  @JoinColumn(name = "person_a", nullable = false)
+  private Person personA;
 
-	@ManyToOne
-	@JoinColumn(name="person_b",nullable = false)
-	private Person personB;
+  @ManyToOne
+  @JoinColumn(name = "relationship", nullable = false)
+  private RelationshipType relationshipType;
 
-	@Column(name= "start_date",length=19)
-	private Date startDate;
+  @ManyToOne
+  @JoinColumn(name = "person_b", nullable = false)
+  private Person personB;
 
-	@Column(name="end_date",length= 19)
-	private Date endDate;
-	
-	// Constructors
-	
-	/** default constructor */
-	public Relationship() {
-	}
-	
-	/** constructor with id */
-	public Relationship(Integer relationshipId) {
-		this.relationshipId = relationshipId;
-	}
-	
-	public Relationship(Person personA, Person personB, RelationshipType type) {
-		this.personA = personA;
-		this.personB = personB;
-		this.relationshipType = type;
-	}
-	
-	/**
-	 * Does a shallow copy of this Relationship. Does NOT copy relationshipId
-	 * 
-	 * @return a copy of this relationship
-	 */
-	public Relationship copy() {
-		return copyHelper(new Relationship());
-	}
-	
-	/**
-	 * The purpose of this method is to allow subclasses of Relationship to delegate a portion of
-	 * their copy() method back to the superclass, in case the base class implementation changes.
-	 * 
-	 * @param target a Relationship that will have the state of <code>this</code> copied into it
-	 * @return the Relationship that was passed in, with state copied into it
-	 */
-	protected Relationship copyHelper(Relationship target) {
-		target.personA = getPersonA();
-		target.relationshipType = getRelationshipType();
-		target.personB = getPersonB();
-		target.setCreator(getCreator());
-		target.setDateCreated(getDateCreated());
-		target.setVoided(getVoided());
-		target.setVoidedBy(getVoidedBy());
-		target.setDateVoided(getDateVoided());
-		target.setVoidReason(getVoidReason());
-		return target;
-	}
-	
-	// Property accessors
-	
-	/**
-	 * @return Returns the personA.
-	 */
-	public Person getPersonA() {
-		return personA;
-	}
-	
-	/**
-	 * @param personA The person to set.
-	 */
-	public void setPersonA(Person personA) {
-		this.personA = personA;
-	}
-	
-	/**
-	 * @return Returns the relationship type.
-	 */
-	public RelationshipType getRelationshipType() {
-		return relationshipType;
-	}
-	
-	/**
-	 * @param type The relationship type to set.
-	 */
-	public void setRelationshipType(RelationshipType type) {
-		this.relationshipType = type;
-	}
-	
-	/**
-	 * @return Returns the relationshipId.
-	 */
-	public Integer getRelationshipId() {
-		return relationshipId;
-	}
-	
-	/**
-	 * @param relationshipId The relationshipId to set.
-	 */
-	public void setRelationshipId(Integer relationshipId) {
-		this.relationshipId = relationshipId;
-	}
-	
-	/**
-	 * @return Returns the personB.
-	 */
-	public Person getPersonB() {
-		return personB;
-	}
-	
-	/**
-	 * @param personB The relative to set.
-	 */
-	public void setPersonB(Person personB) {
-		this.personB = personB;
-	}
-	
-	/**
-	 * If not null, this indicates that the relationship started on a particular date
-	 * @since 1.9
-	 * @return the relationship's start date.
-	 */
-	public Date getStartDate() {
-		return this.startDate;
-	}
-	
-	/**
-	 * @since 1.9
-	 * @param startDate relationship's start date.
-	 */
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-	
-	/**
-	 * If not null, this indicates that the relationship ended on a particular date
-	 * @since 1.9
-	 * @return Returns relationship's end date.
-	 */
-	public Date getEndDate() {
-		return this.endDate;
-	}
-	
-	/**
-	 * @since 1.9
-	 * @param endDate relationship's end date.
-	 */
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-	
-	@Override
-	public String toString() {
-		String relType = getRelationshipType() == null ? "NULL" : getRelationshipType().getaIsToB();
-		return personA + " is the " + relType + " of " + personB;
-	}
-	
-	/**
-	 * @since 1.5
-	 * @see org.openmrs.OpenmrsObject#getId()
-	 */
-	@Override
-	public Integer getId() {
-		
-		return getRelationshipId();
-	}
-	
-	/**
-	 * @since 1.5
-	 * @see org.openmrs.OpenmrsObject#setId(java.lang.Integer)
-	 */
-	@Override
-	public void setId(Integer id) {
-		setRelationshipId(id);
-		
-	}
-	
+  @Column(name = "start_date", length = 19)
+  private Date startDate;
+
+  @Column(name = "end_date", length = 19)
+  private Date endDate;
+
+  // Constructors
+
+  /** default constructor */
+  public Relationship() {}
+
+  /** constructor with id */
+  public Relationship(Integer relationshipId) {
+    this.relationshipId = relationshipId;
+  }
+
+  public Relationship(Person personA, Person personB, RelationshipType type) {
+    this.personA = personA;
+    this.personB = personB;
+    this.relationshipType = type;
+  }
+
+  /**
+   * Does a shallow copy of this Relationship. Does NOT copy relationshipId
+   *
+   * @return a copy of this relationship
+   */
+  public Relationship copy() {
+    return copyHelper(new Relationship());
+  }
+
+  /**
+   * The purpose of this method is to allow subclasses of Relationship to delegate a portion of
+   * their copy() method back to the superclass, in case the base class implementation changes.
+   *
+   * @param target a Relationship that will have the state of <code>this</code> copied into it
+   * @return the Relationship that was passed in, with state copied into it
+   */
+  protected Relationship copyHelper(Relationship target) {
+    target.personA = getPersonA();
+    target.relationshipType = getRelationshipType();
+    target.personB = getPersonB();
+    target.setCreator(getCreator());
+    target.setDateCreated(getDateCreated());
+    target.setVoided(getVoided());
+    target.setVoidedBy(getVoidedBy());
+    target.setDateVoided(getDateVoided());
+    target.setVoidReason(getVoidReason());
+    return target;
+  }
+
+  // Property accessors
+
+  /**
+   * @return Returns the personA.
+   */
+  public Person getPersonA() {
+    return personA;
+  }
+
+  /**
+   * @param personA The person to set.
+   */
+  public void setPersonA(Person personA) {
+    this.personA = personA;
+  }
+
+  /**
+   * @return Returns the relationship type.
+   */
+  public RelationshipType getRelationshipType() {
+    return relationshipType;
+  }
+
+  /**
+   * @param type The relationship type to set.
+   */
+  public void setRelationshipType(RelationshipType type) {
+    this.relationshipType = type;
+  }
+
+  /**
+   * @return Returns the relationshipId.
+   */
+  public Integer getRelationshipId() {
+    return relationshipId;
+  }
+
+  /**
+   * @param relationshipId The relationshipId to set.
+   */
+  public void setRelationshipId(Integer relationshipId) {
+    this.relationshipId = relationshipId;
+  }
+
+  /**
+   * @return Returns the personB.
+   */
+  public Person getPersonB() {
+    return personB;
+  }
+
+  /**
+   * @param personB The relative to set.
+   */
+  public void setPersonB(Person personB) {
+    this.personB = personB;
+  }
+
+  /**
+   * If not null, this indicates that the relationship started on a particular date
+   * @since 1.9
+   * @return the relationship's start date.
+   */
+  public Date getStartDate() {
+    return this.startDate;
+  }
+
+  /**
+   * @since 1.9
+   * @param startDate relationship's start date.
+   */
+  public void setStartDate(Date startDate) {
+    this.startDate = startDate;
+  }
+
+  /**
+   * If not null, this indicates that the relationship ended on a particular date
+   * @since 1.9
+   * @return Returns relationship's end date.
+   */
+  public Date getEndDate() {
+    return this.endDate;
+  }
+
+  /**
+   * @since 1.9
+   * @param endDate relationship's end date.
+   */
+  public void setEndDate(Date endDate) {
+    this.endDate = endDate;
+  }
+
+  @Override
+  public String toString() {
+    String relType = getRelationshipType() == null
+      ? "NULL"
+      : getRelationshipType().getaIsToB();
+    return personA + " is the " + relType + " of " + personB;
+  }
+
+  /**
+   * @since 1.5
+   * @see org.openmrs.OpenmrsObject#getId()
+   */
+  @Override
+  public Integer getId() {
+    return getRelationshipId();
+  }
+
+  /**
+   * @since 1.5
+   * @see org.openmrs.OpenmrsObject#setId(java.lang.Integer)
+   */
+  @Override
+  public void setId(Integer id) {
+    setRelationshipId(id);
+  }
 }
