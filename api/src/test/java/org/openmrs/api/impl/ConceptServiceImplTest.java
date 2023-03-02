6d88f818c7f7f98ed5ce1f638319e56b932a7cd5
Michael Seaton <mseaton@pih.org>
2023-03-02 03:41:46 -0500

    TRUNK-6164 - Fix to enable adding new drug reference maps during drug creation

--- api/src/test/java/org/openmrs/api/impl/ConceptServiceImplTest.java
+++ api/src/test/java/org/openmrs/api/impl/ConceptServiceImplTest.java
@@ -39,6 +39,7 @@ import org.openmrs.ConceptSearchResult;
 import org.openmrs.ConceptSet;
 import org.openmrs.ConceptSource;
 import org.openmrs.Drug;
+import org.openmrs.DrugReferenceMap;
 import org.openmrs.api.APIException;
 import org.openmrs.api.ConceptNameType;
 import org.openmrs.api.ConceptService;
@@ -286,6 +287,89 @@ public class ConceptServiceImplTest extends BaseContextSensitiveTest {
 		Context.getConceptService().saveConcept(concept);
 		//Then
 		assertTrue(concept.getSet());
+	}
+	
+	/**
+	 * @see ConceptServiceImpl#saveDrug(Drug) 
+	 */
+	@Test
+	public void saveDrug_shouldPutGeneratedIdOntoReturnedDrug() {
+		Drug drug = new Drug();
+		Concept concept = new Concept();
+		concept.addName(new ConceptName("Concept", new Locale("en", "US")));
+		concept.addDescription(new ConceptDescription("Description", new Locale("en", "US")));
+		concept.setConceptClass(new ConceptClass(1));
+		concept.setDatatype(new ConceptDatatype(1));
+		Concept savedConcept = conceptService.saveConcept(concept);
+		drug.setConcept(savedConcept);
+		assertNull(drug.getDrugId());
+		Drug savedDrug = conceptService.saveDrug(drug);
+		assertNotNull(savedDrug.getDrugId());
+	}
+
+	/**
+	 * @see ConceptServiceImpl#saveDrug(Drug)
+	 */
+	@Test
+	public void saveDrug_shouldCreateNewDrugInDatabase() {
+		Drug drug = new Drug();
+		Concept concept = new Concept();
+		concept.addName(new ConceptName("Concept", new Locale("en", "US")));
+		concept.addDescription(new ConceptDescription("Description", new Locale("en", "US")));
+		concept.setConceptClass(new ConceptClass(1));
+		concept.setDatatype(new ConceptDatatype(1));
+		Concept savedConcept = conceptService.saveConcept(concept);
+		drug.setConcept(savedConcept);
+		drug.setName("Drug");
+		assertNull(conceptService.getDrug("Drug"));
+		Drug savedDrug = conceptService.saveDrug(drug);
+		assertNotNull(conceptService.getDrug(savedDrug.getDrugId()));
+	}
+
+	/**
+	 * @see ConceptServiceImpl#saveDrug(Drug)
+	 */
+	@Test
+	public void saveDrug_shouldUpdateDrugAlreadyExistingInDatabase() {
+		Drug drug = new Drug();
+		Concept concept = new Concept();
+		concept.addName(new ConceptName("Concept", new Locale("en", "US")));
+		concept.addDescription(new ConceptDescription("Description", new Locale("en", "US")));
+		concept.setConceptClass(new ConceptClass(1));
+		concept.setDatatype(new ConceptDatatype(1));
+		Concept savedConcept = conceptService.saveConcept(concept);
+		drug.setConcept(savedConcept);
+		drug.setCombination(false);
+		Drug savedDrug = conceptService.saveDrug(drug);
+		assertFalse(savedDrug.getCombination());
+		savedDrug.setCombination(true);
+		conceptService.saveDrug(savedDrug);
+		assertTrue(conceptService.getDrug(savedDrug.getDrugId()).getCombination());
+	}
+
+	/**
+	 * @see ConceptServiceImpl#saveDrug(Drug)
+	 */
+	@Test
+	public void saveDrug_shouldSaveNewDrugReferenceMap() {
+		Drug drug = new Drug();
+		Concept concept = new Concept();
+		concept.addName(new ConceptName("Concept", new Locale("en", "US")));
+		concept.addDescription(new ConceptDescription("Description", new Locale("en", "US")));
+		concept.setConceptClass(new ConceptClass(1));
+		concept.setDatatype(new ConceptDatatype(1));
+		Concept savedConcept = conceptService.saveConcept(concept);
+		drug.setConcept(savedConcept);
+		drug.setName("Example Drug");
+		ConceptMapType sameAs = conceptService.getConceptMapTypeByUuid(ConceptMapType.SAME_AS_MAP_TYPE_UUID);
+		ConceptSource snomedCt = conceptService.getConceptSourceByName("SNOMED CT");
+		DrugReferenceMap map = new DrugReferenceMap();
+		map.setDrug(drug);
+		map.setConceptMapType(sameAs);
+		map.setConceptReferenceTerm(new ConceptReferenceTerm(snomedCt, "example", ""));
+		drug.addDrugReferenceMap(map);
+		drug = conceptService.saveDrug(drug);
+		assertEquals(1, conceptService.getDrug(drug.getDrugId()).getDrugReferenceMaps().size());
 	}
 	
 	/**
