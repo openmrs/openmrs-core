650f36eea766605c819aa65eac4185ca372308fd
Michael Seaton <mseaton@pih.org>
2023-03-21 10:38:50 -0400

    TRUNK-6167 Condition should not lose the associated encounter or formNamespaceAndPath on edit. (#4280)

--- api/src/main/java/org/openmrs/Condition.java
+++ api/src/main/java/org/openmrs/Condition.java
@@ -121,12 +121,15 @@ public class Condition extends BaseFormRecordableOpenmrsData {
 	public static Condition copy(Condition fromCondition, Condition toCondition) {
 		toCondition.setPreviousVersion(fromCondition.getPreviousVersion());
 		toCondition.setPatient(fromCondition.getPatient());
+		toCondition.setEncounter(fromCondition.getEncounter());
+		toCondition.setFormNamespaceAndPath(fromCondition.getFormNamespaceAndPath());
 		toCondition.setClinicalStatus(fromCondition.getClinicalStatus());
 		toCondition.setVerificationStatus(fromCondition.getVerificationStatus());
 		toCondition.setCondition(fromCondition.getCondition());
 		toCondition.setOnsetDate(fromCondition.getOnsetDate());
 		toCondition.setAdditionalDetail(fromCondition.getAdditionalDetail());
 		toCondition.setEndDate(fromCondition.getEndDate());
+		toCondition.setEndReason(fromCondition.getEndReason());
 		toCondition.setVoided(fromCondition.getVoided());
 		toCondition.setVoidedBy(fromCondition.getVoidedBy());
 		toCondition.setVoidReason(fromCondition.getVoidReason());
