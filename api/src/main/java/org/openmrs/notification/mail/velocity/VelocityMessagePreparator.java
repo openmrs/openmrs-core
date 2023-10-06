6aab8796ce925ad02f9f007ee654b7c9069bb119
Michael Seaton <mseaton@pih.org>
2023-10-06 14:39:13 -0400

    Change from SimpleLog4JLogSystem to Log4JLogChute

--- api/src/main/java/org/openmrs/notification/mail/velocity/VelocityMessagePreparator.java
+++ api/src/main/java/org/openmrs/notification/mail/velocity/VelocityMessagePreparator.java
@@ -14,6 +14,7 @@ import java.util.Properties;
 
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.log.Log4JLogChute;
 import org.openmrs.notification.Message;
 import org.openmrs.notification.MessageException;
 import org.openmrs.notification.MessagePreparator;
@@ -42,7 +43,7 @@ public class VelocityMessagePreparator implements MessagePreparator {
 		try {
 			engine = new VelocityEngine();
 			Properties props = new Properties();
-			props.put("runtime.log.logsystem.class", "org.apache.velocity.runtime.log.SimpleLog4JLogSystem");
+			props.put("runtime.log.logsystem.class", Log4JLogChute.class.getName());
 			props.put("runtime.log.logsystem.log4j.category", "velocity");
 			props.put("runtime.log.logsystem.log4j.logger", "velocity");
 			engine.init(props);
