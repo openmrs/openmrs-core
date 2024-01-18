f931b9b86837d3d640d543a2be965b0fdcac907b
Seremba Patrick <pserembae@gmail.com>
2024-01-18 15:51:09 +0300

    TRUNK-6214: Update OpenMRS version in fix_liquibase_snapshots.sh script to current release

--- liquibase/scripts/fix_liquibase_snapshots.sh
+++ liquibase/scripts/fix_liquibase_snapshots.sh
@@ -17,6 +17,6 @@
 #  The updated snapshots are written to the openmrs-core/liquibase/snapshots folder.
 #
 
-openmrs_version=2.4.0-SNAPSHOT
+openmrs_version=2.6.0-SNAPSHOT
 
 java -jar ./target/openmrs-liquibase-${openmrs_version}-jar-with-dependencies.jar
