f57b1d6760d1218607433c4915693ba694121cc3
Wikum Weerakutti <wikum@linuxdeveloper.space>
2024-05-31 18:40:58 +0530

    TRUNK-6239: AlertRecipient - Switching from Hibernate Mapping to JPA annotations (#4646)

--- api/src/main/java/org/openmrs/notification/AlertRecipient.java
+++ api/src/main/java/org/openmrs/notification/AlertRecipient.java
@@ -14,26 +14,45 @@ import java.util.Date;
 import org.openmrs.BaseOpenmrsObject;
 import org.openmrs.User;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
 /**
  * This class is essentially a wrapper for the user object. The alert is assigned to each recipient.
  * A recipient then has either "read" the alert or has not.
  * 
  * @see org.openmrs.notification.Alert
  */
+@Entity
+@Table(name = "notification_alert_recipient")
 public class AlertRecipient extends BaseOpenmrsObject {
 	
 	private static final long serialVersionUID = -507111109155L;
 	
+	@JoinColumn(name = "alert_id")
+	@Id
+	@ManyToOne
 	private Alert alert;
-	
+
+	@JoinColumn(name = "user_id", updatable = false, insertable = false)
+	@Id
+	@ManyToOne
 	private User recipient;
 	
+	@Column(name = "alert_read", length = 1)
 	private Boolean alertRead = false;
 	
+	@Column(name = "date_changed", length = 19, updatable = false, insertable = false)
 	private Date dateChanged;
 	
 	// necessary for hql queries
-	private transient Integer recipientId;
+	@Column(name = "user_id", nullable = false, updatable = false, insertable = false)
+	private Integer recipientId;
 	
 	/** Default empty constructor */
 	public AlertRecipient() {
