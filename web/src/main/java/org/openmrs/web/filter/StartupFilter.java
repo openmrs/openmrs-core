9b4b688045ad635dc42f009e46ba6f712c166a9c
Ian <52504170+ibacher@users.noreply.github.com>
2024-08-15 14:48:04 -0400

    TRUNK-6261: Set the appropriate MIME-type for files served by initialization filter (#4717)

--- web/src/main/java/org/openmrs/web/filter/StartupFilter.java
+++ web/src/main/java/org/openmrs/web/filter/StartupFilter.java
@@ -16,6 +16,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
@@ -63,6 +64,7 @@ import org.openmrs.web.filter.util.FilterUtil;
 import org.openmrs.web.filter.util.LocalizationTool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 
 /**
  * Abstract class used when a small wizard is needed before Spring, jsp, etc has been started up.
@@ -125,9 +127,10 @@ public abstract class StartupFilter implements Filter {
 			if (servletPath.startsWith("/images") || servletPath.startsWith("/initfilter/scripts")) {
 				// strip out the /initfilter part
 				servletPath = servletPath.replaceFirst("/initfilter", "/WEB-INF/view");
-				// writes the actual image file path to the response
+				// writes the actual file path to the response
 				Path filePath = Paths.get(filterConfig.getServletContext().getRealPath(servletPath)).normalize();
 				Path fullFilePath = filePath;
+				
 				if (httpRequest.getPathInfo() != null) {
 					fullFilePath = fullFilePath.resolve(httpRequest.getPathInfo());
 					if (!(fullFilePath.normalize().startsWith(filePath))) {
@@ -136,8 +139,24 @@ public abstract class StartupFilter implements Filter {
 					}
 				}
 				
-				try (InputStream imageFileInputStream = new FileInputStream(fullFilePath.normalize().toFile())) {
-					OpenmrsUtil.copyFile(imageFileInputStream, httpResponse.getOutputStream());
+				String contentType = httpRequest.getServletContext().getMimeType(fullFilePath.toString());
+				if (contentType == null || contentType.isEmpty()) {
+					try {
+						contentType = Files.probeContentType(fullFilePath);
+					} catch (IOException ignored) {}
+				}
+
+				MediaType mediaType;
+				if (contentType != null && !contentType.isEmpty()) {
+					mediaType = MediaType.parseMediaType(contentType);
+				} else {
+					mediaType = MediaType.APPLICATION_OCTET_STREAM;
+				}
+				
+				response.setContentType(mediaType.toString());
+				
+				try (InputStream fis = new FileInputStream(fullFilePath.normalize().toFile())) {
+					OpenmrsUtil.copyFile(fis, httpResponse.getOutputStream());
 				}
 				catch (FileNotFoundException e) {
 					log.error("Unable to find file: {}", filePath, e);
