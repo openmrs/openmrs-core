4c901c1a14782a5014d075cfa636ef77d2b703dc
ManojLL <manojlakshan421@gmail.com>
2024-11-09 07:52:59 +0530

    TRUNK-5797: Added annotated class to saveOrder_shouldFailIfTheJavaTypeOfThePreviousOrderDoesNotMatch test

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -2740,7 +2740,7 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 			.addAnnotatedClass(ProgramAttributeType.class)
 			.addAnnotatedClass(HL7InError.class)
 			.addAnnotatedClass(OrderType.class)
-			.addAnnotatedClass(Field.class)
+			.addAnnotatedClass(org.openmrs.Field.class)
 			.getMetadataBuilder().build();
 
 
