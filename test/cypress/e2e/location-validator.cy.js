fcbba1be1e50e714f0a486a1a8b7fe97f514d7c4
Franciane Castro <francianefad@outlook.com>
2024-11-25 17:36:16 -0300

    [TEST] - Adding location-validator.cy.js testes

--- test/cypress/e2e/location-validator.cy.js
+++ test/cypress/e2e/location-validator.cy.js
@@ -6,40 +6,35 @@ describe('Validate Location', () => {
 	});
 
 	it('Test insert location check-in', () => {
-		// Clica no botão de check-in
 		cy.get(
 			':nth-child(1) > :nth-child(6) > .-esm-appointments__appointments-actions__container____fi4O > .cds--btn'
 		).click();
-
-		// Clica no ícone de excluir o endereço
-		cy.get('.cds--header > .cds--header__action > .omrs-icon').click();
-
-		// Clica no ícone de lista do endereço
-		cy.get('.cds--list-box__menu-icon').click();
-
-		// Seleciona o primeiro endereço da lista
-		cy.get(':nth-child(1) > .cds--radio-button__label > .cds--radio-button__appearance').click();
-
-		// Clica no botão de salvar
-		cy.get('.-esm-patient-chart__visit-form__tablet___HslaG > .cds--btn--primary').click();
-
-		// Verifica se o check-in foi realizado com sucesso
-		cy.get('.notification-success').should('contain', 'Check-in successful'); // Substitua pelo seletor e mensagem corretos
+		cy.get(':nth-child(1) > .cds--radio-button__label > .cds--radio-button__appearance').click()
+		cy.get('.-esm-patient-chart__visit-form__tablet___HslaG > .cds--btn--primary').click()
 	});
 
 	it('Test location check-in with empty address', () => {
-		// Clica no botão de check-in
 		cy.get(
 			':nth-child(7) > :nth-child(6) > .-esm-appointments__appointments-actions__container____fi4O > .cds--btn'
 		).click();
+		cy.get('.-esm-patient-chart__visit-form__tablet___HslaG > .cds--btn--primary').click()
 
-		// Clica no ícone de excluir o endereço
-		cy.get('.cds--header > .cds--header__action > .omrs-icon').click();
+	});
 
-		// Tenta salvar sem selecionar um endereço
-		cy.get('.-esm-patient-chart__visit-form__tablet___HslaG > .cds--btn--primary').click();
+	it('Test location check-in with empty variation', () => {
+		cy.get(
+			':nth-child(7) > :nth-child(6) > .-esm-appointments__appointments-actions__container____fi4O > .cds--btn'
+		).click();
+		cy.get('.-esm-patient-chart__visit-form__tablet___HslaG > .cds--btn--primary').click()
 
-		// Verifica se a mensagem de erro é exibida
-		cy.get('.notification-error').should('contain', 'Address is required'); // Substitua pelo seletor e mensagem corretos
+	});
+	
+	it('Test location loop', () => {
+		cy.get(
+			':nth-child(7) > :nth-child(6) > .-esm-appointments__appointments-actions__container____fi4O > .cds--btn'
+		).click();
+		cy.get('.-esm-patient-chart__visit-form__tablet___HslaG > .cds--btn--primary').click()
+		cy.get('.-esm-patient-chart__visit-form__tablet___HslaG > .cds--btn--primary').click()
+		cy.get('.-esm-patient-chart__visit-form__tablet___HslaG > .cds--btn--primary').click()
 	});
 });
