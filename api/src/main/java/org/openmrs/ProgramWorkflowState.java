ffe80b8fc6c244de7c67449673ee744f9b23845c
Rishabh <rishabhrawat.171105@gmail.com>
2024-12-07 17:20:34 +0530

    TRUNK-5891 : ProgramWorkflowState Domain - Switching from Hibernate Mappings to Annotations

--- api/src/main/java/org/openmrs/ProgramWorkflowState.java
+++ api/src/main/java/org/openmrs/ProgramWorkflowState.java
@@ -10,11 +10,23 @@
 package org.openmrs;
 
 import org.hibernate.envers.Audited;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 /**
  * ProgramWorkflowState
  */
 @Audited
+@Entity
+@Table(name = "program_workflow_state")
 public class ProgramWorkflowState extends BaseChangeableOpenmrsMetadata {
 	
 	private static final long serialVersionUID = 1L;
@@ -23,14 +35,28 @@ public class ProgramWorkflowState extends BaseChangeableOpenmrsMetadata {
 	// Properties
 	// ******************
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "program_workflow_state_id_seq")
+	@GenericGenerator(
+			name = "program_workflow_state_id_seq",
+			strategy = "native",
+			parameters = @Parameter(name = "sequence", value = "program_workflow_state_program_workflow_state_id_seq")
+			)
+	@Column(name = "program_workflow_state_id")
 	private Integer programWorkflowStateId;
 	
+	@ManyToOne
+	@JoinColumn(name = "program_workflow_id", nullable = false)
 	private ProgramWorkflow programWorkflow;
 	
+	@ManyToOne
+	@JoinColumn(name = "concept_id", nullable = false)
 	private Concept concept;
 	
+	@Column(name = "initial", length = 1, nullable = false)
 	private Boolean initial;
 	
+	@Column(name = "terminal", length = 1, nullable = false)
 	private Boolean terminal;
 	
 	// ******************
