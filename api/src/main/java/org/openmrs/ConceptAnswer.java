52816f92d804c6035014920cb1950ab4006823a7
Kavindu Ranasinghe <kavindu.ranasinghe07@gmail.com>
2025-01-21 03:08:53 +0530

    TRUNK-5803 Migrate ConceptAnswer from Hibernate Mapping XML to JPA annotations

--- api/src/main/java/org/openmrs/ConceptAnswer.java
+++ api/src/main/java/org/openmrs/ConceptAnswer.java
@@ -11,6 +11,15 @@ package org.openmrs;
 
 import org.hibernate.envers.Audited;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 /**
@@ -20,34 +29,64 @@ import java.util.Date;
  *
  * @see Concept#getAnswers()
  */
+@Entity
+@Table(name = "concept_answer")
 @Audited
 public class ConceptAnswer extends BaseOpenmrsObject implements Auditable, java.io.Serializable, Comparable<ConceptAnswer> {
 	
+	
 	public static final long serialVersionUID = 3744L;
 	
+	
 	// Fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "concept_answer_id",nullable = false,updatable = false)
 	private Integer conceptAnswerId;
 	
 	/**
 	 * The question concept that this object is answering
 	 */
+	@NotNull
+	@ManyToOne(fetch = FetchType.LAZY,optional = false)
+	@Column(name = "concept_id", nullable = false)
 	private Concept concept;
 	
 	/**
 	 * The answer to the question
 	 */
+	@NotNull
+	@ManyToOne(fetch = FetchType.LAZY,optional = false)
+	@Column(name = "answer_concept", nullable = false)
 	private Concept answerConcept;
 	
 	/**
 	 * The {@link Drug} answer to the question. This can be null if this does not represent a drug
 	 * type of answer
 	 */
+	@ManyToOne(fetch = FetchType.LAZY)
+	@Column(name = "answer_drug")
 	private Drug answerDrug;
 	
+	/**
+	 * The creator of this concept answer
+	 */
+	@NotNull(message = "Creator must not be null")
+	@Column(name = "creator", nullable = false, updatable = false)
 	private User creator;
 	
+	/**
+	 * The date this concept answer was created
+	 */
+	@NotNull
+	@Column(name = "date_created", nullable = false, updatable = false)
 	private Date dateCreated;
 	
+	/**
+	 * The sort weight of this concept answer. This is used to sort the answers to a question in a
+	 * specific order
+	 */
+	@Column(name = "sort_weight")
 	private Double sortWeight;
 	
 	// Constructors
@@ -73,6 +112,7 @@ public class ConceptAnswer extends BaseOpenmrsObject implements Auditable, java.
 	/**
 	 * @return Returns the answerConcept.
 	 */
+	
 	public Concept getAnswerConcept() {
 		return answerConcept;
 	}
