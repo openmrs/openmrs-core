deffa935b579610fb32d9847d8ba5b8e059672b9
txandmj <txandmj@outlook.com>
2025-03-02 01:18:45 -0800

    handle the copying logic, making it easier to mock and test

---
+++ api/src/main/java/org/openmrs/ConditionCopier.java
@@ -0,0 +1,22 @@
+package org.openmrs;
+
+public class ConditionCopier {
+	public Condition copy(Condition fromCondition, Condition toCondition) {
+		toCondition.setPreviousVersion(fromCondition.getPreviousVersion());
+		toCondition.setPatient(fromCondition.getPatient());
+		toCondition.setEncounter(fromCondition.getEncounter());
+		toCondition.setFormNamespaceAndPath(fromCondition.getFormNamespaceAndPath());
+		toCondition.setClinicalStatus(fromCondition.getClinicalStatus());
+		toCondition.setVerificationStatus(fromCondition.getVerificationStatus());
+		toCondition.setCondition(fromCondition.getCondition());
+		toCondition.setOnsetDate(fromCondition.getOnsetDate());
+		toCondition.setAdditionalDetail(fromCondition.getAdditionalDetail());
+		toCondition.setEndDate(fromCondition.getEndDate());
+		toCondition.setEndReason(fromCondition.getEndReason());
+		toCondition.setVoided(fromCondition.getVoided());
+		toCondition.setVoidedBy(fromCondition.getVoidedBy());
+		toCondition.setVoidReason(fromCondition.getVoidReason());
+		toCondition.setDateVoided(fromCondition.getDateVoided());
+		return toCondition;
+	}
+}
