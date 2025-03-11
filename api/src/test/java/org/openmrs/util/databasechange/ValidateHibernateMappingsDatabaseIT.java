469185564f2688db203c1da0ba022ffd96993c48
rkorytkowski <rafal.korytkowski@gmail.com>
2025-03-11 10:35:46 +0100

    TRUNK-6301 Add ElasticSearch backend for full text search

--- api/src/test/java/org/openmrs/util/databasechange/ValidateHibernateMappingsDatabaseIT.java
+++ api/src/test/java/org/openmrs/util/databasechange/ValidateHibernateMappingsDatabaseIT.java
@@ -94,8 +94,8 @@ public class ValidateHibernateMappingsDatabaseIT extends DatabaseIT {
 		configuration.setProperty(Environment.USE_SECOND_LEVEL_CACHE, "false");
 		configuration.setProperty(Environment.USE_QUERY_CACHE, "false");
 		configuration.setProperty("hibernate.integration.envers.enabled", "false");
-		configuration.setProperty("hibernate.search.backend.analysis.configurer", 
-			"class:org.openmrs.api.db.hibernate.search.lucene.LuceneAnalysisConfigurer");
+		configuration.setProperty("hibernate.search.backend.type", "lucene");
+		configuration.setProperty("hibernate.search.backend.analysis.configurer", "class:org.openmrs.api.db.hibernate.search.lucene.LuceneConfig");
 		// Validate HBMs against the actual schema
 		configuration.setProperty(Environment.HBM2DDL_AUTO, "validate");
 		
