a1043bcfc2ef5497dca30e8189305836d1f0e5c6
ManojLL <manojlakshan421@gmail.com>
2025-03-12 13:04:40 +0530

    TRUNK-5803: Migrate ConceptAnswer hibernate mapping from XML to annotations

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.api.Test;
 import org.openmrs.Allergy;
 import org.openmrs.CareSetting;
 import org.openmrs.Concept;
+import org.openmrs.ConceptAnswer;
 import org.openmrs.ConceptClass;
 import org.openmrs.ConceptDatatype;
 import org.openmrs.ConceptDescription;
@@ -2742,6 +2743,7 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 			.addAnnotatedClass(ProgramAttributeType.class)
 			.addAnnotatedClass(HL7InError.class)
 			.addAnnotatedClass(OrderType.class)
+			.addAnnotatedClass(ConceptAnswer.class)
 			.getMetadataBuilder().build();
 
 
