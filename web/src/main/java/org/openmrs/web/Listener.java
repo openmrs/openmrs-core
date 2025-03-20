02babbcd3d63f717d3f1a118e790b5b957dfa11d
Wikum Weerakutti <wikum@linuxdeveloper.space>
2025-03-20 18:34:05 +0900

    TRUNK-6309: Fix logging issue at Listener.java  (#4969)
    * TRUNK-6309: Fix logging issue at Listener.java (#4967)
    
    (cherry picked from commit c478272185fe8c354ed6b6ebfd9a59c33e3ba418)
    
    * Remove unused imports in Listener.java (#4968)
    
    (cherry picked from commit 9692b9644b6e7c83226d932968ae5026ca9a3612)

--- web/src/main/java/org/openmrs/web/Listener.java
+++ web/src/main/java/org/openmrs/web/Listener.java
@@ -230,10 +230,9 @@ public final class Listener extends ContextLoader implements ServletContextListe
 				if (StringUtils.hasLength(appDataRuntimeProperty)) {
 					OpenmrsUtil.setApplicationDataDirectory(null);
 				}
-				
 				//ensure that we always log the runtime properties file that we are using
 				//since openmrs is just booting, the log levels are not yet set. TRUNK-4835
-				OpenmrsLoggingUtil.applyLogLevel(getClass().toString(), "INFO");
+				OpenmrsLoggingUtil.applyLogLevel(getClass().getName(), "INFO");
 				log.info("Using runtime properties file: {}",
 				         OpenmrsUtil.getRuntimePropertiesFilePathName(WebConstants.WEBAPP_NAME));
 			}
