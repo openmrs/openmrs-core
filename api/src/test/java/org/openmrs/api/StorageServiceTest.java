3e1e333dcdfdbd96c7f8dd2904eebf63dd07e283
nanna <navkishoredas@gmail.com>
2025-04-07 18:55:31 +0530

    Fix StorageServiceTest to improve test reliability

--- api/src/test/java/org/openmrs/api/StorageServiceTest.java
+++ api/src/test/java/org/openmrs/api/StorageServiceTest.java
@@ -580,14 +580,14 @@ class StorageServiceTest extends BaseContextSensitiveTest {
 
 		assertThat(localStorageService.exists("test"), is(false));
 	}
-	
+
 	@Test
 	void getDataShouldFailIfKeyTriesToAccessFilesOutsideStorageDir() throws IOException {
 		IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
 			localStorageService.getData("/test");
 		});
-		assertThat(e.getMessage(), is("Key must not point outside legacy storage dir. Wrong key: /test"));
-		
+		assertThat(e.getMessage(), is("Key must not point outside storage dir. Wrong key: /test"));
+
 		Path testFile = Paths.get(OpenmrsUtil.getApplicationDataDirectory(), "../test");
 		try {
 			testFile.toFile().createNewFile();
@@ -601,5 +601,4 @@ class StorageServiceTest extends BaseContextSensitiveTest {
 			}
 		}
 	}
-	
 }
