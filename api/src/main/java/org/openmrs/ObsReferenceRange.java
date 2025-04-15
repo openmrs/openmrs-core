bbfa2cbafa0af9310d25954a0c4e7eb7a71dfd64
dkayiwa <dkayiwa@openmrs.org>
2025-04-15 16:41:24 +0300

    TRUNK-6245 Load saved reference ranges bug fix

--- api/src/main/java/org/openmrs/ObsReferenceRange.java
+++ api/src/main/java/org/openmrs/ObsReferenceRange.java
@@ -18,6 +18,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -41,6 +42,7 @@ public class ObsReferenceRange extends BaseReferenceRange {
 	private Integer obsReferenceRangeId;
 	
 	@OneToOne
+	@MapsId
 	@JoinColumn(name = "obs_id", referencedColumnName = "obs_id", unique = true)
 	private Obs obs;
 
