6e0863d2ccc7614c2f4bf00c6356620d6493944f
wodpachua <molarag21996@gmail.com>
2025-04-30 01:54:37 +0300

    TRUNK-5893: VisitType Domain - Switching from Hibernate Mappings to Annotations

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -66,6 +66,7 @@ import org.openmrs.SimpleDosingInstructions;
 import org.openmrs.TestOrder;
 import org.openmrs.User;
 import org.openmrs.Visit;
+import org.openmrs.VisitType;
 import org.openmrs.VisitAttributeType;
 import org.openmrs.FormResource;
 import org.openmrs.api.builder.DrugOrderBuilder;
@@ -2747,6 +2748,7 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 			.addAnnotatedClass(ConceptAnswer.class)
 			.addAnnotatedClass(ConceptClass.class)
 			.addAnnotatedClass(FormResource.class)
+			.addAnnotatedClass(VisitType.class)
 			.getMetadataBuilder().build();
 
 
