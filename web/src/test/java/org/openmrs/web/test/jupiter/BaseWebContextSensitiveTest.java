5ef312dd1edc3962d3c11ec4e3c84fee9693752f
rkorytkowski <rafal.korytkowski@gmail.com>
2025-06-03 13:07:24 +0200

    TRUNK-6345 Fix testing framework to be used by modules for openmrs-core 2.4+
    (cherry picked from commit 976e9f4ba8dd54a6c1eb98176d169f5f4fb09762)

--- web/src/test/java/org/openmrs/web/test/jupiter/BaseWebContextSensitiveTest.java
+++ web/src/test/java/org/openmrs/web/test/jupiter/BaseWebContextSensitiveTest.java
@@ -7,29 +7,21 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
-package org.openmrs.web.test;
+package org.openmrs.web.test.jupiter;
 
-import org.openmrs.test.BaseContextSensitiveTest;
+import org.openmrs.test.jupiter.BaseContextSensitiveTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * Web tests for controllers, etc. should use this class instead of the general
- * {@link org.openmrs.test.BaseContextSensitiveTest} one. 
+ * {@link BaseContextSensitiveTest} one. 
  * <p>
  * The {@link ContextConfiguration} annotation adds the
  * openmrs-servlet.xml context file so that controller tests can pick up the
  * right type of controller, etc.
- *
- * 
- * @deprecated as of 2.4
- *   <p>openmrs-core migrated its tests from JUnit 4 to JUnit 5.
- *   JUnit 4 helpers are still supported so module developers can gradually migrate tests from JUnit 4 to JUnit 5. 
- *   To migrate your tests follow <a href="https://wiki.openmrs.org/display/docs/How+to+migrate+to+JUnit+5">How to migrate to JUnit 5</a>.
- *   The JUnit 5 version of the class is {@link org.openmrs.web.test.jupiter.BaseWebContextSensitiveTest}.<p>
  */
 @WebAppConfiguration	
 @ContextConfiguration(locations = { "classpath*:openmrs-servlet.xml", "classpath*:AltAuthSchemeTestingApplicationContext.xml" })
-@Deprecated
 public abstract class BaseWebContextSensitiveTest extends BaseContextSensitiveTest {
 }
