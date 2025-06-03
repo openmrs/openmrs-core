d97f968ba9575809080f5ba7afd7dcc3b6e08fb9
rkorytkowski <rafal.korytkowski@gmail.com>
2025-06-03 13:07:24 +0200

    TRUNK-6345 Fix testing framework to be used by modules for openmrs-core 2.4+
    (cherry picked from commit 202eb8b653cbb2d204d6f74098f784a4f3a86fd0)

--- api/src/test/java/org/openmrs/test/jupiter/BaseContextSensitiveTest.java
+++ api/src/test/java/org/openmrs/test/jupiter/BaseContextSensitiveTest.java
@@ -12,8 +12,19 @@ package org.openmrs.test.jupiter;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.springframework.test.context.TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Window;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -106,7 +117,7 @@ import org.xml.sax.InputSource;
  * 
  * @since 2.4.0
  */
-@ContextConfiguration(locations = { "classpath:applicationContext-service.xml", "classpath*:openmrs-servlet.xml",
+@ContextConfiguration(locations = { "classpath:applicationContext-service.xml",
         "classpath*:moduleApplicationContext.xml", "classpath*:TestingApplicationContext.xml" })
 @TestExecutionListeners(
 	listeners = { SkipBaseSetupAnnotationExecutionListener.class,
@@ -160,7 +171,7 @@ public abstract class BaseContextSensitiveTest {
 	/**
 	 * Allows to determine if the DB is initialized with standard data
 	 */
-	private static boolean isBaseSetup;
+	private static volatile boolean isBaseSetup;
 	
 	/**
 	 * Stores a user authenticated for running tests which allows to discover a situation when some
@@ -177,8 +188,6 @@ public abstract class BaseContextSensitiveTest {
 	 */
 	@InjectMocks
 	protected ContextMockHelper contextMockHelper;
-	
-	private static volatile BaseContextSensitiveTest instance;
 	
 	/**
 	 * Basic constructor for the super class to all openmrs api unit tests. This constructor sets up
@@ -198,13 +207,12 @@ public abstract class BaseContextSensitiveTest {
 		
 		Properties props = getRuntimeProperties();
 		
-		log.debug("props: {}", props);
+		if (log.isDebugEnabled())
+			log.debug("props: {}", props);
 		
 		Context.setRuntimeProperties(props);
 		
 		loadCount++;
-		
-		instance = this;
 	}
 	
 	/**
@@ -890,11 +898,11 @@ public abstract class BaseContextSensitiveTest {
 			connection.commit();
 			
 			updateSearchIndex();
-			
-			isBaseSetup = false;
 		}
 		catch (SQLException | DatabaseUnitException e) {
 			throw new DatabaseUnitRuntimeException(e);
+		} finally {
+			isBaseSetup = false;
 		}
 	}
 	
@@ -1010,18 +1018,6 @@ public abstract class BaseContextSensitiveTest {
 	 */
 	@AfterAll
 	public static synchronized void closeSessionAfterEachClass() throws Exception {
-		//Some tests add data via executeDataset()
-		//We need to delete it in order not to interfere with others
-		if (instance != null) {
-			try {
-				instance.deleteAllData();
-			}
-			catch (Exception ex) {
-				//No need to worry about this
-			}
-			instance = null;
-		}
-		
 		// clean up the session so we don't leak memory
 		if (Context.isSessionOpen()) {
 			Context.closeSession();
