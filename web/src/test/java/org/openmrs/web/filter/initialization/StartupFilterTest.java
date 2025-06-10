a7cb9f31c5c6bfacb9d6a92e3b77feeb183ca1c2
Bhargav Kodali <kodalibhargav019@gmail.com>
2025-06-10 10:53:28 -0700

    add license header

--- web/src/test/java/org/openmrs/web/filter/initialization/StartupFilterTest.java
+++ web/src/test/java/org/openmrs/web/filter/initialization/StartupFilterTest.java
@@ -1,3 +1,12 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.web.filter.initialization;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,7 +25,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import static org.hamcrest.CoreMatchers.is;
 
 
-public class OpenmrsLivenessTest {
+public class StartupFilterTest {
 	
 	private StartupFilter startupFilter;
 	
