b8fc1e014ed093f4fce00f14d9ce817c0d6f50de
Isabirye1515 <isabiryeelijah15@gmail.com>
2025-06-16 16:18:54 +0300

    resolving conflicts

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -37,6 +37,7 @@ import org.openmrs.DrugReferenceMap;
 import org.openmrs.DrugIngredient;
 import org.openmrs.DrugOrder;
 import org.openmrs.Encounter;
+import org.openmrs.Form;
 import org.openmrs.FreeTextDosingInstructions;
 import org.openmrs.GlobalProperty;
 import org.openmrs.Location;
@@ -2751,6 +2752,7 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 			.addAnnotatedClass(FormResource.class)
 			.addAnnotatedClass(VisitType.class)
 			.addAnnotatedClass(ProviderRole.class)
+			.addAnnotatedClass(Form.class)
 			.getMetadataBuilder().build();
 
 
