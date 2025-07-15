9acd4102df95836b6fe2585296449f3be4a2fe07
Wikum Weerakutti <wikum@linuxdeveloper.space>
2025-07-15 15:15:33 +0530

    TRUNK-6377:Implement getProviderRole in the core (#5131)

--- api/src/main/java/org/openmrs/api/impl/ProviderServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/ProviderServiceImpl.java
@@ -17,6 +17,7 @@ import org.openmrs.Person;
 import org.openmrs.Provider;
 import org.openmrs.ProviderAttribute;
 import org.openmrs.ProviderAttributeType;
+import org.openmrs.ProviderRole;
 import org.openmrs.api.APIException;
 import org.openmrs.api.ProviderService;
 import org.openmrs.api.context.Context;
@@ -301,5 +302,14 @@ public class ProviderServiceImpl extends BaseOpenmrsService implements ProviderS
 	public Provider getUnknownProvider() {
 		return getProviderByUuid(Context.getAdministrationService().getGlobalProperty(
 		    OpenmrsConstants.GP_UNKNOWN_PROVIDER_UUID));
+	}
+
+	/**
+	 * @see ProviderService#getProviderRole(Integer) 
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public ProviderRole getProviderRole(Integer providerRoleId) {
+		return dao.getProviderRole(providerRoleId);
 	}
 }
