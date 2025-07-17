4fac7d80ed8914aa7b79cb368f5a721e855af389
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2025-07-18 01:06:43 +0200

    TRUNK-6318: Back-port LocalStorageServcie and StreamDataService fixes
    (cherry picked from commit 658a3df03c39450ab3acc7a1bbce9eb75285740b)
    (cherry picked from commit f7d12ffc91688fff5c4f796f8d2e99e7fa7241fe)

--- api/src/main/java/org/openmrs/api/storage/BaseStorageService.java
+++ api/src/main/java/org/openmrs/api/storage/BaseStorageService.java
@@ -9,13 +9,20 @@
  */
 package org.openmrs.api.storage;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openmrs.api.StorageService;
 import org.openmrs.api.impl.BaseOpenmrsService;
 import org.openmrs.api.stream.StreamDataService;
@@ -31,6 +38,8 @@ public abstract class BaseStorageService extends BaseOpenmrsService implements S
 	private final StreamDataService streamService;
 	
 	private final Path tempDir = Paths.get(System.getProperty("java.io.tmpdir"));
+
+	private final DateTimeFormatter keyDateTimeFormat = DateTimeFormatter.ofPattern("yyyy/MM-dd/yyyy-MM-dd-HH-mm-ss-SSS-");
 	
 	public BaseStorageService(@Autowired StreamDataService streamService) {
 		this.streamService = streamService;
@@ -72,5 +81,40 @@ public abstract class BaseStorageService extends BaseOpenmrsService implements S
 
 	public String saveTempData(StreamDataWriter writer, ObjectMetadata metadata) throws IOException {
 		return saveTempData(streamService.streamData(writer, metadata != null ? metadata.getLength() : null), metadata);
+	}
+
+	protected String newKey(String moduleIdOrGroup, String keySuffix, String filename) {
+		if (keySuffix == null) {
+			keySuffix = LocalDateTime.now().format(keyDateTimeFormat) + RandomStringUtils.randomAlphanumeric(8);
+		}
+		if (filename != null) {
+			keySuffix += '-' + filename.replace(File.separator, "");
+		}
+		
+		if (moduleIdOrGroup == null) {
+			return keySuffix;
+		} else {
+			if (!moduleIdOrGroupPattern.matcher(moduleIdOrGroup).matches()) {
+				throw new IllegalArgumentException("moduleIdOrGroup '" + moduleIdOrGroup + "' does not match [\\w-./]+");
+			}
+			return moduleIdOrGroup + '/' + keySuffix;
+		}
+	}
+
+	protected String decodeKey(String key) {
+		try {
+			return URLDecoder.decode(key, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected String encodeKey(String key) {
+		try {
+			return URLEncoder.encode(key, "UTF-8").replace(".", "%2E")
+				.replace("*", "%2A").replace("%2F", "/");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
