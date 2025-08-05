9a1836ebd5ba20b38981dc52389d87a25c64e0da
Timug-123 <mugabitimothy1@gmail.com>
2025-08-05 16:23:37 +0300

    Concept Domain - Switching from Hibernate Mappings to Annotations

--- api/src/main/java/org/openmrs/ConceptComplex.java
+++ api/src/main/java/org/openmrs/ConceptComplex.java
@@ -9,6 +9,9 @@
  */
 package org.openmrs;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import org.hibernate.envers.Audited;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 import org.openmrs.obs.ComplexObsHandler;
@@ -19,10 +22,13 @@ import org.openmrs.obs.ComplexObsHandler;
  * @since 1.5
  */
 @Audited
+@Entity
+@Table(name = "concept_complex")
 public class ConceptComplex extends Concept {
 	
 	public static final long serialVersionUID = 473231233L;
 	
+	@Column(name = "handler", length = 255)
 	private String handler;
 	
 	/**
