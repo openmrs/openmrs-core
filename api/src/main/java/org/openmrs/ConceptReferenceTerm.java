139e096e243113f152444306d11d823c811c5ac7
Jai Vora <jaivora@Jais-MacBook-Pro.local>
2025-08-06 02:06:46 -0700

    TRUNK-5906: Migrate `ConceptReferenceTerm` from hibernate mappings to JPA annotations

--- api/src/main/java/org/openmrs/ConceptReferenceTerm.java
+++ api/src/main/java/org/openmrs/ConceptReferenceTerm.java
@@ -1,45 +1,73 @@
 /**
- * This Source Code Form is subject to the terms of the Mozilla Public License,
- * v. 2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
- * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
- * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
- * graphic logo is a trademark of OpenMRS Inc.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under the terms
+ * of the Healthcare Disclaimer located at http://openmrs.org/license. Copyright (C) OpenMRS Inc. OpenMRS is a registered
+ * trademark and the OpenMRS graphic logo is a trademark of OpenMRS Inc.
  */
 package org.openmrs;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 import org.hibernate.envers.Audited;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.DocumentId;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
 
 /**
- * A concept reference term is typically name for a concept by which it is referred in another
- * institution like ICD9, ICD10, SNOMED that keeps a concept dictionary or any other OpenMRS
- * implementation
+ * A concept reference term is typically name for a concept by which it is referred in another institution like ICD9, ICD10,
+ * SNOMED that keeps a concept dictionary or any other OpenMRS implementation
  *
  * @since 1.9
  */
 @Audited
+@Entity
+@Table(name = "concept_reference_term")
+@AttributeOverrides({
+	@AttributeOverride(name = "name", column = @Column(name = "name"))
+})
 public class ConceptReferenceTerm extends BaseChangeableOpenmrsMetadata {
 	
 	private static final long serialVersionUID = 1L;
 	
 	@DocumentId
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "concept_reference_term_id_seq")
+	@GenericGenerator(
+		name = "concept_reference_term_id_seq",
+		parameters = @Parameter(name = "sequence", value = "concept_reference_term_concept_reference_term_id_seq")
+	)
+	@Column(name = "concept_reference_term_id")
 	private Integer conceptReferenceTermId;
 	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "concept_source_id", nullable = false)
 	private ConceptSource conceptSource;
 	
 	//The unique code used to identify the reference term in it's reference terminology
 	@GenericField
+	@Column(name = "code", nullable = false)
 	private String code;
 	
+	@Column(name = "version", length = 50)
 	private String version;
 	
-	private Set<ConceptReferenceTermMap> conceptReferenceTermMaps;
+	@OneToMany(mappedBy = "termA", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+	private Set<ConceptReferenceTermMap> conceptReferenceTermMaps = new LinkedHashSet<>();
 	
 	/** default constructor */
 	public ConceptReferenceTerm() {
@@ -54,8 +82,8 @@ public class ConceptReferenceTerm extends BaseChangeableOpenmrsMetadata {
 	 * Convenience constructor with the required fields filled in
 	 *
 	 * @param source the ConceptSource belongs in
-	 * @param code the code within that concept
-	 * @param name the user readable name of this term
+	 * @param code   the code within that concept
+	 * @param name   the user readable name of this term
 	 * @since 1.9.2, 1.10.0
 	 */
 	public ConceptReferenceTerm(ConceptSource source, String code, String name) {
@@ -154,18 +182,17 @@ public class ConceptReferenceTerm extends BaseChangeableOpenmrsMetadata {
 	}
 	
 	/**
-	 * Add the given {@link ConceptReferenceTermMap} object to this concept reference term's list of
-	 * concept reference term maps. If there is already a corresponding ConceptReferenceTermMap
-	 * object for this concept reference term already, this one will not be added.
+	 * Add the given {@link ConceptReferenceTermMap} object to this concept reference term's list of concept reference term
+	 * maps. If there is already a corresponding ConceptReferenceTermMap object for this concept reference term already, this
+	 * one will not be added.
 	 *
-	 * @param conceptReferenceTermMap
-	 * <strong>Should</strong> not add a map where termB is itself
-	 * <strong>Should</strong> set termA as the term to which a mapping is being added
-	 * <strong>Should</strong> not add duplicate concept reference term maps
+	 * @param conceptReferenceTermMap <strong>Should</strong> not add a map where termB is itself
+	 *                                <strong>Should</strong> set termA as the term to which a mapping is being added
+	 *                                <strong>Should</strong> not add duplicate concept reference term maps
 	 */
 	public void addConceptReferenceTermMap(ConceptReferenceTermMap conceptReferenceTermMap) {
 		if (conceptReferenceTermMap != null && conceptReferenceTermMap.getTermB() != null
-		        && !this.equals(conceptReferenceTermMap.getTermB())) {
+			&& !this.equals(conceptReferenceTermMap.getTermB())) {
 			//can't map a term to itself
 			conceptReferenceTermMap.setTermA(this);
 			if (conceptReferenceTermMaps == null) {
