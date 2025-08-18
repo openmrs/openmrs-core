640a84020b7f397006ec8cd2c5e8c283d06d5377
Wikum Weerakutti <wikum@linuxdeveloper.space>
2025-08-18 15:53:19 +0530

    TRUNK-5899: ConceptAttributeType Domain - Switching from Hibernate Mappings to Annotations (#5216)

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -23,6 +23,7 @@ import org.openmrs.Allergy;
 import org.openmrs.CareSetting;
 import org.openmrs.Concept;
 import org.openmrs.ConceptAnswer;
+import org.openmrs.ConceptAttributeType;
 import org.openmrs.ConceptClass;
 import org.openmrs.ConceptDatatype;
 import org.openmrs.ConceptDescription;
@@ -2775,6 +2776,7 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 				.addAnnotatedClass(ConceptStateConversion.class)
 				.addAnnotatedClass(OrderGroup.class)
 				.addAnnotatedClass(Template.class)
+				.addAnnotatedClass(ConceptAttributeType.class)
 				.getMetadataBuilder().build();
 
 
