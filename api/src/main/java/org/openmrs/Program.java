07f8ac2164fb1f4cc82eb79d3eb1ac0f9aefd593
Debbielou <denakamatte@gmail.com>
2025-08-31 01:02:44 +0300

    Removed deprecated generator from Program

--- api/src/main/java/org/openmrs/Program.java
+++ api/src/main/java/org/openmrs/Program.java
@@ -20,14 +20,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 import org.hibernate.envers.Audited;
 import org.openmrs.annotation.AllowDirectAccess;
 
@@ -46,8 +45,7 @@ public class Program extends BaseChangeableOpenmrsMetadata {
 	// Properties
 	// ******************
 	@Id
-	@GeneratedValue(generator = "native")
-	@GenericGenerator(name = "native", strategy = "native", parameters = @Parameter(name = "sequence", value = "program_program_id_seq"))
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "program_id")
 	private Integer programId;
 	
