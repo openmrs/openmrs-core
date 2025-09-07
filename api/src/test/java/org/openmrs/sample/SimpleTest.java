0a05be3efc2a35e27bca9332c9eaa41ee42e07b5
Xuqii-Dawne <dawnecheh@gmail.com>
2025-09-07 12:26:10 +0800

    add SimpleTest.java

---
+++ api/src/test/java/org/openmrs/sample/SimpleTest.java
@@ -0,0 +1,12 @@
+package org.openmrs.sample;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+    @Test
+    public void alwaysPasses() {
+        // This is a simple test to prove CI works
+        assertTrue(true);
+    }
+}
