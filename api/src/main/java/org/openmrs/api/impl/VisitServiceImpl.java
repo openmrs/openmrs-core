ccf9eb81f3f0e476bec6b92609308982762134ce
Ruhanga <41738040+Ruhanga@users.noreply.github.com>
2025-10-23 01:43:43 +0300

    TRUNK-6351: Added Jackson Serializer/Deserializer (#5099)
    * TRUNK-6351: Added Jackson Serializer/Deserializer
    
    * TRUNK-6351: Improved whitelist checks
    
    * TRUNK-6351: Addressing PR reviews
    
    * TRUNK-6351: Fixed randomly failing test
    
    * TRUNK-6351: Fixed randomly failing test
    
    * TRUNK-6351: Rebased on upstream to fix tests
    
    * TRUNK-6351: Fixed tests failing on windows platform
    
    * TRUNK-6351: Make Fields visible
    
    * TRUNK-6351: Migrated from XStream to Jackson
    
    * TRUNK-6351: Ignore self referencing getter
    
    * TRUNK-6351: Added relevant liquibase migration
    
    * TRUNK-6351: Improved test coverage
    
    * TRUNK-6351: Fixed documentation oversight
    
    * TRUNK-6351: Applied review suggestions
    
    * TRUNK-6351: DomainService utility to become standard Transactional OpenmrsService
    
    * TRUNK-6351: Revert Address and Name Template migration to Jackson
    
    * TRUNK-6351: Minimize serialized detail to relevant fields.
    
    * TRUNK-6351: Addressing PR review
    
    * TRUNK-6351: Minor cleanup
    
    * TRUNK-6351: Prefer RefByUuid implementations over Reflections usage.
    
    * Undo unneccessary changes
    
    * Undo unneccessary changes
    
    * Undo unneccessary changes
    
    * Undo unneccessary changes
    
    * Undo unneccessary changes
    
    * Undo unneccessary changes
    
    * TRUNK-6351: Addressing PR review.
    
    * Undo unneccessary changes
    
    * Undo unneccessary changes
    
    * Improved test coverage
    
    * TRUNK-6351: Resolve merge conflicts
    
    * TRUNK-6351: Prefer Spring's dependency injection over OpenMRS' ServiceContext
    
    * TRUNK-6351: Address PR reviews.
    
    * TRUNK-6351: Addressed PR reviews
    
    * TRUNK-6351: Addressed PR reviews and improved test coverage
    
    * Add import for Arrays in ProviderServiceImpl
    
    ---------
    
    Co-authored-by: dkayiwa <dkayiwa@openmrs.org>

--- api/src/main/java/org/openmrs/api/impl/VisitServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/VisitServiceImpl.java
@@ -9,6 +9,8 @@
  */
 package org.openmrs.api.impl;
 
+
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -26,6 +28,7 @@ import org.openmrs.VisitAttribute;
 import org.openmrs.VisitAttributeType;
 import org.openmrs.VisitType;
 import org.openmrs.api.APIException;
+import org.openmrs.api.RefByUuid;
 import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.db.VisitDAO;
@@ -46,7 +49,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service("visitService")
 @Transactional
-public class VisitServiceImpl extends BaseOpenmrsService implements VisitService {
+public class VisitServiceImpl extends BaseOpenmrsService implements VisitService, RefByUuid {
 	
 	@Autowired
 	private VisitDAO dao;
@@ -420,5 +423,28 @@ public class VisitServiceImpl extends BaseOpenmrsService implements VisitService
 		}
 		return result;
 	}
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T getRefByUuid(Class<T> type, String uuid) {
+        if (Visit.class.equals(type)) {
+            return (T) getVisitByUuid(uuid);
+        }
+        if (VisitType.class.equals(type)) {
+            return (T) getVisitTypeByUuid(uuid);
+        }
+        if (VisitAttribute.class.equals(type)) {
+            return (T) getVisitAttributeByUuid(uuid);
+        }
+        if (VisitAttributeType.class.equals(type)) {
+            return (T) getVisitAttributeTypeByUuid(uuid);
+        }
+        throw new APIException("Unsupported type for getRefByUuid: " + type != null ? type.getName() : "null");
+    }
+
+    @Override
+    public List<Class<?>> getRefTypes() {
+        return Arrays.asList(Visit.class, VisitType.class, VisitAttribute.class, VisitAttributeType.class);
+    }
 
 }
