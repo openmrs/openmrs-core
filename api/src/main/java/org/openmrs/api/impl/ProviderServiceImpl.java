faf491e9802ee5869ba1d2f9934f744a9197d40e
dkayiwa <dkayiwa@openmrs.org>
2025-10-23 01:28:25 +0300

    Add import for Arrays in ProviderServiceImpl

--- api/src/main/java/org/openmrs/api/impl/ProviderServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/ProviderServiceImpl.java
@@ -26,6 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
