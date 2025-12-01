c6908436f63d5852d0dc8f12e86a12b68f01082c
sudhanshu_raj <rajsudhanshu9431@gmail.com>
2025-12-01 22:02:49 +0530

    TRUNK-6489: Switching ConceptAttribute Domain from xml to annotation based

--- api/src/test/java/org/openmrs/api/OrderServiceTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceTest.java
@@ -83,6 +83,7 @@ import org.openmrs.Visit;
 import org.openmrs.VisitType;
 import org.openmrs.VisitAttributeType;
 import org.openmrs.FormResource;
+import org.openmrs.ConceptAttribute;
 import org.openmrs.api.builder.DrugOrderBuilder;
 import org.openmrs.api.builder.OrderBuilder;
 import org.openmrs.api.context.Context;
@@ -2939,6 +2940,7 @@ public class OrderServiceTest extends BaseContextSensitiveTest {
 				.addAnnotatedClass(LoginCredential.class)
 				.addAnnotatedClass(ConceptDatatype.class)
 				.addAnnotatedClass(ProviderAttribute.class)
+				.addAnnotatedClass(ConceptAttribute.class)
 				.getMetadataBuilder().build();
 
 
