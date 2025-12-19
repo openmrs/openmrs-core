d330e0df841425988805712508ad483e160f7175
Chinmay Chaudhari <chaudharichinmay227@gmail.com>
2025-12-19 08:32:43 +0530

    test(api): add OrderAttribute fulfiller location test (TRUNK-6190)

--- api/src/test/java/org/openmrs/api/OrderServiceOrderAttributeTest.java
+++ api/src/test/java/org/openmrs/api/OrderServiceOrderAttributeTest.java
@@ -1,46 +1,52 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.api;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
-import org.openmrs.*;
+import org.openmrs.DrugOrder;
+import org.openmrs.Location;
+import org.openmrs.OrderAttribute;
+import org.openmrs.OrderAttributeType;
 import org.openmrs.customdatatype.datatype.LocationDatatype;
 import org.openmrs.test.BaseContextSensitiveTest;
 
 /**
- * Tests OrderAttribute behavior for fulfillerLocation WITHOUT persistence.
- * Persistence is intentionally avoided due to valueReference limitations.
+ * Tests OrderAttribute behavior for fulfiller location without persistence.
  */
 public class OrderServiceOrderAttributeTest extends BaseContextSensitiveTest {
 
 	@Test
 	public void shouldAttachAndRetrieveFulfillerLocationAttributeOnOrder() {
 
-		// Attribute type
 		OrderAttributeType attributeType = new OrderAttributeType();
 		attributeType.setName("Fulfiller Location");
 		attributeType.setDatatypeClassname(LocationDatatype.class.getName());
 
-		// Order (no DB save)
 		DrugOrder order = new DrugOrder();
 
-		// Location
 		Location location = new Location();
 		location.setUuid("test-location-uuid");
 
-		// Attribute
 		OrderAttribute attribute = new OrderAttribute();
 		attribute.setAttributeType(attributeType);
 
-		// Correct lifecycle
 		order.addAttribute(attribute);
 		attribute.setValue(location);
 
-		// Verify
-		OrderAttribute retrieved =
-			order.getAttributes().iterator().next();
+		assertNotNull(order.getAttributes());
+		assertEquals(1, order.getAttributes().size());
 
-		assertNotNull(retrieved);
-		assertEquals(location, retrieved.getValue());
+		OrderAttribute savedAttribute = order.getAttributes().iterator().next();
+		assertEquals(location, savedAttribute.getValue());
 	}
 }
