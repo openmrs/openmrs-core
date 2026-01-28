5d45e88ce9599f76289f91f63b8aa97dc54581ba
Binayak490-cyber <binayak490@gmail.com>
2026-01-28 16:57:12 +0530

    TRUNK-6465: Fix build issues and update imports

--- api/src/main/java/org/openmrs/api/impl/OrderServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/OrderServiceImpl.java
@@ -57,6 +57,7 @@ import org.openmrs.util.OpenmrsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
