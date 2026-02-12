14e70179df8f0133e01f339c55eda96fa10d8303
YO_WHATS_UP2 <sairamb2007.21@gmail.com>
2026-02-12 22:33:01 +0530

    TRUNK-6553: supresh CODEQL warnings added a coment explaining

--- api/src/main/java/org/openmrs/util/Security.java
+++ api/src/main/java/org/openmrs/util/Security.java
@@ -250,7 +250,8 @@ public class Security {
 		catch (GeneralSecurityException e) {
 			try {
 				@SuppressWarnings("java:S5542")
-				/* AES/CBC/PKCS5Padding IS USED AS A FALLBACK to decrypt legacy data that was encrypted before the patch. NEW DATA USES AES/GCM/NoPadding*/	
+				/* AES/CBC/PKCS5Padding IS USED AS A FALLBACK to decrypt legacy data that was encrypted before the patch. NEW DATA USES AES/GCM/NoPadding*/
+		        // codeql[java/weak-cryptographic-algorithm]	
 				Cipher legacyCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 				IvParameterSpec ivSpec = new IvParameterSpec(initVector);
 				legacyCipher.init(Cipher.DECRYPT_MODE, secret, ivSpec);
