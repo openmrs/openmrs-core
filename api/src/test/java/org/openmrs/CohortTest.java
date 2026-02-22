b7e486483c06d39f065e8cc5574fde96e4a94929
Sagar Khandagre <sagar.khandagre998@gmail.com>
2026-02-22 10:20:02 +0530

    Add stream.Collectors import to CohortTest

--- api/src/test/java/org/openmrs/CohortTest.java
+++ api/src/test/java/org/openmrs/CohortTest.java
@@ -23,6 +23,7 @@ import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
