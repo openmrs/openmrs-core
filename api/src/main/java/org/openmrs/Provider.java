0d5f14e62f03a867d53f6ff482f2f937c81311a3
Debbielou <denakamatte@gmail.com>
2026-02-22 21:19:53 +0300

    Reworked annotations in Provider to handle formatting issues

--- api/src/main/java/org/openmrs/Provider.java
+++ api/src/main/java/org/openmrs/Provider.java
@@ -34,40 +34,38 @@ import org.slf4j.LoggerFactory;
 @Audited
 @AttributeOverrides({ @AttributeOverride(name = "name", column = @Column(name = "name", nullable = true)) })
 public class Provider extends BaseCustomizableMetadata<ProviderAttribute> {
-
+	
 	private static final Logger log = LoggerFactory.getLogger(Provider.class);
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "provider_id")
 	private Integer providerId;
-
+	
 	@ManyToOne(cascade = CascadeType.MERGE)
 	@JoinColumn(name = "person_id")
 	private Person person;
-
+	
 	@Column(name = "identifier", length = 255)
 	private String identifier;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "role_id")
 	private Concept role;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "speciality_id")
 	private Concept speciality;
 
-	@ManyToOne
-	@JoinColumn(name = "provider_role_id")
 	private ProviderRole providerRole;
-
+	
 	public Provider() {
 	}
-
+	
 	public Provider(Integer providerId) {
 		this.providerId = providerId;
 	}
-
+	
 	/**
 	 * @see org.openmrs.OpenmrsObject#getId()
 	 */
@@ -75,7 +73,7 @@ public class Provider extends BaseCustomizableMetadata<ProviderAttribute> {
 	public Integer getId() {
 		return getProviderId();
 	}
-
+	
 	/**
 	 * @see org.openmrs.OpenmrsObject#setId(java.lang.Integer)
 	 */
@@ -83,82 +81,82 @@ public class Provider extends BaseCustomizableMetadata<ProviderAttribute> {
 	public void setId(Integer id) {
 		setProviderId(id);
 	}
-
+	
 	/**
 	 * @param providerId the providerId to set
 	 */
 	public void setProviderId(Integer providerId) {
 		this.providerId = providerId;
 	}
-
+	
 	/**
 	 * @return the providerId
 	 */
 	public Integer getProviderId() {
 		return providerId;
 	}
-
+	
 	/**
 	 * @param person the person to set
 	 */
 	public void setPerson(Person person) {
 		this.person = person;
 	}
-
+	
 	/**
 	 * @return the person
 	 */
 	public Person getPerson() {
 		return person;
 	}
-
+	
 	/**
 	 * @param identifier the identifier to set
 	 */
 	public void setIdentifier(String identifier) {
 		this.identifier = identifier;
 	}
-
+	
 	/**
 	 * @return the identifier
 	 */
 	public String getIdentifier() {
 		return identifier;
 	}
-
+	
 	/**
 	 * Sets the role concept
-	 *
+	 * 
 	 * @since 2.2
 	 * @param role the role to set
 	 */
 	public void setRole(Concept role) {
 		this.role = role;
 	}
-
+	
 	/**
 	 * Gets the role concept
-	 *
+	 * 
 	 * @since 2.2
 	 * @return the role
 	 */
 	public Concept getRole() {
 		return role;
 	}
-
+	
 	/**
 	 * Sets the speciality concept
-	 *
+	 * 
 	 * @since 2.2
 	 * @param speciality the speciality to set
 	 */
 	public void setSpeciality(Concept speciality) {
 		this.speciality = speciality;
 	}
-
+	
 	/**
 	 * Gets the speciality concept
-	 *
+	 * 
 	 * @since 2.2
 	 * @return the speciality
 	 */
@@ -168,7 +166,7 @@ public class Provider extends BaseCustomizableMetadata<ProviderAttribute> {
 
 	/**
 	 * Gets the provider role
-	 *
+	 * 
 	 * @since 2.8.0
 	 * @return the provider role
 	 */
@@ -178,24 +176,25 @@ public class Provider extends BaseCustomizableMetadata<ProviderAttribute> {
 
 	/**
 	 * Sets the provider role
-	 *
+	 * 
 	 * @since 2.8.0
 	 * @param providerRole the provider role to set
 	 */
 	public void setProviderRole(ProviderRole providerRole) {
 		this.providerRole = providerRole;
 	}
-
+	
 	@Override
 	public String toString() {
 		String provider = String.valueOf(providerId) + " providerName:" + ((person != null) ? person.getNames() : "");
 		return "[Provider: providerId:" + provider + " ]";
 	}
-
+	
 	/**
 	 * @see org.openmrs.BaseOpenmrsMetadata#getName()
 	 * <strong>Should</strong> return person full name if person is not null or null otherwise
 	 */
+	
 	@Override
 	public String getName() {
 		if (getPerson() != null && getPerson().getPersonName() != null) {
