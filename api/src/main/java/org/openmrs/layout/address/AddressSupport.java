c6aae2c9ad86f80c818d89024cf9538f65c1a7ba
Juliet wamalwa <julietwamalwa@Juliets-MacBook-Air.local>
2026-02-24 15:28:39 +0300

    LUI-199: Editing the Address Template does not save the address template correctly

--- api/src/main/java/org/openmrs/layout/address/AddressSupport.java
+++ api/src/main/java/org/openmrs/layout/address/AddressSupport.java
@@ -9,12 +9,10 @@
  */
 package org.openmrs.layout.address;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.openmrs.GlobalProperty;
-import org.openmrs.api.GlobalPropertyListener;
+import org.openmrs.api.APIException;
 import org.openmrs.api.context.Context;
 import org.openmrs.layout.LayoutSupport;
 import org.openmrs.serialization.SerializationException;
@@ -22,151 +20,197 @@ import org.openmrs.util.OpenmrsConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @since 1.12
  */
-public class AddressSupport extends LayoutSupport<AddressTemplate> implements GlobalPropertyListener {
-	
-	private static AddressSupport singleton;
-	
-	private boolean initialized = false;
-	
-	private static final Logger log = LoggerFactory.getLogger(AddressSupport.class);
-	
-	private AddressSupport() {
-		if (singleton == null) {
-			singleton = this;
-		}
-		log.debug("Setting singleton: " + singleton);
-	}
-	
-	public static AddressSupport getInstance() {
-		synchronized (AddressSupport.class) {
-			if (singleton == null) {
-				singleton = new AddressSupport();
-			}
-		}
-		singleton.init();
-		return singleton;
-		
-	}
-	
-	private void init() {
-		if (!initialized) {
-			Context.getAdministrationService().addGlobalPropertyListener(singleton);
-			
-			String layoutTemplateXml = Context.getAdministrationService().getGlobalProperty(
-			    OpenmrsConstants.GLOBAL_PROPERTY_ADDRESS_TEMPLATE);
-			setAddressTemplate(StringEscapeUtils.unescapeXml(layoutTemplateXml));
-			
-			List<String> specialTokens = new ArrayList<>();
-			specialTokens.add("address1");
-			specialTokens.add("address2");
-			specialTokens.add("address3");
-			specialTokens.add("address4");
-			specialTokens.add("address5");
-			specialTokens.add("address6");
-			specialTokens.add("cityVillage");
-			specialTokens.add("countyDistrict");
-			specialTokens.add("stateProvince");
-			specialTokens.add("country");
-			specialTokens.add("latitude");
-			specialTokens.add("longitude");
-			specialTokens.add("postalCode");
-			specialTokens.add("startDate");
-			specialTokens.add("endDate");
-			
-			setSpecialTokens(specialTokens);
-			initialized = true;
-		}
-	}
-	
-	/**
-	 * @return Returns the defaultLayoutFormat
-	 */
-	@Override
-	public String getDefaultLayoutFormat() {
-		return defaultLayoutFormat;
-	}
-	
-	/**
-	 * @param addressTemplates The addressTemplates to set.
-	 */
-	public void setAddressTemplate(List<AddressTemplate> addressTemplates) {
-		this.layoutTemplates = addressTemplates;
-		setDefaultLayoutFormat(layoutTemplates.get(0).getCodeName());
-		
-	}
-	
-	/**
-	 * @return Returns the addressTemplates.
-	 */
-	
-	public List<AddressTemplate> getAddressTemplate() {
-		if (layoutTemplates == null) {
-			try {
-				String xml = Context.getAdministrationService().getGlobalProperty(
-				    OpenmrsConstants.GLOBAL_PROPERTY_ADDRESS_TEMPLATE);
-				setAddressTemplate(xml);
-			}
-			catch (Exception ex) {
-				//The old AddressTemplate prevails
-			}
-		}
-		return layoutTemplates;
-	}
-	
-	/**
-	 * @see org.openmrs.api.GlobalPropertyListener#supportsPropertyName(String)
-	 */
-	@Override
-	public boolean supportsPropertyName(String propertyName) {
-		return OpenmrsConstants.GLOBAL_PROPERTY_ADDRESS_TEMPLATE.equals(propertyName);
-	}
-	
-	/**
-	 * @see org.openmrs.api.GlobalPropertyListener#globalPropertyChanged(org.openmrs.GlobalProperty)
-	 */
-	@Override
-	public void globalPropertyChanged(GlobalProperty newValue) {
-		if (!OpenmrsConstants.GLOBAL_PROPERTY_ADDRESS_TEMPLATE.equals(newValue.getProperty())) {
-			return;
-		}
-		try {
-			setAddressTemplate(newValue.getPropertyValue());
-		}
-		catch (Exception ex) {
-			log.error("Error in new xml global property value", ex);
-			setAddressTemplate(new ArrayList<>());
-		}
-	}
-	
-	private void setAddressTemplate(String xml) {
-		AddressTemplate addressTemplate;
-		try {
-			
-			addressTemplate = Context.getSerializationService().getDefaultSerializer().deserialize(StringEscapeUtils.unescapeXml(xml),
-			    AddressTemplate.class);
-		}
-		catch (SerializationException e) {
-			log.error("Error in deserializing address template", e);
-			addressTemplate = new AddressTemplate("Error while deserializing address layout template.");
-		}
-		
-		List<AddressTemplate> list = new ArrayList<>();
-		list.add(addressTemplate);
-		setAddressTemplate(list);
-	}
-	
-	/**
-	 * @see org.openmrs.api.GlobalPropertyListener#globalPropertyDeleted(String)
-	 */
-	@Override
-	public void globalPropertyDeleted(String propertyName) {
-		if (!OpenmrsConstants.GLOBAL_PROPERTY_ADDRESS_TEMPLATE.equals(propertyName)) {
-			return;
-		}
-		setAddressTemplate(new ArrayList<>());
-	}
-	
+public class AddressSupport extends LayoutSupport<AddressTemplate> implements org.openmrs.api.GlobalPropertyListener {
+
+    private static AddressSupport singleton;
+
+    private volatile boolean initialized = false;
+
+    private static final Logger log = LoggerFactory.getLogger(AddressSupport.class);
+
+    private AddressSupport() {
+        if (singleton == null) {
+            singleton = this;
+        }
+        log.debug("Setting singleton: " + singleton);
+    }
+
+    public static AddressSupport getInstance() {
+        synchronized (AddressSupport.class) {
+            if (singleton == null) {
+                singleton = new AddressSupport();
+            }
+        }
+        singleton.init();
+        return singleton;
+
+    }
+
+    private synchronized void init() {
+        if (!initialized) {
+            Context.getAdministrationService().addGlobalPropertyListener(singleton);
+
+            String layoutTemplateXml = Context.getAdministrationService().getGlobalProperty(
+                    OpenmrsConstants.GLOBAL_PROPERTY_ADDRESS_TEMPLATE);
+            setAddressTemplate(layoutTemplateXml);
+
+            List<String> specialTokens = new ArrayList<>();
+            specialTokens.add("address1");
+            specialTokens.add("address2");
+            specialTokens.add("address3");
+            specialTokens.add("address4");
+            specialTokens.add("address5");
+            specialTokens.add("address6");
+            specialTokens.add("cityVillage");
+            specialTokens.add("countyDistrict");
+            specialTokens.add("stateProvince");
+            specialTokens.add("country");
+            specialTokens.add("latitude");
+            specialTokens.add("longitude");
+            specialTokens.add("postalCode");
+            specialTokens.add("startDate");
+            specialTokens.add("endDate");
+
+            setSpecialTokens(specialTokens);
+            initialized = true;
+        }
+    }
+
+    /**
+     * @return Returns the defaultLayoutFormat
+     */
+    @Override
+    public String getDefaultLayoutFormat() {
+        return defaultLayoutFormat;
+    }
+
+    /**
+     * @param addressTemplates The addressTemplates to set.
+     */
+    public void setAddressTemplate(List<AddressTemplate> addressTemplates) {
+        this.layoutTemplates = addressTemplates;
+        if (layoutTemplates == null || layoutTemplates.isEmpty() || layoutTemplates.get(0) == null) {
+            setDefaultLayoutFormat(null);
+        } else {
+            setDefaultLayoutFormat(layoutTemplates.get(0).getCodeName());
+        }
+
+    }
+
+    /**
+     * @return Returns the addressTemplates.
+     */
+    public List<AddressTemplate> getAddressTemplate() {
+        if (layoutTemplates == null) {
+            try {
+                String xml = Context.getAdministrationService().getGlobalProperty(
+                        OpenmrsConstants.GLOBAL_PROPERTY_ADDRESS_TEMPLATE);
+                setAddressTemplate(xml);
+            } catch (Exception ex) {
+                //The old AddressTemplate prevails
+                log.debug("Error while attempting to load address template on first access", ex);
+            }
+        }
+        return layoutTemplates;
+    }
+
+    /**
+     * @see org.openmrs.api.GlobalPropertyListener#supportsPropertyName(String)
+     */
+    @Override
+    public boolean supportsPropertyName(String propertyName) {
+        return OpenmrsConstants.GLOBAL_PROPERTY_ADDRESS_TEMPLATE.equals(propertyName);
+    }
+
+    /**
+     * @see
+     * org.openmrs.api.GlobalPropertyListener#globalPropertyChanged(org.openmrs.GlobalProperty)
+     */
+    @Override
+    public void globalPropertyChanged(GlobalProperty newValue) {
+        if (!OpenmrsConstants.GLOBAL_PROPERTY_ADDRESS_TEMPLATE.equals(newValue.getProperty())) {
+            return;
+        }
+        try {
+            setAddressTemplate(newValue.getPropertyValue());
+        } catch (Exception ex) {
+            log.error("Error in new xml global property value", ex);
+            setAddressTemplate(new ArrayList<>());
+        }
+    }
+
+    /**
+     * Centralized deserialization using multiple fallbacks:
+     * - try raw xml
+     * - try unescapeXml(xml)
+     * - try unescapeHtml4(xml)
+     *
+     * This method throws SerializationException or APIException to the caller.
+     * It's package-visible to make it testable.
+     */
+    AddressTemplate deserializeAddressTemplate(String xml) throws SerializationException, APIException {
+        if (StringUtils.isBlank(xml)) {
+            xml = OpenmrsConstants.DEFAULT_ADDRESS_TEMPLATE;
+        }
+
+        // attempt 1: raw xml
+        try {
+            return Context.getSerializationService()
+                    .getDefaultSerializer()
+                    .deserialize(xml, AddressTemplate.class);
+        } catch (SerializationException | APIException rawEx) {
+            log.debug("Raw deserialize failed, will try unescapeXml()", rawEx);
+
+            // attempt 2: unescape XML entities
+            try {
+                String unescapedXml = StringEscapeUtils.unescapeXml(xml);
+                return Context.getSerializationService()
+                        .getDefaultSerializer()
+                        .deserialize(unescapedXml, AddressTemplate.class);
+            } catch (SerializationException | APIException unescapeXmlEx) {
+                log.debug("unescapeXml() deserialize failed, will try unescapeHtml4()", unescapeXmlEx);
+
+                // attempt 3: unescape HTML entities
+                String unescapedHtml = StringEscapeUtils.unescapeHtml4(xml);
+                return Context.getSerializationService()
+                        .getDefaultSerializer()
+                        .deserialize(unescapedHtml, AddressTemplate.class);
+            }
+        }
+    }
+
+    private void setAddressTemplate(String xml) {
+        AddressTemplate addressTemplate;
+
+        try {
+            addressTemplate = deserializeAddressTemplate(xml);
+        } catch (SerializationException | APIException e) {
+            // avoid writing the entire GP into logs; truncate to first 200 chars to help debugging
+            final String truncated = xml == null ? "null" : (xml.length() > 200 ? xml.substring(0, 200) + "..." : xml);
+            log.error("Error in deserializing address template (truncated original content: {}).", truncated, e);
+            addressTemplate = new AddressTemplate("Error while deserializing address layout template.");
+        }
+
+        List<AddressTemplate> list = new ArrayList<>();
+        list.add(addressTemplate);
+        setAddressTemplate(list);
+    }
+
+    /**
+     * @see org.openmrs.api.GlobalPropertyListener#globalPropertyDeleted(String)
+     */
+    @Override
+    public void globalPropertyDeleted(String propertyName) {
+        if (!OpenmrsConstants.GLOBAL_PROPERTY_ADDRESS_TEMPLATE.equals(propertyName)) {
+            return;
+        }
+        setAddressTemplate(new ArrayList<>());
+    }
+
 }
--- api/src/main/java/org/openmrs/validator/PersonAddressValidator.java
+++ api/src/main/java/org/openmrs/validator/PersonAddressValidator.java
@@ -9,125 +9,154 @@
  */
 package org.openmrs.validator;
 
-import java.util.Date;
-import java.util.List;
-
-import org.apache.commons.beanutils.PropertyUtils;
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.openmrs.PersonAddress;
 import org.openmrs.annotation.Handler;
+import org.openmrs.api.APIException;
 import org.openmrs.api.context.Context;
 import org.openmrs.layout.address.AddressTemplate;
+import org.openmrs.serialization.SerializationException;
 import org.openmrs.util.OpenmrsUtil;
+import org.openmrs.util.OpenmrsConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+import java.util.List;
 
 /**
  * This class validates a PersonAddress object.
  *
  * @since 1.9
  */
-@Handler(supports = { PersonAddress.class }, order = 50)
+@Handler(supports = {PersonAddress.class}, order = 50)
 public class PersonAddressValidator implements Validator {
-	
-	private static final Logger log = LoggerFactory.getLogger(PersonAddressValidator.class);
-	
-	/**
-	 * @see org.springframework.validation.Validator#supports(java.lang.Class)
-	 */
-	@Override
-	public boolean supports(Class<?> c) {
-		return PersonAddress.class.isAssignableFrom(c);
-	}
-	
-	/**
-	 * @see org.springframework.validation.Validator#validate(java.lang.Object,
-	 *      org.springframework.validation.Errors)
-	 * <strong>Should</strong> pass if all the dates are valid
-	 * <strong>Should</strong> fail if the startDate is in the future
-	 * <strong>Should</strong> fail if the endDate is before the startDate
-	 * <strong>Should</strong> pass if startDate and endDate are both null
-	 * <strong>Should</strong> pass if startDate is null
-	 * <strong>Should</strong> pass if endDate is null
-	 * <strong>Should</strong> fail if required fields are empty
-	 * <strong>Should</strong> pass if required fields are not empty
-	 * <strong>Should</strong> pass validation if field lengths are correct
-	 * <strong>Should</strong> fail validation if field lengths are not correct
-	 */
-	@Override
-	public void validate(Object object, Errors errors) {
-		//TODO Validate other aspects of the personAddress object
-		log.debug("{}.validate...", this.getClass().getName());
-		
-		if (object == null) {
-			throw new IllegalArgumentException("The personAddress object should not be null");
-		}
-		
-		PersonAddress personAddress = (PersonAddress) object;
-		
-		//resolve a shorter name to display along with the error message
-		String addressString;
-		if (StringUtils.isNotBlank(personAddress.getAddress1())) {
-			addressString = personAddress.getAddress1();
-		} else if (StringUtils.isNotBlank(personAddress.getAddress2())) {
-			addressString = personAddress.getAddress2();
-		} else if (StringUtils.isNotBlank(personAddress.getCityVillage())) {
-			addressString = personAddress.getCityVillage();
-		} else {
-			addressString = personAddress.toString();
-		}
-		
-		if (OpenmrsUtil.compareWithNullAsEarliest(personAddress.getStartDate(), new Date()) > 0) {
-			errors.rejectValue("startDate", "PersonAddress.error.startDateInFuture", new Object[] { "'" + addressString
-			        + "'" }, "The Start Date for address '" + addressString + "' shouldn't be in the future");
-		}
-		
-		if (personAddress.getStartDate() != null
-		        && OpenmrsUtil.compareWithNullAsLatest(personAddress.getStartDate(), personAddress.getEndDate()) > 0) {
-			errors.rejectValue("endDate", "PersonAddress.error.endDateBeforeStartDate", new Object[] { "'" + addressString
-			        + "'" }, "The End Date for address '" + addressString + "' shouldn't be earlier than the Start Date");
-		}
-		
-		String xml = Context.getLocationService().getAddressTemplate();
-		List<String> requiredElements;
-		
-		try {
-			AddressTemplate addressTemplate = Context.getSerializationService().getDefaultSerializer().deserialize(StringEscapeUtils.unescapeXml(xml),
-			    AddressTemplate.class);
-			requiredElements = addressTemplate.getRequiredElements();
-		}
-		catch (Exception e) {
-			errors.reject(Context.getMessageSourceService().getMessage("AddressTemplate.error"));
-			return;
-		}
-		
-		if (requiredElements != null) {
-			for (String fieldName : requiredElements) {
-				try {
-					Object value = PropertyUtils.getProperty(personAddress, fieldName);
-					if (StringUtils.isBlank((String) value)) {
-						//required field not found
-						errors.reject(Context.getMessageSourceService().getMessage(
-						    "AddressTemplate.error.requiredAddressFieldIsBlank", new Object[] { fieldName },
-						    Context.getLocale()));
-					}
-				}
-				catch (Exception e) {
-					//wrong field declared in template
-					errors
-					        .reject(Context.getMessageSourceService().getMessage(
-					            "AddressTemplate.error.fieldNotDeclaredInTemplate", new Object[] { fieldName },
-					            Context.getLocale()));
-				}
-			}
-		}
-		
-		ValidateUtil.validateFieldLengths(errors, object.getClass(), "address1", "address2", "cityVillage", "stateProvince",
-		    "postalCode", "country", "latitude", "longitude", "voidReason", "countyDistrict", "address3", "address4",
-		    "address5", "address6", "address7", "address8", "address9", "address10", "address11", "address12", "address13", 
-		    "address14", "address15");
-	}
+
+    private static final Logger log = LoggerFactory.getLogger(PersonAddressValidator.class);
+
+    /**
+     * @see org.springframework.validation.Validator#supports(java.lang.Class)
+     */
+    @Override
+    public boolean supports(Class<?> c) {
+        return PersonAddress.class.isAssignableFrom(c);
+    }
+
+    /**
+     * @see org.springframework.validation.Validator#validate(java.lang.Object,
+     * org.springframework.validation.Errors)
+     * <strong>Should</strong> pass if all the dates are valid
+     * <strong>Should</strong> fail if the startDate is in the future
+     * <strong>Should</strong> fail if the endDate is before the startDate
+     * <strong>Should</strong> pass if startDate and endDate are both null
+     * <strong>Should</strong> pass if startDate is null
+     * <strong>Should</strong> pass if endDate is null
+     * <strong>Should</strong> fail if required fields are empty
+     * <strong>Should</strong> pass if required fields are not empty
+     * <strong>Should</strong> pass validation if field lengths are correct
+     * <strong>Should</strong> fail validation if field lengths are not correct
+     */
+    @Override
+    public void validate(Object object, Errors errors) {
+        //TODO Validate other aspects of the personAddress object
+        log.debug("{}.validate...", this.getClass().getName());
+
+        if (object == null) {
+            throw new IllegalArgumentException("The personAddress object should not be null");
+        }
+
+        PersonAddress personAddress = (PersonAddress) object;
+
+        //resolve a shorter name to display along with the error message
+        String addressString;
+        if (StringUtils.isNotBlank(personAddress.getAddress1())) {
+            addressString = personAddress.getAddress1();
+        } else if (StringUtils.isNotBlank(personAddress.getAddress2())) {
+            addressString = personAddress.getAddress2();
+        } else if (StringUtils.isNotBlank(personAddress.getCityVillage())) {
+            addressString = personAddress.getCityVillage();
+        } else {
+            addressString = personAddress.toString();
+        }
+
+        if (OpenmrsUtil.compareWithNullAsEarliest(personAddress.getStartDate(), new Date()) > 0) {
+            errors.rejectValue("startDate", "PersonAddress.error.startDateInFuture", new Object[]{"'" + addressString
+                    + "'"}, "The Start Date for address '" + addressString + "' shouldn't be in the future");
+        }
+
+        if (personAddress.getStartDate() != null
+                && OpenmrsUtil.compareWithNullAsLatest(personAddress.getStartDate(), personAddress.getEndDate()) > 0) {
+            errors.rejectValue("endDate", "PersonAddress.error.endDateBeforeStartDate", new Object[]{"'" + addressString
+                    + "'"}, "The End Date for address '" + addressString + "' shouldn't be earlier than the Start Date");
+        }
+
+        String xml = Context.getLocationService().getAddressTemplate();
+        List<String> requiredElements;
+
+        try {
+            if (StringUtils.isBlank(xml)) {
+                xml = OpenmrsConstants.DEFAULT_ADDRESS_TEMPLATE;
+            }
+
+            AddressTemplate addressTemplate;
+            try {
+                addressTemplate = Context.getSerializationService()
+                        .getDefaultSerializer()
+                        .deserialize(xml, AddressTemplate.class);
+            } catch (SerializationException | APIException rawEx) {
+                try {
+                    addressTemplate = Context.getSerializationService()
+                            .getDefaultSerializer()
+                            .deserialize(StringEscapeUtils.unescapeXml(xml), AddressTemplate.class);
+                } catch (SerializationException | APIException unescapeXmlEx) {
+                    addressTemplate = Context.getSerializationService()
+                            .getDefaultSerializer()
+                            .deserialize(StringEscapeUtils.unescapeHtml4(xml), AddressTemplate.class);
+                }
+            }
+
+            requiredElements = addressTemplate.getRequiredElements();
+        } catch (SerializationException | APIException e) {
+            errors.reject(Context.getMessageSourceService().getMessage("AddressTemplate.error"));
+            return;
+        }
+
+        if (requiredElements != null) {
+            for (String fieldName : requiredElements) {
+                try {
+                    Object value = PropertyUtils.getProperty(personAddress, fieldName);
+                    boolean blank;
+                    if (value == null) {
+                        blank = true;
+                    } else {
+                        blank = StringUtils.isBlank(String.valueOf(value));
+                    }
+
+                    if (blank) {
+                        // required field not found or blank
+                        errors.reject(Context.getMessageSourceService().getMessage(
+                                "AddressTemplate.error.requiredAddressFieldIsBlank", new Object[]{fieldName},
+                                Context.getLocale()));
+                    }
+                } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    // wrong field declared in template (or inaccessible getter)
+                    errors.reject(Context.getMessageSourceService().getMessage(
+                            "AddressTemplate.error.fieldNotDeclaredInTemplate",
+                            new Object[]{fieldName},
+                            Context.getLocale()));
+                }
+            }
+        }
+
+        ValidateUtil.validateFieldLengths(errors, object.getClass(), "address1", "address2", "cityVillage", "stateProvince",
+                "postalCode", "country", "latitude", "longitude", "voidReason", "countyDistrict", "address3", "address4",
+                "address5", "address6", "address7", "address8", "address9", "address10", "address11", "address12", "address13",
+                "address14", "address15");
+    }
 }
