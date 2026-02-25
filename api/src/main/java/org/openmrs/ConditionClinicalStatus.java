93de14d70f6384e9981a64eae6d38b1d7d14cf42
ADARSH DUBEY <139523805+adarshiiit0117@users.noreply.github.com>
2026-02-26 01:26:46 +0530

    TRUNK-6543: Add explicit @Deprecated annotation to HISTORY_OF enum constant

--- api/src/main/java/org/openmrs/ConditionClinicalStatus.java
+++ api/src/main/java/org/openmrs/ConditionClinicalStatus.java
@@ -21,41 +21,43 @@ package org.openmrs;
  * @since 2.2
  * */
 public enum ConditionClinicalStatus {
+
 	/**
 	 * This is where the patient is currently experiencing the symptoms of the condition or there is evidence of the 
 	 * condition.
-	 * */
+	 */
 	ACTIVE,
-	
+
 	/**
 	 * There is where the patient is no longer experiencing the symptoms of the condition or there is no longer 
 	 * evidence of the condition.
-	 * */
+	 */
 	INACTIVE,
-	
+
 	/**
 	 * This maps most closely to the "remission" status in FHIR, but we want to be more clear about
 	 * the common OpenMRS use case.
 	 * Remission is where the patient is no longer experiencing the symptoms of the condition, 
 	 * but there is a risk of the symptoms returning.
-	 * 
+	 *
 	 * @deprecated as of 2.6.0
-	 * */
+	 */
+	@Deprecated(since = "2.6.0", forRemoval = false)
 	HISTORY_OF,
-	
+
 	/**
 	 * The Patient is experiencing a re-occurrence or repeating of a previously resolved condition, 
 	 * e.g. urinary tract infection, pancreatitis, cholangitis, conjunctivitis.
 	 */
 	RECURRENCE,
-	
+
 	/**
 	 * The Patient is experiencing a return of a condition, or signs and symptoms after a period of 
 	 * improvement or remission, e.g. relapse of cancer, multiple sclerosis, rheumatoid arthritis, 
 	 * systemic lupus erythematosus, bipolar disorder, [psychotic relapse of] schizophrenia, etc.
 	 */
 	RELAPSE,
-	
+
 	/**
 	 * The Patient is no longer experiencing the symptoms of the condition, but there is a risk 
 	 * of the symptoms returning.
@@ -67,5 +69,4 @@ public enum ConditionClinicalStatus {
 	 * negligible perceived risk of the symptoms returning.
 	 */
 	RESOLVED
-	
 }
