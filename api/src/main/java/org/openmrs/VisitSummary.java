02155d1754397889ed679a3183f3d78c0a4fc3f2
Aadvik <aadvikyadav@Aadviks-MacBook-Air.local>
2026-03-05 01:59:55 +0530

    WIP: Add VisitSummary DTO for patient visit summary printing

---
+++ api/src/main/java/org/openmrs/VisitSummary.java
@@ -0,0 +1,46 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs;
+
+public class VisitSummary {
+	
+	private String patientName;
+	
+	private String visitStartDate;
+	
+	private int encounterCount;
+	
+	public VisitSummary() {
+	}
+	
+	public String getPatientName() {
+		return patientName;
+	}
+	
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+	
+	public String getVisitStartDate() {
+		return visitStartDate;
+	}
+	
+	public void setVisitStartDate(String visitStartDate) {
+		this.visitStartDate = visitStartDate;
+	}
+	
+	public int getEncounterCount() {
+		return encounterCount;
+	}
+	
+	public void setEncounterCount(int encounterCount) {
+		this.encounterCount = encounterCount;
+	}
+}
