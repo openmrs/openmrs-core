d275378b6867782247c39e3a19ec9d8a6afe636c
Ian <ian.c.bacher@gmail.com>
2026-03-27 16:05:45 -0400

    Follow-up for Java 8 support in ModuleResourcesServlet

--- web/src/main/java/org/openmrs/module/web/WebModuleUtil.java
+++ web/src/main/java/org/openmrs/module/web/WebModuleUtil.java
@@ -16,6 +16,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.StringReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayDeque;
