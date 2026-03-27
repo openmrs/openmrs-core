416087e761d22c51f69ab8e7c9ba869c86dfda3a
manoj-1407 <manojkumar148700@gmail.com>
2026-03-27 17:16:47 +0000

    fix: add null guard to purgePatient consistent with voidPatient and unvoidPatient

--- api/src/main/java/org/openmrs/api/impl/PatientServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/PatientServiceImpl.java
@@ -354,9 +354,12 @@ public class PatientServiceImpl extends BaseOpenmrsService implements PatientSer
 	 * @see org.openmrs.api.PatientService#purgePatient(org.openmrs.Patient)
 	 */
 	@Override
-	public void purgePatient(Patient patient) throws APIException {
-		dao.deletePatient(patient);
-	}
+public void purgePatient(Patient patient) throws APIException {
+    if (patient == null) {
+        return;
+    }
+    dao.deletePatient(patient);
+}
 
 	// patient identifier section
 
