9a46b16a1e1d43f5f1945864092d1f78c150d6d0
jwnasambu <33891016+jwnasambu@users.noreply.github.com>
2026-04-06 10:28:03 -0400

    LUI-199: Editing the Address Template does not save the address template correctly (#5839)

--- api/src/main/java/org/openmrs/validator/PersonAddressValidator.java
+++ api/src/main/java/org/openmrs/validator/PersonAddressValidator.java
@@ -9,15 +9,16 @@
  */
 package org.openmrs.validator;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.beanutils.PropertyUtils;
-import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.openmrs.PersonAddress;
 import org.openmrs.annotation.Handler;
 import org.openmrs.api.context.Context;
+import org.openmrs.layout.address.AddressSupport;
 import org.openmrs.layout.address.AddressTemplate;
 import org.openmrs.util.OpenmrsUtil;
 import org.slf4j.Logger;
@@ -32,9 +33,9 @@ import org.springframework.validation.Validator;
  */
 @Handler(supports = { PersonAddress.class }, order = 50)
 public class PersonAddressValidator implements Validator {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(PersonAddressValidator.class);
-	
+
 	/**
 	 * @see org.springframework.validation.Validator#supports(java.lang.Class)
 	 */
@@ -42,7 +43,7 @@ public class PersonAddressValidator implements Validator {
 	public boolean supports(Class<?> c) {
 		return PersonAddress.class.isAssignableFrom(c);
 	}
-	
+
 	/**
 	 * @see org.springframework.validation.Validator#validate(java.lang.Object,
 	 *      org.springframework.validation.Errors)
@@ -61,13 +62,13 @@ public class PersonAddressValidator implements Validator {
 	public void validate(Object object, Errors errors) {
 		//TODO Validate other aspects of the personAddress object
 		log.debug("{}.validate...", this.getClass().getName());
-		
+
 		if (object == null) {
 			throw new IllegalArgumentException("The personAddress object should not be null");
 		}
-		
+
 		PersonAddress personAddress = (PersonAddress) object;
-		
+
 		//resolve a shorter name to display along with the error message
 		String addressString;
 		if (StringUtils.isNotBlank(personAddress.getAddress1())) {
@@ -79,31 +80,34 @@ public class PersonAddressValidator implements Validator {
 		} else {
 			addressString = personAddress.toString();
 		}
-		
+
 		if (OpenmrsUtil.compareWithNullAsEarliest(personAddress.getStartDate(), new Date()) > 0) {
 			errors.rejectValue("startDate", "PersonAddress.error.startDateInFuture", new Object[] { "'" + addressString
 			        + "'" }, "The Start Date for address '" + addressString + "' shouldn't be in the future");
 		}
-		
+
 		if (personAddress.getStartDate() != null
 		        && OpenmrsUtil.compareWithNullAsLatest(personAddress.getStartDate(), personAddress.getEndDate()) > 0) {
 			errors.rejectValue("endDate", "PersonAddress.error.endDateBeforeStartDate", new Object[] { "'" + addressString
 			        + "'" }, "The End Date for address '" + addressString + "' shouldn't be earlier than the Start Date");
 		}
-		
-		String xml = Context.getLocationService().getAddressTemplate();
+
 		List<String> requiredElements;
-		
+
 		try {
-			AddressTemplate addressTemplate = Context.getSerializationService().getDefaultSerializer().deserialize(StringEscapeUtils.unescapeXml(xml),
-			    AddressTemplate.class);
-			requiredElements = addressTemplate.getRequiredElements();
+			List<AddressTemplate> addressTemplates = AddressSupport.getInstance().getAddressTemplate();
+			if (addressTemplates == null || addressTemplates.isEmpty()) {
+				errors.reject(Context.getMessageSourceService().getMessage("AddressTemplate.error"));
+				return;
+			}
+
+			requiredElements = addressTemplates.get(0).getRequiredElements();
 		}
 		catch (Exception e) {
 			errors.reject(Context.getMessageSourceService().getMessage("AddressTemplate.error"));
 			return;
 		}
-		
+
 		if (requiredElements != null) {
 			for (String fieldName : requiredElements) {
 				try {
@@ -115,19 +119,22 @@ public class PersonAddressValidator implements Validator {
 						    Context.getLocale()));
 					}
 				}
-				catch (Exception e) {
+				catch (IllegalAccessException | ClassCastException | InvocationTargetException | NoSuchMethodException e) {
 					//wrong field declared in template
-					errors
-					        .reject(Context.getMessageSourceService().getMessage(
-					            "AddressTemplate.error.fieldNotDeclaredInTemplate", new Object[] { fieldName },
-					            Context.getLocale()));
+					errors.reject(Context.getMessageSourceService().getMessage(
+					    "AddressTemplate.error.fieldNotDeclaredInTemplate", new Object[] { fieldName },
+					    Context.getLocale()));
+				}
+				catch (Exception e) {
+					//for any other exception
+					errors.reject(Context.getMessageSourceService().getMessage("AddressTemplate.error"));
 				}
 			}
 		}
-		
+
 		ValidateUtil.validateFieldLengths(errors, object.getClass(), "address1", "address2", "cityVillage", "stateProvince",
 		    "postalCode", "country", "latitude", "longitude", "voidReason", "countyDistrict", "address3", "address4",
-		    "address5", "address6", "address7", "address8", "address9", "address10", "address11", "address12", "address13", 
+		    "address5", "address6", "address7", "address8", "address9", "address10", "address11", "address12", "address13",
 		    "address14", "address15");
 	}
 }
