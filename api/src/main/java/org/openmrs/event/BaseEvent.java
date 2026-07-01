c4aca3af4154dc99969cb6aed48a1e71c9a70055
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2026-07-01 11:17:09 +0200

    Porting TRUNK-6429 and TRUNK-6515 to master (#6233)
    * TRUNK-6429: Create application events for service method calls and entity changes (#6084)
    
    (cherry picked from commit 144fee399f79b7152f2fcbaade069a7cfd3285e3)
    
    * TRUNK-6516: Provide CDC mechanism with Debezium (#6151)
    
    (cherry picked from commit 57f4018ffbcdb27dac86677ca2791d9fae6cfc89)
    
    * TRUNK-6429: Addressing post commit review (#6190)
    
    (cherry picked from commit a4331e297c2ad33b9d615174f7b6e52b26ef236c)

---
+++ api/src/main/java/org/openmrs/event/BaseEvent.java
@@ -0,0 +1,39 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.event;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.openmrs.event.outbox.OutboxableEvent;
+
+/**
+ * See {@link EntityEvent} or {@link AggregatedEntityEvent} instead.
+ *
+ * @since 2.9.0
+ */
+public abstract class BaseEvent implements OutboxableEvent {
+
+	private static final long serialVersionUID = 1L;
+
+	protected final Set<String> tags;
+
+	protected BaseEvent() {
+		this.tags = new HashSet<>();
+	}
+
+	protected BaseEvent(Set<String> tags) {
+		this.tags = tags;
+	}
+
+	public Set<String> getTags() {
+		return tags;
+	}
+}
---
+++ api/src/main/java/org/openmrs/event/outbox/OutboxEventService.java
@@ -0,0 +1,139 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.event.outbox;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Query;
+
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * API for OutboxEvent.
+ *
+ * @since 2.9.0
+ */
+@Service
+public class OutboxEventService {
+
+	private static final Logger log = LoggerFactory.getLogger(OutboxEventService.class);
+
+	private final SessionFactory sessionFactory;
+
+	private final Duration listenerTimeout;
+
+	private final int retryLimit;
+
+	public OutboxEventService(SessionFactory sessionFactory,
+	    @Value("${outboxevent.listener.timeout:120}") int listenerTimeout,
+	    @Value("${outboxevent.retry.limit:16}") int retryLimit) {
+		this.sessionFactory = sessionFactory;
+		this.listenerTimeout = Duration.ofSeconds(listenerTimeout);
+		this.retryLimit = retryLimit;
+	}
+
+	/**
+	 * Reset retry count for outbox event to continue retrying.
+	 *
+	 * @param uuid
+	 */
+	@Transactional
+	public void retryFailedOutboxEvent(String uuid) {
+		org.hibernate.query.Query<OutboxEvent> query = sessionFactory.getCurrentSession()
+		        .createQuery("from OutboxEvent where uuid = :uuid and status = 'FAILED'", OutboxEvent.class);
+		query.setParameter("uuid", uuid);
+		OutboxEvent event = query.uniqueResult();
+
+		if (event != null) {
+			event.setStatus(OutboxEvent.Status.PENDING);
+			event.setErrorCount(0);
+			event.setErrorMessage(null);
+			sessionFactory.getCurrentSession().persist(event);
+		}
+	}
+
+	/**
+	 * Revert tasks stuck in PROCESSING for more than {@code outboxevent.listener.timeout} .
+	 */
+	@Transactional(noRollbackFor = OutboxException.class)
+	public void resetStuckEvent() throws OutboxException {
+		Date threshold = Date.from(Instant.now().minus(listenerTimeout));
+		Query resetStuckQuery = sessionFactory.getCurrentSession().createQuery(
+		    "update OutboxEvent set status = case when coalesce(errorCount, 0) + 1 >= :retryLimit then 'FAILED' else 'PENDING' end, "
+		            + "errorCount = coalesce(errorCount, 0) + 1, errorMessage = :errorMessage, dateChanged = :now "
+		            + "where status = 'PROCESSING' and dateChanged < :threshold");
+		resetStuckQuery.setParameter("now", new Date());
+		resetStuckQuery.setParameter("threshold", threshold);
+		resetStuckQuery.setParameter("retryLimit", retryLimit);
+		resetStuckQuery.setParameter("errorMessage",
+		    "Stuck in PROCESSING state for more than " + listenerTimeout.getSeconds() + " seconds");
+		int resetCount = resetStuckQuery.executeUpdate();
+		if (resetCount > 0) {
+			// Throw an error to increase visibility of the event
+			throw new OutboxException("Reset stuck outbox item(s) from PROCESSING back to PENDING or FAILED");
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public void warnOnTooManyPendingEvents() {
+		Query query = sessionFactory.getCurrentSession()
+		        .createQuery("select count(*) from OutboxEvent where status in ('PENDING')");
+		long count = query.getSingleResult() == null ? 0 : (long) query.getSingleResult();
+		if (count > 10000) {
+			log.warn("Too many pending ({}) outbox events to process in the system! "
+			        + "Please review your listeners to fix performance issues so outbox events do no pile up!",
+			    count);
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public List<OutboxEvent> getProcessingAndPendingEvents() {
+		Query query = sessionFactory.getCurrentSession()
+		        .createQuery("from OutboxEvent where status in ('PENDING', 'PROCESSING') order by id asc");
+		query.setMaxResults(100);
+
+		return query.getResultList();
+	}
+
+	@Transactional(readOnly = true)
+	public List<OutboxEvent> getFailingEvents() {
+		Query query = sessionFactory.getCurrentSession()
+		        .createQuery("from OutboxEvent where status in ('FAILED') order by id asc");
+		query.setMaxResults(100);
+
+		return query.getResultList();
+	}
+
+	@Transactional
+	public boolean lockEventForProcessing(OutboxEvent outboxEvent) {
+		Query claimQuery = sessionFactory.getCurrentSession().createQuery(
+		    "update OutboxEvent set status = 'PROCESSING', dateChanged = :now where id = :id and status = 'PENDING'");
+
+		claimQuery.setParameter("id", outboxEvent.getId());
+		claimQuery.setParameter("now", new Date());
+
+		int updatedCount = claimQuery.executeUpdate();
+		return updatedCount == 1;
+	}
+
+	@Transactional
+	public void saveOutboxEvent(OutboxEvent outboxEvent) {
+		outboxEvent.setDateChanged(new Date());
+		sessionFactory.getCurrentSession().persist(outboxEvent);
+	}
+}
