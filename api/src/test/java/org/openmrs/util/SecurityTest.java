871fb240a088a3bbee44eaf5b35f70b2292f06cf
Juliet wamalwa <julietwamalwa@Juliets-MacBook-Air.local>
2026-07-11 23:48:55 +0300

    TRUNK-6662: Remove unused import

--- api/src/test/java/org/openmrs/util/SecurityTest.java
+++ api/src/test/java/org/openmrs/util/SecurityTest.java
@@ -11,7 +11,6 @@ package org.openmrs.util;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
