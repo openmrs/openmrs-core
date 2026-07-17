eb0a1006c7e188d4408deb298685c98ea1fef321
Ian <52504170+ibacher@users.noreply.github.com>
2026-07-17 07:59:02 -0400

    TRUNK-6688: Fix circular initialization error in logging setup (#6341)

--- api/src/test/java/org/openmrs/api/context/ServiceContextTest.java
+++ api/src/test/java/org/openmrs/api/context/ServiceContextTest.java
@@ -23,6 +23,7 @@ import org.openmrs.util.InputRequiredException;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -47,6 +48,11 @@ public class ServiceContextTest extends BaseContextSensitiveTest {
 	@AfterEach
 	public void tearDown() {
 		serviceContext.setUseSystemClassLoader(isUseSystemClassLoader);
+	}
+
+	@Test
+	public void isInstantiated_shouldReturnTrueWhenSingletonExists() {
+		assertTrue(ServiceContext.isInstantiated());
 	}
 
 	@Test
