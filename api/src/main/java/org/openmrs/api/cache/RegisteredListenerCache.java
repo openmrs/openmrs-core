87a8e67456b2b85e93c0135ab872f81694d43b61
IamMujuziMoses <mujuzimoses@gmail.com>
2026-08-20 20:21:17 +0300

    TRUNK-6696: Cache PrivilegeListener lookup instead of scanning Spring contexts on every privilege check

---
+++ api/src/main/java/org/openmrs/api/cache/RegisteredListenerCache.java
@@ -0,0 +1,31 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.api.cache;
+
+import org.openmrs.api.context.ServiceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RegisteredListenerCache implements ApplicationListener<ContextRefreshedEvent> {
+
+	private static final Logger log = LoggerFactory.getLogger(RegisteredListenerCache.class);
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		if (ServiceContext.isInstantiated()) {
+			log.debug("Clearing registered listener cache after context refresh");
+			ServiceContext.getInstance().clearCachedListeners();
+		}
+	}
+}
