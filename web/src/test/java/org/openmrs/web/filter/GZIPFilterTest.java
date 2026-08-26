2e9f1ab4756e444b54faf1703ceb5916979723c5
Ian <ian.c.bacher@gmail.com>
2026-08-26 13:30:57 -0400

    Fix spotless

--- web/src/test/java/org/openmrs/web/filter/GZIPFilterTest.java
+++ web/src/test/java/org/openmrs/web/filter/GZIPFilterTest.java
@@ -14,7 +14,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.zip.GZIPOutputStream;
 
